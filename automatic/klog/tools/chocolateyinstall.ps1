d0a54db6e3d68bad3a3378c83a79c29250bedee2
chtof <clefebvre.62@free.fr>
2019-02-28 22:28:31 +0100

    klog chocolatey script migrated from manual to automatic

---
+++ automatic/klog/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = 'https://download.savannah.gnu.org/releases/klog/win/KLog-0.9.6-windows-installer.exe'
+
+  softwareName  = 'klog*'
+
+  checksum      = 'e772c161fa55124fe258f73a3154846be671960f2a06803495bc03e961905673'
+  checksumType  = 'sha256'
+  
+  silentArgs    = "--mode unattended"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/klog/update.ps1
@@ -0,0 +1,22 @@
+import-module au
+
+$releases = 'https://download.savannah.gnu.org/releases/klog/win/'
+
+function global:au_GetLatest {
+     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	 $regex   = 'KLog-(.*)-windows-installer.exe$'
+	 $url = $download_page.links | ? href -match $regex | select -Last 1
+	 $version = $url -split '-|.exe' | select -Last 1 -Skip 3
+     return @{ Version = $version ; URL32 = $releases+$url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

8f611b2a5b8c492defc2256fe6b7463c1a336553
chtof <clefebvre.62@free.fr>
2019-03-01 20:53:46 +0100

    log4om migrated from manual to automatic

--- automatic/log4om/tools/chocolateyinstall.ps1
+++ automatic/log4om/tools/chocolateyinstall.ps1
@@ -1,19 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.log4om.com/log4om/release/Log4OM_1_36_0.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = $url
+  url           = 'http://www.log4om.com/log4om/release/Log4OM_1_36_0.zip'
   file          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\Log4OM_1_36_0.exe"
-  defaultName	= "Log4OM_1_36_0.zip"
 
   softwareName  = 'log4om*'
 
-  checksum      = '153F7EE37793483BEC0C49EAB574A22FF38C742DA6C7FC32AD4AB3508E83269B'
+  checksum      = '153f7ee37793483bec0c49eab574a22ff38c742da6c7fc32ad4ab3508e83269b'
   checksumType  = 'sha256'
 
   validExitCodes= @(0, 3010, 1641)

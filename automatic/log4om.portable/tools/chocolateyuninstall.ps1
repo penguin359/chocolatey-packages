c995cb8c271da06e15bf81e6baae88091077f894
chtof <chtof62@gmail.com>
2019-03-02 02:38:53 +0100

    Create chocolateyuninstall.ps1

---
+++ automatic/log4om.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,11 @@
+﻿$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+
+$shortcutFilePath = Join-Path $programs "LogOMUI.lnk"
+if(Test-Path $shortcutFilePath) {
+    del $shortcutFilePath
+}
+
+$shortcutFilePath = Join-Path $programs "Log4OmCommunicator.lnk"
+if(Test-Path $shortcutFilePath) {
+    del $shortcutFilePath
+}

bc3e64c1ed07c7c77893c97719fd90b4d53b8e10
chtof <chtof62@gmail.com>
2019-03-02 02:27:41 +0100

    log4om.portable initial commit

---
+++ automatic/log4om.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$fileName = "LogOMUI.exe"
+$linkName = "LogOMUI.lnk"
+$fileName_Log4OmCommunicator = "Log4OmCommunicator.exe"
+$linkName_Log4OmCommunicator = "Log4OmCommunicator.lnk"
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName  = "log4om.portable"
+  unzipLocation = $toolsDir
+  fileType      = 'zip'
+  url           = 'http://www.log4om.com/log4om/release/Log4OM_1_36_0_Portable.zip'
+  checksum      = 'DF15688CF768556C520DC654508657B78A58F7222DD2B9AC6F2323A5596BDD49'
+  checksumType  = 'sha256'
+}
+    
+Install-ChocolateyZipPackage @packageArgs
+
+#Install start menu shortcuts
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+
+$shortcutFilePath = Join-Path $programs $linkName
+$targetPath = Join-Path $toolsDir "portable\$fileName"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
+
+$shortcutFilePath = Join-Path $programs $linkName_Log4OmCommunicator
+$targetPath = Join-Path $toolsDir "portable\$fileName_Log4OmCommunicator"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
---
+++ automatic/log4om.portable/update.ps1
@@ -0,0 +1,23 @@
+import-module au
+
+$releases = 'http://www.log4om.com/dl/'
+
+function global:au_GetLatest {
+     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	 $regex   = '\.zip$'
+	 $regex_version = "Actual Version is (.*?)<"
+	 $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+	 $download_page -match $regex_version
+	 return @{ Version = $matches[1] ; URL32 = $url }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

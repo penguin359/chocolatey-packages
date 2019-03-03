faba5c3718fece13a453e2473921475a0b3ea46b
chtof <chtof62@gmail.com>
2019-03-03 20:22:22 +0100

    vagrant-manager initial commit

---
+++ automatic/vagrant-manager/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = 'https://github.com/lanayotech/vagrant-manager-windows/releases/download/1.0.0.6/vagrant-manager-windows-1.0.0.6.zip'
+  file          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\vagrant-manager-windows-1.0.0.6.exe"
+    
+  checksum      = '1D1917BDBDA8F03C061C614AB456E14D9BB258D5B7D9693D47853AE952AD075E'
+  checksumType  = 'sha256'
+  
+  silentArgs = '/verysilent /norestart'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs

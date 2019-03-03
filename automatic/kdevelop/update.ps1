c8fb3a4c7a9ee46c208ff8f4e7860278c1e27b43
chtof <chtof62@gmail.com>
2019-03-03 19:57:38 +0100

    minor updates

--- automatic/kdevelop/update.ps1
+++ automatic/kdevelop/update.ps1
@@ -1,20 +1,22 @@
 ﻿import-module au
 
-$releases = 'https://__XXX__'
+$releases = 'https://www.kdevelop.org/download'
 
 function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	 $regex   = '\.exe$'
-	 $url = $download_page.links | ? href -match $regex | select -Last 1
+	 $url = $download_page.links | ? href -match $regex | select -First 2	 
 	 $version = $url -split '-|.exe' | select -Last 1 -Skip 3
-     return @{ Version = $version ; URL32 = $releases+$url.href }
+     return @{ Version = $version ; URL32 = $url.href[1] ; URL64 = $url.href[0] }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
 			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+			"(^(\s)*url64bit\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }

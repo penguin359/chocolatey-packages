3899005a9b5435c939d2a74027db4e42097fefcb
chtof <chtof62@gmail.com>
2019-03-04 19:54:18 +0100

    minor changes

--- automatic/kdevelop/update.ps1
+++ automatic/kdevelop/update.ps1
@@ -7,17 +7,21 @@ function global:au_GetLatest {
 	 $regex   = '\.exe$'
 	 $url = $download_page.links | ? href -match $regex | select -First 2	 
 	 $version = $url -split '-|.exe' | select -Last 1 -Skip 3
-     return @{ Version = $version ; URL32 = $url.href[1] ; URL64 = $url.href[0] }
+	 $version_release = $version -replace '[.]',''	 
+     return @{ Version = $version ; VersionRelease = $version_release ; URL32 = $url.href[1] ; URL64 = $url.href[0] }
 }
 
 function global:au_SearchReplace {
     @{
+		"kdevelop.nuspec" = @{
+			"<releaseNotes>https://www.kdevelop.org/news/kdevelop-(.*?)-released</releaseNotes>" = "<releaseNotes>https://www.kdevelop.org/news/kdevelop-'$($Latest.VersionRelease)'-released</releaseNotes>"
+        }
         "tools\chocolateyInstall.ps1" = @{
 			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
 			"(^(\s)*url64bit\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
 			"(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-        }
+        }		
     }
 }
 

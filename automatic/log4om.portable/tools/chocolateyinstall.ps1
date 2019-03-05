c87aa1678fb620b0c1e196b497298458e64a3eea
chtof <clefebvre.62@free.fr>
2019-03-05 20:10:49 +0100

    Update chocolateyinstall.ps1

--- automatic/log4om.portable/tools/chocolateyinstall.ps1
+++ automatic/log4om.portable/tools/chocolateyinstall.ps1
@@ -1,4 +1,5 @@
-﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName  = "log4om.portable"
@@ -7,15 +8,6 @@ $packageArgs = @{
   url           = 'http://www.log4om.com/log4om/release/Log4OM_1_36_0_Portable.zip'
   checksum      = 'DF15688CF768556C520DC654508657B78A58F7222DD2B9AC6F2323A5596BDD49'
   checksumType  = 'sha256'
-}
-
-# Prerequisites, KB2919355 need to be installed and computer restarted before Log4OM install.
-$result = choco list -lo | Where-object { $_.ToLower().StartsWith("kb2919355") }
-if ($result -eq $null){
-  Write-Error "For a successfull install, please follow this procedure:"
-  Write-Error "1. Install KB2919355 (choco install KB2919355) and restart your computer"
-  Write-Error "2. Install Log4OM: choco install Log4OM and restart your computer"
-  Exit 1
 }
 
 Install-ChocolateyZipPackage @packageArgs

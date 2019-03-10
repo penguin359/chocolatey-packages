c75d398998a246571fb7b0b208dad0aa92beff51
chtof <chtof62@gmail.com>
2019-03-10 11:46:56 +0100

    Update update.ps1

--- automatic/jtalert/update.ps1
+++ automatic/jtalert/update.ps1
@@ -1,21 +1,21 @@
 import-module au
 
-$releases = 'https://download.savannah.gnu.org/releases/klog/win/'
+$releases = 'https://hamapps.com/'
 
 function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	 
-	 $regex_win10 = 'HamApps_JTAlert_AL_(.*?)_Setup.exe'
-	 $url_win10 = $download_page.links | ? href -match $regex_win10 | select -Last 1
-	 $url_win10 -split '-|.exe' | select -First 1
+	 $regex_win10 = 'HamApps_JTAlert_AL_(.*?)_Setup.exe">Download'
+	 $url_win10 = $download_page.links | ? outerHTML -match $regex_win10 | select -Last 1
+	 $url_win10 -split '-|_Setup.exe' | select -First 1
 	 $version_win10 = $matches[1];
 	 
-	 $regex = 'HamApps_JTAlert_(.*?)_Setup.exe'
-	 $url = $download_page.links | ? href -match $regex | select -Last 1
-	 $url -split '-|.exe' | select -First 1
+	 $regex = 'HamApps_JTAlert_(.*?)_Setup.exe">Download'
+	 $url = $download_page.links | ? outerHTML -match $regex | select -First 1
+	 $url -split '-|_Setup.exe' | select -First 1
 	 $version = $matches[1];
 	 
-     return @{ Version = $version ; URL32 = $url ; Version_win10 = $version_win10 ; URL32_win10 = $url_win10}
+     return @{ Version = $version ; URL32 = $url.href ; Version_win10 = $version_win10 ; URL32_win10 = $url_win10.href}
 }
 
 function global:au_SearchReplace {

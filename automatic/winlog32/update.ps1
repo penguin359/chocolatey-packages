9d2d98e03cfbfe3d137da0dcf371b9c5b84928b0
chtof <chtof62@gmail.com>
2019-03-10 22:27:45 +0100

    Update update.ps1

--- automatic/winlog32/update.ps1
+++ automatic/winlog32/update.ps1
@@ -1,13 +1,17 @@
 ﻿import-module au
 
-$releases = 'https://__XXX__'
+$releases = 'http://www.winlog32.co.uk/dl_wl32update.htm'
+$url = 'http://www.winlog32.co.uk/files/'
+http://www.winlog32.co.uk/files/wl32v73.exe
+
 
 function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	 $regex   = '\.exe$'
-	 $url = $download_page.links | ? href -match $regex | select -Last 1
-	 $version = $url -split '-|.exe' | select -Last 1 -Skip 3
-     return @{ Version = $version ; URL32 = $releases+$url.href }
+	 $regex   = 'v(.*?) \(rev'
+	 $download_page -match $regex
+	 $version = $matches[1]
+	 $version_url = $version.replace('.','')
+     return @{ Version = $version ; URL32 = $url + "wl32v" + $version_url + ".exe" }
 }
 
 function global:au_SearchReplace {

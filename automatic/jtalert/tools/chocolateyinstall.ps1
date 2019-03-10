49ced2a29555fe49d11da6b38f66e7aa61e0f8df
chtof <chtof62@gmail.com>
2019-03-10 12:46:26 +0100

    jtalert update script

--- automatic/jtalert/tools/chocolateyinstall.ps1
+++ automatic/jtalert/tools/chocolateyinstall.ps1
@@ -2,8 +2,10 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 if ( [environment]::OSVersion.Version.Major -ge 10 )  {
-    $url = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_AL_2.12.10_Setup.exe'
-	$checksum = 'CD9E1C4079D4C64D6A057A948F60A288FF3771E336A29088CD36BD117BE17654'
+    $url_win10 = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_AL_2.12.10_Setup.exe'
+	$checksum_win10 = 'CD9E1C4079D4C64D6A057A948F60A288FF3771E336A29088CD36BD117BE17654'
+	$url = $url_win10
+	$checksum = $checksum_win10
   } else {
     $url = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_2.12.10_Setup.exe'
 	$checksum = '5D5E55A96DADF22104B8484C59125C7665BA6585925C6B899BB0F7CED2B5C6E0'

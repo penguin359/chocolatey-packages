b7fa4df0bd00b2f4967190ef4c5a3be5294f1484
chtof <chtof62@gmail.com>
2019-03-13 21:10:15 +0100

    Update chocolateyinstall.ps1

--- automatic/laragon.install/tools/chocolateyinstall.ps1
+++ automatic/laragon.install/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@ $packageName= 'laragon.install'
 
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
+  packageName   = $packageName  
   url           = 'https://sourceforge.net/projects/laragon/files/releases/4.0/laragon-full.exe'
   
   checksum      = 'D8718426A5633BD279BB1C7F18E1CC80D63F840D7B38C04ED47F41764A054403'
   checksumType  = 'sha256'
+  
   silentArgs	= '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components="'
 }
 

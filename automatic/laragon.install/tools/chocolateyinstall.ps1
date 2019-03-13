623dd1bb770a73cda5d8c8d46cf9d3f6649f57dc
chtof <chtof62@gmail.com>
2019-03-13 20:37:42 +0100

    laragon initial commit

---
+++ automatic/laragon.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'laragon.install'
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = 'https://sourceforge.net/projects/laragon/files/releases/4.0/laragon-full.exe'
+  
+  checksum      = 'D8718426A5633BD279BB1C7F18E1CC80D63F840D7B38C04ED47F41764A054403'
+  checksumType  = 'sha256'
+  silentArgs	= '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components="'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/laragon.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'laragon.portable'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'zip'
+  url           = 'https://sourceforge.net/projects/laragon/files/Portable/laragon.7z'
+  file         =  'laragon.exe'  
+
+  checksum      = '221FD74474310C1E4756CD6758EB68361635CFB8DB3692173106EA54BF0B0DFC'
+  checksumType  = 'sha256'  
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+#Install start menu shortcuts
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+
+$shortcutFilePath = Join-Path $programs "Laragon.lnk"
+$targetPath = Join-Path $toolsDir "Laragon.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

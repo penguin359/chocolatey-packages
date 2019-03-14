9dfad0fb4e86186c378337015588e518db441e2d
chtof <chtof62@gmail.com>
2019-03-14 22:54:59 +0100

    Update chocolateyinstall.ps1

--- automatic/jtalert/tools/chocolateyinstall.ps1
+++ automatic/jtalert/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 if ( [environment]::OSVersion.Version.Major -ge 10 )  {
     $url_win10 = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_AL_2.13.1_Setup.exe'
-	$checksum_win10 = ''
+	$checksum_win10 = '045AA74C38AD0F5894CCDACBC57AF3E78F2FE10B0F80E712FDF46CC8126C72FD'
 	$url = $url_win10
 	$checksum = $checksum_win10
   } else {

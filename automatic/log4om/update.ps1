0b8b6a490f988ed530c0031144146387388af594
chtof <clefebvre.62@free.fr>
2019-03-16 12:19:01 +0100

    Update update.ps1

--- automatic/log4om/update.ps1
+++ automatic/log4om/update.ps1
@@ -1,8 +1,16 @@
-. $PSScriptRoot\..\log4om.install\update.ps1
+import-module au
 
-function global:au_BeforeUpdate { 
-    cp  $PSScriptRoot\..\log4om.install\README.md $PSScriptRoot\README.md
+$releases = 'http://www.log4om.com/dl/'
+
+function global:au_GetLatest {
+     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	 $regex   = '\.zip$'
+	 $regex_version = "Actual Version is (.*?)<"
+	 $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+	 $download_page -match $regex_version
+	 return @{ Version = $matches[1]}
 }
+
 function global:au_SearchReplace {
    @{
         "$($Latest.PackageName).nuspec" = @{

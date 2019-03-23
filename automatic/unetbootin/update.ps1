de7ef231628285469006a585b3dddf3fdc239971
chtof <chtof62@gmail.com>
2019-03-23 20:56:38 +0100

    Update update.ps1

--- automatic/unetbootin/update.ps1
+++ automatic/unetbootin/update.ps1
@@ -3,10 +3,10 @@ $github_repository = "unetbootin/unetbootin"
 $releases = "https://github.com/" + $github_repository + "/releases/latest"
 
 function global:au_GetLatest {	
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	 
+     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	 $regex   = $github_repository + '/releases/download/.*/unetbootin-windows-(?<Version>.*).exe'
+	 $url = $download_page.links | ? href -match $regex
 	 $version = $matches.Version -replace '\w','$&.' -replace '\.$', ''
-	 $url = $download_page.links | ? href -match $regex	 
      return @{ Version = $version ; URL32 = "https://github.com" + $url.href }
 }
 

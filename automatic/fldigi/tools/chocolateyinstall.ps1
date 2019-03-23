26be77b627bbacbba3f560a42130eb5d596338e1
chtof <chtof62@gmail.com>
2019-03-24 00:46:26 +0100

    Update chocolateyinstall.ps1

--- automatic/fldigi/tools/chocolateyinstall.ps1
+++ automatic/fldigi/tools/chocolateyinstall.ps1
@@ -1,16 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop';
+$silentArgs = '/S'
+
+$pp = Get-PackageParameters
+if ($pp['DIR']){
+  $silentArgs += " /D=`"$($pp['DIR'])`""
+}
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType      = 'exe'  
-  url           = 'http://www.w1hkj.com/files/fldigi/fldigi-4.1.01_setup.exe'
-    
-  softwareName  = 'fldigi*'
+  fileType      = 'exe'
   
+  url           = 'http://www.w1hkj.com/files/fldigi/fldigi-4.1.01_setup.exe'
   checksum      = '26986F0938FB51E3E321205EB1509167FA7505D4310BBEB84735E407276D7B3F'
   checksumType  = 'sha256'
   
-  silentArgs   = '/S'
+  silentArgs	= $silentArgs
 }
 
 Install-ChocolateyPackage @packageArgs

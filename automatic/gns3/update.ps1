57bf4c6547eecc8c7d52f2c5ea0621a329bfa171
chtof <chtof62@gmail.com>
2019-03-23 21:16:12 +0100

    Update update.ps1

--- automatic/gns3/update.ps1
+++ automatic/gns3/update.ps1
@@ -6,7 +6,7 @@ function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	 $regex   = $github_repository + '/releases/download/.*/GNS3-(?<Version>[\d\.]*)[\w-]*.exe'
 	 $url = $download_page.links | ? href -match $regex
-     return @{ Version = $version ; URL32 = "https://github.com" + $url.href }
+     return @{ Version = $matches.Version ; URL32 = "https://github.com" + $url.href }
 }
 
 function global:au_SearchReplace {

caf50bbb4ef3c7ec539229b82a89caa48a4f655f
chtof <chtof62@gmail.com>
2019-03-24 14:10:02 +0100

    Update chocolateyuninstall.ps1

--- automatic/botpress/tools/chocolateyuninstall.ps1
+++ automatic/botpress/tools/chocolateyuninstall.ps1
@@ -1,6 +1,13 @@
-﻿$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+﻿$botpressDir = Join-Path $env:ProgramData $env:ChocolateyPackageName
+$zipFileName = "botpress-v11_6_3-win-x64.zip"
 
+Uninstall-ChocolateyZipPackage $packageName $zipFileName
+# If uninstalling and not just upgrading
+# we will remove all node versions that were installed
+# and therefore any globally installed modules
+if (Test-Path $botpressDir) { Get-ChildItem $botpressDir -Exclude data | Remove-Item -Recurse -Force }
+
+# Delete start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Botpress.lnk"
-if(Test-Path $shortcutFilePath) {
-    del $shortcutFilePath
-}
+if(Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

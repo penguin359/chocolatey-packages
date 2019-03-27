58e8396720a95ec16af0e2ca4e83822e91d2ec88
chtof <chtof62@gmail.com>
2019-03-27 21:19:43 +0100

    myfamilytree-languagepack initial commit

---
+++ automatic/myfamilytree-languagepack/ReadMe.md
@@ -0,0 +1,8 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/myfamilytree-languagepack/myfamilytree-languagepack.png" width="48" height="48"/> [My Family Tree (Language Pack)](https://chocolatey.org/packages/myfamilytree-languagepack)
+
+Language Pack for "My Family Tree".
+
+== Translations
+If you want to help with translating into your language check [the localisation guide](https://chronoplexsoftware.com/localisation/index.htm) for ways to contribute.
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/myfamilytree-languagepack/screenshot.png)
---
+++ automatic/myfamilytree-languagepack/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType		= "msi"
+  
+  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack8.6.2000.0.msi'
+  checksum      = '38AF5A24004729D325A2CFC2D5947C8AAE722537CA003623717EDB6B9B12C0AD'
+  checksumType  = 'sha256'
+  
+  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack8.6.2000.0x64.msi'
+  checksum64    = '124F1D465D45CC32CE00CCBC603CB1D4BC0AB356144F5D3215DF5A2036ED33EF'
+  checksumType64= 'sha256'
+  
+  silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+}
+
+Install-ChocolateyPackage @packageArgs

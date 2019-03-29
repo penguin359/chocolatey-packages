4c74d960fed2e7d5f3ed90714e1cba6301d4c2ad
chtof <chtof62@gmail.com>
2019-03-29 14:48:32 +0100

    Create update.ps1

---
+++ automatic/myfamilytree/update.ps1
@@ -0,0 +1,27 @@
+﻿import-module au
+$releases       = 'https://chronoplexsoftware.com/myfamilytree/configuration.js'
+$download_url   = 'https://chronoplexsoftware.com/myfamilytree/downloads'
+
+function global:au_GetLatest {
+     $download_page = Invoke-WebRequest -Uri $releases     
+	 $regex   = 'var mftVersionNumber = "(?<Version>.*)";'
+	 $download_page -match $regex
+     $version = $matches.Version
+     return @{ Version = $version ; URL32 = $download_url + "/myfamilytree" + $version + ".msi" ; URL64 = $download_url + "/myfamilytree" + $version + "x64.msi"}
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*url64bit\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }    
+        "tools\myfamilytree.nuspec" = @{            `
+            "(^\s*<dependency id=`"myfamilytree-languagepack`" version=`"\[).*(\]`" />)" = "`$1'$($Latest.Version)'`$2`
+$2"     }
+    }    
+}
+
+update

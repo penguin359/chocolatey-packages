f916150fe5dcc7f53a91d23d4d20a158448882de
chtof <chtof62@gmail.com>
2019-03-29 13:32:31 +0100

    Update chocolateyInstall.ps1

--- automatic/pspad/tools/chocolateyInstall.ps1
+++ automatic/pspad/tools/chocolateyInstall.ps1
@@ -2,22 +2,16 @@
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
+  softwareName   = 'PSPad editor'
   unzipLocation  = "$( Split-Path -Parent $MyInvocation.MyCommand.Definition )"
   fileType       = 'exe'
+
   url            = 'http://pspad.poradna.net/release/pspad501_setup.exe'
   checksum       = 'DBBD1B7044D925744FE9A873C181CAA2F6F931E02BB316C1A9D08659110C7BDD'
   checksumType   = 'sha256'
-
-  skipShims      = @('phpCB.exe', 'TiDy.exe')
-  softwareName   = 'PSPad editor'
   
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
-}
-
-foreach ($file in $packageArgs.skipShims) {
-  New-Item -Path $packageArgs.unzipLocation -Name "$file.ignore" -Type file -Force | 
-      Out-Null
 }
 
 Install-ChocolateyPackage @packageArgs

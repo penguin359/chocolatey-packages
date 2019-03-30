8d44c7332af8dbe13b27cfe02fc141f13db4a506
chtof <chtof62@gmail.com>
2019-03-30 13:24:33 +0100

    tapwindows initial commit

---
+++ automatic/tapwindows/ReadMe.md
@@ -0,0 +1,3 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/tapwindows/tapwindows.png" width="48" height="48"/> [OpenVPN TAP Windows adapter](https://chocolatey.org/packages/tapwindows)
+
+NDIS 6.20 implementation of the TAP-Windows driver, used by OpenVPN and other apps. NDIS 6.20 drivers can run on Windows 7 or higher.
---
+++ automatic/tapwindows/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  url           = 'https://build.openvpn.net/downloads/releases/tap-windows-9.21.2.exe'
+
+  checksum      = '645BEE92BA4E9F32DDFDD9F8519DC1B9F9FF0B0A8E87E342F08D39DA77E499A9'
+  checksumType  = 'sha256'
+  
+  silentArgs	= "/S"
+}
+
+Install-ChocolateyPackage @packageArgs

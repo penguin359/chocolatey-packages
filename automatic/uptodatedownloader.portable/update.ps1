fac0c1d669c0e3bb64e2dc6d78db2142b7834c3d
chtof <chtof62@gmail.com>
2019-04-05 23:38:09 +0200

    Create update.ps1

---
+++ automatic/uptodatedownloader.portable/update.ps1
@@ -0,0 +1,18 @@
+﻿import-module au
+$releases = 'https://www.wintools.info/index.php/uptodate-downloader-changelog'
+
+function global:au_GetLatest {
+  $regex   = '>Version: (?<Version>[\d\.]*) \|'
+  (Invoke-WebRequest -Uri $releases) -match $regex
+  return @{ Version = $matches.Version }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{			
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

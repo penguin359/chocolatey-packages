df0f31dc18d226a190a91f4a66b1427ef6176cb2
chtof <chtof62@gmail.com>
2019-04-06 13:39:27 +0200

    Update chocolateyinstall.ps1

--- automatic/wakeuponstandby/tools/chocolateyinstall.ps1
+++ automatic/wakeuponstandby/tools/chocolateyinstall.ps1
@@ -9,9 +9,7 @@ $packageArgs = @{
 
   url           = 'https://dennisbabkin.com/php/downloads/WakeupOnStandBy.zip'
   checksum      = '58189BBB715303A5C8038924C50641BA14E4C964AB1847F04ADB163FA0DEDF81'
-  checksumType  = 'sha256'
-  
-  silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  checksumType  = 'sha256'  
 }
 
 Install-ChocolateyZipPackage @packageArgs

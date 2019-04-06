03fa39154149731f426ad71783a2d75c4caa76cd
chtof <chtof62@gmail.com>
2019-04-06 22:15:09 +0200

    Update chocolateyuninstall.ps1

--- automatic/log4om.portable/tools/chocolateyuninstall.ps1
+++ automatic/log4om.portable/tools/chocolateyuninstall.ps1
@@ -1,11 +1,7 @@
 ﻿$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 
 $shortcutFilePath = Join-Path $programs "LogOMUI.lnk"
-if(Test-Path $shortcutFilePath) {
-    del $shortcutFilePath
-}
+if(Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
 
 $shortcutFilePath = Join-Path $programs "Log4OmCommunicator.lnk"
-if(Test-Path $shortcutFilePath) {
-    del $shortcutFilePath
-}
+if(Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

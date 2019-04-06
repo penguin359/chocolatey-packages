c0d1f78d510e83e8b2c7160a9ca93cf6db1bf802
chtof <chtof62@gmail.com>
2019-04-06 23:09:11 +0200

    Some changes for review

--- manual/portqry/ReadMe.md
+++ manual/portqry/ReadMe.md
@@ -1,6 +1,7 @@
 ﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/manual/portqry/portqry.png" width="48" height="48"/> [PortQry Command Line Port Scanner](https://chocolatey.org/packages/portqry)
 
 Portqry.exe reports the status of a TCP/IP port in one of the following three ways:
+
 - Listening
 
 A process is listening on the port on the computer that you selected. Portqry.exe received a response from the port.
--- manual/portqry/tools/chocolateyinstall.ps1
+++ manual/portqry/tools/chocolateyinstall.ps1
@@ -3,15 +3,14 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
-  destination   = $toolsDir
-  FileFullPath  = "$toolsDir\PortQryV2.exe"  
+  unzipLocation = $toolsDir
 
   url           = 'https://download.microsoft.com/download/0/d/9/0d9d81cf-4ef2-4aa5-8cea-95a935ee09c9/PortQryV2.exe'
   checksum      = 'CD9C75B72EE50D2F59D812533CB38B35F67EB31E92F92319842CDC5EFEECFCF4'
   checksumType  = 'sha256'  
 }
 
-Get-ChocolateyUnzip @packageArgs
+Install-ChocolateyZipPackage @packageArgs
 
 $packageArgs = @{
   packageName   = $packageName

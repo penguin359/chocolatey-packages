dcf3cdfd3184f3b47b44765ad31858aa378aacef
chtof <chtof62@gmail.com>
2019-04-06 22:33:22 +0200

    Create update.ps1

---
+++ automatic/marktext.install/update.ps1
@@ -0,0 +1,21 @@
+﻿import-module au
+$github_repository = "marktext/marktext"
+$releases = "https://github.com/" + $github_repository + "/releases/latest"
+
+function global:au_GetLatest {	
+     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	 $regex   = $github_repository + '/releases/download/.*/marktext-setup-(?<Version>[\d\.]*)[\w-]*.exe'
+	 $url = $download_page.links | ? href -match $regex
+     return @{ Version = $matches.Version ; URL32 = "https://github.com" + $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }        
+    }
+}
+
+update

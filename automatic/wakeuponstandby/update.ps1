f43cad2126b8b4bda6b48426900cb0868e6ee63a
chtof <chtof62@gmail.com>
2019-04-06 13:54:31 +0200

    Create update.ps1

---
+++ automatic/wakeuponstandby/update.ps1
@@ -0,0 +1,18 @@
+﻿import-module au
+$releases = "https://dennisbabkin.com/wosb/"
+$regex   = 'ver=(?<Version>[\d\.]*)">Report Errors and Glitches'
+
+function global:au_GetLatest {
+     (Invoke-WebRequest -Uri $releases) -match $regex
+     return @{ Version = $matches.Version }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{			
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

34f35839c233d61e1bdf8dedc1474af9eed65bff
chtof <chtof62@gmail.com>
2019-04-06 20:03:13 +0200

    Update chocolateyinstall.ps1

--- automatic/gns3/tools/chocolateyinstall.ps1
+++ automatic/gns3/tools/chocolateyinstall.ps1
@@ -7,8 +7,6 @@ $packageArgs = @{
   url           = 'https://github.com/GNS3/gns3-gui/releases/download/v2.1.15/GNS3-2.1.15-all-in-one.exe'
   checksum      = '98DE258AF4D0190260E232891B44440552E591B6465059A0AE2DE45245BABAEC'
   checksumType  = 'sha256'
-  
-  #silentArgs	= "_?=$Env:PROGRAMFILES\GNS3"
 }
 
 Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""

e397c9fb96042f56999a95a4afa99ade09971c34
chtof <chtof62@gmail.com>
2019-04-07 19:30:48 +0200

    Update chocolateyinstall.ps1

--- automatic/manuskript/tools/chocolateyinstall.ps1
+++ automatic/manuskript/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir  
+  unzipLocation = $toolsDir
 
   url           = 'https://github.com/olivierkes/manuskript/releases/download/0.9.0/manuskript-0.9.0-win32.zip'
   checksum      = '2C928D4FEE67FD4D94DE813218E9A23650E6880EB6DDD823A3FA5356EBC96DAF'

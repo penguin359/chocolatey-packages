bca6647479348cd94c9556ae7f033726a30b70ef
chtof <chtof62@gmail.com>
2019-04-07 13:54:25 +0200

    Update chocolateyinstall.ps1

--- automatic/laragon.portable/tools/chocolateyinstall.ps1
+++ automatic/laragon.portable/tools/chocolateyinstall.ps1
@@ -1,14 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
-$packageName= 'laragon.portable'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $packageName
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'zip'
-  url           = 'https://github.com/leokhoa/laragon/releases/download/4.0.12/laragon.7z'
-  file         =  'laragon.exe'  
+  fileType      = 'zip'  
+  file         =  'laragon.exe'
 
+  url           = 'https://github.com/leokhoa/laragon/releases/download/4.0.12/laragon.7z'
   checksum      = '221fd74474310c1e4756cd6758eb68361635cfb8db3692173106ea54bf0b0dfc'
   checksumType  = 'sha256'  
 }

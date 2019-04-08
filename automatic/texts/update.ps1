7fe647a6f3a6d3d454201c32b48599d732e3bbc4
chtof <chtof62@gmail.com>
2019-04-08 21:27:10 +0200

    Create update.ps1

---
+++ automatic/texts/update.ps1
@@ -0,0 +1,20 @@
+﻿import-module au
+$projectUrl = 'http://www.texts.io'
+$releases = $projectUrl + '/download/'
+$regex = '\/(?<File>Texts-(?<Version>[\d\.]*).msi)$'
+
+function global:au_GetLatest {
+     $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
+     return @{ Version = $matches.Version ; URL32 = $projectUrl + $matches.File }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

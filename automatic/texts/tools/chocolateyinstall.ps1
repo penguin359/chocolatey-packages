1c9536f32a34fae8231214b1bc94c6f19aaceb2e
chtof <chtof62@gmail.com>
2019-04-08 19:49:01 +0200

    Update chocolateyinstall.ps1

--- automatic/texts/tools/chocolateyinstall.ps1
+++ automatic/texts/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   checksum      = 'F696FBF90D00315C179DE2E070C3BC6131488DE5D4E06F8DE150D952BA0A1C79'
   checksumType  = 'sha256'
   
-  silentArgs	= "/qn /norestart /l*v C:\Users\Chris\AppData\Local\Temp\$env:ChocolateyPackageName.$env:chocolateyPackageVersion.MsiInstall.log"
+  silentArgs	= "/qn /norestart /l*v $env:Temp\$env:ChocolateyPackageName.$env:chocolateyPackageVersion.MsiInstall.log"
 }
 
 Install-ChocolateyPackage @packageArgs

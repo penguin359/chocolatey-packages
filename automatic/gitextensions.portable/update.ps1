ee495bd0da801fdd699c2e231b961d3c7e4809f3
chtof <chtof62@gmail.com>
2019-04-08 21:59:33 +0200

    Create update.ps1

---
+++ automatic/gitextensions.portable/update.ps1
@@ -0,0 +1,22 @@
+﻿import-module au
+$github_repository = "gitextensions/gitextensions"
+$releases = "https://github.com/" + $github_repository + "/releases/latest"
+
+function global:au_GetLatest {	
+     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+     # Note: Version of the downloaded file is like "w.x.y.z" but we keep only "w.x.y"
+	 $regex   = $github_repository + '/releases/download/.*/GitExtensions-Portable-(?<Version>[\d\.]+)\.\d+[\w-]*.zip'
+	 $url = $download_page.links | ? href -match $regex     
+     return @{ Version = $matches.Version ; URL32 = "https://github.com" + $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }        
+    }
+}
+
+update

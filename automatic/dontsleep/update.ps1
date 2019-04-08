158eff980627124a090fbd93c12b2e2ff347aba7
chtof <chtof62@gmail.com>
2019-04-08 22:16:38 +0200

    Create update.ps1

---
+++ automatic/dontsleep/update.ps1
@@ -0,0 +1,18 @@
+﻿import-module au
+$releases = 'https://www.softwareok.com/?seite=Microsoft/DontSleep'
+$regex = "<title>Don't Sleep (?<Version>[\d\.]+) Prevent Shutdown, Stand By, Turn Off, Restart</title>"
+
+function global:au_GetLatest {
+     (Invoke-WebRequest -Uri $releases) -match $regex | out-null
+     return @{ Version = $matches.Version }
+}
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+        }
+    }
+}
+
+update

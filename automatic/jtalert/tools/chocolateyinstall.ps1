e56433421f190c25c3fd11ccf50f596e6375b51d
chtof <chtof62@gmail.com>
2019-04-09 00:02:43 +0200

    Update chocolateyinstall.ps1

--- automatic/jtalert/tools/chocolateyinstall.ps1
+++ automatic/jtalert/tools/chocolateyinstall.ps1
@@ -4,8 +4,10 @@
 	$url            = $url_win10
 	$checksum       = $checksum_win10
 } else {
-  $url            = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_2.13.2_Setup.exe'
-	$checksum       = '88e4d491d5145307c2c036502b1bac8c11aac6895ad7ba2b28e6eeb497a22748'
+  $url_others     = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_2.13.2_Setup.exe'
+	$checksum_others= '88e4d491d5145307c2c036502b1bac8c11aac6895ad7ba2b28e6eeb497a22748'
+  $url            = $url_others
+  $checksum       = $checksum_others
 }
   
 $packageArgs = @{

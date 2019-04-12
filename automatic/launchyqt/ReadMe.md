ae222c09816a7adcdb51f5ddf26b8e9f34e2ff1c
chtof <chtof62@gmail.com>
2019-04-12 13:53:51 +0200

    launchy initial commit

---
+++ automatic/launchyqt/ReadMe.md
@@ -0,0 +1,18 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/launchyqt/launchyqt.png" width="48" height="48"/> [Launchy NG](https://chocolatey.org/packages/launchyqt)
+
+Launchy is a free cross-platform utility designed to help you forget about your start menu, the icons on your desktop, and even your file manager.
+
+Launchy indexes the programs in your start menu and can launch your documents, project files, folders, and bookmarks with just a few keystrokes!
+
+## Screenshots
+Below are shown various shots of Launchy in action, opening a variety of files and programs - in just a few taps of the keyboard.
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/launchyqt/screenshot1.png)
+
+Launchy opening an .mp3 using the Default skin:
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/launchyqt/screenshot2.png)
+
+Launchy opening a song using the Black Glass skin:
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/launchyqt/screenshot3.png)
---
+++ automatic/launchyqt/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+    
+  url           = 'https://github.com/samsonwang/LaunchyQt/releases/download/v3.0.7/Launchy-3.0.7-win-x86.7z'
+  checksum      = 'C367E422491EBC591B0B57D78D2C18CC63B44BF9C85C1326A121F920896CCB92'
+  checksumType  = 'sha256'
+  
+  url64bit      = 'https://github.com/samsonwang/LaunchyQt/releases/download/v3.0.7/Launchy-3.0.7-win-amd64.7z'
+  checksum64    = 'D580F66CDC44335DFDD9489F60417D89150DB255771E2253982500C3F18AF78C'
+  checksumType64= 'sha256'
+}
+
+foreach ( $file in 'python.exe', 'pythonw.exe' ) {
+  New-Item -path $toolsDir\Launchy-3.0.7-win-amd64 -name "$file.ignore" -type File -force | Out-Null
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "Launchy.lnk"
+$targetPath = Join-Path $toolsDir "Launchy-3.0.7-win-amd64\Launchy.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

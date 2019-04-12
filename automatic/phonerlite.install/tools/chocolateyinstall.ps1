1c1fbb142b94d519c817cb9c5991fb46534959b6
chtof <chtof62@gmail.com>
2019-04-12 21:27:28 +0200

    Update chocolateyinstall.ps1

--- automatic/phonerlite.install/tools/chocolateyinstall.ps1
+++ automatic/phonerlite.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://www.phoner.de/PhonerLiteSetup.exe'
+  url           = 'https://www.phoner.de/PhonerLiteSetup_271.exe'
   checksum      = '2E58A59EDF817B3F54D0444B649ADE866ABB51DBA9AD1B13A1F143A8CB37CBB1'
   checksumType  = 'sha256'  
   

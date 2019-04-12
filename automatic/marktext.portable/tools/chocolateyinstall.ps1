92d75057eeba70c706e1afd07df9760730420cac
chtof <chtof62@gmail.com>
2019-04-12 21:51:27 +0200

    Update chocolateyinstall.ps1

--- automatic/marktext.portable/tools/chocolateyinstall.ps1
+++ automatic/marktext.portable/tools/chocolateyinstall.ps1
@@ -7,16 +7,13 @@ $packageArgs = @{
   fileFullPath  = "$toolsDir\marktext-0.13.65.exe"
 
   checksum      = 'FB0B9527A172E600CB73A786C9F1498333083B5AC8A8C14D321502DF4706DDA8'
-  checksumType  = 'sha256'
-  
-  silentArgs	= "/S _?=$Env:PROGRAMFILES\$packageName"
+  checksumType  = 'sha256'  
 }
 
 Get-ChocolateyWebFile @packageArgs
 
 #Install start menu shortcuts
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-
 $shortcutFilePath = Join-Path $programs "Mark Text.lnk"
 $targetPath = Join-Path $toolsDir "marktext-0.13.65.exe"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

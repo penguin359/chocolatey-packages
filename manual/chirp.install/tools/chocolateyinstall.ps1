9d08f69f0b384901ff4e403fdef43b217bc74547
chtof <chtof62@gmail.com>
2019-04-13 14:28:58 +0200

    Update chocolateyinstall.ps1

--- manual/chirp.install/tools/chocolateyinstall.ps1
+++ manual/chirp.install/tools/chocolateyinstall.ps1
@@ -2,11 +2,10 @@
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName	= 'CHIRP'
-  version		= '2019.03.21' 
+  softwareName	= 'CHIRP'  
   fileType		= 'exe'
   
-  url           = 'https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-20190411-installer.exe'
+  url           = 'https://trac.chirp.danplanet.com/chirp_daily/daily-20190411/chirp-daily-20190411-installer.exe'
   checksum      = '8124848005b116d165d69f565260c2b8118d6605ec82dece846c3f80262a7cb0'
   checksumType  = 'sha256' 
   

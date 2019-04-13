fe3a71fafd43986121c3bb117091ac01b704d88c
chtof <chtof62@gmail.com>
2019-04-13 23:44:35 +0200

    Update update.ps1

--- automatic/klog/update.ps1
+++ automatic/klog/update.ps1
@@ -6,7 +6,7 @@ function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	 $url = $download_page.links | ? href -match $regex | select -Last 1
 	 $version = $url -split '-|.exe' | select -Last 1 -Skip 3
-     return @{ Version = $version ; URL32 = $releases + $url.href }
+     return @{ Version = $version + "00" ; URL32 = $releases + $url.href }
 }
 
 function global:au_SearchReplace {

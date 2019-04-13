9af5d249a4a4b4ba6ba3e86422460d3e58e67d36
chtof <chtof62@gmail.com>
2019-04-13 14:32:50 +0200

    Update update.ps1

--- manual/chirp.portable/update.ps1
+++ manual/chirp.portable/update.ps1
@@ -7,7 +7,8 @@ function global:au_GetLatest {
 	 $download_page.links | ? href -match $regex
 	 $version_YY = $matches.date_url.substring(0, $matches.date_url.length-4)
 	 $version_MM = $matches.date_url.substring($matches.date_url.length-4, 2)
-	 $version_DD = $matches.date_url.substring($matches.date_url.length-2)	      
+	 $version_DD = $matches.date_url.substring($matches.date_url.length-2)
+     $version = $version_YY + "." + $version_MM + "." + $version_DD
      return @{
         Version = $version
         URL32 = 'https://trac.chirp.danplanet.com/chirp_daily/daily-' + $matches.date_url + "/chirp-daily-" + $matches.date_url + '-win32.zip' }

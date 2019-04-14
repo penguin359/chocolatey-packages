e487cdacdded6b4d4387ddc866c52f66f7d378c5
chtof <chtof62@gmail.com>
2019-04-14 22:51:01 +0200

    acestream initial commit

---
+++ automatic/acestream/tools/chocolateyinstall.ahk
@@ -0,0 +1,26 @@
+#NoEnv
+; #Warn  ; Enable warnings to assist with detecting common errors.
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetTitleMatchMode, 1  ; A windows's title must start with the specified WinTitle to be a match.
+SetControlDelay 0  
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+acestream_version = 3.1.32
+
+; License agreement
+winTitle1 = % "Ace Stream Media 3.1.32 Setup"
+WinWait, %winTitle1%, License Agreement, 300
+ControlClick, Button4, %winTitle1% ahk_class #32770,,,NA ; I accept
+ControlClick, Button2, %winTitle1% ahk_class #32770,,,NA ; &Next >
+
+; Choose Components
+WinWait, %winTitle1%, Choose Components, 10
+ControlClick, Button2, %winTitle1% ahk_class #32770,,,NA ; &Next >
+ 
+; Choose cache drive
+WinWait, %winTitle1%, Choose cache drive, 10
+ControlClick, Button2, %winTitle1% ahk_class #32770,,,NA ; &Next >
+
+; Completing Ace Stream Media Setup
+WinWait, %winTitle1%, Completing Ace Stream Media 3.1.32 Setup, 60
+; ControlClick, Button4, %winTitle1% ahk_class #32770,,,NA ; "Visit Ace Stream website and test the installed software"
+ControlClick, Button2, %winTitle1% ahk_class #32770,,,NA ; &Finish
---
+++ automatic/acestream/tools/chocolateyuninstall.ps1
@@ -0,0 +1,40 @@
+﻿$ErrorActionPreference = 'Stop';
+$installdir = "$env:APPDATA\ACEStream"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Ace Stream Media *'
+  fileType      = 'exe'
+  file          = "$installdir\uninstall.exe"
+  silentArgs	= '/S'
+}
+
+# Close remaining acestream processes
+foreach ( $processName in 'ace_update', 'ace_engine', 'ace_player' ) {
+  $process = Get-Process $processName -ErrorAction SilentlyContinue
+  if ($process) {
+    $process | Stop-Process
+    $process | Wait-Process
+  }
+}
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {     
+    Uninstall-ChocolateyPackage @packageArgs    
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
+
+# Remove installation directory and its remaining content.
+if (Test-Path $installdir) { Remove-Item -Recurse $installdir }
+
+Write-Warning "Remaining files in `"$env:APPDATA\.ACEStream`", to remove if needed."

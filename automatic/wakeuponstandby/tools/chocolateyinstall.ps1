578eecda484ac68d09491bf7a8e1fa904053fa49
chtof <chtof62@gmail.com>
2019-04-20 18:08:54 +0200

    Update chocolateyinstall.ps1

--- automatic/wakeuponstandby/tools/chocolateyinstall.ps1
+++ automatic/wakeuponstandby/tools/chocolateyinstall.ps1
@@ -4,11 +4,10 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir  
-  file         =  'wosb.exe'
 
   url           = 'https://dennisbabkin.com/php/downloads/WakeupOnStandBy.zip'
   checksum      = '58189BBB715303A5C8038924C50641BA14E4C964AB1847F04ADB163FA0DEDF81'
-  checksumType  = 'sha256'  
+  checksumType  = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs

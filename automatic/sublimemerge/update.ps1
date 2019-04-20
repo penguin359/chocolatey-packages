54699e836896afc3fb5e1f673547df516d45be99
chtof <chtof62@gmail.com>
2019-04-20 12:33:10 +0200

    Create update.ps1

---
+++ automatic/sublimemerge/update.ps1
@@ -0,0 +1,23 @@
+﻿import-module au
+$releases = 'https://www.sublimemerge.com/download'
+$regex = "https://download.sublimetext.com/sublime_merge_build_(?<Version>[\d]+)_x64_setup.exe"
+
+function global:au_GetLatest {
+     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	 
+	 $url = $download_page.links | ? href -match $regex | select -Last 1	 
+     return @{
+        Version = "0.0." + $matches.Version
+        URL32 = $url.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

8684f620267d6a0e4bf02f69c8797e0ce503a4da
chtof <chtof62@gmail.com>
2019-04-20 14:05:56 +0200

    embedded version

--- automatic/marktext.portable/tools/chocolateyinstall.ps1
+++ automatic/marktext.portable/tools/chocolateyinstall.ps1
@@ -1,19 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  url           = 'https://github.com/marktext/marktext/releases/download/v0.14.0/marktext-0.14.0.exe'
-  fileFullPath  = "$toolsDir\marktext-0.13.65.exe"
-
-  checksum      = 'a9a5024758895fc92a48ed4220091a78475434ff591100d8464ec4a381a07d4c'
-  checksumType  = 'sha256'  
-}
-
-Get-ChocolateyWebFile @packageArgs
-
 #Install start menu shortcuts
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Mark Text.lnk"
-$targetPath = Join-Path $toolsDir "marktext-0.13.65.exe"
+$targetPath = Join-Path $toolsDir "marktext-0.14.0.exe"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
--- automatic/marktext.portable/update.ps1
+++ automatic/marktext.portable/update.ps1
@@ -2,6 +2,16 @@
 $github_repository = "marktext/marktext"
 $releases = "https://github.com/" + $github_repository + "/releases/latest"
 
+function global:au_BeforeUpdate {
+    Remove-Item "$PSScriptRoot\tools\*.exe"
+
+    $filePath = "$PSScriptRoot\tools\$($Latest.FileName)"
+    Invoke-WebRequest $Latest.URL32 -OutFile $filePath
+
+    $Latest.ChecksumType = "sha256"
+    $Latest.Checksum = Get-FileHash -Algorithm $Latest.ChecksumType -Path $filePath | ForEach-Object Hash
+}
+
 function global:au_GetLatest {	
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	 $regex   = $github_repository + '/releases/download/.*/marktext-(?<Version>[\d\.]*)[\w-]*.exe$'
@@ -11,11 +21,20 @@ function global:au_GetLatest {
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-        }        
+        "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32).*"      = "`${1}$($Latest.URL32)"
+            "(?i)(x64).*"      = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{			
+            "marktext-[\d\.]+.exe" = "marktext-'$($Latest.Checksum32)'.exe"
+        }
     }
 }
 
-update
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none -NoCheckUrl
+}

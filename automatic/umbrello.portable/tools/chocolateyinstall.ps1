20f1f5542ef36b06df590e63183f95235b2a571d
chtof <chtof62@gmail.com>
2019-04-22 11:44:43 +0200

    Update chocolateyinstall.ps1

--- automatic/umbrello.portable/tools/chocolateyinstall.ps1
+++ automatic/umbrello.portable/tools/chocolateyinstall.ps1
@@ -15,8 +15,8 @@ $packageArgs = @{
 }
 
 # Shim only umbrello.exe
-Get-ChildItem -Path $toolsDir\umbrello-2.27.3\bin -Recurse -Filter '*.exe' -Exclude 'umbrello.exe' | ForEach-Object {
-  New-Item -path $toolsDir\umbrello-2.27.3\bin -name "$($_.Name).ignore" -type File -force | Out-Null
+foreach ( $file in 'dbus-daemon.exe', 'dbus-launch.exe', 'dbus-monitor.exe', 'dbus-send.exe', 'dbus-update-activation-environment.exe', 'drkonqi.exe', 'enchant-lsmod.exe', 'enchant.exe', 'gspawn-win64-helper-console.exe', 'gspawn-win64-helper.exe', 'kbuildsycoca4.exe', 'kcmshell4.exe', 'kconfig_compiler.exe', 'kconf_update.exe', 'kcookiejar4.exe', 'kdbgwin.exe', 'kde4-config.exe', 'kdebugdialog.exe', 'kded4.exe', 'kdeinit4.exe', 'khc_indexbuilder.exe', 'khelpcenter.exe', 'kioslave.exe', 'kio_http_cache_cleaner.exe', 'kjs.exe', 'kjscmd.exe', 'klauncher.exe', 'kmailservice.exe', 'kross.exe', 'ksendbugmail.exe', 'ktelnetservice.exe', 'ktrash.exe', 'kuiserver.exe', 'kwalletd.exe', 'kwinshutdown.exe', 'kwinstartmenu.exe', 'kwrapper4.exe', 'makekdewidgets.exe', 'po2xmi.exe', 'qdbus.exe', 'qdbusviewer.exe', 'update-mime-database.exe', 'xmi2pot.exe' ){
+  New-Item -path $toolsDir\umbrello-2.27.3\bin -name "$file.ignore" -type File -force | Out-Null
 }
 
 Install-ChocolateyZipPackage @packageArgs

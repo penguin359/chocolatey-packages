51ce0b3fdacfda548818bba6b2501004be47f043
chtof <chtof62@gmail.com>
2019-04-27 14:58:31 +0200

    Update chocolateyinstall.ps1

--- automatic/virtualdub2/tools/chocolateyinstall.ps1
+++ automatic/virtualdub2/tools/chocolateyinstall.ps1
@@ -3,10 +3,25 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
     
   file          = 'https://freefr.dl.sourceforge.net/project/vdfiltermod/VirtualDub%20pack/version%2020/VirtualDub2_43385.zip'
   checksum      = '9A87A5CD949251616A41D20CD2253BFD1B1E60090ED5BEFF79D9D2F9377F5D41'
   checksumType  = 'sha256'
+}
+
+# Shims
+if (Get-OSArchitectureWidth -Compare "32"){
+  foreach ( $file in 'vdub64.exe', 'VirtualDub64.exe'){
+    New-Item -path $toolsDir -name "$file.ignore" -type File -force | Out-Null
+  }
+} else {
+  foreach ( $file in 'vdub.exe', 'VirtualDub.exe'){
+    New-Item -path $toolsDir -name "$file.ignore" -type File -force | Out-Null
+  }
+}
+foreach ( $file in 'auxsetup.exe', 'vdlaunch.exe', 'vdlaunch64.exe'){
+  New-Item -path $toolsDir\extra -name "$file.ignore" -type File -force | Out-Null
 }
 
 Install-ChocolateyZipPackage @packageArgs
@@ -15,5 +30,5 @@ Install-ChocolateyZipPackage @packageArgs
 if (Get-OSArchitectureWidth -Compare "32") { $exe_file = "VirtualDub.exe" } else { $exe_file = "VirtualDub64.exe" }
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "VirtualDub2.lnk"
-$targetPath = Join-Path $toolsDir "portable\$exe_file"
+$targetPath = Join-Path $toolsDir "$exe_file"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

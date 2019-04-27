05fb4e85a6eeccee6419fca2225028bff455d922
chtof <chtof62@gmail.com>
2019-04-27 15:18:46 +0200

    Update chocolateyinstall.ps1

--- automatic/virtualdub2/tools/chocolateyinstall.ps1
+++ automatic/virtualdub2/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
+  destination  = $toolsDir  
     
-  file          = 'https://freefr.dl.sourceforge.net/project/vdfiltermod/VirtualDub%20pack/version%2020/VirtualDub2_43385.zip'
+  file          = "$toolsDir\VirtualDub2_43385.zip"
   checksum      = '9A87A5CD949251616A41D20CD2253BFD1B1E60090ED5BEFF79D9D2F9377F5D41'
   checksumType  = 'sha256'
 }
@@ -24,7 +24,7 @@ foreach ( $file in 'auxsetup.exe', 'vdlaunch.exe', 'vdlaunch64.exe'){
   New-Item -path $toolsDir\extra -name "$file.ignore" -type File -force | Out-Null
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs
     
 # Install start menu shortcut
 if (Get-OSArchitectureWidth -Compare "32") { $exe_file = "VirtualDub.exe" } else { $exe_file = "VirtualDub64.exe" }

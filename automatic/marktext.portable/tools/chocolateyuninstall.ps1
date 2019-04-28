7fba0756c4191b479905d9965a50b71108e7c18f
chtof <chtof62@gmail.com>
2019-04-28 12:49:27 +0200

    Update chocolateyuninstall.ps1

--- automatic/marktext.portable/tools/chocolateyuninstall.ps1
+++ automatic/marktext.portable/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
-﻿$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-
+﻿# Remove start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Mark Text.lnk"
 if(Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

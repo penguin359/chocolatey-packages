a1d3ef34f44a518c783d6db51bffd3800cd6ac56
chtof <chtof62@gmail.com>
2019-05-01 21:30:51 +0200

    Update chocolateyinstall.ps1

--- automatic/mp3skyperecorder/tools/chocolateyinstall.ps1
+++ automatic/mp3skyperecorder/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   checksum      = '98407E99F736642A1F4B9E1F3D9665448630D0E6398925374EFD601B5DCD8D4C'
   checksumType  = 'sha256'  
   
-  silentArgs	= "/qn /norestart /l*v C:\Users\Chris\AppData\Local\Temp\$env:ChocolateyPackageName.MsiInstall.log"
+  silentArgs	= "/qn /norestart /l*v $env:Temp\$env:ChocolateyPackageName.MsiInstall.log"
 }
 
 Install-ChocolateyPackage @packageArgs

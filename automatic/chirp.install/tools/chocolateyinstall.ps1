84c97cd34a475734cd5506e64be7521cbc0975a8
chtof <chtof62@gmail.com>
2019-05-01 10:59:08 +0200

    embedded version

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -1,15 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  softwareName	= 'CHIRP'  
+  softwareName	= 'CHIRP'
   fileType		= 'exe'
-  
-  url           = 'https://trac.chirp.danplanet.com/chirp_daily/daily-20190424/chirp-daily-20190424-installer.exe'
-  checksum      = '08740562681687a8d88c95e4b4f0d999afccc46894801dee93d5ab4215bf183e'
-  checksumType  = 'sha256' 
-  
-  silentArgs = '/S'
+  file          = "$toolsDir\chirp-daily-20190429-installer.exe"
+  silentArgs    = '/S'
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs

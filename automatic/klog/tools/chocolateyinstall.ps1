7231a380e64ab8af2ec5bcdeef065762711d4c16
chtof <chtof62@gmail.com>
2019-05-01 16:19:44 +0200

    embedded version

--- automatic/klog/tools/chocolateyinstall.ps1
+++ automatic/klog/tools/chocolateyinstall.ps1
@@ -1,14 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'KLog'
   fileType      = 'exe'
-
-  url           = 'https://download.savannah.gnu.org/releases/klog/win/KLog-0.9.7.2-windows-installer.exe'
-  checksum      = '2979BF01310905D48661FE96B38222D3471CF60D6A4BC0C8636FF1A4C53F9761'
-  checksumType  = 'sha256'
-
+  file          = "$ToolsDir\KLog-0.9.7.2-windows-installer.exe"
   silentArgs    = "--mode unattended"
 }
 
--- automatic/klog/update.ps1
+++ automatic/klog/update.ps1
@@ -2,6 +2,8 @@ import-module au
 $releases = 'https://download.savannah.gnu.org/releases/klog/win/'
 $regex   = 'KLog-(.*)-windows-installer.exe$'
 
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
 function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	 $url = $download_page.links | ? href -match $regex | select -Last 1
@@ -14,11 +16,21 @@ function global:au_GetLatest {
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyInstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"
+            "(?i)(Get-RemoteChecksum ).*" = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
         }
     }
 }
 
-update
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

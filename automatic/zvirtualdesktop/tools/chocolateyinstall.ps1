eae190eaa2c0605eaa868964bd0dbac6d991c7da
chtof <chtof62@gmail.com>
2019-05-01 21:31:14 +0200

    Update chocolateyinstall.ps1

--- automatic/zvirtualdesktop/tools/chocolateyinstall.ps1
+++ automatic/zvirtualdesktop/tools/chocolateyinstall.ps1
@@ -3,14 +3,18 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
+  unzipLocation = "$toolsDir"
   fileType      = 'exe'
   file          = "$toolsDir\zVirtualDesktop.exe"
       
   url           = 'https://zomp.co/Files.aspx?id=zVD'
-  checksum      = '3132C41D4D479DC776B9AA38D9937336079A666095C412FBFDCDB8B5ECC8C2E7'
+  checksum      = 'E0C15F3A67505C1DDAED0181F074A2D8EE4AA413C7713411EDF72BBA4CBE7114'
   checksumType  = 'sha256'
 }
 
-Install-ChocolateyZipPackage @packageArgs
-Start-Process -FilePath $file
+if ((Get-Item "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue('ReleaseID') -ge "1803") {
+  Install-ChocolateyZipPackage @packageArgs
+  Start-Process -FilePath $packageArgs["file"]
+} else {
+  Write-Error "This version of zVirtualDesktop runs only with Windows 10 version 1803 or greater" 
+}

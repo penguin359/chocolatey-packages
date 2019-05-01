6ce93ec81709b00bfd8a65578f4a16071e26d393
chtof <chtof62@gmail.com>
2019-05-01 11:28:03 +0200

    Update chocolateyinstall.ps1

--- automatic/jtalert/tools/chocolateyinstall.ps1
+++ automatic/jtalert/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿if ( [environment]::OSVersion.Version.Major -ge 10 )  {
-  $url_win10      = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_AL_2.13.2_Setup.exe'
-	$checksum_win10 = '9DB2BAAB29516741FAC2DE8CE5E361F55DD18CF8F1AFDAA3FB6460908E2DAC51'
+  $url_win10      = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_AL_2.13.4_Setup.exe'
+	$checksum_win10 = '9c9f18cadd6dda184bf236eb81eb61846636edbdc84d70f46e489907507a26c7'
 	$url            = $url_win10
 	$checksum       = $checksum_win10
 } else {
-  $url_others     = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_2.13.2_Setup.exe'
-	$checksum_others= '88e4d491d5145307c2c036502b1bac8c11aac6895ad7ba2b28e6eeb497a22748'
+  $url_others     = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_2.13.4_Setup.exe'
+	$checksum_others= 'F5AF76C3A3D7E0C4420D550D303DE4BF8DC0E6418B74B4611F37858E5197543C'
   $url            = $url_others
   $checksum       = $checksum_others
 }

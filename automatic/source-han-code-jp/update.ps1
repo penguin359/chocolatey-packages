e53b7230172b55ca23b69efb0931273185aa1235
chtof <chtof62@gmail.com>
2019-05-02 21:59:59 +0200

    Create update.ps1

---
+++ automatic/source-han-code-jp/update.ps1
@@ -0,0 +1,23 @@
+﻿import-module au
+$github_repository = "adobe-fonts/source-han-code-jp"
+$releases = "https://github.com/" + $github_repository + "/releases/latest"
+$regex   = "/archive/(?<Version>[\d\.]+)[A-Z]*.zip$"
+
+function global:au_GetLatest {	
+  $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
+  return @{
+    Version = $matches.Version
+    URL32 = "https://github.com" + $url.href
+  }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }        
+    }
+}
+
+update

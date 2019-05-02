682baf7edb9076174799146f9efd2a24946555af
chtof <chtof62@gmail.com>
2019-05-02 22:14:03 +0200

    Create update.ps1

---
+++ automatic/dupeguru/update.ps1
@@ -0,0 +1,23 @@
+﻿import-module au
+$github_repository = "arsenetar/dupeguru"
+$releases = "https://github.com/" + $github_repository + "/releases/latest"
+$regex   = "/dupeGuru_win64_(?<Version>[\d\.]+).exe$"
+
+function global:au_GetLatest {	
+	$url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
+	return @{
+		Version = $matches.Version
+		URL32 = "https://github.com" + $url.href
+	}
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }        
+    }
+}
+
+update

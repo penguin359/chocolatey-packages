648a3f3dca7084d0311587ab5226e18f551d98dd
chtof <clefebvre.62@free.fr>
2019-05-04 10:27:15 +0200

    embedded version

--- automatic/dnscrypt-proxy/tools/chocolateyinstall.ps1
+++ automatic/dnscrypt-proxy/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = 'https://github.com/jedisct1/dnscrypt-proxy/releases/download/2.0.22/dnscrypt-proxy-win32-2.0.22.zip'
-  file64        = 'https://github.com/jedisct1/dnscrypt-proxy/releases/download/2.0.22/dnscrypt-proxy-win64-2.0.22.zip'
+  filefullpath  = "$toolsDir\dnscrypt-proxy-win32-2.0.23_x32.zip"
+  filefullpath64= "$toolsDir\dnscrypt-proxy-win64-2.0.23_x64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
---
+++ automatic/dnscrypt-proxy/update.ps1
@@ -0,0 +1,39 @@
+﻿import-module au
+$github_repository = "jedisct1/dnscrypt-proxy"
+$releases = "https://github.com/" + $github_repository + "/releases/latest"
+$regex32 = "/dnscrypt-proxy-win32-(?<Version>[\d\.]+).zip$"
+$regex64 = "/dnscrypt-proxy-win64-(?<Version>[\d\.]+).zip$"
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
+function global:au_GetLatest {	
+     $urls = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links
+     $url32 = $urls | ? href -match $regex32
+     $url64 = $urls | ? href -match $regex64
+     return @{
+     	Version = $matches.Version
+     	URL32 = "https://github.com/" + $url32.href
+     	URL64 = "https://github.com/" + $url64.href
+     	}
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+        	"(?i)(^\s*filefullpath\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        	"(?i)(^\s*filefullpath64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""          
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

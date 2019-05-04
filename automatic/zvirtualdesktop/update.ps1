379272fcdfa56ad038ed5db0082a4fe316338f44
chtof <chtof62@gmail.com>
2019-05-04 11:33:49 +0200

    Create update.ps1

---
+++ automatic/zvirtualdesktop/update.ps1
@@ -0,0 +1,22 @@
+﻿import-module au
+$github_repository = "mzomparelli/zVirtualDesktop"
+$releases          = "https://github.com/" + $github_repository + "/releases/latest"
+$regex_version     = "/archive/(?<Version>[\d\.]+).zip$"
+$regex_url         = "^(https://zomp.co/Files.aspx\?id=.*)$"
+
+function global:au_GetLatest {	
+	(Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex_version | Out-Null
+    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex_url
+	return @{ Version = $matches.Version ; URL32 = $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }        
+    }
+}
+
+update

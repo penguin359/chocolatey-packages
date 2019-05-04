33c7b2f9478ff0552abf583fdccfe6e28dd8c92a
chtof <chtof62@gmail.com>
2019-05-04 13:08:50 +0200

    raidrive initial commit

---
+++ automatic/raidrive/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\RaiDrive_1.6.2.416.exe"
+  silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs

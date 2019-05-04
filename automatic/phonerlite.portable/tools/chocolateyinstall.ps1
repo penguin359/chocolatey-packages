0a972005168be6bb2bdadc737029a7bcb3af5093
chtof <chtof62@gmail.com>
2019-05-04 11:17:27 +0200

    embedded version

--- automatic/phonerlite.portable/tools/chocolateyinstall.ps1
+++ automatic/phonerlite.portable/tools/chocolateyinstall.ps1
@@ -3,14 +3,11 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir  
-  
-  url           = 'https://www.phoner.de/PhonerLite.zip'
-  checksum      = '18189C34F8EF4BCADB94A96AAE8E6E5DA97ADE060D43F9D0EED78DE316CD9001'
-  checksumType  = 'sha256'
+  destination   = "$toolsDir"
+  file          = "$toolsDir\PhonerLite.zip"  
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs
 
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
---
+++ automatic/phonerlite.portable/update.ps1
@@ -0,0 +1,30 @@
+﻿import-module au
+$releases = 'http://phonerlite.de/download_en.htm'
+$regex    = '<td>(?<Version>[\d\.]+)</td>'
+ 
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
+function global:au_GetLatest {	
+     (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+     return @{ Version = $matches.Version ; URL32 = 'https://www.phoner.de/PhonerLite.zip' }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

c818061b22df49e6ede64a34bc2f3ba7af33c079
chtof <chtof62@gmail.com>
2019-05-05 20:47:46 +0200

    Update chocolateyinstall.ps1

--- automatic/octave.install/tools/chocolateyinstall.ps1
+++ automatic/octave.install/tools/chocolateyinstall.ps1
@@ -3,17 +3,18 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  destination   = $toolsDir
+  destination   = "$toolsDir"
     
-  url           = "https://ftpmirror.gnu.org/octave/windows/octave-5.1.0-w32-installer.exe"
+  url           = "http://ftpmirror.gnu.org/octave/windows/octave-5.1.0-w32-installer.exe"
   checksum      = 'D2E4D5821496A0C7533ABB660810BB31C73CF6DC1BC19430B6B35586BC2F5FF9'
   checksumType  = 'sha256'
   
-  url64         = "https://ftpmirror.gnu.org/octave/windows/octave-5.1.0-w64-installer.exe"
+  url64         = "http://ftpmirror.gnu.org/octave/windows/octave-5.1.0-w64-installer.exe"
   checksum64    = '9F80C37BAF041BD741C2660E7E5E12AFBD19DAAB7E59AABA96288E630282122B'
   checksumType64= 'sha256'
   
-  silentArgs	= "/S"
+  # Note: silentArgs doesn't work as expected for octave.install
+  # silentArgs	= "/S"
 }
 
 # Don't create shims for any executables
@@ -22,4 +23,5 @@ foreach ($file in $files) {
     New-Item "$file.ignore" -type file -force | Out-Null
 }
 
+Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
 Install-ChocolateyPackage @packageArgs

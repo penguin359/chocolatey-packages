4b6a1c3d02d33df3477840b346185f696e89996e
chtof <chtof62@gmail.com>
2019-05-05 20:02:31 +0200

    Update chocolateyinstall.ps1

--- automatic/mp3skyperecorder/tools/chocolateyinstall.ps1
+++ automatic/mp3skyperecorder/tools/chocolateyinstall.ps1
@@ -4,11 +4,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   filetype      = 'msi'
 
-  url           = 'https://voipcallrecording.com/MP3SkypeRecorderSetup.msi?version=4.50.1'
-  checksum      = '98407E99F736642A1F4B9E1F3D9665448630D0E6398925374EFD601B5DCD8D4C'
+  url           = 'https://voipcallrecording.com/MP3SkypeRecorderSetup.msi?version=4.51'
+  checksum      = '00EA323F4545E82EBBAD38F57249F5F68ECC208183422114FA035CDB52DB45EF'
   checksumType  = 'sha256'
   
   silentArgs	= "/qn /norestart /l*v $env:Temp\$env:ChocolateyPackageName.MsiInstall.log"
+  validExitCodes= @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs

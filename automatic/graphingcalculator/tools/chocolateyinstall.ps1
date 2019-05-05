0a1cd033effbcabb1aa5f537e432952538e95418
chtof <chtof62@gmail.com>
2019-05-05 21:56:29 +0200

    graphingcalculator initial commit

---
+++ automatic/graphingcalculator/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\GeoGebraGraphing-Windows-Installer-6-0-535-0.exe"  
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "GeoGebra Graphing Calculator.lnk"
+$targetPath = Join-Path $toolsDir "GeoGebraGraphing-Windows-Installer-6-0-535-0.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

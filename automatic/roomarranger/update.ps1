ec973381dd3288fd0ed1b656320843148687c4b6
chtof <chtof62@gmail.com>
2019-05-08 12:09:07 +0200

    Create update.ps1

---
+++ automatic/roomarranger/update.ps1
@@ -0,0 +1,32 @@
+﻿import-module au
+$releases = 'http://www.roomarranger.com/whatsnew.txt'
+$regex = 'version (?<Version>[\d\.]+) \('
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
+function global:au_GetLatest {
+     (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+     $version_url = $matches.Version -Replace '\.', ''
+     return @{ Version = $matches.Version ; URL32 = 'https://s3.eu-central-1.amazonaws.com/rooarr/rooarr' + $version_url + '.exe' }
+}
+
+function global:au_SearchReplace {
+    @{
+        "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

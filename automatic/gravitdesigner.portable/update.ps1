a3695b436b3497a5738e1e496360607ca9fbd927
chtof <chtof62@gmail.com>
2019-05-08 18:37:15 +0200

    Create update.ps1

---
+++ automatic/gravitdesigner.portable/update.ps1
@@ -0,0 +1,20 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://www.designer.io/changelog'
+    $regex   = '<h3>Version (?<Version>[\d\.\-]+)</h3>'
+    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+    $version = $matches.Version -replace '-', '.'
+    	 
+	 return @{ Version = $version }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{            
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }        
+    }
+}
+
+update

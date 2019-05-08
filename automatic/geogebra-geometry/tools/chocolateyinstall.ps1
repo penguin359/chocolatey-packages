7165700fa76071505b165cb7d2531c521d098a02
chtof <chtof62@gmail.com>
2019-05-08 12:36:34 +0200

    Update chocolateyinstall.ps1

--- automatic/geogebra-geometry/tools/chocolateyinstall.ps1
+++ automatic/geogebra-geometry/tools/chocolateyinstall.ps1
@@ -1,10 +1,11 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$file     = 'GeoGebraGeometry-Windows-Installer-6-0-535-0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\GeoGebraGeometry-Windows-Installer-6-0-535-0.exe"
+  filefullpath  = "$toolsDir\$file"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
@@ -12,5 +13,5 @@ Install-ChocolateyInstallPackage @packageArgs
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "GeoGebra Geometry.lnk"
-$targetPath = Join-Path $toolsDir "GeoGebraGeometry-Windows-Installer-6-0-535-0.exe"
+$targetPath = Join-Path $toolsDir $file
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

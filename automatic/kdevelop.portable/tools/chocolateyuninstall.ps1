6eed2c06e778ea6e7a032dee810702a42a35a55e
chtof <clefebvre.62@free.fr>
2019-05-10 21:02:51 +0200

    kdevelop.portable embedded version

--- automatic/kdevelop.portable/tools/chocolateyuninstall.ps1
+++ automatic/kdevelop.portable/tools/chocolateyuninstall.ps1
@@ -1,6 +1,4 @@
-﻿$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-
+# Remove start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Kdevelop.lnk"
-if(Test-Path $shortcutFilePath) {
-    del $shortcutFilePath
-}
+if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

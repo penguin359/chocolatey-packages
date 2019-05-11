8285aa3aa8dcb3f3950acc07b5445667740b95c2
chtof <chtof62@gmail.com>
2019-05-11 10:06:30 +0200

    Update update.ps1

--- automatic/laragon/update.ps1
+++ automatic/laragon/update.ps1
@@ -1,12 +1,12 @@
 import-module au
 
-$releases = 'https://github.com/leokhoa/laragon/releases/latest'
-
 function global:au_GetLatest {
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	 $regex   = '/releases/download/(?<Version>.*?)/laragon-full.exe'
-	 $url = $download_page.links | ? href -match $regex
-     return @{ Version = $matches.Version }
+    $releases = 'https://github.com/leokhoa/laragon/releases/latest'
+    $regex   = '/releases/download/(?<Version>.*?)/laragon-full.exe'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url = $download_page.links | ? href -match $regex
+    return @{ Version = $matches.Version }
 }
 
 function global:au_SearchReplace {

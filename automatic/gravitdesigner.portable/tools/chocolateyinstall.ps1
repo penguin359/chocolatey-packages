56401a20c384fb70d8cec32447ffd929b8ec3866
chtof <chtof62@gmail.com>
2019-05-11 10:19:15 +0200

    Update chocolateyinstall.ps1

--- automatic/gravitdesigner.portable/tools/chocolateyinstall.ps1
+++ automatic/gravitdesigner.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = "$env:ChocolateyPackageName"
   fileFullPath  = "$toolsDir\GravitDesigner.exe"
-    
+
   url           = 'https://designer.gravit.io/_downloads/windows/GravitDesigner.exe'
   checksum      = 'd8507f21dde3a1aedb4b310160995dbb80922df8f1828cf92fe9f036e1db9edc'
   checksumType  = 'sha256'

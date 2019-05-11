6900936717df5971f2125b7cd308cedb183424ba
chtof <chtof62@gmail.com>
2019-05-11 17:13:38 +0200

    Update chocolateyinstall.ps1

--- automatic/flrig/tools/chocolateyinstall.ps1
+++ automatic/flrig/tools/chocolateyinstall.ps1
@@ -3,14 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $silentArgs = '/S'
 
 $pp = Get-PackageParameters
-if ($pp['DIR']){	
+if ($pp['DIR']){
   $silentArgs += " /D=`"$($pp['DIR'])`""
 }
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'exe'  
-  file          = "$toolsDir\flrig-1.3.44_setup_x32.exe"  
+  packageName   = $env:ChocolateyPackageName  
+  file          = "$toolsDir\flrig-1.3.44_setup_x32.exe"
   silentArgs    = $silentArgs
 }
 

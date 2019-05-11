7ef1905f1e04e4fc03de32c1f514d17e0c23e881
chtof <chtof62@gmail.com>
2019-05-11 17:01:53 +0200

    Create update.ps1

---
+++ automatic/station/update.ps1
@@ -0,0 +1,22 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://github.com/getstation/desktop-app-releases/releases/latest'
+    $regex   = 'browserX-Setup-(?<Version>[\d\.]+).exe$'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$url = $download_page.links | ? href -match $regex
+	
+    return @{ Version = $matches.version ; URL32 = 'https://github.com' + $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

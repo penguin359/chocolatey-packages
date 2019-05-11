8acf0f926ab433f58ea51f485ed77df1e4c586e5
chtof <chtof62@gmail.com>
2019-05-11 16:46:54 +0200

    embedded version

---
+++ automatic/skympc/update.ps1
@@ -0,0 +1,33 @@
+﻿import-module au
+ 
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+  $releases = 'http://www.soramimi.jp/skympc'  
+  $regex    = '/SkyMPC-(?<Version>[\d\.]+)-windows.zip$'
+  
+  $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing)
+  $url = $download_page.links | ? href -match $regex
+
+  return @{ Version = $matches.Version ; URL32 = $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

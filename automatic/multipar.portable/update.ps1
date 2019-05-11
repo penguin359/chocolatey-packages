3789b646d7c8f32dcb174633946d610559681114
chtof <chtof62@gmail.com>
2019-05-11 18:45:59 +0200

    Update update.ps1

--- automatic/multipar.portable/update.ps1
+++ automatic/multipar.portable/update.ps1
@@ -4,8 +4,7 @@ function global:au_GetLatest {
     $releases = 'https://multipar.eu'
     $regex    = 'MultiPar(?<Version>[\d]+)_setup.exe$'
 
-    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
-    $version = 
+    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex    
     
     return @{
         Version = $matches.Version -replace "(\d+)(\d)(\d)(\d)", '$1.$2.$3.$4'

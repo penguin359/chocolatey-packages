c8d6348309adec58d01e5748d52b456d981b646e
chtof <chtof62@gmail.com>
2019-05-11 18:47:55 +0200

    Create update.ps1

---
+++ automatic/multipar/update.ps1
@@ -0,0 +1,20 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://multipar.eu'
+    $regex    = 'MultiPar(?<Version>[\d]+)_setup.exe$'
+
+    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
+        
+    return @{ Version = $matches.Version -replace "(\d+)(\d)(\d)(\d)", '$1.$2.$3.$4' }
+}
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update

7b4a0e96caf9edc2728a4b4c285d9a95f206de7f
chtof <chtof62@gmail.com>
2019-05-11 12:41:23 +0200

    Update update.ps1

--- automatic/phonerlite.install/update.ps1
+++ automatic/phonerlite.install/update.ps1
@@ -1,27 +1,25 @@
 ﻿import-module au
 $releases = "http://phonerlite.de/download_en.htm"
-$regex   = '<td>(?<Version>[\d\.]+)</td>'
+$regex    = '<td>(?<Version>[\d\.]+)</td>'
  
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
-function global:au_GetLatest {	
-     (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null	
-     return @{
-        Version = $matches.Version        
-        URL32 = 'https://www.phoner.de/PhonerLiteSetup_' + $matches.Version + '.exe'  }
+function global:au_GetLatest {
+     (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+     return @{ Version = $matches.Version ; URL32 = 'https://www.phoner.de/PhonerLiteSetup_' + $matches.Version + '.exe' }
 }
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{            
+       "legal\VERIFICATION.txt"  = @{
             "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
         }
 
-        "tools\chocolateyinstall.ps1" = @{        
+        "tools\chocolateyinstall.ps1" = @{
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
         }
     }

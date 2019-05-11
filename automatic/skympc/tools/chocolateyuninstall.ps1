754b72bdae450684334f8fc09b1f45b016e129e7
chtof <chtof62@gmail.com>
2019-05-11 18:51:15 +0200

    Update chocolateyuninstall.ps1

--- automatic/skympc/tools/chocolateyuninstall.ps1
+++ automatic/skympc/tools/chocolateyuninstall.ps1
@@ -1,3 +1,4 @@
-﻿$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+﻿# Remove start menu shortcuts
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "SkyMPC.lnk"
-if(Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
+if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

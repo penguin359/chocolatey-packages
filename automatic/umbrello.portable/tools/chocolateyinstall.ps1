031fcd0bcc6ce890c30905dda0988f2180b9c8d8
chtof <chtof62@gmail.com>
2019-05-11 14:07:10 +0200

    umbrello.portable embedded

--- automatic/umbrello.portable/tools/chocolateyinstall.ps1
+++ automatic/umbrello.portable/tools/chocolateyinstall.ps1
@@ -3,15 +3,10 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
+  destination   = "$toolsDir"
 
-  url           = 'https://download.kde.org/stable/umbrello/latest/win32/umbrello-i686-w64-mingw32-2.27.3-10.4-bin.7z'
-  checksum      = 'A31186E99A82DF32D46B12778F1DB4E19DFFB91F0EB2B9FD20CF9247B553EB21'
-  checksumType  = 'sha256'
-  
-  url64bit      = 'https://download.kde.org/stable/umbrello/latest/win64/umbrello-x86_64-w64-mingw32-2.27.3-82.4-bin.7z'
-  checksum64    = '629E445C4EC5E42917F3FCAF154BD0700E9D9F39119155A784777106CD7B8BBF'
-  checksumType64= 'sha256'  
+  file          = "$toolsDir\umbrello-i686-w64-mingw32-2.27.3-10.4-bin.7z"
+  file64        = "$toolsDir\umbrello-x86_64-w64-mingw32-2.27.3-82.4-bin.7z"
 }
 
 # Shim only umbrello.exe
@@ -19,7 +14,7 @@ foreach ( $file in 'dbus-daemon.exe', 'dbus-launch.exe', 'dbus-monitor.exe', 'db
   New-Item -path $toolsDir\umbrello-2.27.3\bin -name "$file.ignore" -type File -force | Out-Null
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Get-UnzipPackage @packageArgs
     
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
---
+++ automatic/umbrello.portable/update.ps1
@@ -0,0 +1,34 @@
+﻿import-module au
+ 
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+  $releases = 'https://download.kde.org/stable/umbrello/latest/win32/'
+  $regex    = 'umbrello-i686-w64-mingw32-(?<Version>[\d\.]+).*-bin.7z$'
+  
+  $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing)
+  $file = $download_page.links | ? href -match $regex
+  return @{ Version = $matches.Version ; URL32 = 'https://download.kde.org/stable/umbrello/latest/win32/' + $file.href }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
+          "umbrello-[\d\.]+(\bin\umbrello.exe)" = "umbrello-$($Latest.Version)`$1"
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

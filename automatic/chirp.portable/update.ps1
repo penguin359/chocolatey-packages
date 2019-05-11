00208619377537759b4aa34b3d320eed17a5ddb8
chtof <chtof62@gmail.com>
2019-05-11 13:12:40 +0200

    Update update.ps1

--- automatic/chirp.portable/update.ps1
+++ automatic/chirp.portable/update.ps1
@@ -1,17 +1,17 @@
 ﻿import-module au
-$releases = "https://trac.chirp.danplanet.com/chirp_daily/LATEST/"
 
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {	
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	 
-	 $regex   = '^chirp-daily-(?<date_url>\d+)-win32.zip$'
-	 $download_page.links | ? href -match $regex
-	 $version_YY = $matches.date_url.substring(0, $matches.date_url.length-4)
-	 $version_MM = $matches.date_url.substring($matches.date_url.length-4, 2)
-	 $version_DD = $matches.date_url.substring($matches.date_url.length-2)
-     $version = $version_YY + "." + $version_MM + "." + $version_DD
-     return @{
+    $releases = "https://trac.chirp.danplanet.com/chirp_daily/LATEST/"
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regex   = '^chirp-daily-(?<date_url>\d+)-win32.zip$'
+    $download_page.links | ? href -match $regex
+    $version_YY = $matches.date_url.substring(0, $matches.date_url.length-4)
+    $version_MM = $matches.date_url.substring($matches.date_url.length-4, 2)
+    $version_DD = $matches.date_url.substring($matches.date_url.length-2)
+    $version = $version_YY + "." + $version_MM + "." + $version_DD
+    return @{
         Version = $version
         URL32 = 'https://trac.chirp.danplanet.com/chirp_daily/daily-' + $matches.date_url + "/chirp-daily-" + $matches.date_url + '-win32.zip' }
 }
@@ -20,8 +20,7 @@ function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{            
             "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(Get-RemoteChecksum ).*" = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"

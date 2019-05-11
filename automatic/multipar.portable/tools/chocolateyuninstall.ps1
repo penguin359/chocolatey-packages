f37790360b41cda05fb1ba4fb01d090657ad6de5
chtof <chtof62@gmail.com>
2019-05-11 18:44:32 +0200

    Update chocolateyuninstall.ps1

--- automatic/multipar.portable/tools/chocolateyuninstall.ps1
+++ automatic/multipar.portable/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
-﻿$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-
+﻿# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "MultiPar.lnk"
 if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

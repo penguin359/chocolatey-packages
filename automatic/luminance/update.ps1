02399cf6af75ac478443b6d629ace9b0b0c08e84
chtof <chtof62@gmail.com>
2019-05-11 13:37:31 +0200

    Create update.ps1

---
+++ automatic/luminance/update.ps1
@@ -0,0 +1,20 @@
+﻿import-module au
+ 
+function global:au_GetLatest {
+  $releases = 'http://qtpfsgui.sourceforge.net/?page_id=10'  
+  $regex    = 'Luminance-HDR-x64-SETUP-v(?<Version>[\d\.]+).exe'
+
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing 
+  $url = $download_page.links | ? href -match $regex  
+  return @{ Version = $matches.Version }
+}
+
+function global:au_SearchReplace {
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

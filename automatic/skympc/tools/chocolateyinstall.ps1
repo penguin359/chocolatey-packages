d0329aa74de138a86841854b0c49899a0bf1dc37
chtof <chtof62@gmail.com>
2019-05-11 18:51:06 +0200

    Update chocolateyinstall.ps1

--- automatic/skympc/tools/chocolateyinstall.ps1
+++ automatic/skympc/tools/chocolateyinstall.ps1
@@ -1,17 +1,16 @@
 $ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = "$toolsDir"
-  file          = "$toolsDir\SkyMPC-1.6.4-windows.zip"
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\SkyMPC-1.6.4-windows.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
 
-#Install start menu shortcuts
+# Install start menu shortcuts
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-
 $shortcutFilePath = Join-Path $programs "SkyMPC.lnk"
 $targetPath = Join-Path $toolsDir "SkyMPC\SkyMPC.exe"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

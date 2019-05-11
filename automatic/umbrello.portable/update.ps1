0fb55970d0adeb30979bbec7a1a82a010f69f5dd
chtof <chtof62@gmail.com>
2019-05-11 14:25:52 +0200

    Update update.ps1

--- automatic/umbrello.portable/update.ps1
+++ automatic/umbrello.portable/update.ps1
@@ -3,25 +3,35 @@
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-  $releases = 'https://download.kde.org/stable/umbrello/latest/win32/'
-  $regex    = 'umbrello-i686-w64-mingw32-(?<Version>[\d\.]+).*-bin.7z$'
-  
-  $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing)
-  $file = $download_page.links | ? href -match $regex
-  return @{ Version = $matches.Version ; URL32 = 'https://download.kde.org/stable/umbrello/latest/win32/' + $file.href }
+  $releases_32 = 'https://download.kde.org/stable/umbrello/latest/win32'
+  $regex_32    = 'umbrello-i686-w64-mingw32-(?<Version>[\d\.]+).*-bin.7z$'
+  $releases_64 = 'https://download.kde.org/stable/umbrello/latest/win64'
+  $regex_64    = 'umbrello-x86_64-w64-mingw32-[\d\.]+.*-bin.7z$'
+
+  $download_page_32 = (Invoke-WebRequest -Uri $releases_32 -UseBasicParsing)
+  $file_32 = $download_page_32.links | ? href -match $regex_32
+  $version = $matches.Version
+
+  $download_page_64 = (Invoke-WebRequest -Uri $releases_64 -UseBasicParsing)
+  $file_64 = $download_page_64.links | ? href -match $regex_64
+
+  return @{
+    Version = $version
+    URL32 = "$releases_32/$file_32.href"
+    URL64 = "$releases_64/$file_64.href"
 }
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"            
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
-        "tools\chocolateyinstall.ps1" = @{        
+        "tools\chocolateyinstall.ps1" = @{
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
           "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
           "umbrello-[\d\.]+\\" = "umbrello-$($Latest.Version)\\"

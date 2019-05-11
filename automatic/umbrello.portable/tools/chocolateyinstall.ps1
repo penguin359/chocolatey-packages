124a4e280ecd874bef54fa5055993d68c4f4baf3
chtof <chtof62@gmail.com>
2019-05-11 14:11:45 +0200

    Update chocolateyinstall.ps1

--- automatic/umbrello.portable/tools/chocolateyinstall.ps1
+++ automatic/umbrello.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,6 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-
   file          = "$toolsDir\umbrello-i686-w64-mingw32-2.27.3-10.4-bin.7z"
   file64        = "$toolsDir\umbrello-x86_64-w64-mingw32-2.27.3-82.4-bin.7z"
 }

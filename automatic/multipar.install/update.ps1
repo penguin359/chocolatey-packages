32f820eb34973100098f7489ae05f06962f24809
chtof <chtof62@gmail.com>
2019-05-11 18:46:41 +0200

    Create update.ps1

---
+++ automatic/multipar.install/update.ps1
@@ -0,0 +1,24 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://multipar.eu'
+    $regex    = 'MultiPar(?<Version>[\d]+).zip$'
+
+    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
+        
+    return @{
+        Version = $matches.Version -replace "(\d+)(\d)(\d)(\d)", '$1.$2.$3.$4'
+        URL32 = $releases + "/" + $url.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

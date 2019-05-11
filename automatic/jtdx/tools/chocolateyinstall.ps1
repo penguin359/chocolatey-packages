b7bbb5086ec1879b6a7739e59d88f11a4823a701
chtof <chtof62@gmail.com>
2019-05-11 15:44:20 +0200

    Update chocolateyinstall.ps1

--- automatic/jtdx/tools/chocolateyinstall.ps1
+++ automatic/jtdx/tools/chocolateyinstall.ps1
@@ -3,11 +3,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  destination = $toolsDir
-    
-  file          = "$toolsDir\JTDX-18.0.0.133-GA-win32.zip"
-  checksum      = 'EAD9608470E9B18D61B67D19BAB01A5536F92CE2771459D656A047351556BE79'
-  checksumType  = 'sha256'
+  destination   = "$toolsDir"    
+  file          = "$toolsDir\JTDX-18.0.0.133-GA-win32.zip"  
 }
 
 Get-ChocolateyUnzip @packageArgs

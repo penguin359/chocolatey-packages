fb6c102513e1a04fa86d353151564078ede70283
chtof <chtof62@gmail.com>
2019-05-12 13:47:06 +0200

    Update chocolateyinstall.ps1

--- automatic/uncrustify/tools/chocolateyinstall.ps1
+++ automatic/uncrustify/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = "$env:ChocolateyPackageName"
   destination   = "$toolsDir"    
-  file          = "$file\uncrustify-0.68.1-win32.zip"
-  file64        = "$file\uncrustify-0.68.1-win64.zip"
+  file          = "$toolsDir\uncrustify-0.68.1-win32.zip"
+  file64        = "$toolsDir\uncrustify-0.68.1-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

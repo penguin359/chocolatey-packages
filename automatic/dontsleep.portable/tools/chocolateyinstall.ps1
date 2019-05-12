5d978701345a807e5215fd485fd950407d99a3bf
chtof <chtof62@gmail.com>
2019-05-12 21:55:49 +0200

    Update chocolateyinstall.ps1

--- automatic/dontsleep.portable/tools/chocolateyinstall.ps1
+++ automatic/dontsleep.portable/tools/chocolateyinstall.ps1
@@ -10,10 +10,9 @@ $packageArgs = @{
 
 Get-ChocolateyUnzip @packageArgs
     
-#Install start menu shortcuts
+# Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 if (Get-OSArchitectureWidth -Compare "32") { $exe_file = "DontSleep_p.exe" } else { $exe_file = "DontSleep_x64_p.exe" }
-
 $shortcutFilePath = Join-Path $programs "Dont Sleep.lnk"
 $targetPath = Join-Path $toolsDir $exe_file
 Install-ChocolateyShortcut -RunAsAdmin -shortcutFilePath $shortcutFilePath -targetPath $targetPath

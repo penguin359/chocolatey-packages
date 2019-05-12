30ab81df3b2b3aa1a021d6ae9e29534c73e2ef5e
chtof <chtof62@gmail.com>
2019-05-12 11:15:10 +0200

    Update update.ps1

--- automatic/dupeguru/update.ps1
+++ automatic/dupeguru/update.ps1
@@ -1,14 +1,12 @@
 ﻿import-module au
-$github_repository = "arsenetar/dupeguru"
-$releases = "https://github.com/" + $github_repository + "/releases/latest"
-$regex   = "/dupeGuru_win64_(?<Version>[\d\.]+).exe$"
 
-function global:au_GetLatest {	
+function global:au_GetLatest {
+    $releases = "https://dupeguru.voltaicideas.net"
+    $regex    = "/dupeGuru_win64_(?<Version>[\d\.]+).exe$"
+
 	$url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
-	return @{
-		Version = $matches.Version
-		URL32 = "https://github.com" + $url.href
-	}
+
+	return @{ Version = $matches.Version ; URL32 = $url.href }
 }
 
 function global:au_SearchReplace {

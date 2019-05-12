e7e57a38dac8a2172b1ad22a3422c3ce257fa663
chtof <chtof62@gmail.com>
2019-05-12 12:34:28 +0200

    Update update.ps1

--- automatic/hugin/update.ps1
+++ automatic/hugin/update.ps1
@@ -12,7 +12,7 @@ function global:au_SearchReplace {
     @{
         "$($Latest.PackageName).nuspec" = @{         
             "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
-        }        
+        }
     }
 }
 

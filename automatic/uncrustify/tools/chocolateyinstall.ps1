d07deeae89514346b479dbcb261a3d0f4d750389
chtof <chtof62@gmail.com>
2019-05-12 11:38:58 +0200

    embedded

--- automatic/uncrustify/tools/chocolateyinstall.ps1
+++ automatic/uncrustify/tools/chocolateyinstall.ps1
@@ -2,16 +2,10 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-    
-  url           = 'https://sourceforge.net/projects/uncrustify/files/uncrustify/uncrustify-0.68.1/uncrustify-0.68.1-win32.zip/download'
-  checksum      = 'C7EA58B9059E8AE0691AB80D80DAE413EEF02E04895609E4BA08F901B8FDD801'
-  checksumType  = 'sha256'
-  
-  url64bit      = 'https://sourceforge.net/projects/uncrustify/files/uncrustify/uncrustify-0.68.1/uncrustify-0.68.1-win64.zip/download'
-  checksum64    = '3C027D5E33C24A7EE5956084C2F1A26F419412C38DAF30A55A428CABBE84ED30'
-  checksumType64= 'sha256'
+  packageName   = "$env:ChocolateyPackageName"
+  destination   = "$toolsDir"    
+  file          = "$file\uncrustify-0.68.1-win32.zip"
+  file64        = "$file\uncrustify-0.68.1-win64.zip"
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs

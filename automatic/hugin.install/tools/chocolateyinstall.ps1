385c62a10f11ec0c4d9a1b153ba5bcc7b1c35e67
chtof <chtof62@gmail.com>
2019-05-12 12:18:50 +0200

    embedded

--- automatic/hugin.install/tools/chocolateyinstall.ps1
+++ automatic/hugin.install/tools/chocolateyinstall.ps1
@@ -1,15 +1,12 @@
 $ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  filetype      = 'msi'
-    
-  url           = 'https://freefr.dl.sourceforge.net/project/hugin/hugin/hugin-2019.0/Hugin-2019.0.0-win64.msi'
-  checksum      = 'D9692E6A87AF591A15B04EDE07B1A75263AA6B57543F01E3E6ADA15B25F61A35'
-  checksumType  = 'sha256'
-
+  filetype      = 'MSI'    
+  file          = "$toolsDir\Hugin-2019.0.0-win64.msi"
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
--- automatic/hugin.install/update.ps1
+++ automatic/hugin.install/update.ps1
@@ -1,20 +1,33 @@
 ﻿import-module au
-$releases = 'http://hugin.sourceforge.net/download'
-$regex = "https://sourceforge.net/projects/hugin/files/hugin/hugin-[\d\.]+/Hugin-(?<Version>[\d\.]+)-win64.msi/download"
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	 
-	 $url = $download_page.links | ? href -match $regex	 
-     return @{ Version = $matches.Version ; URL32 = $url.href }
+    $releases = 'http://hugin.sourceforge.net/download'
+    $regex = "https://sourceforge.net/projects/hugin/files/hugin/hugin-[\d\.]+/Hugin-(?<Version>[\d\.]+)-win64.msi/download"
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$url = $download_page.links | ? href -match $regex
+    
+    return @{ Version = $matches.Version ; URL32 = $url.href }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
         }
     }
 }
 
-update
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

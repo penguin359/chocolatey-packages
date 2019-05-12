010a5f705d7fb4b91e440cc2741c18939d59159a
chtof <chtof62@gmail.com>
2019-05-12 12:52:14 +0200

    Create update.ps1

---
+++ automatic/raidrive/update.ps1
@@ -0,0 +1,32 @@
+﻿import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://www.raidrive.com/download'
+    $regex   = '\<strong style="font-size: 28px;"\>(?<Version>[\d\.]+)\</strong\>'
+
+    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+
+    return @{ Version = $matches.Version }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

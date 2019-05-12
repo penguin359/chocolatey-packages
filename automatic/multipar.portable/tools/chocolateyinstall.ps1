fe34332aba72aa05c798bb5e39f7497be053930d
chtof <chtof62@gmail.com>
2019-05-12 11:18:01 +0200

    Update chocolateyinstall.ps1

--- automatic/multipar.portable/tools/chocolateyinstall.ps1
+++ automatic/multipar.portable/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$toolsDir"
-  url           = 'https://multipar.eu/downloads/1.3.0/MultiPar1305_setup.exe'
-  checksum      = '98207E8F0E1011EAB4E9E0947F3ED6AA09E28EF92EC62C2786F89D1BE96C606C'
+  url           = 'https://multipar.eu/downloads/1.3.0/MultiPar1305.zip'
+  checksum      = '8cbcc49e7034bf2b5aae35378e9173e68c03adc269fb2d0a6eb2b2f87d97c7a3'
   checksumType  = 'sha256'
 }
 

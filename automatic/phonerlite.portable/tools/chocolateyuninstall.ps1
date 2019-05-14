ce6b95febb641f1fc1cb24f55f75ac50fa6243af
chtof <chtof62@gmail.com>
2019-05-14 13:46:54 +0200

    Update chocolateyuninstall.ps1

--- automatic/phonerlite.portable/tools/chocolateyuninstall.ps1
+++ automatic/phonerlite.portable/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
+# Remove start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-
 $shortcutFilePath = Join-Path $programs "PhonerLite.lnk"
 if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

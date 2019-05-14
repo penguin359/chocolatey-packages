a3ffadeb069931cb41e11c927fe56633ea858d3e
chtof <chtof62@gmail.com>
2019-05-14 12:06:36 +0200

    Update chocolateyinstall.ps1

--- automatic/moneydance/tools/chocolateyinstall.ps1
+++ automatic/moneydance/tools/chocolateyinstall.ps1
@@ -1,12 +1,11 @@
 $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  url           = 'https://infinitekind.com/stabledl/current/Moneydance_windows_amd64.exe'
-  checksum      = '7A8C247D64D55D67E9AD1D2BF7916859950D79020E8AC5F3276C2DC26C498AD3'
-  checksumType  = 'sha256'
-  
-  silentArgs	= "-q"
+  packageName    = $env:ChocolateyPackageName
+  url64          = 'https://infinitekind.com/stabledl/current/Moneydance_windows_amd64.exe'
+  checksum64     = '7A8C247D64D55D67E9AD1D2BF7916859950D79020E8AC5F3276C2DC26C498AD3'
+  checksumType64 = 'sha256'  
+  silentArgs	 = "-q"
 }
 
 Install-ChocolateyPackage @packageArgs

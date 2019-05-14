53071384b0321390d990eaa404abe6074fe89151
chtof <chtof62@gmail.com>
2019-05-14 12:06:44 +0200

    Update update.ps1

--- automatic/moneydance/update.ps1
+++ automatic/moneydance/update.ps1
@@ -15,8 +15,8 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }        
     }
 }

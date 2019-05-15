ac4d1b2b76210fad462425ce9cfc98c190256199
chtof <chtof62@gmail.com>
2019-05-15 12:16:07 +0200

    fix for handle the x64 version

--- automatic/luminance.install/tools/chocolateyinstall.ps1
+++ automatic/luminance.install/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName      
-  file          = "$toolsDir\Luminance-HDR-x64-SETUP-v2.5.1.exe"  
+  file64        = "$toolsDir\Luminance-HDR-x64-SETUP-v2.5.1.exe"  
   silentArgs	= "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 
--- automatic/luminance.install/update.ps1
+++ automatic/luminance.install/update.ps1
@@ -3,26 +3,24 @@
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-  $releases = 'http://qtpfsgui.sourceforge.net/?page_id=10'  
-  $regex    = 'Luminance-HDR-x64-SETUP-v(?<Version>[\d\.]+).exe'
+  $releases = 'http://qtpfsgui.sourceforge.net/?page_id=10'
+  $regex    = '(?<Url>.*/Luminance-HDR-x64-SETUP-v(?<Version>[\d\.]+).exe).*'
 
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing 
-  $url = $download_page.links | ? href -match $regex  
-  return @{ Version = $matches.Version ; URL32 = $url.href }
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $download_page.links | ? href -match $regex | Out-Null
+  return @{ Version = $matches.Version ; URL64 = $matches.Url }
 }
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+       "legal\VERIFICATION.txt"  = @{                        
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"            
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }

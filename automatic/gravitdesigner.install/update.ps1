303f40fd2264d71c2aebfd93ac862a1f402125bd
chtof <chtof62@gmail.com>
2019-05-15 12:15:18 +0200

    Update update.ps1

--- automatic/gravitdesigner.install/update.ps1
+++ automatic/gravitdesigner.install/update.ps1
@@ -12,9 +12,8 @@ function global:au_GetLatest {
     $releases = 'https://www.designer.io/changelog'
     $regex   = '<h3>Version (?<Version>[\d\.\-]+)</h3>'
     (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
-    $version = $matches.Version -replace '-', '.'
-    	 
-	return @{ Version = $version }
+
+	return @{ Version = $matches.Version -replace '-', '.' }
 }
 
 function global:au_SearchReplace {

29d19352572f937b2610205715e3a9364ddcf091
chtof <chtof62@gmail.com>
2019-05-15 19:30:07 +0200

    Update chocolateyinstall.ps1

--- automatic/raidrive/tools/chocolateyinstall.ps1
+++ automatic/raidrive/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\RaiDrive_1.6.2.416.exe"
+  file          = "$toolsDir\RaiDrive.exe"
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }

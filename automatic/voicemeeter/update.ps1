a14845457304a3e9ec6904a036f704427765798d
chtof <chtof62@gmail.com>
2019-05-16 12:31:53 +0200

    Create update.ps1

---
+++ automatic/voicemeeter/update.ps1
@@ -0,0 +1,21 @@
+﻿import-module au
+ 
+function global:au_GetLatest {
+  $releases       = 'https://www.vb-audio.com/Voicemeeter'  
+  $regex_version  = 'Voicemeeter (?<Version>[\d\.]+).*\(EXE file\)'
+
+  $download_page  = (Invoke-WebRequest -Uri $releases)
+  $download_page.RawContent -match $regex_version
+  
+  return @{ Version = $matches.Version }
+}
+
+function global:au_SearchReplace {
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

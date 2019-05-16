922662c22e3d49a1348742570a8c5a1eae41a1d9
chtof <chtof62@gmail.com>
2019-05-16 15:12:42 +0200

    office-to-pdf initial commit

---
+++ automatic/office-to-pdf/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\OfficeToPDF.exe"  
+}

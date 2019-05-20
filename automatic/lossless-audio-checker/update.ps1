a7ee6be8f44c737007914e64e1382d6edc1dff06
chtof <chtof62@gmail.com>
2019-05-20 20:02:57 +0200

    Create update.ps1

---
+++ automatic/lossless-audio-checker/update.ps1
@@ -0,0 +1,28 @@
+﻿import-module au
+ 
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+  $releases = 'http://losslessaudiochecker.com'  
+  $regex    = 'GUI v(?<Version>[\d\.]+) \(Regular users\)'
+
+  $download_page = (Invoke-WebRequest -Uri $releases).RawContent -match $regex
+  
+  return @{ Version = $matches.Version }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

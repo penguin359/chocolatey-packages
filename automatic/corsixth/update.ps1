3db4b246b0dbd402ea0082830bd0faa4f022ff34
chtof <chtof62@gmail.com>
2019-05-21 12:56:01 +0200

    Create update.ps1

---
+++ automatic/corsixth/update.ps1
@@ -0,0 +1,24 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $github_repository = "CorsixTH/CorsixTH"
+    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $regex = '<title>Release CorsixTH (?<Version>[\d\.]+) · '
+
+    $download_page = (Invoke-WebRequest -Uri $releases).RawContent -match $regex
+    $version = $matches.Version
+
+    return @{
+        Version = $version
+    }
+}
+
+function global:au_SearchReplace {
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+        }
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

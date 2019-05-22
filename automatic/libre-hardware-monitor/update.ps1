c3ebd9f0326f9e866e48e23fae7c4782749fc9fa
chtof <chtof62@gmail.com>
2019-05-22 22:01:16 +0200

    Create update.ps1

---
+++ automatic/libre-hardware-monitor/update.ps1
@@ -0,0 +1,35 @@
+﻿import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://ci.appveyor.com/project/LibreHardwareMonitor/LibreHardwareMonitor/history'
+    $regex    = 'title="(?<Version>[\d\.]+)"'
+
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
+    
+    return @{
+        Version = $matches.Version
+        URL32 = 'https://ci.appveyor.com/api/buildjobs/s56wn5cxknhip56f/artifacts/bin/Release.zip'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

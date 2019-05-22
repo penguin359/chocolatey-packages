1d48ed116588007e52b063a361c56cb0c23b4954
chtof <chtof62@gmail.com>
2019-05-22 22:07:07 +0200

    Update chocolateyuninstall.ps1

--- automatic/agwchateaux.portable/tools/chocolateyuninstall.ps1
+++ automatic/agwchateaux.portable/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
-﻿# Delete start menu shortcut
+﻿# Remove start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "AGW Châteaux.lnk"
 if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

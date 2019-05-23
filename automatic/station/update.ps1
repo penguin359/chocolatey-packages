c00c76556843b29fe63e7587b857ac7ef5048215
chtof <clefebvre.62@free.fr>
2019-05-23 22:56:34 +0200

    Update update.ps1

--- automatic/station/update.ps1
+++ automatic/station/update.ps1
@@ -6,14 +6,14 @@ function global:au_GetLatest {
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	$url = $download_page.links | ? href -match $regex
-	
+
     return @{ Version = $matches.version ; URL32 = 'https://github.com' + $url.href }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }

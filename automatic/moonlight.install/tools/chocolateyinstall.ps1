d3bea7780398f598600a8650b2fa7574c30084e8
chtof <chtof62@gmail.com>
2019-05-24 15:31:15 +0200

    moonlight initial commit

---
+++ automatic/moonlight.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\MoonlightSetup-x86-1.0.0.exe"
+  file64        = "$toolsDir\MoonlightSetup-x64-1.0.0.exe"
+  silentArgs	= "/S"
+}
+
+Install-ChocolateyInstallPackage @packageArgs
---
+++ automatic/moonlight.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\MoonlightPortable-x86-1.0.0.zip"
+  file64        = "$toolsDir\MoonlightPortable-x64-1.0.0.zip"
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "Moonlight.lnk"
+$targetPath = Join-Path $toolsDir "Moonlight.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

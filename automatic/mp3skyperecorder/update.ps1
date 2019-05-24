571cf8b88534fb89f32718e07e2bea19682cf3a6
chtof <chtof62@gmail.com>
2019-05-24 12:32:10 +0200

    Update update.ps1

--- automatic/mp3skyperecorder/update.ps1
+++ automatic/mp3skyperecorder/update.ps1
@@ -2,12 +2,16 @@ import-module au
 
 function global:au_GetLatest {
     $releases = 'https://voipcallrecording.com'
-    $regex   = 'version=(?<Version>[\d\.]+)$'
+    $regex    = 'version=(?<Version>[\d\.]+)$'    
     
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url = $download_page.links | ? href -match $regex | select -First 1
+    $url           = $download_page.links | ? href -match $regex | select -First 1
+    $versionUrl    = $matches.Version -Replace '\.', ''
 
-    return @{ Version = $matches.Version ; URL32 = $url.href }
+    return @{
+        Version = $matches.Version
+        URL32   = $releases + '/old/MP3SkypeRecorderSetup' + $versionUrl + '.msi'
+    }
 }
 
 function global:au_SearchReplace {

549227824ff875dcaeccdd3f6cdaf51db94821eb
chtof <chtof62@gmail.com>
2019-05-24 12:35:19 +0200

    Update chocolateyinstall.ps1

--- automatic/lossless-audio-checker/tools/chocolateyinstall.ps1
+++ automatic/lossless-audio-checker/tools/chocolateyinstall.ps1
@@ -2,13 +2,19 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
-  file          = "$toolsDir\Lossless-Audio-Checker-Windows-32bit.zip"
-  file64        = "$toolsDir\Lossless-Audio-Checker-Windows-64bit.zip"  
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = "$toolsDir"
+
+  url            = 'http://losslessaudiochecker.com/dl/Lossless-Audio-Checker-Windows-32bit.zip'
+  checksum       = '8105B6A865C38CC09F24C9852DBC19357893E3E099E86B2E2E2C9D2F3855B32A'
+  checksumType   = 'sha256'
+
+  url64          = 'http://losslessaudiochecker.com/dl/Lossless-Audio-Checker-Windows-64bit.zip'
+  checksum64     = 'A5370AA6D42086666081FC0626B5C7877EC8FB0BAB21017BF36F6FF1E94B63F7'
+  checksumType64 = 'sha256'
 }
 
-Get-ChocolateyUnzip @packageArgs
+Install-ChocolateyZipPackage @packageArgs
 
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)

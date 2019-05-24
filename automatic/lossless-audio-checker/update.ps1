5ef889bd5683e88e566a4ae256644b6731b8691e
chtof <chtof62@gmail.com>
2019-05-24 12:50:01 +0200

    Update update.ps1

--- automatic/lossless-audio-checker/update.ps1
+++ automatic/lossless-audio-checker/update.ps1
@@ -1,28 +1,27 @@
 ﻿import-module au
  
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
-
 function global:au_GetLatest {
   $releases = 'http://losslessaudiochecker.com'  
   $regex    = 'GUI v(?<Version>[\d\.]+) \(Regular users\)'
 
   $download_page = (Invoke-WebRequest -Uri $releases).RawContent -match $regex
   
-  return @{ Version = $matches.Version }
+  return @{
+    Version = $matches.Version
+    URL32   = 'http://losslessaudiochecker.com/dl/Lossless-Audio-Checker-Windows-32bit.zip'
+    URL64   = 'http://losslessaudiochecker.com/dl/Lossless-Audio-Checker-Windows-64bit.zip'
+  }
 }
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
-        }
+        "tools\chocolateyinstall.ps1" = @{
+            "(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }        
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update

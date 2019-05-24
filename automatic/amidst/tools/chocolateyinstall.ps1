a2de96004dccdcd9492a2a6366ac0f8d9b4f328c
chtof <chtof62@gmail.com>
2019-05-25 00:46:50 +0200

    amidst initial commit

---
+++ automatic/amidst/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "Amidst.lnk"
+$targetPath = Join-Path $toolsDir "amidst-v4-2.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

7234cd2f903681032619a0aa9d0ce6633264632c
chtof <chtof62@gmail.com>
2019-05-24 21:48:05 +0200

    moonlight-qt.install initial commit

---
+++ automatic/moonlight-qt.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  file          = "$toolsDir\MoonlightSetup-x86-1.0.0.exe"
+  file64        = "$toolsDir\MoonlightSetup-x64-1.0.0.exe"
+  silentArgs	= "/S"
+}
+
+Install-ChocolateyInstallPackage @packageArgs

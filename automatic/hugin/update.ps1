09a4fb446d05c728da84f939f09a9da6dfe9c994
chtof <chtof62@gmail.com>
2019-05-24 13:00:49 +0200

    Update update.ps1

--- automatic/hugin/update.ps1
+++ automatic/hugin/update.ps1
@@ -1,10 +1,12 @@
 ﻿import-module au
-$releases = 'http://hugin.sourceforge.net/download'
-$regex = "https://sourceforge.net/projects/hugin/files/hugin/hugin-[\d\.]+/Hugin-(?<Version>[\d\.]+)-win64.msi/download"
 
 function global:au_GetLatest {
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	 
-	 $url = $download_page.links | ? href -match $regex	 
+    $releases = 'http://hugin.sourceforge.net/download'
+    $regex    = 'https://sourceforge.net/projects/hugin/files/hugin/hugin-[\d\.]+/Hugin-(?<Version>[\d\.]+)-win64.msi/download'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url = $download_page.links | ? href -match $regex
+
      return @{ Version = $matches.Version ; URL32 = $url.href }
 }
 

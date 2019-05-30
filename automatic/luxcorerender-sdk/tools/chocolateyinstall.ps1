46a0fa5392bdac0c9d32594b2e196ab35dd12353
chtof <chtof62@gmail.com>
2019-05-30 20:44:42 +0200

    Update chocolateyinstall.ps1

--- automatic/luxcorerender-sdk/tools/chocolateyinstall.ps1
+++ automatic/luxcorerender-sdk/tools/chocolateyinstall.ps1
@@ -9,8 +9,14 @@ $packageArgs = @{
 
 Get-ChocolateyUnzip @packageArgs
 
+foreach ($file in 'embree3.dll', 'OpenImageIO.dll', 'tbb.dll'){
+    if (-Not (Test-Path ${env:SystemRoot}\System32\$file -PathType Leaf)){
+    Copy-Item $toolsDir\luxcorerender-v2.1-win64-sdk\lib\$file ${env:SystemRoot}\System32
+    }
+}
+
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Lux Core Render.lnk"
-$targetPath = Join-Path $toolsDir "luxcorerender-v2.1-win64-opencl-sdk\bin\luxcoreui.exe"
+$targetPath = Join-Path $toolsDir "luxcorerender-v2.1-win64-sdk\bin\luxcoreui.exe"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

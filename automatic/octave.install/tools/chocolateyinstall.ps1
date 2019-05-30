56a2c752626b31ed749c00b3a09d0e02dc44c1a9
chtof <chtof62@gmail.com>
2019-05-30 15:23:20 +0200

    Update chocolateyinstall.ps1

--- automatic/octave.install/tools/chocolateyinstall.ps1
+++ automatic/octave.install/tools/chocolateyinstall.ps1
@@ -14,7 +14,8 @@ $packageArgs = @{
   checksumType64= 'sha256'
   
   # Note: silentArgs doesn't work as expected for octave.install
-  silentArgs	= "/S"
+  # An autohotkey is used as workaround.
+  # silentArgs	= "/S"
 }
 
 # Don't create shims for any executables
@@ -23,4 +24,5 @@ foreach ($file in $files) {
     New-Item "$file.ignore" -type file -force | Out-Null
 }
 
+Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
 Install-ChocolateyPackage @packageArgs

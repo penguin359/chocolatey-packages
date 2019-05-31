b7e922aed5796b99fce23c5aec7d2990921ac1db
chtof <chtof62@gmail.com>
2019-05-31 18:57:47 +0200

    Create update.ps1

---
+++ automatic/octave/update.ps1
@@ -0,0 +1,22 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://ftpmirror.gnu.org/octave/windows/'
+    $regex32  = 'octave-(?<Version>[\d\.]+)-w32-installer.exe$'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url32         = $download_page.links | ? href -match $regex32 | select -Last 1
+    $version       = $matches.Version
+
+    return @{ Version = $version }
+}
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

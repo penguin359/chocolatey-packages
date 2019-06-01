899dd8cc08b85b7486c07346e6a07d339a5171b2
chtof <chtof62@gmail.com>
2019-06-01 14:51:15 +0200

    infinitex/infinitex.install/infinitex.portable initial commit

---
+++ automatic/infinitex.install/tools/chocolateyuninstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'InfiniTex *'
+  fileType      = 'exe'
+  silentArgs    = "/allusers /S"
+}
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)" -Replace " /allusers", ""
+    Uninstall-ChocolateyPackage @packageArgs
+    # ["C:\Program Files\InfiniTex\Uninstall InfiniTex.exe" /allusers" /S ]. This may take a while, depending on the statements.
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
---
+++ automatic/infinitex/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/infinitex/infinitex.png" width="48" height="48"/> [InfiniTex](https://chocolatey.org/packages/infinitex)
+
+The one part of the app is a LaTeX editor with search functionality and simplified bibliography and packages handling. The other part is a WYSIWYG editor based on quilljs. The simple part also supports encrypted documents.
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/infinitex/screenshot.gif)

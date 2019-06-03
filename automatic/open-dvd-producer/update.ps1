487baf43d70441551bd6be5923cb6371fd86e828
chtof <clefebvre.62@free.fr>
2019-06-03 20:05:49 +0200

    Create update.ps1

---
+++ automatic/open-dvd-producer/update.ps1
@@ -0,0 +1,35 @@
+﻿import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $github_repository = "jonata/opendvdproducer"
+    $releases          = "https://github.com/" + $github_repository + "/releases/latest"
+    https://github.com/jonata/opendvdproducer/releases/download/16.11/opendvdproducer_16.11.exe.7z
+    $regex             = 'opendvdproducer_(?<Version>[\d\.]+).exe.7z$'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url = $download_page.links | ? href -match $regex
+
+    return @{ Version = $matches.Version ; URL32 = "https://github.com" + $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

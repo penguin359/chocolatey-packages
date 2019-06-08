a110770ef568f0349a93348f17d03c613d4810f0
chtof <chtof62@gmail.com>
2019-06-08 18:38:34 +0200

    extraterm/extraterm.install/extraterm.portable initial commit

---
+++ automatic/extraterm.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\extraterm-0.42.2-win32-x64.zip"  
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "Extraterm.lnk"
+$targetPath = Join-Path $toolsDir "extraterm-0.42.2-win32-x64\extraterm.exe"
+Install-ChocolateyShortcut -RunAsAdmin -shortcutFilePath $shortcutFilePath -targetPath $targetPath

ca70263bb412781152babb6d44d1703754491e6a
chtof <chtof62@gmail.com>
2019-06-08 17:49:43 +0200

    spacedesk-client initial commit

---
+++ automatic/spacedesk-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  filetype      = 'MSI'
+
+  url           = 'https://spacedesk.datronic.de/download/spacedeskWindowsVIEWER_v0915_BETA.msi'
+  checksum      = '952607dd90da95d21663034074316520bde880402a845ae183f09e9045af1c88'
+  checksumType  = 'sha256'
+
+  silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+}
+
+Install-ChocolateyPackage @packageArgs

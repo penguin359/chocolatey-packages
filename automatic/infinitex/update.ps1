03065b3d125f4e319f93e69eff9ea1c5b3125209
chtof <chtof62@gmail.com>
2019-06-08 13:01:58 +0200

    Create update.ps1

---
+++ automatic/infinitex/update.ps1
@@ -0,0 +1,22 @@
+﻿import-module au
+
+function global:au_GetLatest {	
+    $github_repository = "fetacore/Infinitex"
+    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $regex    = 'InfiniTex-Setup-(?<Version>[\d\.]+).exe$'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
+	$url = $download_page.links | ? href -match $regex
+
+    return @{ Version = $matches.Version }
+}
+
+function global:au_SearchReplace {
+    @{        
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update

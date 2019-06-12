f48efb3b24d8d1d6c68f4f1b18e6e9a9a3cc861d
chtof <chtof62@gmail.com>
2019-06-12 19:20:13 +0200

    Update update.ps1

--- automatic/dontsleep/update.ps1
+++ automatic/dontsleep/update.ps1
@@ -1,18 +1,20 @@
 ﻿import-module au
-$releases = 'https://www.softwareok.com/?seite=Microsoft/DontSleep'
-$regex = "<title>Don't Sleep (?<Version>[\d\.]+) Prevent Shutdown, Stand By, Turn Off, Restart</title>"
 
 function global:au_GetLatest {
-     (Invoke-WebRequest -Uri $releases) -match $regex | out-null
-     return @{ Version = $matches.Version }
+	$releases = 'https://www.softwareok.com/?seite=Microsoft/DontSleep'
+	$regex    = "<title>Don't Sleep (?<Version>[\d\.]+) Prevent Shutdown, Stand By, Turn Off, Restart</title>"
+
+    (Invoke-WebRequest -Uri $releases) -match $regex | out-null
+
+    return @{ Version = $matches.Version }
 }
 
 function global:au_SearchReplace {
     @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
         }
     }
 }
 
-update
+update -ChecksumFor none -NoCheckUrl

edfe87fea87fd3f1e05e42d4f7c2138017515a09
chtof <chtof62@gmail.com>
2019-06-12 19:19:45 +0200

    Update update.ps1

--- automatic/dontsleep.portable/update.ps1
+++ automatic/dontsleep.portable/update.ps1
@@ -7,9 +7,9 @@ function global:au_GetLatest {
     $regex = "<title>Don't Sleep (?<Version>[\d\.]+) Prevent Shutdown, Stand By, Turn Off, Restart</title>"
     $urlDownload = 'https://www.softwareok.com/Download/'
 
-     (Invoke-WebRequest -Uri $releases) -match $regex | out-null     
+     (Invoke-WebRequest -Uri $releases) -match $regex | out-null
      return @{
-        Version = $matches.Version ;
+        Version = $matches.Version
         URL32 = 'https://www.softwareok.com/Download/DontSleep_Portable.zip'
         URL64 = 'https://www.softwareok.com/Download/DontSleep_x64_Portable.zip'
     }
@@ -19,19 +19,17 @@ function global:au_SearchReplace {
     @{
         "legal\VERIFICATION.txt"  = @{            
             "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"            
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
-        "tools\chocolateyinstall.ps1" = @{        
+        "tools\chocolateyinstall.ps1" = @{
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
           "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none

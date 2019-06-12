7bee77a5bb6b9f0b6489cdc72d4cd0fa27742513
chtof <chtof62@gmail.com>
2019-06-12 19:19:40 +0200

    Update update.ps1

--- automatic/dontsleep.install/update.ps1
+++ automatic/dontsleep.install/update.ps1
@@ -4,12 +4,11 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
     $releases = 'https://www.softwareok.com/?seite=Microsoft/DontSleep'
-    $regex = "<title>Don't Sleep (?<Version>[\d\.]+) Prevent Shutdown, Stand By, Turn Off, Restart</title>"
-    $urlDownload = 'https://www.softwareok.com/Download/'
-    
-    (Invoke-WebRequest -Uri $releases) -match $regex | out-null     
+    $regex    = "<title>Don't Sleep (?<Version>[\d\.]+) Prevent Shutdown, Stand By, Turn Off, Restart</title>"
+
+    (Invoke-WebRequest -Uri $releases) -match $regex | out-null
      return @{
-        Version = $matches.Version ;
+        Version = $matches.Version
         URL32 = 'https://www.softwareok.com/Download/DontSleep.zip'
         URL64 = 'https://www.softwareok.com/Download/DontSleep_x64.zip'
     }
@@ -17,21 +16,19 @@ function global:au_GetLatest {
 
 function global:au_SearchReplace {
     @{
-        "legal\VERIFICATION.txt"  = @{            
+        "legal\VERIFICATION.txt"  = @{
             "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"            
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
-        "tools\chocolateyinstall.ps1" = @{        
+        "tools\chocolateyinstall.ps1" = @{
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
           "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none

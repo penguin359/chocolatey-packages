ba6fe3f7af670af584f7e4f2e146be7fabf7fc4a
chtof <chtof62@gmail.com>
2019-06-15 20:45:24 +0200

    Update chocolateyinstall.ps1

--- automatic/log4om.install/tools/chocolateyinstall.ps1
+++ automatic/log4om.install/tools/chocolateyinstall.ps1
@@ -4,13 +4,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\Log4OM_1_37_0.zip"  
+  fileFullPath  = "$toolsDir\Log4OM_1_38_0.zip"  
 }
 Get-ChocolateyUnzip @packageArgs
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
-  file          = "$toolsDir\Log4OM_1_37_0.zip"
+  file          = "$toolsDir\Log4OM_1_38_0.exe"
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components=""'
 }
 Install-ChocolateyInstallPackage @packageArgs

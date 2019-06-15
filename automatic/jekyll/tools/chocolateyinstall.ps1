f122981ff310818e267a57a5aa16a51c56ad44d4
chtof <chtof62@gmail.com>
2019-06-15 20:46:11 +0200

    Update chocolateyinstall.ps1

--- automatic/jekyll/tools/chocolateyinstall.ps1
+++ automatic/jekyll/tools/chocolateyinstall.ps1
@@ -1,14 +1,15 @@
-refreshenv
+﻿refreshenv
 ridk install 3
 gem install bundler jekyll -v 3.8.5
 
-Write-Warning ""
-Write-Warning "###    Jekyll - QUICKSTART    ###"
-Write-Warning ""
-Write-Warning "> jekyll new c:\srv\www\mywebsite"
-Write-Warning "> cd c:\srv\www\mywebsite"
-Write-Warning "> bundle exec jekyll serve"
-Write-Warning ""
-Write-Warning "- Browse to http://localhost:4000"
-Write-Warning "#################################"
-Write-Warning ""
+Write-Warning @'
+###    Jekyll - QUICKSTART    ###
+
+> jekyll new c:\srv\www\mywebsite
+> cd c:\srv\www\mywebsite
+> bundle exec jekyll serve
+
+- Browse to http://localhost:4000
+#################################
+
+'@

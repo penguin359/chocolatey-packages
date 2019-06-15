f338b0d00d838ccebaeff9c656fe52c8dff59db2
chtof <chtof62@gmail.com>
2019-06-15 10:58:55 +0200

    Create update.ps1

---
+++ automatic/speedcrunch.install/update.ps1
@@ -0,0 +1,31 @@
+import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'http://speedcrunch.org/download.html'    
+    $regex    = 'SpeedCrunch-(?<Version>[\d\.]+)-win32.exe$'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url = $download_page.links | ? href -match $regex
+
+    return @{ Version = $matches.Version ; URL32 = $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

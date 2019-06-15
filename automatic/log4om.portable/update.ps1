7cdb36d37e4ce6a0f2a67425f66e5a80e7cdab77
chtof <clefebvre.62@free.fr>
2019-06-15 20:52:35 +0200

    Update update.ps1

--- automatic/log4om.portable/update.ps1
+++ automatic/log4om.portable/update.ps1
@@ -4,7 +4,7 @@ function global:au_GetLatest {
     $releases = 'http://www.log4om.com/dl/'
     $regex    = 'Log4OM_(?<Version>[\d_]+)_Portable.zip'
 
-    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | select -First 1
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | select -First 1 | Out-Null
     $version = $matches.Version -Replace "_", "."
 	
 	return @{ Version = $version ; URL32 = 'http://www.log4om.com/log4om/release/Log4OM_' + $matches.Version + '_Portable.zip' }

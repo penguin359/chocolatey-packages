48a97b1cfd8ee3102862864b37013b4f81c01485
chtof <chtof62@gmail.com>
2019-06-15 12:22:59 +0200

    Update update.ps1

--- automatic/radiant/update.ps1
+++ automatic/radiant/update.ps1
@@ -1,33 +1,21 @@
 ﻿import-module au
  
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
-
 function global:au_GetLatest {
   $releases = 'https://www.radiantviewer.com/download/?src=mbst&f=setup'
   $regex    = '/files/RadiAnt-(?<Version>[\d\.]+)-Setup.exe$'
 
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $url = $download_page.links | ? href -match $regex
+  $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
 
   return @{ Version = $matches.Version ; URL32 = 'https://www.radiantviewer.com' + $url.href }
 }
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{
-            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
-        }
-
-        "tools\chocolateyinstall.ps1" = @{
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        "tools\chocolateyInstall.ps1" = @{
+          "(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+          "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update

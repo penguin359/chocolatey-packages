ded252ca30e3ae51678c300eaf1482bb5eb9bfa6
chtof <chtof62@gmail.com>
2019-06-15 14:45:08 +0200

    luminance 2.6.0

--- automatic/luminance.install/tools/chocolateyinstall.ps1
+++ automatic/luminance.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName      
-  file64        = "$toolsDir\Luminance-HDR-x64-SETUP-v2.5.1.exe"  
+  file64        = "$toolsDir\Luminance-HDR_v.2.6.0_Windows_64.exe"  
   silentArgs	= "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 

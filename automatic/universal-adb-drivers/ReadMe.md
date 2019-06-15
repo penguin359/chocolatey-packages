0711dc0344dee85d1b6872ea1a79084940c4e498
chtof <chtof62@gmail.com>
2019-06-15 11:39:30 +0200

    universal-adb-drivers initial commit

---
+++ automatic/universal-adb-drivers/ReadMe.md
@@ -0,0 +1,3 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/universal-adb-drivers/universal-adb-drivers.png" width="48" height="48"/> [Universal ADB Drivers](https://chocolatey.org/packages/universal-adb-drivers)
+
+One size fits all Windows Drivers for Android Debug Bridge.
---
+++ automatic/universal-adb-drivers/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  file          = "$toolsDir\UniversalAdbDriverSetup.msi"
+  filetype      = "MSI"  
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+}
+
+Install-ChocolateyInstallPackage @packageArgs

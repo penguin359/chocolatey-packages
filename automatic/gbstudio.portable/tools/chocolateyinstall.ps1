c1b90231ced4f7900d1b0e038f41e2b034e9e889
chtof <chtof62@gmail.com>
2019-06-15 14:32:40 +0200

    gbstudio/gbstudio.install/gbstudio.portable initial commit

--- automatic/gbstudio.portable/tools/chocolateyinstall.ps1
+++ automatic/gbstudio.portable/tools/chocolateyinstall.ps1
@@ -3,12 +3,17 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
-  file          = "$toolsDir\GB.Studio-win32-ia32-1.1.0.zip"
-  file64        = "$toolsDir\GB.Studio-win32-x64-1.1.0.zip"  
+
+  url           = "https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-win32-ia32-1.1.0.zip"
+  checksum      = 'D4B32C095BB13B0F5AEA23AF54A626C6BA02EC0FE59FC3B273B491B3CE97E8DB'
+  checksumType  = 'sha256'
+
+  url64         = "https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-win32-x64-1.1.0.zip"
+  checksum64    = 'F471AFA9254ECB628CFC9851D7946FC70263A92CC6880CE41D252688089DEFC4'
+  checksumType64= 'sha256'
 }
 
-Get-ChocolateyUnzip @packageArgs
+Install-ChocolateyZipPackage @packageArgs
 
 # Prevent chocolatey from creating shims for supplementary executables
 New-Item -path $toolsDir -name "Update.exe.ignore" -type File -force | Out-Null

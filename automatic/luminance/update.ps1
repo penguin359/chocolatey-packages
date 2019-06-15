65416e1c50b41c498ee3ee36c4c070478d2b0a00
chtof <chtof62@gmail.com>
2019-06-15 15:52:41 +0200

    Update update.ps1

--- automatic/luminance/update.ps1
+++ automatic/luminance/update.ps1
@@ -1,11 +1,11 @@
 ﻿import-module au
  
 function global:au_GetLatest {
-  $releases = 'https://sourceforge.net/projects/qtpfsgui/files/luminance'  
-  # $regex    = 'Luminance-HDR-x64-SETUP-v(?<Version>[\d\.]+).exe'
-  $regex    = 'Luminance-HDR_v.(?<Version>[\d\.]+)_Windows_64.exe'
+ $releases = 'https://sourceforge.net/projects/qtpfsgui'
+  # $regex    = 'Luminance-HDR-x64-SETUP-v(?<Version>[\d\.]+).exe'  
+  $regex    = 'Luminance-HDR_v\.(?<Version>[\d\.]+)_Windows_64.exe'
 
-  (Invoke-WebRequest -Uri $releases).RawContent -match $regex
+  (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null  
 
   return @{ Version = $matches.Version }
 }

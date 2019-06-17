85a099050b85057a7e0903d2a936972f566101db
chtof <clefebvre.62@free.fr>
2019-06-17 19:43:08 +0200

    Create update.ps1

---
+++ automatic/gbstudio/update.ps1
@@ -0,0 +1,24 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $github_repository = "chrismaltby/gb-studio"
+    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $regex64  = 'GB.Studio-win32-x64-squirrel-(?<Version>[\d\.]+).zip$'
+
+    $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links
+    $download_page | ? href -match $regex64 | Out-Null
+
+    return @{
+        Version = $matches.Version
+    }
+}
+
+function global:au_SearchReplace {
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+        }
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

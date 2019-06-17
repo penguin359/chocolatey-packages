64498d55f54c985473ea88d6fb0f45c2e0e90e73
chtof <clefebvre.62@free.fr>
2019-06-17 19:42:28 +0200

    Create update.ps1

---
+++ automatic/gbstudio.install/update.ps1
@@ -0,0 +1,31 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $github_repository = "chrismaltby/gb-studio"
+    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $regex32  = 'GB.Studio-win32-ia32-squirrel-([\d\.]+).zip$'
+    $regex64  = 'GB.Studio-win32-x64-squirrel-(?<Version>[\d\.]+).zip$'
+
+    $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links
+    $url32         = $download_page | ? href -match $regex32
+    $url64         = $download_page | ? href -match $regex64
+
+    return @{
+        Version = $matches.Version
+        URL32   = 'https://github.com/' + $url32.href
+        URL64   = 'https://github.com/' + $url64.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update

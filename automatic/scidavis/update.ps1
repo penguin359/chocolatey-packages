e47527e7fd0f4a2698cf143bb31acc7791fe8662
chtof <clefebvre.62@free.fr>
2019-06-18 19:39:04 +0200

    Create update.ps1

---
+++ automatic/scidavis/update.ps1
@@ -0,0 +1,36 @@
+import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases          = "https://sourceforge.net/projects/scidavis"
+    $regex             = 'scidavis.(?<Version>[\d\.]+)-win-dist.msi'
+
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex
+    $version = $matches.Version
+
+    return @{
+      Version = $version
+      URL32   = 'https://netix.dl.sourceforge.net/project/scidavis/SciDAVis/' + $version + '/scidavis.' + $version + '-win-dist.msi'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

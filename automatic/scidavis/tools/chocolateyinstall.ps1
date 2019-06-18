7e362367ed587d2f54f5db832cdbe6ec0aa1ba47
chtof <clefebvre.62@free.fr>
2019-06-18 19:38:58 +0200

    Update chocolateyinstall.ps1

--- automatic/scidavis/tools/chocolateyinstall.ps1
+++ automatic/scidavis/tools/chocolateyinstall.ps1
@@ -6,6 +6,7 @@ $packageArgs = @{
   file          = "$toolsDir\scidavis.1.25-win-dist.msi"
   filetype      = "MSI"
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
 }
 
 Install-ChocolateyInstallPackage @packageArgs

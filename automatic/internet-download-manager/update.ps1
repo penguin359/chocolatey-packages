6ac9ae63d391e8707d4943928b070087ac38cf30
chtof <clefebvre.62@free.fr>
2019-06-19 20:33:42 +0200

    Create update.ps1

---
+++ automatic/internet-download-manager/update.ps1
@@ -0,0 +1,27 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases_version = 'http://www.internetdownloadmanager.com'
+    $regex            = 'Internet Download Manager v(?<Version>[\d\.]+)\.'
+    $releases = 'http://www.internetdownloadmanager.com/download.html'
+    $regex_url        = 'idman(.*?).exe$'
+
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
+    $version    = $matches.Version
+    $versionUrl = $matches.version -Replace "\.", ""
+
+    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex_url | Select -First 1
+
+    return @{ Version = $version ; URL32 = $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

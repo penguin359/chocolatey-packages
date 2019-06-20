3b2b50955361b23dc2c882899d371f425785d415
chtof <clefebvre.62@free.fr>
2019-06-20 19:19:37 +0200

    Update chocolateyinstall.ps1

--- automatic/ocenaudio.portable/tools/chocolateyinstall.ps1
+++ automatic/ocenaudio.portable/tools/chocolateyinstall.ps1
@@ -2,16 +2,16 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"  
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = "$toolsDir"
 
-  url           = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_portable.zip'
-  checksum      = 'b3356cbb44f9d2eb4665c040f4f317ef881123a6cdd85d0c696a5584e582f724'
-  checksumType  = 'sha256'
+  url            = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_portable.zip'
+  checksum       = 'b3356cbb44f9d2eb4665c040f4f317ef881123a6cdd85d0c696a5584e582f724'
+  checksumType   = 'sha256'
 
-  url64bit      = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio64_portable.zip'
-  checksum64    = '132fb9903228d9513eefc1b5a567c6f7403ab090abdfd1e2f36c1d8ba1f6bb25'
-  checksumType64= 'sha256'
+  url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio64_portable.zip'
+  checksum64     = '132fb9903228d9513eefc1b5a567c6f7403ab090abdfd1e2f36c1d8ba1f6bb25'
+  checksumType64 = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs

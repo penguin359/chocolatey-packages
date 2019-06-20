ace756685aa4b45d722ebcc6e3076c854c79886e
chtof <clefebvre.62@free.fr>
2019-06-20 19:20:23 +0200

    Create update.ps1

---
+++ automatic/ocenaudio/update.ps1
@@ -0,0 +1,20 @@
+﻿import-module au
+
+function global:au_GetLatest {  
+    $releases = 'https://www.ocenaudio.com'
+    $regex    = '\<h1\>ocenaudio (?<Version>[\d\.]+)\</h1\>'
+
+    (Invoke-WebRequest -Uri $releases).RawContent  -match $regex | Out-Null
+
+    return @{ Version = $matches.Version }
+}
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update

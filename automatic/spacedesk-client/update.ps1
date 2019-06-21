0a89d86c8261f3adc978f40fe8945c96e18b12ce
chtof <chtof62@gmail.com>
2019-06-21 13:04:43 +0200

    Update update.ps1

--- automatic/spacedesk-client/update.ps1
+++ automatic/spacedesk-client/update.ps1
@@ -2,20 +2,21 @@
 
 function global:au_GetLatest {
     $releases = 'https://spacedesk.net'
-    $regex = 'Current version for Windows:.*v(?<Version>[\d\.]+)\</p\>'
+    $regex    = 'Current version for Windows:.*v(?<Version>[\d\.]+)\</p\>'
     
     $url = Get-RedirectedUrl https://spacedesk.net/downloadclient
     (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
 
     return @{
         Version = $matches.Version
-        URL32 = $url 
+        URL32   = $url 
     }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyinstall.ps1" = @{			
+        "tools\chocolateyinstall.ps1" = @{
+            "(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }

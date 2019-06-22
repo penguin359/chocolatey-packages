71a34088cad0b2e53c829b40b7e2c6aee972d014
chtof <chtof62@gmail.com>
2019-06-22 14:40:01 +0200

    Update chocolateyinstall.ps1

--- automatic/eclipse-java-oxygen/tools/chocolateyinstall.ps1
+++ automatic/eclipse-java-oxygen/tools/chocolateyinstall.ps1
@@ -3,8 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
+  unzipLocation = $toolsDir  
 
   url           = 'https://ftp.halifax.rwth-aachen.de/eclipse//technology/epp/downloads/release/2019-06/R/eclipse-java-2019-06-R-win32-x86_64.zip'
   checksum      = '36650e72f7f9a6d45f4c49f33c5f43ec9d985c79131ef123240f0422e2de6d18'

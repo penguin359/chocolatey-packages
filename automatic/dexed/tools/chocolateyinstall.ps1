615d2218fc233b3dc6134147073afca06889e14b
chtof <chtof62@gmail.com>
2019-06-22 14:39:47 +0200

    dexed initial commit

---
+++ automatic/dexed/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\dexed-0.9.4-win.zip"
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName  
+  fileFullPath  = "$toolsDir\dexed-0.9.4hf1-win.exe"
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+}
+
+Install-ChocolateyInstallPackage @packageArgs

625d929e449328467784625ea09b536a5f4e503d
chtof <chtof62@gmail.com>
2019-06-22 15:17:07 +0200

    wireguard initial commit

---
+++ automatic/wireguard/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+
+  filetype      = "MSI"
+  file          = "$toolsDir\wireguard-x86-0.0.14.msi"
+  file64        = "$toolsDir\wireguard-amd64-0.0.14.msi"
+
+  silentArgs	= "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""  
+}
+
+Install-ChocolateyInstallPackage @packageArgs

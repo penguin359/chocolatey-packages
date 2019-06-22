272b45a2296c32d2c1cafe0c0543b10a900281bb
chtof <chtof62@gmail.com>
2019-06-22 21:17:56 +0200

    pandafreeantivirus initial commit

--- automatic/pandafreeantivirus/tools/chocolateyinstall.ps1
+++ automatic/pandafreeantivirus/tools/chocolateyinstall.ps1
@@ -7,8 +7,6 @@ $packageArgs = @{
   url           = 'http://acs.pandasoftware.com/Panda/FREEAV/190612/PANDAFREEAV.exe'
   checksum      = '00CEF69FF557BFEE8CED3E7567B744F2D444374872B9C10A5B72AFDAFAC8E25E'
   checksumType  = 'sha256'
-
-
 }
 
 Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""

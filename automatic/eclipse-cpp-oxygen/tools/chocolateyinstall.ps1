2332a641db5e6e3f4ed43e5faea622cbf61fde2b
chtof <chtof62@gmail.com>
2019-06-22 13:38:05 +0200

    Update chocolateyinstall.ps1

--- automatic/eclipse-cpp-oxygen/tools/chocolateyinstall.ps1
+++ automatic/eclipse-cpp-oxygen/tools/chocolateyinstall.ps1
@@ -5,12 +5,18 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$toolsDir"
 
-  url           = 'http://mirror.dkm.cz/eclipse/technology/epp/downloads/release/2019-06/R/eclipse-cpp-2019-06-R-win32-x86_64.zip'
+  url           = 'https://mirror.dkm.cz/eclipse/technology/epp/downloads/release/2019-06/R/eclipse-cpp-2019-06-R-win32-x86_64.zip'
   checksum      = 'F5452535CCDC89F33B24B2844A3AA7F31769D031141B1A7AE52F76D3A1DE2A15'
   checksumType  = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
+
+# Don't create shims for other executables
+$files = Get-ChildItem "$toolsDir" -Recurse -Include *.exe -Exclude eclipse.exe,eclipsec.exe
+foreach ($file in $files) {
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
 
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)

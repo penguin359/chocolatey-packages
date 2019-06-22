53c4b6c5d57cc1666223d613ef46d4e6e89ace33
chtof <chtof62@gmail.com>
2019-06-22 18:33:11 +0200

    mfcmapi initial commit

---
+++ automatic/mfcmapi/ReadMe.md
@@ -0,0 +1,3 @@
+﻿# [MFCMapi](https://chocolatey.org/packages/mfcmapi)
+
+MFCMAPI provides access to MAPI stores to facilitate investigation of Exchange and Outlook issues and to provide developers with a sample for MAPI development.
---
+++ automatic/mfcmapi/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageName   = $env:ChocolateyPackageName
+$binRoot       =  Get-ToolsLocation
+$installDir    = Join-Path "$binRoot" "$packageName"
+
+$file          = "$toolsDir\MFCMAPI.exe.19.0.19115.01.zip"
+$file64        = "$toolsDir\MFCMAPI.exe.x64.19.0.19115.01.zip"
+
+# We're going to copy both the 32-bit and 64-bit executables, because
+# the bitness of Outlook won't necessarily match the bitness of the
+# OS.
+Get-ChocolateyUnzip -PackageName $packageName -Destination "$InstallDir\x86" -FileFullPath "$toolsDir\MFCMAPI.exe.19.0.19115.01.zip" 
+Get-ChocolateyUnzip -PackageName $packageName -Destination "$InstallDir\x64" -FileFullPath "$toolsDir\MFCMAPI.exe.x64.19.0.19115.01.zip"

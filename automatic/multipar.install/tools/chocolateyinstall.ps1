d865d70ae72e2456a5cbd7ed5e0fe465356b7230
chtof <clefebvre.62@free.fr>
2019-06-25 21:13:20 +0200

    Update chocolateyinstall.ps1

--- automatic/multipar.install/tools/chocolateyinstall.ps1
+++ automatic/multipar.install/tools/chocolateyinstall.ps1
@@ -4,10 +4,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://multipar.eu/downloads/1.3.0/MultiPar1305_setup.exe'
+  options       = @{Headers=@{"referer"="https://multipar.eu"}}
   checksum      = '98207E8F0E1011EAB4E9E0947F3ED6AA09E28EF92EC62C2786F89D1BE96C606C'
   checksumType  = 'sha256'
 
-  silentArgs	= "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 
 Install-ChocolateyPackage @packageArgs

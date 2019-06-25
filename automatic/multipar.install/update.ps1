1fd584442af630ebeb7e5cab9e76b5bb93c6296d
chtof <clefebvre.62@free.fr>
2019-06-25 21:13:28 +0200

    Update update.ps1

--- automatic/multipar.install/update.ps1
+++ automatic/multipar.install/update.ps1
@@ -1,13 +1,24 @@
 ﻿import-module au
 
+function global:au_BeforeUpdate { 
+    $releases = 'https://multipar.eu'
+    $regex    = 'MultiPar[\d]+_setup.exe$'
+    $url      = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
+
+    Invoke-WebRequest -Uri $url -Headers @{"referer"="https://multipar.eu"} -OutFile "${env:tmp}\multipar.exe" | Out-Null
+    $Latest.Checksum32 = checksum -t sha256 "${env:tmp}\multipar.exe"
+    $Latest.URL32 = $url
+
+    Remove-Item -Path "${env:tmp}\multipar.exe"
+}
+
 function global:au_GetLatest {
     $releases = 'https://multipar.eu'
     $regex    = 'MultiPar(?<Version>[\d]+)_setup.exe$'
-
     $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
     $version = $matches.Version -replace "(\d+)(\d)(\d)(\d)", '$1.$2.$3.$4'
 
-    return @{ Version = $version + "00" ; URL32 = $releases + "/" + $url.href }
+    return @{ Version = $version + "00" }
 }
 
 function global:au_SearchReplace {
@@ -19,4 +30,4 @@ function global:au_SearchReplace {
     }
 }
 
-update
+update -NoCheckUrl -ChecksumFor none

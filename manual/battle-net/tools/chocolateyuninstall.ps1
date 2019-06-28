4bc146729a895e2f9bbc8b0edf54ddf800232c82
chtof <chtof62@gmail.com>
2019-06-28 21:09:07 +0200

    Update chocolateyuninstall.ps1

--- manual/battle-net/tools/chocolateyuninstall.ps1
+++ manual/battle-net/tools/chocolateyuninstall.ps1
@@ -1,27 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'Battle.net'
-  fileType      = 'exe'
-}
-
-$uninstalled = $false
-[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
-
-if ($key.Count -eq 1) {
-  $key | % {
-    #'"C:\ProgramData\Battle.net\Agent\Blizzard Uninstaller.exe" --lang=enUS --uid=battle.net --displayname="Battle.net"'
-    $packageArgs['file'] = "$($_.UninstallString)" -Replace "Blizzard Uninstaller.exe`".*", "Blizzard Uninstaller.exe`""
-    Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyuninstall.ahk`""
-    Uninstall-ChocolateyPackage @packageArgs
-  }
-} elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
-}
+Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyuninstall.ahk`""
+#Start-Process "C:\ProgramData\Battle.net\Agent\Blizzard Uninstaller.exe" -Wait -PassThru -Verb runas -ArgumentList "--lang=enUS --uid=battle.net --displayname=`"Battle.net`""
+& 'C:\ProgramData\Battle.net\Agent\Blizzard Uninstaller.exe' --lang=enUS --uid=battle.net --displayname=`"Battle.Net`"

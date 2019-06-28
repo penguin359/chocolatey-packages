f9cbe6db64f86eb702a4b7e735503eae8c0298f5
chtof <chtof62@gmail.com>
2019-06-28 20:04:22 +0200

    battle.net initial commit

---
+++ manual/battle-net/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/manual/battle-net/battle-net.png" width="48" height="48"/> [Blizzard Battle.net Desktop App](https://chocolatey.org/packages/battle-net)
+
+[[Description]]
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/manual/battle-net/screenshot.png)
---
+++ manual/battle-net/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  url           = 'https://eu.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe'
+}
+
+Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
+Install-ChocolateyPackage @packageArgs

4223e71b79ace510b57cfb95b91bb718ccaf77ea
chtof <chtof62@gmail.com>
2019-06-28 13:57:39 +0200

    Update chocolateyinstall.ps1

--- manual/pandafreeantivirus/tools/chocolateyinstall.ps1
+++ manual/pandafreeantivirus/tools/chocolateyinstall.ps1
@@ -9,5 +9,12 @@ $packageArgs = @{
   checksumType  = 'sha256'
 }
 
+# Panda Dome Free Antivirus supports Windows 10, Windows 8/8.1, Windows 7, Window Vista and Windows XP (SP3 or later)
+# So, Panda Dome Free Antivirus supports only Workstation OS.
+if ( (Get-CimInstance -ClassName Win32_OperatingSystem).ProductType -ne 1 ) {
+    Write-Warning "Skipping installation because Panda Dome Free Antivirus supports only Windows 10, Windows 8/8.1, Windows 7, Window Vista and Windows XP (SP3 or later)."
+    return
+}
+
 Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
 Install-ChocolateyPackage @packageArgs

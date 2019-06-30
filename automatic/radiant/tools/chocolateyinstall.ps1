7aa01d0a8522a1f21bbba8ae406c8c3960999703
chtof <chtof62@gmail.com>
2019-06-30 11:02:27 +0200

    Update chocolateyinstall.ps1

--- automatic/radiant/tools/chocolateyinstall.ps1
+++ automatic/radiant/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   checksum      = '0b5beaea2ed2e2fbc5c6fa4a6fc6327b142b8c6e1110aafb00e4da4b2a3c997a'
   checksumType  = 'sha256'
 
-  silentArgs	= "/S"
+  silentArgs    = "/S"
 }
 
 Install-ChocolateyPackage @packageArgs

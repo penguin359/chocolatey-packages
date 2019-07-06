4f9498e4de182761e3517f034c1cda7ebb89b41d
chtof <chtof62@gmail.com>
2019-07-06 14:04:05 +0200

    carotdav/carotdav.install/carotdav.portable initial commit

---
+++ automatic/carotdav.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\CarotDAV1.15.5.portable.zip"  
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -Path $packageArgs.file
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "CarotDAV.lnk"
+$targetPath = Join-Path $toolsDir "CarotDAV\CarotDAV.exe"
+Install-ChocolateyShortcut -RunAsAdmin -shortcutFilePath $shortcutFilePath -targetPath $targetPath

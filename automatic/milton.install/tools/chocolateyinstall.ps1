963e1a031c13cf85f64bb69d17afa15d77813d67
chtof <chtof62@gmail.com>
2019-07-06 16:07:04 +0200

    Update chocolateyinstall.ps1

--- automatic/milton.install/tools/chocolateyinstall.ps1
+++ automatic/milton.install/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"  
   file64        = "$toolsDir\MiltonSetup_1.6.2_x64.exe"
-  silentArgs	= "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 
 Install-ChocolateyInstallPackage @packageArgs

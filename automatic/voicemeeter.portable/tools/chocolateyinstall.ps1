c145947abba67920ad8858da123f0d48e9282286
Chocolatey <chocolatey@realdimensions.net>
2019-07-09 16:24:06 +0000

    AU: 6 updated - ocenaudio ocenaudio.install ocenaudio.portable voicemeeter voicemeeter.install voicemeeter.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/8d0ac5beafd741f5e101e4afe7e52199e0a586fb

--- automatic/voicemeeter.portable/tools/chocolateyinstall.ps1
+++ automatic/voicemeeter.portable/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\VoicemeeterSetup_v1067.zip"
+  file          = "$toolsDir\VoicemeeterSetup_v1068.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

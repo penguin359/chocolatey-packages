cf66c6bd27aa105c02340d142c619fad8a56cd4e
chtof <chtof62@gmail.com>
2019-07-13 21:31:33 +0200

    altcopy initial commit

---
+++ automatic/altcopy/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+
+  url           = 'https://altcopy.net/setup.exe'
+  checksum      = '9BDEE97828744F724B362907C0B16D170B45F374AE173F5289C2F8307042349C'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/S"
+}
+
+Install-ChocolateyPackage @packageArgs

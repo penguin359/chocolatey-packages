376042f3ef47dfb7766c70b620fbeac1802844c7
chtof <chtof62@gmail.com>
2019-07-14 14:39:04 +0200

    weasel initial commit

---
+++ automatic/weasel/ReadMe.md
@@ -0,0 +1,3 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/weasel/weasel.png" width="48" height="48"/> [Weasel](https://chocolatey.org/packages/weasel)
+
+Open source technology based on Zhongzhou rhyme input method engine/Rime Input Method Engine.
---
+++ automatic/weasel/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  file          = "$toolsDir\weasel-0.14.3.0-installer.exe"
+  silentArgs    = "/S"
+}
+
+Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
+Install-ChocolateyInstallPackage @packageArgs

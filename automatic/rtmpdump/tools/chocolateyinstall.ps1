c6c0e14688041c44cff247f5a4ba0dc3106bed2d
chtof <chtof62@gmail.com>
2019-07-14 18:25:01 +0200

    rtmpdump initial commit

---
+++ automatic/rtmpdump/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\rtmpdump-2.4-2015-12-15.zip"
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -Path $packageArgs.file
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "TITLE.lnk"
+$targetPath = Join-Path $toolsDir "FILE"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
+# Remove start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "TITLE.lnk"
+if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

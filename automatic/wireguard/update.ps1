557d70a7e08b690c143d090fdc2b383feec36dd2
chtof <chtof62@gmail.com>
2019-07-16 21:24:55 +0200

    Create update.ps1

---
+++ automatic/wireguard/update.ps1
@@ -0,0 +1,42 @@
+﻿import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://download.wireguard.com/windows-client/'
+    $regex32  = 'wireguard-x86-([\d\.]+).msi'
+    $regex64  = 'wireguard-amd64-(?<Version>[\d\.]+).msi'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	
+	$url32    = $download_page.links | ? href -match $regex32
+    $url64    = $download_page.links | ? href -match $regex64
+    $version  = $matches.Version
+
+	return @{
+        Version = $version
+        URL32   = $releases + $url32.href
+        URL64   = $releases + $url64.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

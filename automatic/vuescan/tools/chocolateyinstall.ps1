5af02b13ec7536c32ad5ce9975ce01b1703b8d53
chtof <clefebvre.62@free.fr>
2019-07-18 19:35:08 +0200

    Update chocolateyinstall.ps1

--- automatic/vuescan/tools/chocolateyinstall.ps1
+++ automatic/vuescan/tools/chocolateyinstall.ps1
@@ -2,11 +2,15 @@ $ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
+  packageName    = $env:ChocolateyPackageName
 
-  url           = 'https://d2bwyyzfw77fhf.cloudfront.net/vuex6496.exe'
-  checksum      = '92C52C3C27A19E9353EDE74D30B98B91C44DB944913A21612D594F7C5EA2DC4A'
-  checksumType  = 'sha256'
+  url            = 'https://www.hamrick.com/files/vuex3296.exe'
+  checksum       = '4aab8beeca3857462dccb67c095dc0cb6f4e69f0121f2b22ffd9bc27f52307d9'
+  checksumType   = 'sha256'
+
+  url64          = 'https://www.hamrick.com/files/vuex6496.exe'
+  checksum64     = '142bacb60e7047a003b6f62e595bc6b06f7e928d44cad17a156755976444f200'
+  checksumType64 = 'sha256'
 }
 
 # This is needed for unattended installation of Drivers

e3dc78bf5ad797eb2d2fb06daf49942c8b615a48
chtof <chtof62@gmail.com>
2019-07-18 18:38:19 +0200

    Update update.ps1

--- automatic/ncollector-studio-lite/update.ps1
+++ automatic/ncollector-studio-lite/update.ps1
@@ -22,7 +22,7 @@ function global:au_SearchReplace {
         }
 
         "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName)`""
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
         }
     }
 }

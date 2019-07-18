87f79647f6ec1e3841c4e646823d50e42bdacb44
chtof <chtof62@gmail.com>
2019-07-18 19:33:51 +0200

    fluxfonts initial commit

---
+++ automatic/carotdav/update.ps1
@@ -0,0 +1,21 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'http://rei.to/carotdav_en.html'
+    $regex    = 'CarotDAV(?<Version>[\d\.]+).zip$'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $download_page.links | ? href -match $regex | Select -First 1 | Out-Null
+	 
+     return @{ Version = $matches.Version }
+}
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).portable`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+        }
+    }
+}
+
+update
---
+++ automatic/kafka-tool/update.ps1
@@ -0,0 +1,27 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'http://www.kafkatool.com/download.html'
+    $regex    = "Kafka Tool (?<Version>[\d\.]+)<"
+
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
+
+    return @{
+        Version = $matches.Version
+        URL32   = "http://www.kafkatool.com/download2/kafkatool.exe"
+        URL64   = "http://www.kafkatool.com/download2/kafkatool_64bit.exe"
+     }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update

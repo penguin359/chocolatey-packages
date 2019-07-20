bbec8d479eba202dec5cedcdf1a20fd4c2a09622
chtof <chtof62@gmail.com>
2019-07-20 16:46:33 +0200

    Update update.ps1

--- automatic/libre-hardware-monitor/update.ps1
+++ automatic/libre-hardware-monitor/update.ps1
@@ -1,24 +1,29 @@
-﻿import-module au
+import-module au
 
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-    $releases = 'https://ci.appveyor.com/api/projects/LibreHardwareMonitor/LibreHardwareMonitor/history?recordsNumber=1'
-    $regex    = '"version":"(?<Version>[\d\.]+)"'
+    $releasesVersion = 'https://ci.appveyor.com/api/projects/LibreHardwareMonitor/LibreHardwareMonitor/history?recordsNumber=1'
+    $regexVersion    = '"version":"(?<Version>[\d\.]+)"'
+    $releasesUrl     = 'https://ci.appveyor.com/project/LibreHardwareMonitor/librehardwaremonitor/build/artifacts'
+    $regexUrl        = '/artifacts/bin%2FRelease.zip$'
 
-    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
-    
-    return @{
-        Version = $matches.Version
-        URL32 = 'https://ci.appveyor.com/api/buildjobs/s56wn5cxknhip56f/artifacts/bin/Release.zip'
-    }
+    $ie = New-Object -com internetexplorer.application
+    $ie.Visible = $false
+    $ie.Navigate($releasesUrl)
+    while ($ie.Busy -eq $true){Start-Sleep -seconds 4;}
+    $url = ($ie.Document.body.GetElementsByClassName("artifact-type zip") | Select-Object "href").href -Replace "%2F", "/"
+
+    (Invoke-WebRequest -Uri $releasesVersion).RawContent -match $regexVersion | Out-Null
+
+    return @{ Version = $matches.Version ; URL32 = $url }
 }
 
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"            
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"

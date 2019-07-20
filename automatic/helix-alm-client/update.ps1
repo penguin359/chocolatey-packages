31e763f1a478bb33859edc66bb5edd99e578dbce
chtof <chtof62@gmail.com>
2019-07-20 22:03:08 +0200

    Create update.ps1

---
+++ automatic/helix-alm-client/update.ps1
@@ -0,0 +1,25 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://www.perforce.com/products/helix-alm/release-notes'
+    $regex    = '>Helix ALM (?<Version>[\d\.]+)<'
+
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex
+    $version = $matches.Version
+
+    return @{
+        Version = $version
+        URL32   = 'https://cdist2.perforce.com/alm/helixalm/r' + $version + '/ttwinclientinstall.exe'        
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

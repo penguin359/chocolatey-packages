602c44b2a8c97db1d8bba311b7a7c153ff3b5583
chtof <chtof62@gmail.com>
2019-07-20 16:48:02 +0200

    Update chocolateyinstall.ps1

--- automatic/sd/tools/chocolateyinstall.ps1
+++ automatic/sd/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\sdsetup.exe"  
-  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  packageName = $env:ChocolateyPackageName
+  file        = "$toolsDir\sdsetup.exe"  
+  silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
 Install-ChocolateyInstallPackage @packageArgs

90e0c6ea28d3b8d8ba057a4a9c4a821340aa8f90
chtof <chtof62@gmail.com>
2019-07-20 12:35:09 +0200

    Update chocolateyinstall.ps1

--- automatic/gravitdesigner.install/tools/chocolateyinstall.ps1
+++ automatic/gravitdesigner.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://designer.gravit.io/_downloads/windows/GravitDesignerInstaller.exe'
-  checksum      = 'eece02948a5f39f907f92e47970b550115c44ce7f56853ed799c38f95b0f2fff'
+  checksum      = 'c1a8c02c073d298ae1dbba8828cbad70960fccc07b2a6c160504f38ea2a4b22d'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"

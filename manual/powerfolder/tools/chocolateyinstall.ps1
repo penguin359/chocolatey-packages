d71404f63e1619f6d9326353001d8b21bf44cc0f
chtof <chtof62@gmail.com>
2019-07-20 19:24:04 +0200

    Update chocolateyinstall.ps1

--- manual/powerfolder/tools/chocolateyinstall.ps1
+++ manual/powerfolder/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://my.powerfolder.com/dl/firSoD3LMSiXaonvfcs2TQf/PowerFolder_Latest_Installer.exe'
-  checksum      = '51BFC8CA4166D3AE3381D3A390E98274B54572EB45F97425D617FC35D5BFDBEB'
-  checksumType  = 'sha256'  
+  url          = 'https://my.powerfolder.com/dl/firSoD3LMSiXaonvfcs2TQf/PowerFolder_Latest_Installer.exe'
+  checksum     = '51BFC8CA4166D3AE3381D3A390E98274B54572EB45F97425D617FC35D5BFDBEB'
+  checksumType = 'sha256'  
   
-  silentArgs    = "/S"
+  silentArgs   = "/S"
 }
 
 Install-ChocolateyPackage @packageArgs

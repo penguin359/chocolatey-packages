20a02fd9f561c601ecf4401f5111bcb8065f1083
chtof <chtof62@gmail.com>
2019-07-20 17:06:46 +0200

    Update chocolateyinstall.ps1

--- automatic/gsubs/tools/chocolateyinstall.ps1
+++ automatic/gsubs/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\gSubs.Setup.1.0.2.exe"
-  silentArgs    = "/S"
+  packageName = $env:ChocolateyPackageName
+  file        = "$toolsDir\gSubs.Setup.1.0.2.exe"
+  silentArgs  = "/S"
 }
 
 Install-ChocolateyInstallPackage @packageArgs

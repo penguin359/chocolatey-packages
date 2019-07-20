4cdee3048f463f67ec8689998d5104dd744225b9
chtof <chtof62@gmail.com>
2019-07-20 19:23:51 +0200

    Update chocolateyinstall.ps1

--- manual/portqry/tools/chocolateyinstall.ps1
+++ manual/portqry/tools/chocolateyinstall.ps1
@@ -5,18 +5,19 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   unzipLocation = $toolsDir
 
-  url           = 'https://download.microsoft.com/download/0/d/9/0d9d81cf-4ef2-4aa5-8cea-95a935ee09c9/PortQryV2.exe'
-  checksum      = 'CD9C75B72EE50D2F59D812533CB38B35F67EB31E92F92319842CDC5EFEECFCF4'
-  checksumType  = 'sha256'  
+  url          = 'https://download.microsoft.com/download/0/d/9/0d9d81cf-4ef2-4aa5-8cea-95a935ee09c9/PortQryV2.exe'
+  checksum     = 'CD9C75B72EE50D2F59D812533CB38B35F67EB31E92F92319842CDC5EFEECFCF4'
+  checksumType = 'sha256'  
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'exe'
-  file          = "$toolsDir\PORTQR~1.EXE"
-  silentArgs    = "/auto"
+  packageName = $packageName
+  fileType    = 'exe'
+  file        = "$toolsDir\PORTQR~1.EXE"
+  silentArgs  = "$toolsDir /auto"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
+Remove-Item -Path $packageArgs.file

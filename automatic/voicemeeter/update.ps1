bb489df113cddef8d0c499a65b673dce225230e3
chtof <chtof62@gmail.com>
2019-07-20 18:28:49 +0200

    Update update.ps1

--- automatic/voicemeeter/update.ps1
+++ automatic/voicemeeter/update.ps1
@@ -1,10 +1,10 @@
 ﻿import-module au
  
 function global:au_GetLatest {
-  $releases       = 'https://www.vb-audio.com/Voicemeeter'  
-  $regex_version  = 'Voicemeeter (?<Version>[\d\.]+).*\(EXE file\)'
+  $releases      = 'https://www.vb-audio.com/Voicemeeter'  
+  $regex_version = 'Voicemeeter (?<Version>[\d\.]+).*\(EXE file\)'
 
-  $download_page  = (Invoke-WebRequest -Uri $releases)
+  $download_page = (Invoke-WebRequest -Uri $releases)
   $download_page.RawContent -match $regex_version
   
   return @{ Version = $matches.Version }

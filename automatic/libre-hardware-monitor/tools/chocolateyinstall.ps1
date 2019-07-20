cec7c56c9500c535e7c5dfd79df0ab90e6e6c79b
chtof <chtof62@gmail.com>
2019-07-20 16:46:29 +0200

    Update chocolateyinstall.ps1

--- automatic/libre-hardware-monitor/tools/chocolateyinstall.ps1
+++ automatic/libre-hardware-monitor/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
-  file          = "$toolsDir\Release.zip"  
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\Release.zip"  
 }
 
 Get-ChocolateyUnzip @packageArgs

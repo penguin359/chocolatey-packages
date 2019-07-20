e18f0144988f5c141ad31d8abc0094d741a92670
chtof <chtof62@gmail.com>
2019-07-20 21:49:18 +0200

    Create chocolateyinstall.ps1

---
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+
+  url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
+  checksum     = '9FC82C75D74F968828F6B875E03CD7F6541148FCD9492BAE46D1C669FA68660F'
+  checksumType = 'sha256'
+
+  silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+}
+
+Install-ChocolateyPackage @packageArgs

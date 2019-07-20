d04c3f04fc66b791ba3973c40224ffcda596647c
chtof <chtof62@gmail.com>
2019-07-20 18:28:07 +0200

    Update chocolateyinstall.ps1

--- automatic/voicemeeter.portable/tools/chocolateyinstall.ps1
+++ automatic/voicemeeter.portable/tools/chocolateyinstall.ps1
@@ -2,17 +2,17 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
-  file          = "$toolsDir\VoicemeeterSetup_v1068.zip"
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\VoicemeeterSetup_v1068.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
 Remove-Item -Path $packageArgs.file
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName  
-  fileFullPath  = "$toolsDir\VoicemeeterSetup.exe"
+  packageName  = $env:ChocolateyPackageName  
+  fileFullPath = "$toolsDir\VoicemeeterSetup.exe"
 }
 
 Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""

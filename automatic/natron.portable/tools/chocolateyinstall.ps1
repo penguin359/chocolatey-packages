92d55dd2c96cc794d440b4a62ef2d043d24a97fe
chtof <chtof62@gmail.com>
2019-07-20 12:57:42 +0200

    Update chocolateyinstall.ps1

--- automatic/natron.portable/tools/chocolateyinstall.ps1
+++ automatic/natron.portable/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   unzipLocation  = "$toolsDir"
 
   url            = 'https://github.com/NatronGitHub/Natron/releases/download/2.3.14/Natron-2.3.14-Windows-x86_32bit-no-installer.zip'
-  checksum       = ''
+  checksum       = '3d119398063e3f8f5bc5126a65483cdd5363b2db94ee7460c7bd96e353e432ea'
   checksumType   = 'sha256'
   
   url64          = 'https://github.com/NatronGitHub/Natron/releases/download/2.3.14/Natron-2.3.14-Windows-x86_64bit-no-installer.zip'
-  checksum64     = ''
+  checksum64     = 'b693dceeb564420964030175060488241d4745f42c7b7796aaaeb90e7f152a3c'
   checksumType64 = 'sha256'
 }
 

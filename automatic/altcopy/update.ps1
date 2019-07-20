599a7c4743931b838bb46f234cd52b0795c7bbcb
chtof <chtof62@gmail.com>
2019-07-20 17:54:19 +0200

    Update update.ps1

--- automatic/altcopy/update.ps1
+++ automatic/altcopy/update.ps1
@@ -1,7 +1,5 @@
 import-module au
 
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
-
 function global:au_GetLatest {
     $releases = 'https://altcopy.net'
     $regex    = '<p id="versionPc">Version: (?<Version>[\d\.]+)</p>'
@@ -12,21 +10,16 @@ function global:au_GetLatest {
     while ($ie.Busy -eq $true){Start-Sleep -seconds 4;}
     $ie.Document.body.outerHTML -match '<p id="versionPc">Version: (?<Version>[\d\.]+)</p>' | Out-Null
 
-    return @{ Version = $matches.Version ; URL32 = $url }
+    return @{ Version = $matches.Version ; URL32 = 'https://altcopy.net/setup.exe' }
 }
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"            
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        "tools\chocolateyInstall.ps1" = @{
+            "(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update

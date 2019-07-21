ce9fb331e414aa9f660df3c6730a5c4170982e79
chtof <chtof62@gmail.com>
2019-07-21 12:03:20 +0200

    Update chocolateyinstall.ps1

--- automatic/smtube/tools/chocolateyinstall.ps1
+++ automatic/smtube/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
-  file          = "$toolsDir\smtube-19.6.0-win32.exe"  
-  file64        = "$toolsDir\smtube-19.6.0-x64.exe"  
-  silentArgs	= "/S"
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\smtube-19.6.0-win32.exe"
+  file64      = "$toolsDir\smtube-19.6.0-x64.exe"
+  silentArgs  = "/S"
 }
 
 Install-ChocolateyInstallPackage @packageArgs

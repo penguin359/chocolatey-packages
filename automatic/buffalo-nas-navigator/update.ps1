2eda91bfe375d158fbf2c2c4668cdbe965436399
chtof <chtof62@gmail.com>
2019-07-21 20:39:33 +0200

    Update update.ps1

--- automatic/buffalo-nas-navigator/update.ps1
+++ automatic/buffalo-nas-navigator/update.ps1
@@ -3,7 +3,7 @@
 function global:au_GetLatest {
     $releases = 'https://www.buffalotech.com/support'
 
-    $regex    = 'nasnavi-(?<VersionMajor>\d)(?<VersionMinor>\d+).zip$'
+    $regex    = 'nasnavi-(?<VersionMajor>\d)(?<VersionMinor>\d\d).zip$'
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	$url = $download_page.links | ? href -match $regex
 

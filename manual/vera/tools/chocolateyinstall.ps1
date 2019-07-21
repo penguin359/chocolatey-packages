195694a7c651ca408498742cb63d1cb261e634ea
chtof <chtof62@gmail.com>
2019-07-21 14:48:48 +0200

    Update chocolateyinstall.ps1

--- manual/vera/tools/chocolateyinstall.ps1
+++ manual/vera/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\vera++-1.3.0-win32.exe"
-  silentArgs    = "/S"
+  packageName = $env:ChocolateyPackageName
+  file        = "$toolsDir\vera++-1.3.0-win32.exe"
+  silentArgs  = "/S"
 }
 
 Install-ChocolateyInstallPackage @packageArgs

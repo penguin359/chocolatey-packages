2be05789d6a5b8426554eb9aa18be30e8f645550
chtof <chtof62@gmail.com>
2019-07-21 12:22:25 +0200

    Update chocolateyinstall.ps1

--- automatic/dupeguru/tools/chocolateyinstall.ps1
+++ automatic/dupeguru/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
+  packageName  = $env:ChocolateyPackageName
     
-  url           = 'https://github.com/arsenetar/dupeguru/releases/download/4.0.4/dupeGuru_win64_4.0.4.exe'
-  checksum      = '72637B8D526D97C7B1836857F4012DE831A07B3F156028B1B854F1E6E6FD1D84'
-  checksumType  = 'sha256'
+  url          = 'https://github.com/arsenetar/dupeguru/releases/download/4.0.4/dupeGuru_win64_4.0.4.exe'
+  checksum     = '72637B8D526D97C7B1836857F4012DE831A07B3F156028B1B854F1E6E6FD1D84'
+  checksumType = 'sha256'
 
-  silentArgs	= "/S"
+  silentArgs   = "/S"
 }
 
 Install-ChocolateyPackage @packageArgs

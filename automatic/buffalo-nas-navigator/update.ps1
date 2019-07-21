bb1b47a6d5f02a498588f0b861e48248ccc56dc2
chtof <chtof62@gmail.com>
2019-07-21 20:35:56 +0200

    Create update.ps1

---
+++ automatic/buffalo-nas-navigator/update.ps1
@@ -0,0 +1,22 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://www.buffalotech.com/support'
+
+    $regex    = 'nasnavi-(?<Version>\d+).zip$'
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$url = $download_page.links | ? href -match $regex
+
+    return @{ Version = $version ; URL32 = $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

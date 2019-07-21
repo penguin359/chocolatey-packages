37db910802ded83e28d53d160ea5de0112d70da3
chtof <chtof62@gmail.com>
2019-07-21 12:21:14 +0200

    Update update.ps1

--- automatic/internet-download-manager/update.ps1
+++ automatic/internet-download-manager/update.ps1
@@ -1,15 +1,11 @@
 ﻿import-module au
 
 function global:au_GetLatest {
-    $releases_version = 'http://www.internetdownloadmanager.com'
-    $regex            = 'Internet Download Manager v(?<Version>[\d\.]+)\.'
-    $releases         = 'http://www.internetdownloadmanager.com/download.html'
-    $regex_url        = 'idman(.*?).exe$'
+    $releases = 'http://www.internetdownloadmanager.com/download.html'    
+    $regex    = 'idman(?<VersionMajor>[\d])(?<VersionMinor>[\d]+)build(?<Build>[\d]+)\.exe'
 
-    (Invoke-WebRequest -Uri $releases_version).RawContent -match $regex | Out-Null
-    $version    = $matches.Version
-
-    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex_url | Select -First 1
+    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Select -First 1
+    $version = $matches.VersionMajor + '.' + $matches.VersionMinor + '.' + $matches.Build
 
     return @{ Version = $version ; URL32 = $url.href }
 }

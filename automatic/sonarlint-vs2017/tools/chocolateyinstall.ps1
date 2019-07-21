6cbe4d0b6f0ab093a97a2ad771cac5e92af0e8d2
chtof <chtof62@gmail.com>
2019-07-21 12:03:26 +0200

    Update chocolateyinstall.ps1

--- automatic/sonarlint-vs2017/tools/chocolateyinstall.ps1
+++ automatic/sonarlint-vs2017/tools/chocolateyinstall.ps1
@@ -1,12 +1,11 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$file    = "$toolsDir\SonarLint.VSIX-4.10.0.9879-2017.vsix"  
+$file = "$toolsDir\SonarLint.VSIX-4.10.0.9879-2017.vsix"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   vsixUrl     = 'file://' + $file.Replace('\', '/')
-  VsVersion   = 15
 }
 
-Install-ChocolateyVsixPackage @packageArgs
+Install-VisualStudioVsixExtension @packageArgs

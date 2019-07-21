09e04410bc165591e668b925fb0c2948f77ff52a
chtof <chtof62@gmail.com>
2019-07-21 12:21:03 +0200

    Update chocolateyinstall.ps1

--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman633build2.exe'
-  checksum      = '98F7C90403F2C9844962BC8C4E7CEE0C5928DA018B30802A6242E9B3B0559F47'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman633build3.exe'
+  checksum      = '5bcd5ba46297289dc32184ad1ba9dd141bf39ad013e9afdfca2e2fd0781c9ce5'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"

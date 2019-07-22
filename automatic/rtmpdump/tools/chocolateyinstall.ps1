5746fcd53b1166ec91061fd978725104b8dbed78
chtof <chtof62@gmail.com>
2019-07-22 19:43:11 +0200

    Update chocolateyinstall.ps1

--- automatic/rtmpdump/tools/chocolateyinstall.ps1
+++ automatic/rtmpdump/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
-  file          = "$toolsDir\rtmpdump-2.4-2015-12-15.zip"
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\rtmpdump-2.4-2015-12-15.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

f72512a3afb6f51ccef3d5cd5c1276aeb914e42e
chtof <chtof62@gmail.com>
2019-07-22 19:41:39 +0200

    Update chocolateyinstall.ps1

--- automatic/jtalert/tools/chocolateyinstall.ps1
+++ automatic/jtalert/tools/chocolateyinstall.ps1
@@ -1,21 +1,21 @@
 ﻿if ( [environment]::OSVersion.Version.Major -ge 10 )  {
-  $url_win10      = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_AL_2.13.10_Setup.exe'
-	$checksum_win10 = '7d457e78b3422e4c8a9182be75d36ae2e2fbb710d5093f2a3ccd7c3c109c23a9'
-	$url            = $url_win10
-	$checksum       = $checksum_win10
+  $url_win10       = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_AL_2.13.10_Setup.exe'
+	$checksum_win10  = '7d457e78b3422e4c8a9182be75d36ae2e2fbb710d5093f2a3ccd7c3c109c23a9'
+	$url             = $url_win10
+	$checksum        = $checksum_win10
 } else {
-  $url_others     = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_2.13.10_Setup.exe'
-	$checksum_others= '99fb8737ea506df894b26079d050a08ceeda79d813826743054f29944ccfc4c1'
-  $url            = $url_others
-  $checksum       = $checksum_others
+  $url_others      = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_2.13.10_Setup.exe'
+	$checksum_others = '99fb8737ea506df894b26079d050a08ceeda79d813826743054f29944ccfc4c1'
+  $url             = $url_others
+  $checksum        = $checksum_others
 }
   
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
+  packageName  = $env:ChocolateyPackageName
 
-  url           = $url
-  checksum      = $checksum
-  checksumType  = 'sha256'
+  url          = $url
+  checksum     = $checksum
+  checksumType = 'sha256'
   
   silentArgs 	= '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components=""'
 }

772401e02a17bd73ba69e89a13c12b0ff06aefa8
chtof <chtof62@gmail.com>
2019-07-23 13:41:32 +0200

    Update chocolateyinstall.ps1

--- automatic/buffalo-nas-navigator/tools/chocolateyinstall.ps1
+++ automatic/buffalo-nas-navigator/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   unzipLocation = "$toolsDir"
   fileType      = 'exe'  
 
-  url           = 'https://www.buffalotech.com/support/download/nasnavi-297.zip'
-  checksum      = 'D6BDEDDB7067CA0F2B6010283028636447F850D024388462C61FC96C81C3B4C0'
+  url           = 'https://www.buffalotech.com/support/download/nasnavi-299.zip'
+  checksum      = 'ACB78EAA5D60DFD24A213A65DFE4ADC3DA1064939BD9B847F77B8162FDB85506'
   checksumType  = 'sha256'  
 }
 Install-ChocolateyZipPackage @packageArgs
@@ -15,7 +15,7 @@ Install-ChocolateyZipPackage @packageArgs
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName 
   fileType      = 'exe'
-  file          = "$toolsDir\nasnavi-297\NASNaviInst.exe"
+  file          = "$toolsDir\nasnavi-299\NASNaviInst.exe"
 }
 Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
 Install-ChocolateyInstallPackage @packageArgs

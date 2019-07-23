ba292270a8cdbc23911df8b61dc92debccc4d4e4
chtof <chtof62@gmail.com>
2019-07-23 20:45:40 +0200

    Update chocolateyinstall.ps1

--- manual/pandafreeantivirus/tools/chocolateyinstall.ps1
+++ manual/pandafreeantivirus/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
+  packageName  = $env:ChocolateyPackageName
     
-  url           = 'http://acs.pandasoftware.com/Panda/FREEAV/190612/PANDAFREEAV.exe'
-  checksum      = '00CEF69FF557BFEE8CED3E7567B744F2D444374872B9C10A5B72AFDAFAC8E25E'
-  checksumType  = 'sha256'
+  url          = 'http://acs.pandasoftware.com/Panda/FREEAV/190612/PANDAFREEAV.exe'
+  checksum     = '00CEF69FF557BFEE8CED3E7567B744F2D444374872B9C10A5B72AFDAFAC8E25E'
+  checksumType = 'sha256'
 }
 
 # Panda Dome Free Antivirus supports Windows 10, Windows 8/8.1, Windows 7, Window Vista and Windows XP (SP3 or later)

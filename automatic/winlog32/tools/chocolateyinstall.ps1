68e2fbe44afb5a5afbdaa6236f6f040335753a22
chtof <chtof62@gmail.com>
2019-07-23 20:38:49 +0200

    Update chocolateyinstall.ps1

--- automatic/winlog32/tools/chocolateyinstall.ps1
+++ automatic/winlog32/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\wl32v73.exe"
-  silentArgs	= '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  packageName = $env:ChocolateyPackageName
+  file        = "$toolsDir\wl32v73.exe"
+  silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
 Install-ChocolateyInstallPackage @packageArgs

5063f6291a68a53b2d851b0d6f00bb34727cad2d
chtof <chtof62@gmail.com>
2019-07-24 20:08:02 +0200

    Create update.ps1

---
+++ automatic/eclipse-java-oxygen/update.ps1
@@ -0,0 +1,22 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://www.eclipse.org/downloads/packages'
+    $regex    = 'eclipse-java-(?<VersionUrl>(?<VersionYear>\d+)-(?<VersionMonth>\d+))-R-win32-x86_64.zip$' 
+
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Out-Null
+	$version = $matches.VersionYear + '.' + $matches.VersionMonth
+
+    return @{ Version = $version ; URL32 = 'https://ftp.halifax.rwth-aachen.de/eclipse//technology/epp/downloads/release/' + $matches.VersionUrl + '/R/eclipse-java-' + $matches.VersionUrl + '-R-win32-x86_64.zip' }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

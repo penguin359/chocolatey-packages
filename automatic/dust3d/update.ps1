e29a9e4e81268e9f471bb684d3ec53dd626c283b
chtof <chtof62@gmail.com>
2019-07-26 19:25:18 +0200

    Create update.ps1

---
+++ automatic/dust3d/update.ps1
@@ -0,0 +1,42 @@
+﻿import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $github_repository = "huxingyi/dust3d"
+    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $regex = 'dust3d-(?<Version>[\d\.]+)(-beta)?(?<VersionBeta>\.[\d\.]*)-x86.zip'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
+	$url = $download_page.links | ? href -match $regex
+
+    if ($matches.VersionBeta) {
+        $version = $matches.Version + $matches.VersionBeta + '-beta'
+    } else {
+        $version = $matches.Version
+    }
+
+    return @{ Version = $version ; URL32 = "https://github.com" + $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

8fb703986d31e71ab58d016801a16def5dc6649f
chtof <chtof62@gmail.com>
2019-07-27 12:38:32 +0200

    Update chocolateyinstall.ps1

--- automatic/uncrustify/tools/chocolateyinstall.ps1
+++ automatic/uncrustify/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = "$env:ChocolateyPackageName"
-  destination   = "$toolsDir"    
-  file          = "$toolsDir\uncrustify-0.69.0-win32.zip"
-  file64        = "$toolsDir\uncrustify-0.69.0-win64.zip"
+  packageName = "$env:ChocolateyPackageName"
+  destination = "$toolsDir"    
+  file        = "$toolsDir\uncrustify-0.69.0-win32.zip"
+  file64      = "$toolsDir\uncrustify-0.69.0-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

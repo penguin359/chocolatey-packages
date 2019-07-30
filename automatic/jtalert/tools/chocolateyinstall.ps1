87cb712aa243ffbfdc34376f203eb1d8888e9609
Chocolatey <chocolatey@realdimensions.net>
2019-07-30 00:23:00 +0000

    AU: 1 updated - jtalert [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/5308b2d8808ba1f6a870588565d3d2b75b672d20

--- automatic/jtalert/tools/chocolateyinstall.ps1
+++ automatic/jtalert/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿if ( [environment]::OSVersion.Version.Major -ge 10 )  {
-  $url_win10       = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_AL_2.14.0_Setup.exe'
-	$checksum_win10  = 'b60230034a466a761c473295ab5821e40135e1232b75ab9e9f1a6bc2b7bc9933'
+  $url_win10       = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_AL_2.14.1_Setup.exe'
+	$checksum_win10  = 'f4ec8fac74bc2269993b18d045014991372076c8c7540a111ddc31084371c075'
 	$url             = $url_win10
 	$checksum        = $checksum_win10
 } else {
-  $url_others      = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_2.14.0_Setup.exe'
-	$checksum_others = 'a52e2ad314923194b55ad9051aa97adcfa8b9ae1426d3335259807e439a9aca0'
+  $url_others      = 'https://dnl.hamapps.com/JTAlert/HamApps_JTAlert_2.14.1_Setup.exe'
+	$checksum_others = '82b6e361b0711e77b96e69f9fdcb037a49b2d9aced657200b4e56015d026875e'
   $url             = $url_others
   $checksum        = $checksum_others
 }

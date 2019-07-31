897b38d0b16b082448fd7d3eb1f62f97a9d86193
chtof <chtof62@gmail.com>
2019-08-01 01:08:31 +0200

    Update chocolateyinstall.ps1

--- manual/little-fighter-2/tools/chocolateyinstall.ps1
+++ manual/little-fighter-2/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
-    
+
   url          = 'http://lf2.net/LF2_v2.0a.exe'
   checksum     = '540C485547E234B0D09595F54D067AAFD7176092C308ACA61868F3CBC03030E1'
   checksumType = 'sha256'

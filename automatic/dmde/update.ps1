2c6090b00191186b660bd3e5663e62b29622a3ea
chtof <chtof62@gmail.com>
2019-07-31 20:28:35 +0200

    Create update.ps1

---
+++ automatic/dmde/update.ps1
@@ -0,0 +1,34 @@
+﻿import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://dmde.com/download.html'
+    $regex    = 'dmde-(?<Version>[\d\.]+)-win32-gui.zip'
+    
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url = $download_page.links | ? href -match $regex | Select -Last 1
+    
+     return @{
+        Version = $matches.Version
+        URL64   = $url.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

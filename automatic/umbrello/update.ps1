58acea24777da78825210d42e693c7378e3e92cb
chtof <chtof62@gmail.com>
2019-08-01 20:44:27 +0200

    Update update.ps1

--- automatic/umbrello/update.ps1
+++ automatic/umbrello/update.ps1
@@ -1,8 +1,8 @@
 ﻿import-module au
- 
+
 function global:au_GetLatest {
   $releases = 'https://download.kde.org/stable/umbrello/latest/win32'
-  $regex    = 'umbrello-i686-w64-mingw32-(?<Version>[\d\.]+).*-setup.exe$'
+  $regex    = 'umbrello-(i686-)?(w64-)?mingw32-(?<Version>[\d\.]+).*-setup.exe$'
 
   $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing)
   $file = $download_page.links | ? href -match $regex  

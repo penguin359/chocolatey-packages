635c76fc71301a4d1c1f1e2bd6b5515e52759d28
chtof <chtof62@gmail.com>
2019-08-02 19:53:42 +0200

    Update update.ps1

--- automatic/sunset-screen.portable/update.ps1
+++ automatic/sunset-screen.portable/update.ps1
@@ -7,7 +7,7 @@ function global:au_GetLatest {
     (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
 
     return @{
-        Version = $version
+        Version = $matches.Version
         URL32   = 'https://www.skytopia.com/software/sunsetscreen/SunsetScreen.exe'
     }
 }

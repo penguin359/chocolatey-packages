c1734c237df06a521b169bd45b9a5cd3c9337efb
chtof <chtof62@gmail.com>
2019-08-02 19:51:34 +0200

    Create update.ps1

---
+++ automatic/sunset-screen/update.ps1
@@ -0,0 +1,20 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://www.skytopia.com/software/sunsetscreen'
+    $regex    = 'SunsetScreen v(?<Version>[\d\.]+)\<'
+
+    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+
+    return @{ Version = $version }
+}
+
+function global:au_SearchReplace {
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

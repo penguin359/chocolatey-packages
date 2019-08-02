80957a93c2a571629e8b2429e1459f2c11968049
chtof <chtof62@gmail.com>
2019-08-02 19:22:36 +0200

    Update chocolateyinstall.ps1

--- automatic/texmacs/tools/chocolateyinstall.ps1
+++ automatic/texmacs/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\TeXmacs-1.99.11-installer.exe"
-  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  packageName = $env:ChocolateyPackageName
+  file        = "$toolsDir\TeXmacs-1.99.11-installer.exe"
+  silentArgs  = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 
 Install-ChocolateyInstallPackage @packageArgs

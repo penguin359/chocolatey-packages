09d491e16a1c9ffa75319d0732e5acbf0b79002e
chtof <chtof62@gmail.com>
2019-08-02 19:51:25 +0200

    Create update.ps1

---
+++ automatic/sunset-screen.install/update.ps1
@@ -0,0 +1,24 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://www.skytopia.com/software/sunsetscreen'
+    $regex    = 'SunsetScreen v(?<Version>[\d\.]+)\<'
+
+    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+
+    return @{
+        Version = $version
+        URL32   = 'https://www.skytopia.com/software/sunsetscreen/SunsetScreen_Setup.exe'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

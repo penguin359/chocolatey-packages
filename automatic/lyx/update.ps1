1bfb882e13a37e02d4b9bea421d94b4d7a4a90b1
chtof <chtof62@gmail.com>
2019-08-02 14:32:53 +0200

    Create update.ps1

---
+++ automatic/lyx/update.ps1
@@ -0,0 +1,36 @@
+import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://www.lyx.org/Download'
+    $regex    = 'https://ftp.lip6.fr/pub/lyx/bin/(?<Version>[\d\.]+)/LyX-[\d]+-Installer-(?<VersionMinor>[\d]+).exe$'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	 
+    $url = $download_page.links | ? href -match $regex
+
+    return @{
+        Version = $matches.Version + '.' + $matches.VersionMinor
+        URL32   = $url.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

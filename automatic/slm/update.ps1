7eafff2b67186caae5cf97718173bb106aae1f01
chtof <chtof62@gmail.com>
2019-08-02 20:22:16 +0200

    Create update.ps1

---
+++ automatic/slm/update.ps1
@@ -0,0 +1,24 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://messenger.softros.com/downloads'
+    $regex    = 'Version: (?<Version>[\d\.]+)'
+    
+    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+    
+    return @{
+        Version = $matches.version
+        URL32   = 'https://download.softros.com/SoftrosLANMessengerSetup.msi'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

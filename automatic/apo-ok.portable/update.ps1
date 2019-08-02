8fa62ad6ff16deeb13065abb97eb91f075d55f98
chtof <chtof62@gmail.com>
2019-08-02 19:34:03 +0200

    Create update.ps1

---
+++ automatic/apo-ok.portable/update.ps1
@@ -0,0 +1,34 @@
+﻿import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://www.softwareok.com/?seite=Microsoft/AutoPowerOptionsOK'
+    $regex    = "<title>AutoPowerOptionsOK (?<Version>[\d\.]+)</title>"
+
+    (Invoke-WebRequest -Uri $releases) -match $regex | out-null
+     return @{
+        Version = $matches.Version
+        URL32 = 'http://www.softwareok.com/Download/AutoPowerOptionsOK_Portable.zip'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

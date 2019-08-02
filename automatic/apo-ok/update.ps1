8d80a07f184a4d5146e8401e3f92fbad8bd2839d
chtof <chtof62@gmail.com>
2019-08-02 19:34:09 +0200

    Create update.ps1

---
+++ automatic/apo-ok/update.ps1
@@ -0,0 +1,21 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://www.softwareok.com/?seite=Microsoft/AutoPowerOptionsOK'
+    $regex    = "<title>AutoPowerOptionsOK (?<Version>[\d\.]+)</title>"
+
+    (Invoke-WebRequest -Uri $releases) -match $regex | out-null
+     return @{
+        Version = $matches.Version
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

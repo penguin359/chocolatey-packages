3dea998f6f6341b48bad3311cbbf49659b453eb6
chtof <chtof62@gmail.com>
2019-08-05 22:16:01 +0200

    Update update.ps1

--- automatic/wireguard/update.ps1
+++ automatic/wireguard/update.ps1
@@ -1,4 +1,4 @@
-﻿import-module au
+import-module au
 
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
@@ -8,32 +8,32 @@ function global:au_GetLatest {
     $regex64  = 'wireguard-amd64-(?<Version>[\d\.]+).msi'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	
-	$url32    = $download_page.links | ? href -match $regex32
-    $url64    = $download_page.links | ? href -match $regex64
-    $version  = $matches.Version
-    $versionMajor = $version -match "(\d)\..*"
 
-    if ($versionMajor -eq 0) { $versionSuffix = "-pre" } else { $versionSuffix = "" }
+	$url32    = ($download_page.links | ? href -match $regex32).href | Select -Last 1
+    $url64    = ($download_page.links | ? href -match $regex64).href | Select -Last 1
+    $version  = $matches.Version
+    $isBeta   = $version -match "^0\..*"
+
+    if ($isBeta) { $versionSuffix = "-pre" } else { $versionSuffix = "" }
 
 	return @{
-        Version = $version
-        URL32   = $releases + $url32.href + $versionSuffix
-        URL64   = $releases + $url64.href + $versionSuffix
+        Version = $version + $versionSuffix
+        URL32   = $releases + $url32
+        URL64   = $releases + $url64
     }
 }
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"            
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"            
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
-        "tools\chocolateyinstall.ps1" = @{        
+        "tools\chocolateyinstall.ps1" = @{
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
           "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
@@ -41,5 +41,5 @@ function global:au_SearchReplace {
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
+    update -ChecksumFor none -WhatIf
 }

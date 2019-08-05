aed81309c3c88f54ee436bfa4cb865f18e8f51e1
chtof <chtof62@gmail.com>
2019-08-05 23:01:14 +0200

    Update update.ps1

--- automatic/zvirtualdesktop/update.ps1
+++ automatic/zvirtualdesktop/update.ps1
@@ -1,13 +1,25 @@
 ﻿import-module au
-$github_repository = "mzomparelli/zVirtualDesktop"
-$releases          = "https://github.com/" + $github_repository + "/releases/latest"
-$regex_version     = "/archive/(?<Version>[\d\.]+).zip$"
-$regex_url         = "^(https://zomp.co/Files.aspx\?id=.*)$"
 
-function global:au_GetLatest {	
+$github_repository = "mzomparelli/zVirtualDesktop"
+
+function global:au_BeforeUpdate {
+    Remove-Item "$PSScriptRoot\tools\*.exe"
+
+    Invoke-WebRequest $Latest.URL32 -OutFile "${env:tmp}\zVirtualDesktop.exe"
+
+    $Latest.ChecksumType = "sha256"
+    $Latest.Checksum32   = Get-FileHash -Algorithm $Latest.ChecksumType -Path "${env:tmp}\zVirtualDesktop.exe" | ForEach-Object Hash
+}
+
+function global:au_GetLatest {
+    $releases      = "https://github.com/" + $github_repository + "/releases/latest"
+    $regex_version = "/archive/(?<Version>[\d\.]+).zip$"
+    $regex_url     = "^(https://zomp.co/Files.aspx\?id=.*)$"
+    
 	(Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex_version | Out-Null
     $version = $matches.Version
-    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex_url
+    $url     = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex_url
+    
 	return @{ Version = $version ; URL32 = $url.href }
 }
 
@@ -20,4 +32,4 @@ function global:au_SearchReplace {
     }
 }
 
-update
+update -NoCheckUrl -ChecksumFor none

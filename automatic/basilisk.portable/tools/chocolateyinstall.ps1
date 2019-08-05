c88a45901c36a660fc7dd38e6c7d6dc820ee7e47
chtof <chtof62@gmail.com>
2019-08-05 20:39:38 +0200

    Update chocolateyinstall.ps1

--- automatic/basilisk.portable/tools/chocolateyinstall.ps1
+++ automatic/basilisk.portable/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   checksum      = 'CF43FA119ABEA7381927163BF359A0B6E0CE27CA4880D4C89438A789C285EA9E'
   checksumType  = 'sha256'
   
-  url64bit      = 'http://eu.basilisk-browser.org/release/basilisk-latest.win64.zip'
+  url64         = 'http://eu.basilisk-browser.org/release/basilisk-latest.win64.zip'
   checksum64    = '674A5272F1FE215211ACB5DC6DA0ED7B749DFB74C4D0733EA6922625F5745643'
   checksumType64= 'sha256'
 }

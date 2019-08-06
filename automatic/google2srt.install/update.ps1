6f1a47a98d9c2a8f931e164a65ac64776794f4ce
chtof <chtof62@gmail.com>
2019-08-06 19:14:49 +0200

    Create update.ps1

---
+++ automatic/google2srt.install/update.ps1
@@ -0,0 +1,36 @@
+﻿import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://sourceforge.net/projects/google2srt/files/Google2SRT'
+    $regex    = 'google2srt-(?<Version>[\d\.]+).msi'
+
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
+    $version = $matches.Version
+
+    return @{
+        Version = $version
+        URL32   = 'https://freefr.dl.sourceforge.net/project/google2srt/Google2SRT/' + $version + '/google2srt-' + $version + '.msi'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

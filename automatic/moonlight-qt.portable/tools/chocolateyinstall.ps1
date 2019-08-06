72db52a200e5ea3661b8fac9b1cbe2dda98cf9a8
Chocolatey <chocolatey@realdimensions.net>
2019-08-06 16:23:24 +0000

    AU: 3 updated - botpress moonlight-qt.install moonlight-qt.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/bfe54d087df8d3613af438d035677942440fcf21

--- automatic/moonlight-qt.portable/tools/chocolateyinstall.ps1
+++ automatic/moonlight-qt.portable/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\MoonlightPortable-x86-1.0.1.zip"
-  file64        = "$toolsDir\MoonlightPortable-x64-1.0.1.zip"
+  file          = "$toolsDir\MoonlightPortable-x86-1.1.0.zip"
+  file64        = "$toolsDir\MoonlightPortable-x64-1.1.0.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

48cafdd120ea39c812bf09df2aee8feda3ae6b3f
chtof <chtof62@gmail.com>
2019-08-06 19:14:53 +0200

    Create update.ps1

---
+++ automatic/google2srt/update.ps1
@@ -0,0 +1,22 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://sourceforge.net/projects/google2srt/files/Google2SRT'
+    $regex    = 'google2srt-(?<Version>[\d\.]+).msi'
+
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
+
+    return @{
+        Version = $matches.Version
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{         
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update

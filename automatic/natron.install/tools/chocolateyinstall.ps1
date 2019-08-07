a49f4ff4462c02d72224335313a7692c5c977997
chtof <chtof62@gmail.com>
2019-08-07 15:44:04 +0200

    natron Download link has changes (v2.3.14 instead of 2.3.14)

--- automatic/natron.install/tools/chocolateyinstall.ps1
+++ automatic/natron.install/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url            = 'https://github.com/NatronGitHub/Natron/releases/download/2.3.14/Natron-2.3.14-Windows-x86_32bit-setup.exe'
+  url            = 'https://github.com/NatronGitHub/Natron/releases/download/v2.3.14/Natron-2.3.14-Windows-x86_32bit-setup.exe'
   checksum       = 'a87339879e8b59dd6d0f3d32b349ec3b42c989ca55e108f95e70b03adfa04a57'
   checksumType   = 'sha256'
-  
-  url64          = 'https://github.com/NatronGitHub/Natron/releases/download/2.3.14/Natron-2.3.14-Windows-x86_64bit-setup.exe'
+
+  url64          = 'https://github.com/NatronGitHub/Natron/releases/download/v2.3.14/Natron-2.3.14-Windows-x86_64bit-setup.exe'
   checksum64     = '7e5365b3964fa2f662719b9eb4748e11f88be37734659badab03e32fc5e01834'
   checksumType64 = 'sha256'
 }

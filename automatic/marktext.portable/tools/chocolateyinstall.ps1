386aef6c9bb6199602546d27a52b06c04ba46d9e
chtof <chtof62@gmail.com>
2019-08-07 19:33:08 +0200

    marktext.portable 32 and 64 bits are now available since version 0.15.0

--- automatic/marktext.portable/tools/chocolateyinstall.ps1
+++ automatic/marktext.portable/tools/chocolateyinstall.ps1
@@ -1,8 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-#Install start menu shortcuts
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\marktext-0.15.0-ia32-win.zip"
+  file64      = "$toolsDir\marktext-0.15.0-x64-win.zip"
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -Path $packageArgs.file,$packageArgs.file64
+
+# Install start menu shortcuts
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Mark Text.lnk"
-$targetPath = Join-Path $toolsDir "marktext-0.14.0.exe"
+$targetPath = Join-Path $toolsDir "Mark Text.exe"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

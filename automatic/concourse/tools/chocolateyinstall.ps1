52c3bb31e71c0f7d1a4f4e7fc873b26bc0e49ca0
chtof <chtof62@gmail.com>
2019-08-07 19:32:02 +0200

    Update chocolateyinstall.ps1

--- automatic/concourse/tools/chocolateyinstall.ps1
+++ automatic/concourse/tools/chocolateyinstall.ps1
@@ -8,13 +8,14 @@ $packageArgs = @{
 }
 
 Get-ChocolateyUnzip @packageArgs
+Remove-Item -Path $packageArgs.file64
 
 $installDir = Join-Path $packageArgs.destination $packageArgs.packageName
 
 $flyArgs = @{
-  File  = Join-Path $installDir "fly-assets\fly-windows-amd64.zip"
-  Destination = Join-Path $installDir "bin"
+  destination    = Join-Path $installDir "bin"
+  fileFullPath64 = Join-Path $installDir "fly-assets\fly-windows-amd64.zip"
 }
-Get-ChocolateyUnzip @flyArgs
 
-Remove-Item -Path $packageArgs.file
+Get-ChocolateyUnzip @flyArgs
+Remove-Item -Path $flyArgs.fileFullPath64

3751f88a927230278c0e395b824bbff7be920eb8
chtof <chtof62@gmail.com>
2019-08-08 18:48:58 +0200

    Create update.ps1

---
+++ automatic/concourse/update.ps1
@@ -0,0 +1,32 @@
+﻿import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $github_repository = "concourse/concourse"
+    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $regex    = 'concourse-(?<Version>[\d\.]+)-windows-amd64.zip$'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
+	$url = $download_page.links | ? href -match $regex
+
+    return @{ Version = $matches.Version ; URL64 = "https://github.com" + $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor 64
+}

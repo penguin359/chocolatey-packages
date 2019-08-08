73822f0019bc6746da045b730d5b9d2ea4c1d4b3
Chocolatey <chocolatey@realdimensions.net>
2019-08-08 00:24:50 +0000

    AU: 2 updated - goodsync umbrello.install [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/ba39c66a74e74a80666c73fb46fad2c47cd06267

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-v10-Setup.msi'
-  checksum       = 'd51d758be742e202c6865d0bb2c8ec37ce740a74e188a546a43d79ee9cd526af'
+  checksum       = 'e6f777a7d5c8c8677aea4fd3fbc7743a822188ca1ab4e4dd61a05f716913baaa'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

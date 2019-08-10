736a355d210661f7b705067fd3fa52952c56545b
chtof <chtof62@gmail.com>
2019-08-10 22:28:40 +0200

    Update chocolateyinstall.ps1

--- automatic/huggle/tools/chocolateyinstall.ps1
+++ automatic/huggle/tools/chocolateyinstall.ps1
@@ -3,9 +3,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\huggle_3.4.9.exe"
+  url         = "$toolsDir\huggle_3.4.9.exe"
+  checksum    = 'AA79F8F8C9879B02043A1879BFE6088E3824A77C9A347059D1828B69A0A383FA'
   file64      = "$toolsDir\huggle_3.4.9_x64.exe"
+  checksum64  = '83E845D92AE4DC20474FEF1864FE66E34CFF090F8A58111227AF193E45200E52'
   silentArgs  = "/S"
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyPackage @packageArgs

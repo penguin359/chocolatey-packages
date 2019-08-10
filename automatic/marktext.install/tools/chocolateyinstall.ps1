b7d4d8d5b3c5e8dadde6852cc4a6d5e62b95a338
chtof <chtof62@gmail.com>
2019-08-10 13:26:57 +0200

    Update chocolateyinstall.ps1

--- automatic/marktext.install/tools/chocolateyinstall.ps1
+++ automatic/marktext.install/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
+  packageName  = $env:ChocolateyPackageName
 
-  url           = 'https://github.com/marktext/marktext/releases/download/v0.15.0/marktext-setup-0.15.0.exe'
-  checksum      = '022315cee8f8157317cffa02385d31b4d4b8b5547ca752fffc6fa3c71fefb6e4'
-  checksumType  = 'sha256'
+  url          = 'https://github.com/marktext/marktext/releases/download/v0.15.0/marktext-setup-0.15.0.exe'
+  checksum     = '022315cee8f8157317cffa02385d31b4d4b8b5547ca752fffc6fa3c71fefb6e4'
+  checksumType = 'sha256'
   
-  silentArgs	= "/S _?=$Env:PROGRAMFILES\$packageName"
+  silentArgs   = "/S"
 }
 
 Install-ChocolateyPackage @packageArgs

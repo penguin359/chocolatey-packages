c829b975e106c4a990d585d722009535af21752a
chtof <chtof62@gmail.com>
2019-08-10 14:57:08 +0200

    davmail initial commit

---
+++ automatic/davmail/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  file        = "$toolsDir\davmail-5.3.0-3065-setup.exe"
+  file64      = "$toolsDir\davmail-5.3.0-3065-setup64.exe"
+  silentArgs  = "/S"
+}
+
+Install-ChocolateyInstallPackage @packageArgs

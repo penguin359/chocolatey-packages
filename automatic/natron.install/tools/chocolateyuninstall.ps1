80794dcecdb1af9ea6ed944a4958f60bd565549a
chtof <chtof62@gmail.com>
2019-08-10 14:13:15 +0200

    Update chocolateyuninstall.ps1

--- automatic/natron.install/tools/chocolateyuninstall.ps1
+++ automatic/natron.install/tools/chocolateyuninstall.ps1
@@ -15,6 +15,10 @@ if ($key.Count -eq 1) {
     $packageArgs['file'] = "$($_.UninstallString)"
 	  Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyuninstall.ahk`""
     Uninstall-ChocolateyPackage @packageArgs
+
+    # Close AutoHotKey
+    $autohotkey = Get-Process AutoHotKey -ErrorAction SilentlyContinue
+    if ($autohotkey) { $autohotkey | Stop-Process }
   }
 } elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."

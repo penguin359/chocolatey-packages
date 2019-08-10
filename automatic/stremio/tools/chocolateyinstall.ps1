8b96c81873613e1ba98763c40ded9c7d5eda9cb5
chtof <chtof62@gmail.com>
2019-08-10 13:31:44 +0200

    Update chocolateyinstall.ps1

--- automatic/stremio/tools/chocolateyinstall.ps1
+++ automatic/stremio/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-    
-  url           = 'https://dl.strem.io/win/v4.4.77/Stremio+4.4.77.exe'
-  checksum      = 'd5e1f5c638d99e17090762585022b649bd71e1c684044f64a55d96211cfef2ca'
-  checksumType  = 'sha256'
-  
-  silentArgs	= "/S"
+  packageName  = $env:ChocolateyPackageName
+
+  url          = 'https://dl.strem.io/win/v4.4.77/Stremio+4.4.77.exe'
+  checksum     = 'bf47ab1312097d2283ed74b2a6ac46978ea3a31fb93e521c115a087060d5b145'
+  checksumType = 'sha256'
+
+  silentArgs   = "/S"
 }
 
 Install-ChocolateyPackage @packageArgs

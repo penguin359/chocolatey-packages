a4a0ba15b9a93b7defe089da17b0f4fdfba0f00d
chtof <chtof62@gmail.com>
2019-08-11 13:14:53 +0200

    eddie/eddie.install/eddie.portable initial commit

---
+++ automatic/eddie.install/ReadMe.md
@@ -0,0 +1,14 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/eddie.install/eddie.install.png" width="48" height="48"/> [Eddie - OpenVPN GUI (Install)](https://chocolatey.org/packages/eddie.install)
+
+OpenVPN GUI (open source under GPLv3 license) with additional features like:
+
+- User-friendly UI
+- Multiplatform support: Windows, OS X, macOS, GNU/Linux (with particular effort to [support a lot of distro](https://airvpn.org/forum/35-client-software-platforms-environments) with multiple architectures: x64, x86, armv7i (Raspberry)
+- Censorship counter-measures, like tunnel over SSH, over SSL, direct Tor integration
+- Network lock / leak prevention
+- Advanced options for DNS, routing, events, leak etc.
+- Support of VPN service providers (under development, currently it supports only AirVPN)
+- Customizable actions triggered by events
+- CLI edition
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/eddie.install/screenshot.png)
---
+++ automatic/eddie.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\eddie-ui_2.16.3_windows-10_x86_portable.zip"
+  file64      = "$toolsDir\eddie-ui_2.16.3_windows-10_x64_portable.zip"
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -Path $packageArgs.file,$packageArgs.file64
+
+# Don't create shims for other executables
+$files = Get-ChildItem "$toolsDir" -Recurse -Include *.exe -Exclude Eddie-CLI.exe,Eddie-UI.exe
+foreach ($file in $files) {
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "Eddie-UI.lnk"
+$targetPath = Join-Path $toolsDir "eddie-ui_2.16.3_windows-10_x64_portable\Eddie-UI.exe"
+Install-ChocolateyShortcut -RunAsAdmin -shortcutFilePath $shortcutFilePath -targetPath $targetPath

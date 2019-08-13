2e8c0cb7ab308ac11dd5d501ab834b2d09488ab6
chtof <chtof62@gmail.com>
2019-08-13 13:19:42 +0200

    Update chocolateyinstall.ps1

--- automatic/lossless-cut/tools/chocolateyinstall.ps1
+++ automatic/lossless-cut/tools/chocolateyinstall.ps1
@@ -5,13 +5,13 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = "$toolsDir"
 
-  url            = "https://github.com/mifi/lossless-cut/releases/download/v2.3.0/LosslessCut-win32-ia32.zip"
-  checksum       = "C6590B06BC65D889563A490F861D26DF998B7CFC355B012EC71503EF79FC41CE"
-  checksumType   = "sha256"
+  url            = 'https://github.com/mifi/lossless-cut/releases/download/v2.3.0/LosslessCut-win32-ia32.zip'
+  checksum       = 'C6590B06BC65D889563A490F861D26DF998B7CFC355B012EC71503EF79FC41CE'
+  checksumType   = 'sha256'
 
-  url64          = "https://github.com/mifi/lossless-cut/releases/download/v2.3.0/LosslessCut-win32-x64.zip"
-  checksum64     = "930B2FC7D4F6FB28D20769229CB9A73DBEDFC19983F6F3F18CB9CFB282EE3A24"
-  checksumType64 = "sha256"  
+  url64          = 'https://github.com/mifi/lossless-cut/releases/download/v2.3.0/LosslessCut-win32-x64.zip'
+  checksum64     = '930B2FC7D4F6FB28D20769229CB9A73DBEDFC19983F6F3F18CB9CFB282EE3A24'
+  checksumType64 = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs

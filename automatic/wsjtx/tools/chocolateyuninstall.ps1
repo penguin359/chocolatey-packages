456402e230192e451fb1aac2c199e3e3274ba4e3
chtof <chtof62@gmail.com>
2019-08-13 23:07:28 +0200

    Update chocolateyuninstall.ps1

--- automatic/wsjtx/tools/chocolateyuninstall.ps1
+++ automatic/wsjtx/tools/chocolateyuninstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName  
-  silentArgs    = '/S _?=C:\WSJT\wsjtx'
-  file			= "c:\WSJT\wsjtx\Uninstall.exe"
+  packageName = $env:ChocolateyPackageName  
+  silentArgs  = '/S _?=C:\WSJT\wsjtx'
+  file        = 'c:\WSJT\wsjtx\Uninstall.exe'
 }
 
 Uninstall-ChocolateyPackage @packageArgs

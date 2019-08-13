d404be86fe8d1cf35b4fbd5de0e26159091f4815
chtof <chtof62@gmail.com>
2019-08-13 13:28:26 +0200

    Update update.ps1

--- automatic/kmymoney/update.ps1
+++ automatic/kmymoney/update.ps1
@@ -3,13 +3,13 @@
 function global:au_GetLatest {
 	$releases_32 = 'https://download.kde.org/stable/kmymoney/latest/win32/'
 	$download_page_32 = Invoke-WebRequest -Uri $releases_32 -UseBasicParsing	
-	$regex_32   = 'kmymoney-i686-w64-mingw32-(?<Version_32>[\d\.]+?)(-[\d\.]+)?-setup.exe$'
+	$regex_32   = 'kmymoney(-i686)?(-w64)?-mingw32-(?<Version_32>[\d\.]+?)(-[\d\.]+)?-setup.exe$'
 	$url_32 = $download_page_32.links |? href -match $regex_32 | select -Last 1	
 	$version = $matches.Version_32
 	
 	$releases_64 = 'https://download.kde.org/stable/kmymoney/latest/win64/'
 	$download_page_64 = Invoke-WebRequest -Uri $releases_64 -UseBasicParsing	
-	$regex_64   = 'kmymoney-x86_64-w64-mingw32-(?<Version_64>[\d\.]+?)(-[\d\.]+)?-setup.exe$'
+	$regex_64   = 'kmymoney(-x86_64)?(-w64)?-mingw\d+-(?<Version_64>[\d\.]+?)(-[\d\.]+)?-setup.exe$'
 	$url_64 = $download_page_64.links |? href -match $regex_64 | select -Last 1
 			
     return @{

3d3ef207f69a90eca6e5cd02d50d0bf457fbe87c
chtof <chtof62@gmail.com>
2019-08-13 19:40:07 +0200

    Update chocolateyinstall.ps1

--- automatic/hmailserver/tools/chocolateyinstall.ps1
+++ automatic/hmailserver/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
+  packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.hmailserver.com/files/hMailServer-5.6.7-B2425.exe'
   checksum     = '761B20B8B077392699F4A3A2C80155587C6109F6CDD600692890DCA9BFE39DD3'

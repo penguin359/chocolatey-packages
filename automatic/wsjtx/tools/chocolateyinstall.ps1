fd6a873dc07f7ea8e54b45ef412075bcd64ebe37
chtof <chtof62@gmail.com>
2019-08-13 23:13:09 +0200

    wsjtx embedded

--- automatic/wsjtx/tools/chocolateyinstall.ps1
+++ automatic/wsjtx/tools/chocolateyinstall.ps1
@@ -3,12 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
-
-  url          = 'https://physics.princeton.edu/pulsar/k1jt/wsjtx-2.1.0-win32.exe'
-  checksum     = 'CF964C8E8DBBD5BD11753B60D71A301A1042CB4E9631E944DFBA9D9D69345EE6'
-  checksumType = 'sha256'
-
+  file         = "$toolsDir\wsjtx-2.1.0-win32.exe"  
   silentArgs   = '/S'
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs

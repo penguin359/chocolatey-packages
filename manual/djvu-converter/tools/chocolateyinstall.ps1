f64dcd2f0b32809f6ccc683f2eca28156c7d2db1
chtof <chtof62@gmail.com>
2019-08-14 20:13:20 +0200

    djvu-converter initial commit

---
+++ manual/djvu-converter/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+
+  url          = 'http://file2.softsea.com/Image_Utility/djvuconverter_setup.exe'
+  checksum     = '600CDFB6A4093429EC792FAA46F5883EB662D9B6E674A24CD9D86408950A1122'
+  checksumType = 'sha256'
+
+  silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+}
+
+Install-ChocolateyPackage @packageArgs

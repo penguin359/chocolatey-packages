23c218682b031af299ccbc30960245fbfa40e450
chtof <chtof62@gmail.com>
2019-08-14 20:44:20 +0200

    djvu-libre initial commit

---
+++ automatic/djvu-libre/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  file        = "$toolsDir\DjVuLibre-3.5.27_DjView-4.11_Setup.exe"
+  silentArgs  = "/S"
+}
+
+Install-ChocolateyInstallPackage @packageArgs

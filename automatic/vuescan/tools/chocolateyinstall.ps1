e041deb26a876b49686b3d29610912e924286794
chtof <chtof62@gmail.com>
2019-08-15 15:50:56 +0200

    Update chocolateyinstall.ps1

--- automatic/vuescan/tools/chocolateyinstall.ps1
+++ automatic/vuescan/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
  
   url            = 'https://www.hamrick.com/files/vuex3296.exe'
-  checksum       = '4aab8beeca3857462dccb67c095dc0cb6f4e69f0121f2b22ffd9bc27f52307d9'
+  checksum       = 'E04286E52E5B85827F0DD1AEE5CAC69449CFF2219591287D2C9DE987520F30D9'
   checksumType   = 'sha256'
  
   url64          = 'https://www.hamrick.com/files/vuex6496.exe'
-  checksum64     = '142bacb60e7047a003b6f62e595bc6b06f7e928d44cad17a156755976444f200'
+  checksum64     = '564EE2BF6C8C3BA48782940A297131A00767363DB81F905D2E18E50A1EAC1427'
   checksumType64 = 'sha256'
 }
 

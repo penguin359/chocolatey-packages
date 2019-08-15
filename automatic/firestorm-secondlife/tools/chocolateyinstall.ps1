378cca0dfe40583622a5f111e3736093cbc2b039
chtof <chtof62@gmail.com>
2019-08-15 17:34:37 +0200

    Update chocolateyinstall.ps1

--- automatic/firestorm-secondlife/tools/chocolateyinstall.ps1
+++ automatic/firestorm-secondlife/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
   url            = 'http://downloads.firestormviewer.org/windows/Phoenix-Firestorm-release-6-2-4-57588_Setup.exe'
-  checksum       = 'AA80ECAFD22AB262BBD6A02318B4603FF3D5E27FEA9A61A5B6DDF38DDBE9DDD9'
+  checksum       = '64a1d9bf2663c8c3d5e0c048043491ecaa7a4dff3b2b421d3291b7efe9cbdbe3'
   checksumType   = 'sha256'
 
   url64          = 'http://downloads.firestormviewer.org/windows/Phoenix-Firestorm-releasex64-6-2-4-57588_Setup.exe'
-  checksum64     = 'F41B827191AC2C3DFB131F5397F5E741A37282289A61A37AC8ADC5E47AE07FDD'
+  checksum64     = 'e5cef7b6cee56b4b42cee9509c4bb85f544bcd905acdb5422617d4da67a872ce'
   checksumType64 = 'sha256'
 
   silentArgs     = "/S"

8946a0d532c4e58cbbf41d1aedc40673c516d178
chtof <chtof62@gmail.com>
2019-08-15 14:05:36 +0200

    firestorm deprecated for firestorm-opensim and firestorm-secondlife
    Firestorm now has separate downloads for Second Life and Open Simulator grids.

---
+++ automatic/firestorm-secondlife/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+
+  url            = 'http://downloads.firestormviewer.org/windows/Phoenix-Firestorm-release-6-2-4-57588_Setup.exe'
+  checksum       = 'AA80ECAFD22AB262BBD6A02318B4603FF3D5E27FEA9A61A5B6DDF38DDBE9DDD9'
+  checksumType   = 'sha256'
+
+  url64          = 'http://downloads.firestormviewer.org/windows/Phoenix-Firestorm-releasex64-6-2-4-57588_Setup.exe'
+  checksum64     = 'F41B827191AC2C3DFB131F5397F5E741A37282289A61A37AC8ADC5E47AE07FDD'
+  checksumType64 = 'sha256'
+
+  silentArgs     = "/S"
+}
+
+Install-ChocolateyPackage @packageArgs

bb33f27b7bcce1c14e3ba7686a8772ed34115c60
chtof <chtof62@gmail.com>
2019-08-15 11:42:00 +0200

    poppler initial commit

---
+++ automatic/poppler/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\poppler-0.68.0_x86.7z"  
+}
+
+Get-ChocolateyUnzip @packageArgs

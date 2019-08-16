16cecd972688b64279ff8b82359a4a781ce3cf14
chtof <chtof62@gmail.com>
2019-08-16 17:42:20 +0200

    Update update.ps1

--- automatic/asn1editor/update.ps1
+++ automatic/asn1editor/update.ps1
@@ -1,38 +1,44 @@
 ﻿import-module au
 
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+function global:au_BeforeUpdate {
+    Remove-Item "$PSScriptRoot\tools\*.zip"
+    Invoke-WebRequest -Uri $Latest.URL32 -OutFile "$PSScriptRoot\tools\$($Latest.FileName32)"
+
+    $Latest.ChecksumType32 = 'sha256'
+    $Latest.Checksum32     = Get-RemoteChecksum $Latest.URL32 -Algorithm $Latest.ChecksumType32
+}
 
 function global:au_GetLatest {
-    $releases = 'https://www.sysadmins.lv/projects/asn1editor/default.aspx'
-    $regex    = 'Asn1Editor-v(?<Version>[\d\.]+)'
-    $regex_url = '(?<Url>/dl/\d+.aspx)'
-    
-    $download_page = Invoke-WebRequest -Uri $releases
-    $download_page -match $regex | Out-Null
-    $version = $matches.Version
+    $releases  = 'https://www.pkisolutions.com/tools/asn1editor/'
+    $regex     = '(?<Filename>Asn1Editor-v(?<Version>[\d\.]+)\.zip)'    
+    $regex_url = 'href="(?<Url>.*/download/\d+/)"'
+
+    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+    $version  = $matches.Version
+    $filename = $matches.Filename
 
     $download_page -match $regex_url | Out-Null
 
-    return @{ Version = $version ; URL32 = 'https://www.sysadmins.lv' + $matches.Url }
+    return @{ Version = $version ; FileName32 = $filename ; URL32 = $matches.Url }
 }
 
 function global:au_SearchReplace {
     @{
         "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
         }
 
-        "tools\chocolateyinstall.ps1" = @{
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
-          "Asn1Editor-v([\d\.]+)\\Asn1Editor-x[$]{OSArchitectureWidth}.exe" = "Asn1Editor-v$($Latest.Version)\Asn1Editor-x`${OSArchitectureWidth}.exe"
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName32)`""
+           "(^.*`"Asn1Editor-v)[\d\.]+(.*)$" = "`${1}$($Latest.Version)`${2}"
         }
     }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
+    update -ChecksumFor none -noCheckUrl
 }

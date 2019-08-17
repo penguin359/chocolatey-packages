98e7408ecca52bb9adf00d0d7d82501c81ef6d46
chtof <chtof62@gmail.com>
2019-08-17 12:19:50 +0200

    Create update.ps1

---
+++ automatic/nip2/update.ps1
@@ -0,0 +1,38 @@
+﻿import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {	
+    $github_repository = 'libvips/nip2'
+    $releases = 'https://github.com/' + $github_repository + '/releases'
+    $regex  = 'nip2-(?<Version>[\d\.]*)-setup.zip$'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$url32 = $download_page.links | ? href -match $regex | Select -First 1
+    
+    return @{
+        Version = $matches.Version
+        URL32 = "https://github.com" + $url32.href    
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32).*"      = "`${1}$($Latest.URL32)"
+            "(?i)(x64).*"      = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{			
+            "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+            "(?i)(^\s*fileFullPath\s*=\s*`"[$]toolsDir\\nip2-)[\d\.]+(.*)`""   = "`$1$($Latest.Version)`$2`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

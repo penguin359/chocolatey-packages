a1073d1f518c21b102d2df75fc18e4ec0ca2ccde
chtof <chtof62@gmail.com>
2019-08-17 12:38:12 +0200

    Create update.ps1

---
+++ automatic/bbc-iplayer/update.ps1
@@ -0,0 +1,21 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = Get-RedirectedUrl 'https://downloads-app.iplayer.api.bbc.co.uk/stable/win32-x64'    
+	$regex   = 'BBCiPlayerDownloadsSetup-(?<Version>[\d\.]+)\.exe$'
+    $releases -Match $regex | Out-Null
+	
+    return @{ Version = $matches.Version ; URL32 = $releases }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\bbciplayerdownloads-)[\d\.]+(.*)`"" = "`$1$($Latest.Version)`$2`""             
+        }
+    }
+}
+
+update

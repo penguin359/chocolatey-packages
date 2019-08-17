9d7dc235977508337b847a829e47f482e8b2c0a7
chtof <chtof62@gmail.com>
2019-08-17 12:43:48 +0200

    Create update.ps1

---
+++ automatic/startup-organizer/update.ps1
@@ -0,0 +1,21 @@
+﻿import-module au
+
+function global:au_GetLatest {     
+     $releases = 'https://metaproducts.com/products/startup-organizer'
+	 $regex    = '<h2>Startup Organizer (?<Version>[\d\.]+)</h2>'
+
+     (Invoke-WebRequest -Uri $releases).RawContent -Match $regex | Out-Null
+
+     return @{ Version = $matches.Version ; URL32 = 'https://metaproducts.com/download/sosetup.exe' }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

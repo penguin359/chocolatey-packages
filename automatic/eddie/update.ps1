e70682c741f1b286c391543347914c02a844af4a
chtof <chtof62@gmail.com>
2019-08-17 12:03:36 +0200

    Create update.ps1

---
+++ automatic/eddie/update.ps1
@@ -0,0 +1,24 @@
+﻿import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://airvpn.org/windows/'
+    $regex    = 'data-version="(?<Version>[\d\.]+)"'
+
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null    
+
+    return @{ Version = $matches.Version }
+}
+
+function global:au_SearchReplace {
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

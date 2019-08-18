89b53a91b968d817a9358c13703acac157afdac7
chtof <chtof62@gmail.com>
2019-08-18 10:59:46 +0200

    Update chocolateyinstall.ps1

--- automatic/extraterm.install/tools/chocolateyinstall.ps1
+++ automatic/extraterm.install/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName  
-  file          = "$toolsDir\extraterm-setup-0.42.2.exe"
-  silentArgs	= "/S"
+  packageName = $env:ChocolateyPackageName  
+  file        = "$toolsDir\extraterm-setup-0.43.0.exe"
+  silentArgs  = "/S"
 }
 
 Install-ChocolateyInstallPackage @packageArgs

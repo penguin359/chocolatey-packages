61d0f5b8aeaf23e74655d38b3072d71016d22530
chtof <chtof62@gmail.com>
2019-08-19 20:18:35 +0200

    jami initial commit

---
+++ automatic/jami/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  filetype    = "MSI"
+  file        = "$toolsDir\jami-x64.msi"
+  silentArgs  = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+}
+
+Install-ChocolateyInstallPackage @packageArgs

981c85f87b5413836dd1c0086c2ee26ee255fb80
chtof <chtof62@gmail.com>
2019-08-20 12:34:13 +0200

    Update chocolateyinstall.ps1

--- automatic/umbrello.install/tools/chocolateyinstall.ps1
+++ automatic/umbrello.install/tools/chocolateyinstall.ps1
@@ -2,10 +2,19 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\umbrello-mingw32-2.28.3-22.1-setup.exe"
-  file64      = "$toolsDir\umbrello-mingw64-2.28.3-95.1-setup.exe"
-  silentArgs  = "/S"
+  packageName  = $env:ChocolateyPackageName
+
+  url          = 'https://download.kde.org/stable/umbrello/latest/win32/umbrello-mingw32-2.29.0-23.1-setup.exe'
+  checksum     = 'ffb5834855436ffd86751b70921d0ae797b790dea42f677634f3bab31b970e0b'
+  checksumType = 'sha256'
+
+  file64       = "$toolsDir\umbrello-mingw64-2.29.0-96.1-setup.exe"
+
+  silentArgs   = "/S"
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+if Get-OSArchitectureWidth -Compare 32 {
+  Install-ChocolateyPackage @packageArgs
+} else {
+  Install-ChocolateyInstallPackage @packageArgs
+}

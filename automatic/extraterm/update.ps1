d18e9f1c31490347276d02b7e5e48598b81c0a16
chtof <chtof62@gmail.com>
2019-08-20 12:36:29 +0200

    Update update.ps1

--- automatic/extraterm/update.ps1
+++ automatic/extraterm/update.ps1
@@ -1,19 +1,17 @@
 ﻿import-module au
 
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
-
 function global:au_GetLatest {
-    $github_repository = "sedwards2009/extraterm"
-    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $github_repository = 'sedwards2009/extraterm'
+    $releases = 'https://github.com/' + $github_repository + '/releases/latest'
     $regex   = 'extraterm-setup-(?<Version>[\d\.]*).exe$'
 
     (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Select -First 1 | Out-Null
 
-    return @{ Version = $matches.Version + "-pre" }
+    return @{ Version = $matches.Version + '-pre' }
 }
 
 function global:au_SearchReplace {
-   @{
+    @{
         "$($Latest.PackageName).nuspec" = @{
             "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
         }

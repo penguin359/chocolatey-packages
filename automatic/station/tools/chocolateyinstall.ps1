5487811fdeece3efa7d58c44f26d20cc8d2ecbdd
Chocolatey <chocolatey@realdimensions.net>
2019-08-20 10:38:24 +0000

    AU: 1 updated - station [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/7a2d88d3027a68c32c7a53e3ce8a596a4826e28c

--- automatic/station/tools/chocolateyinstall.ps1
+++ automatic/station/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://github.com/getstation/desktop-app-releases/releases/download/1.45.2/browserX-Setup-1.45.2.exe'
-  checksum      = '9f1c375d0237764b5a59d066e01122c2480fb6abdbdbd51f5996d3b61f78339c'
+  url           = 'https://github.com/getstation/desktop-app-releases/releases/download/1.45.3/browserX-Setup-1.45.3.exe'
+  checksum      = 'e294bd36ff4ba07ff525c63c19d2ebe317371c2489cbbae0eabae628df23d866'
   checksumType  = 'sha256'
 }
 

b7b7c71c8276960e5cb0a766c23bd7e4da68b60d
chtof <chtof62@gmail.com>
2019-08-21 18:15:00 +0200

    Update update.ps1

--- automatic/lossless-cut/update.ps1
+++ automatic/lossless-cut/update.ps1
@@ -3,15 +3,15 @@
 function global:au_GetLatest {
     $github_repository = "mifi/lossless-cut"
     $releases = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex = $github_repository + '/tree/v(?<Version>[\d\.]+)$'
+    $regex    = $github_repository + '/tree/v(?<Version>[\d\.]+)$'
 
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
-	$url = $download_page.links | ? href -match $regex
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$download_page.links | ? href -match $regex | Out-Null
 
     return @{
         Version = $matches.Version
-        URL32   = "https://github.com/mifi/lossless-cut/releases/download/v2.3.0/LosslessCut-win32-ia32.zip"
-        URL64   = "https://github.com/mifi/lossless-cut/releases/download/v2.3.0/LosslessCut-win32-x64.zip"
+        URL32   = 'https://github.com/mifi/lossless-cut/releases/download/v' + $matches.Version +'/LosslessCut-win32-ia32.zip'
+        URL64   = 'https://github.com/mifi/lossless-cut/releases/download/v' + $matches.Version +'/LosslessCut-win32-x64.zip'
     }
 }
 

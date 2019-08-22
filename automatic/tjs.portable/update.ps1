eba4cf97e0d1424b34f4abe1cf0afdac58b8100f
chtof <chtof62@gmail.com>
2019-08-22 15:30:06 +0200

    Create update.ps1

---
+++ automatic/tjs.portable/update.ps1
@@ -0,0 +1,25 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://community.jaspersoft.com/project/jaspersoft-studio/releases'
+    $regex    = 'TIB_js-studiocomm_(?<Version>[\d\.]+)_windows_x86_64.zip'
+
+    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+    $version = $matches.Version
+
+    return @{
+        Version = $version
+        URL64   = 'https://netcologne.dl.sourceforge.net/project/jasperstudio/JaspersoftStudio-' + $version + '/TIB_js-studiocomm_' + $version + '_windows_x86_64.zip'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update -ChecksumFor 64

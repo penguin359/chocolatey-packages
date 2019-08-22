b92523b18c55ae7d23f6c61c2e55e8a976372469
chtof <chtof62@gmail.com>
2019-08-22 15:30:02 +0200

    Create update.ps1

---
+++ automatic/tjs/update.ps1
@@ -0,0 +1,23 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://community.jaspersoft.com/project/jaspersoft-studio/releases'
+    $regex    = 'TIB_js-studiocomm_(?<Version>[\d\.]+)_windows_x86_64.exe'
+
+    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
+    $version = $matches.Version
+
+    return @{
+        Version = $version       
+    }
+}
+
+function global:au_SearchReplace {
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

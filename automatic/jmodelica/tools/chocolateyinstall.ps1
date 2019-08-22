6c3b59650f119112382ff6c9f8d929fda0477aea
chtof <chtof62@gmail.com>
2019-08-22 19:32:42 +0200

    Update chocolateyinstall.ps1

--- automatic/jmodelica/tools/chocolateyinstall.ps1
+++ automatic/jmodelica/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://jmodelica.org/downloads/JModelica.org-2.10.exe'
-  checksum      = 'c790934c761502e0c06f39148fe11ee2341173e680559ef0349df4a0b23fc627'
-  checksumType  = 'sha256'
+  url          = 'https://jmodelica.org/downloads/JModelica.org-2.10.exe'
+  checksum     = 'c790934c761502e0c06f39148fe11ee2341173e680559ef0349df4a0b23fc627'
+  checksumType = 'sha256'
 
-  silentArgs	= "/S"
+  silentArgs   = "/S"
 }
 
 Install-ChocolateyPackage @packageArgs

c2ee34f7a43066bfdb095554809c1d893599493e
chtof <chtof62@gmail.com>
2019-08-22 15:45:34 +0200

    Update chocolateyinstall.ps1

--- automatic/umbrello.portable/tools/chocolateyinstall.ps1
+++ automatic/umbrello.portable/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
 
 # Shim only umbrello.exe
 foreach ( $file in 'dbus-daemon.exe', 'dbus-launch.exe', 'dbus-monitor.exe', 'dbus-send.exe', 'dbus-update-activation-environment.exe', 'drkonqi.exe', 'enchant-lsmod.exe', 'enchant.exe', 'gspawn-win64-helper-console.exe', 'gspawn-win64-helper.exe', 'kbuildsycoca4.exe', 'kcmshell4.exe', 'kconfig_compiler.exe', 'kconf_update.exe', 'kcookiejar4.exe', 'kdbgwin.exe', 'kde4-config.exe', 'kdebugdialog.exe', 'kded4.exe', 'kdeinit4.exe', 'khc_indexbuilder.exe', 'khelpcenter.exe', 'kioslave.exe', 'kio_http_cache_cleaner.exe', 'kjs.exe', 'kjscmd.exe', 'klauncher.exe', 'kmailservice.exe', 'kross.exe', 'ksendbugmail.exe', 'ktelnetservice.exe', 'ktrash.exe', 'kuiserver.exe', 'kwalletd.exe', 'kwinshutdown.exe', 'kwinstartmenu.exe', 'kwrapper4.exe', 'makekdewidgets.exe', 'po2xmi.exe', 'qdbus.exe', 'qdbusviewer.exe', 'update-mime-database.exe', 'xmi2pot.exe' ){
-  New-Item -path $toolsDir\umbrello-2.24.0\bin -name "$file.ignore" -type File -force | Out-Null
+  New-Item -path $toolsDir\umbrello-2.29.0\bin -name "$file.ignore" -type File -force | Out-Null
 }
 
 Get-ChocolateyUnzip @packageArgs
@@ -18,5 +18,5 @@ Get-ChocolateyUnzip @packageArgs
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Umbrello.lnk"
-$targetPath = Join-Path $toolsDir "umbrello-2.24.0\bin\umbrello.exe"
+$targetPath = Join-Path $toolsDir "umbrello-2.29.0\bin\umbrello.exe"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

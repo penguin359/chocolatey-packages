bbe066e4406094f38a0d0c50679fa6b4770e7de5
Chocolatey <chocolatey@realdimensions.net>
2019-08-23 09:45:11 +0000

    AU: 1 updated - clockify [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/3a2deb94763a67dd4e6db800de48919d447c74ec

--- automatic/clockify/tools/chocolateyinstall.ps1
+++ automatic/clockify/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://clockify-resources.s3.eu-central-1.amazonaws.com/downloads/Clockify_Setup.exe'
-  checksum      = '68D2A2B1514D54C1A62689C1372C313E60B1AF84DEC2DC34505A284FB5326A3E'
-  checksumType  = 'sha256'
+  checksum      = ''
+  checksumType  = ''
 
   silentArgs    = "/S"
 }

dbdafa7040804885e285b92f00d97adf39ea874b
chtof <chtof62@gmail.com>
2019-08-23 12:11:25 +0200

    Update update.ps1

--- automatic/clockify/update.ps1
+++ automatic/clockify/update.ps1
@@ -5,7 +5,7 @@ $releases = 'https://www.spotify.com/en/download/windows/'
 
 function global:au_SearchReplace {
   return @{
-    ".\tools\chocolateyInstall.ps1" = @{
+    ".\tools\chocolateyinstall.ps1" = @{
       "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
       "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
       "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"

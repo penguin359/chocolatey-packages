5ef2dbee28d410a8b8f61b3083842e6bf67b9ab3
chtof <chtof62@gmail.com>
2019-08-26 21:34:46 +0200

    Create update.ps1

---
+++ automatic/labplot/update.ps1
@@ -0,0 +1,36 @@
+﻿import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://download.kde.org/stable/labplot/2.6.0/labplot-2.6.0-64bit-setup.exe.mirrorlist'
+    $regex    = 'labplot-(?<Version>[\d\.]+)-64bit-setup.exe'
+
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Select -First 1
+    $version = matches.Version
+
+    return @{
+        Version = $version
+        URL32   = 'http://ftp.gwdg.de/pub/linux/kde/stable/labplot/' + $version + '/labplot-' + $version + '-64bit-setup.exe'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor 64
+}

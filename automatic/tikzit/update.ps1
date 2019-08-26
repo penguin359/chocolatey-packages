7d2a0baa9ad25358873d4c4212494b7600d0feec
chtof <chtof62@gmail.com>
2019-08-26 21:45:56 +0200

    Create update.ps1

---
+++ automatic/tikzit/update.ps1
@@ -0,0 +1,38 @@
+﻿import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://github.com/tikzit/tikzit/releases'
+    $regex    = '/v(?<Version>[\d\.]+)"'
+
+    (Invoke-WebRequest -Uri $releases).Content -match $regex | Select -First 1
+    $version = $matches.Version
+
+    return @{
+        Version = $version
+        URL32   = 'https://github.com/tikzit/tikzit/releases/download/v' + $version + '/tikzit-win.zip'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

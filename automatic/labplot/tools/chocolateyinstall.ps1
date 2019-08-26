f1f0ae2dd2d5291d743819fdb3a0af0e09af3d2a
chtof <chtof62@gmail.com>
2019-08-26 21:34:37 +0200

    Update chocolateyinstall.ps1

--- automatic/labplot/tools/chocolateyinstall.ps1
+++ automatic/labplot/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\labplot-2.6.0-64bit-setup.exe"
+  file64      = "$toolsDir\labplot-2.6.0-64bit-setup.exe"
   silentArgs  = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 

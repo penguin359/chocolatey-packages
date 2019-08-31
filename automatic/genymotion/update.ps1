81760878102fe5ffd7bfac65bec0b38a0f563633
chtof <chtof62@gmail.com>
2019-08-31 22:38:35 +0200

    Create update.ps1

---
+++ automatic/genymotion/update.ps1
@@ -0,0 +1,33 @@
+﻿import-module au
+
+function global:au_BeforeUpdate {
+    $url = 'https://dl.genymotion.com/releases/genymotion-' + $Latest.Version + '/genymotion-' + $Latest.Version + '.exe'
+
+    Invoke-WebRequest -Uri $url -outFile "${env:tmp}\genymotion.exe"
+    $Latest.Checksum32 = checksum -t sha256 "${env:tmp}\genymotion.exe"
+    Remove-Item -Path "${env:tmp}\genymotion.exe"
+    $Latest.URL32 = $url
+}
+
+function global:au_GetLatest {
+    $releases = 'https://www.genymotion.com/product-release-note/desktop'
+    $regex    = '\>Genymotion (?<Version>[\d\.]+) \('
+
+    (Invoke-WebRequest -Uri $releases).Content -match $regex | Out-Null
+    $version = $matches.Version
+
+    return @{
+        Version = $version
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update -noCheckUrl -checksumFor none

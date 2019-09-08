d119cc0e46ace25e1705bd509baa0fc1a0580423
chtof <chtof62@gmail.com>
2019-09-08 22:28:04 +0200

    Create update.ps1

---
+++ automatic/firestorm-opensim/update.ps1
@@ -0,0 +1,32 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://www.firestormviewer.org/windows-for-open-simulator'
+    $regex32  = 'Phoenix-FirestormOS-release-(?<Version>[\d-]+)-\d+_Setup.exe'
+    $regex64  = 'Phoenix-FirestormOS-releasex64-(?<Version>[\d-]+)-\d+_Setup.exe'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing    
+    $url32 = $download_page.links | ? href -match $regex32
+    $url64 = $download_page.links | ? href -match $regex64
+
+    return @{
+        Version = $version -Replace '-','.'
+        URL32   = $url32.href
+        URL64   = $url64.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType)'"
+            "(^(\s)*url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^(\s)*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+        }
+    }
+}
+
+update

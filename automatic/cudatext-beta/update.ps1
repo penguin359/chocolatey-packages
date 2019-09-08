241f7943ace4131b29458211498e77426c720f4a
chtof <chtof62@gmail.com>
2019-09-08 14:57:22 +0200

    Create update.ps1

---
+++ automatic/cudatext-beta/update.ps1
@@ -0,0 +1,36 @@
+﻿import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://sourceforge.net/projects/cudatext/files/beta_version/'
+    $regex    = 'cudatext-win-x64-(?<Version>[\d\.]+).zip'
+
+    (Invoke-WebRequest -Uri $releases).Content -match $regex | Out-Null
+    $version = $matches.Version
+
+    return @{
+        Version = $version
+        URL64   = 'https://iweb.dl.sourceforge.net/project/cudatext/beta_version/cudatext-win-x64-' + $version + '.zip'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"            
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

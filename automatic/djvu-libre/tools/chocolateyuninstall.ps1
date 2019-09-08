0a62b91bb2f66cbffb08f5cf8a0d7943196efcb6
chtof <chtof62@gmail.com>
2019-09-08 21:16:23 +0200

    Create chocolateyuninstall.ps1

---
+++ automatic/djvu-libre/tools/chocolateyuninstall.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = 'Stop';
+
+$pathToRemove = "$(${env:ProgramFiles(x86)})\DjVuLibre"
+
+foreach ($registryPath in 'hklm:\system\currentcontrolset\control\session manager\environment', 'hkcu:\environment') {
+  $p = (Get-Itemproperty -path "$registryPath" -Name Path).Path
+  if ("$p" | Select-String -SimpleMatch "$pathToRemove"){
+    $newPath = ("$p" -split ';' | ForEach-Object { if (-not ($_ -eq "$pathToRemove")) { $_ } } ) -join ';'
+    Set-ItemProperty -path "$registryPath" -Name Path -Value $NewPath
+  }
+}

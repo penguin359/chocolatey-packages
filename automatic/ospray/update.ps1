ef2fc5dc237321ee312b45c6ec4863310687856f
chtof <chtof62@gmail.com>
2019-09-08 14:18:37 +0200

    Create update.ps1

---
+++ automatic/ospray/update.ps1
@@ -0,0 +1,23 @@
+﻿import-module au
+
+function global:au_GetLatest {	
+  $github_repository = 'ospray/ospray'
+  $releases = 'https://github.com/' + $github_repository + '/releases/latest'
+  $regex   = 'ospray-(?<Version>[\d\.]+).msi$'
+
+  (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Out-Null
+
+	return @{
+		Version = $matches.Version
+	}
+}
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

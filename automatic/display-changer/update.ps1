eeb23fc2af184e74abfbc53e1fe3a63301a2c497
chtof <chtof62@gmail.com>
2019-09-08 13:13:20 +0200

    Create update.ps1

---
+++ automatic/display-changer/update.ps1
@@ -0,0 +1,21 @@
+﻿import-module au
+
+function global:au_GetLatest {
+	$releases = 'https://12noon.com/?page_id=80'
+	$regex    = '<p>(?<Version>[\d\.]+):\ '
+
+    (Invoke-WebRequest -Uri $releases).Content -match $regex | Out-Null
+
+    return @{ Version = $matches.Version ; URL32 = 'https://12noon.com/files/dc-setup.exe' }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

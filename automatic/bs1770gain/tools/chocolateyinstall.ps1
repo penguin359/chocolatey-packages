e753f5c1faf6c74f444c13442805b3aaefcc5be8
Chocolatey <chocolatey@realdimensions.net>
2019-09-12 08:23:36 +0000

    AU: 1 updated - bs1770gain [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/cf05fe4acb0adc162dbdc156619e55826c11a937

--- automatic/bs1770gain/tools/chocolateyinstall.ps1
+++ automatic/bs1770gain/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\bs1770gain-0.6.0-win32.7z"
-  file64      = "$toolsDir\bs1770gain-0.6.0-win64.7z"  
+  file        = "$toolsDir\bs1770gain-0.6.1-win32.7z"
+  file64      = "$toolsDir\bs1770gain-0.6.1-win64.7z"  
 }
 
 Get-ChocolateyUnzip @packageArgs

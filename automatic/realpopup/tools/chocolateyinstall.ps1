4c10012cf490ae882f5575474870c2b9355ed64d
chtof <chtof62@gmail.com>
2019-09-14 13:17:41 +0200

    Update chocolateyinstall.ps1

--- automatic/realpopup/tools/chocolateyinstall.ps1
+++ automatic/realpopup/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
+  packageName  = $env:ChocolateyPackageName
   filetype     = 'MSI'
 
-  url           = 'https://www.realpopup.it/files/RealPopupSetup.msi'
-  checksum      = '66491F1F27B48A76FE704CB8EFC9753C7EF715B3067DF6DA3D0012CD4256DDD7'
-  checksumType  = 'sha256'
+  url          = 'https://www.realpopup.it/files/RealPopupSetup.msi'
+  checksum     = '66491F1F27B48A76FE704CB8EFC9753C7EF715B3067DF6DA3D0012CD4256DDD7'
+  checksumType = 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 }
 
 Install-ChocolateyPackage @packageArgs

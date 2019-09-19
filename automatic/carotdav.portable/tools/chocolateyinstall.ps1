9293506823830a44303e4f07f8e1ca733a2a9c01
Chocolatey <chocolatey@realdimensions.net>
2019-09-19 00:23:55 +0000

    AU: 3 updated - carotdav carotdav.portable winlog32 [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/96f814ae25e818f83302cd19cfd8b29efd8d96c6

--- automatic/carotdav.portable/tools/chocolateyinstall.ps1
+++ automatic/carotdav.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\CarotDAV1.15.6.portable.zip"  
+  file        = "$toolsDir\CarotDAV1.15.7.portable.zip"  
 }
 
 Get-ChocolateyUnzip @packageArgs

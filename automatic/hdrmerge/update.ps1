fd79dbeaa52c3d26ca9069a3dcaf10cc0a190b87
chtof <chtof62@gmail.com>
2019-09-19 19:53:10 +0200

    Create update.ps1

---
+++ automatic/hdrmerge/update.ps1
@@ -0,0 +1,41 @@
+import-module au
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $github_repository = "jcelaya/hdrmerge"
+    $releases          = "https://github.com/" + $github_repository + "/releases/latest"
+    $regex32           = 'hdrmerge-setup64-([\d\.]+).exe$'
+    $regex64           = 'hdrmerge-setup-(?<Version>[\d\.]+).exe$'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url32 = $download_page.links | ? href -match $regex32
+    $url64 = $download_page.links | ? href -match $regex64
+
+    return @{
+        Version = $matches.Version
+        URL32 = "https://github.com" + $url32.href
+        URL64 = "https://github.com" + $url64.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

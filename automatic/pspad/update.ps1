0b9749c4178212ee5c2e627e5c1a34ea6403b3bb
chtof <chtof62@gmail.com>
2019-09-20 20:21:23 +0200

    Update update.ps1

--- automatic/pspad/update.ps1
+++ automatic/pspad/update.ps1
@@ -1,20 +1,26 @@
 import-module au
 
-$releases = 'http://www.pspad.com/en/download.php'
-
 function global:au_GetLatest {
-     $download_page = Invoke-WebRequest -Uri $releases
-	 $regex   = 'pspad(\d+)_setup.exe'
-	 $regex_version = 'PSPad - current version (?<Version>[\d\.]+) \('
-	 $url = $download_page.links | ? href -match $regex
-	 $download_page -match $regex_version
-     return @{ Version = $matches.Version ; URL32 = $url.href }
+    $releases = 'http://www.pspad.com/en/download.php'
+    $regex   = 'pspad(\d+)_setup.exe'
+
+    $download_page = Invoke-WebRequest -Uri $releases
+ 
+	$regex_version = 'PSPad - current version (?<Version>[\d\.]+) \('
+	$download_page -match $regex_version | Out-Null
+    $version    = $matches.Version
+    $versionURL = $version -Replace '\.',''
+
+    return @{
+        Version = $version
+        URL32   = 'https://www.pspad.com/files/pspad/pspad' + $versionURL + '_setup.exe'
+    }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }

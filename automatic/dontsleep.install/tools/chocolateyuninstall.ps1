ff2b2b537ed32cb8447e3ddc0fc9e75fa0839986
chtof <chtof62@gmail.com>
2019-09-27 13:00:10 +0200

    Update chocolateyuninstall.ps1

--- automatic/dontsleep.install/tools/chocolateyuninstall.ps1
+++ automatic/dontsleep.install/tools/chocolateyuninstall.ps1
@@ -1,4 +1,5 @@
-﻿$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+﻿$ErrorActionPreference = 'Stop'
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 
 $shortcutFilePath = Join-Path $programs "Dont Sleep.lnk"
 if(Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

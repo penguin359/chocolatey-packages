f0e4bf66bb88b09fb340c46c3d0b8300854a8104
chtof <chtof62@gmail.com>
2019-09-27 13:02:24 +0200

    Update chocolateyuninstall.ps1

--- automatic/dontsleep.portable/tools/chocolateyuninstall.ps1
+++ automatic/dontsleep.portable/tools/chocolateyuninstall.ps1
@@ -1,4 +1,5 @@
-﻿# Install start menu shortcut
+﻿$ErrorActionPreference = 'Stop'
+# Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Dont Sleep.lnk"
 if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

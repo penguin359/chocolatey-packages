a7e16aaa4b34cb968f8d4fa4688f0709a979bd2e
chtof <chtof62@gmail.com>
2019-09-27 20:29:44 +0200

    Update chocolateyinstall.ps1

--- automatic/dos2unix/tools/chocolateyinstall.ps1
+++ automatic/dos2unix/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
-  file          = "$toolsDir\dos2unix-7.4.1-win32-nls.zip"
-  file64        = "$toolsDir\dos2unix-7.4.1-win64-nls.zip"
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\dos2unix-7.4.1-win32-nls.zip"
+  file64      = "$toolsDir\dos2unix-7.4.1-win64-nls.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

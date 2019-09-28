b9d4b31f3213b4e7ee9b75a7524d36ee42216a1a
chtof <chtof62@gmail.com>
2019-09-28 08:53:34 +0200

    Update chocolateyinstall.ps1

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -1,17 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
  
 $params = @{
-  packageName    = "kdevelop"
-  fileType       = "EXE"
-  silentArgs     = "/S"    
-  
-  url           = ''
-  checksum      = ''
-  checksumType  = 'sha256'
-  
-  url64bit      = 'http://download.kde.org/stable/kdevelop/5.4.2/bin/windows/kdevelop-5.4.2-x64-setup.exe'
-  checksum64    = '61bffb73e76fc1754aaa99bea5afee821fd19de481900819f4e0d3c5e902037a'
-  checksumType64= 'sha256'
+  packageName    = ${env:packageName}
+  fileType       = 'EXE'
+  silentArgs     = '/S'   
+
+  url64bit       = 'http://download.kde.org/stable/kdevelop/5.4.2/bin/windows/kdevelop-5.4.2-x64-setup.exe'
+  checksum64     = '61bffb73e76fc1754aaa99bea5afee821fd19de481900819f4e0d3c5e902037a'
+  checksumType64 = 'sha256'
 }
 
 Install-ChocolateyPackage @params

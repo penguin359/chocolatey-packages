e5242b210018527e06df9ddc676379fdf48dfe12
chtof <chtof62@gmail.com>
2019-09-28 08:38:38 +0200

    Update update.ps1

--- automatic/kdevelop.install/update.ps1
+++ automatic/kdevelop.install/update.ps1
@@ -4,22 +4,23 @@ $releases = 'https://www.kdevelop.org/download'
 
 function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	 $regex   = '\.exe$'
-	 $url = $download_page.links | ? href -match $regex | select -First 2	 
-	 $version = $url -split '-|.exe' | select -Last 1 -Skip 3
-	 $version_release = $version -replace '[.]',''	 
-     return @{ Version = $version ; VersionRelease = $version_release ; URL32 = $url.href[1] ; URL64 = $url.href[0] }
+     $regex   = 'kdevelop-(?<Version>[\d\.]+)-x64-setup.exe$'
+     $url     = $download_page.links | ? href -match $regex
+     $version = $matches.Version
+     $versionRelease = $version -Replace "\.", ""
+ 
+     return @{ Version = $version ; VersionRelease = $version_release ; URL64 = $url.href }
 }
 
 function global:au_SearchReplace {
     @{
 		"tools\chocolateyInstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-			"(^(\s)*url64bit\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+			"(^(\s)*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+                        "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
 			"(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }		
     }
 }
 
-update
+update -checksumFor 64

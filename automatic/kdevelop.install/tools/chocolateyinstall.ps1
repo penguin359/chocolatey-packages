53a73d60a07671151ff4f81813301e7e7b683392
Chocolatey <chocolatey@realdimensions.net>
2019-09-28 06:49:50 +0000

    AU: 1 updated - kdevelop.install [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/61f131e47bffc48d21370509de99e720e1e6e691

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,12 +5,12 @@ $params = @{
   fileType       = "EXE"
   silentArgs     = "/S"    
   
-  url           = 'http://download.kde.org/stable/kdevelop/5.3.2/bin/windows/kdevelop-5.3.2-x86-setup.exe'
-  checksum      = '3c0e8d9369cf40a43499f7353cf2920ef678cdfea4dba5d7ef37d2d69c246019'
+  url           = ''
+  checksum      = ''
   checksumType  = 'sha256'
   
-  url64bit      = 'http://download.kde.org/stable/kdevelop/5.3.2/bin/windows/kdevelop-5.3.2-x64-setup.exe'
-  checksum64    = '9306d8651fbd718f46f1beb022d8f9d58ae57eff7cf1f0c3594c21280c9df00f'
+  url64bit      = 'http://download.kde.org/stable/kdevelop/5.4.2/bin/windows/kdevelop-5.4.2-x64-setup.exe'
+  checksum64    = '61bffb73e76fc1754aaa99bea5afee821fd19de481900819f4e0d3c5e902037a'
   checksumType64= 'sha256'
 }
 

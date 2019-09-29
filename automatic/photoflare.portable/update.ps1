b6e8e234a56dc6f37935158ab4b4b77dab0345ad
chtof <chtof62@gmail.com>
2019-09-29 11:18:40 +0200

    Create update.ps1

---
+++ automatic/photoflare.portable/update.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://launchpad.net/photofiltre-lx'
+    $regex    = 'Photoflare_CE_(?<Version>[\d\.]+)_amd64_win_portable.zip'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$url = $download_page.links | ? href -match $regex
+
+    return @{ Version = $matches.Version ; URL64 = $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"            
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+          "([$]toolsDir `"PhotoFlare_CE_)[\d\.]+(_amd64_win_portable\\ce_photoflare.exe`")" = "`${1}$($Latest.Version)`${2}"
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

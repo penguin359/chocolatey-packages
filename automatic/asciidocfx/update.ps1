c25d32fc044b66b8516cede043884209378cf2f3
chtof <chtof62@gmail.com>
2019-09-30 22:38:21 +0200

    Create update.ps1

---
+++ automatic/asciidocfx/update.ps1
@@ -0,0 +1,23 @@
+﻿import-module au
+
+function global:au_GetLatest {
+    $github_repository = "asciidocfx/AsciidocFX"
+    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $regex    = '/asciidocfx/AsciidocFX/tree/v(?<Version>[\d\.]*)\"'
+
+    (Invoke-WebRequest -Uri $releases).Content -match $regex | Out-Null    
+
+    return @{
+        Version = $matches.Version
+    }
+}
+
+function global:au_SearchReplace {
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+        }
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

8ddc8be23a7f3898be51c196a4e5afbbc796087e
chtof <chtof62@gmail.com>
2019-09-30 21:14:04 +0200

    Update update.ps1

--- automatic/luxcorerender-sdk/update.ps1
+++ automatic/luxcorerender-sdk/update.ps1
@@ -21,8 +21,8 @@ function global:au_SearchReplace {
         }
 
         "tools\chocolateyinstall.ps1" = @{
-          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
-          "(Join-Path [$]toolsDir `"luxcorerender-v)[\d\.]+(-win64-sdk\\luxcoreui.exe`")" = "`$1$($Latest.Version)`$2"
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""          
+          "(Join-Path [$]toolsDir `"luxcorerender-v)[\d\.]+(-win64-sdk\\bin\\luxcoreui.exe`")" = "`$1$($Latest.Version)`$2"
           "([$]toolsDir\\luxcorerender-v)[\d\.]+(-win64-sdk)" = "`$1$($Latest.Version)`$2"
         }
     }

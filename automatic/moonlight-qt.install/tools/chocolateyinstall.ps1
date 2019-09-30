4162273c1f27a5601b0da43e938eb962c7dba136
Chocolatey <chocolatey@realdimensions.net>
2019-09-30 16:25:24 +0000

    AU: 1 updated - moonlight-qt.install [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/22097594b4b67c5194a2090d72daa8e5089353f3

--- automatic/moonlight-qt.install/tools/chocolateyinstall.ps1
+++ automatic/moonlight-qt.install/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\MoonlightSetup-x86-1.1.0.exe"
-  file64        = "$toolsDir\MoonlightSetup-x64-1.1.0.exe"
+  file          = "$toolsDir\MoonlightSetup-x86-1.1.1.exe"
+  file64        = "$toolsDir\MoonlightSetup-x64-1.1.1.exe"
   silentArgs	= "/S"
 }
 

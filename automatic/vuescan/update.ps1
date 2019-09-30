88cdd6b44f14571d42e677a56b3209fc59e33ee7
chtof <chtof62@gmail.com>
2019-09-30 23:01:28 +0200

    Update update.ps1

--- automatic/vuescan/update.ps1
+++ automatic/vuescan/update.ps1
@@ -20,7 +20,7 @@ function global:au_GetLatest {
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyInstall.ps1" = @{
+        "tools\chocolateyinstall.ps1" = @{
             "(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
             "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"

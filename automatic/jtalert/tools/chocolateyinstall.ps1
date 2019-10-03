1d335c21204bdd83026d057f6a73603489ad9eed
chtof <chtof62@gmail.com>
2019-10-03 13:01:46 +0200

    Update chocolateyinstall.ps1

--- automatic/jtalert/tools/chocolateyinstall.ps1
+++ automatic/jtalert/tools/chocolateyinstall.ps1
@@ -1,22 +1,10 @@
-﻿if ( [environment]::OSVersion.Version.Major -ge 10 )  {
-  $url_win10       = 'https://dnl.hamapps.com/JTAlert/JTAlert.AL.2.14.4.Setup.exe'
-	$checksum_win10  = 'd4d3f37d0abf5a14492f37fde60b8cea58bcc92483466b5fb2af6a31f32e6981'
-	$url             = $url_win10
-	$checksum        = $checksum_win10
-} else {
-  $url_others      = 'https://dnl.hamapps.com/JTAlert/JTAlert.2.14.4.Setup.exe'
-	$checksum_others = '08483abb8f0fdf81c614550b67a3caecd2e68f654aab447a47c40793036e9053'
-  $url             = $url_others
-  $checksum        = $checksum_others
-}
-  
-$packageArgs = @{
+﻿$packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = $url
-  checksum     = $checksum
+  url          = 'https://dnl.hamapps.com/JTAlert/JTAlert.2.14.4.Setup.exe'
+  checksum     = '08483abb8f0fdf81c614550b67a3caecd2e68f654aab447a47c40793036e9053'
   checksumType = 'sha256'
-  
+
   silentArgs 	= '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components=""'
 }
 

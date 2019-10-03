14385672db167102d045947814e4b2601f7cafdd
chtof <chtof62@gmail.com>
2019-10-03 13:04:02 +0200

    Update update.ps1

--- automatic/manuskript/update.ps1
+++ automatic/manuskript/update.ps1
@@ -6,8 +6,9 @@ function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	 $regex   = $github_repository + '/releases/download/.*/manuskript-([\d\.]*)-win32.zip'
 	 $url = $download_page.links | ? href -match $regex
-     $url.href[0] -match "manuskript-(?<Version>[\d\.]*)-win32.zip"
-     return @{ Version = $matches.Version ; URL32 = $url.href[0] }
+     $url.href[0] -match "manuskript-(?<Version>[\d\.-]*)-win32.zip"
+
+     return @{ Version = $matches.Version -Replace "-", "." ; URL32 = $url.href[0] }
 }
 
 function global:au_SearchReplace {

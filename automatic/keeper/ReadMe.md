19911261d320826b02409b95a0d64449483a3422
chtof <chtof62@gmail.com>
2019-10-04 20:39:01 +0200

    keeper initial commit

---
+++ automatic/keeper/ReadMe.md
@@ -0,0 +1,17 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/keeper/keeper.png" width="48" height="48"/> [Keeper Desktop App](https://chocolatey.org/packages/keeper)
+
+_Cybersecurity starts with password security._
+
+Keeper is the top-rated password manager for protecting you, your family and your business from password-related data breaches and cyberthreats.
+
+With Keeper, you can securely store and autofill unlimited passwords with our free password manager. You’ll never have to remember or worry about passwords again.
+
+Includes:
+
+- Unlimited password storage
+- Autofill Passwords (Mobile Only)
+- Unlimited payment & identity info
+- Fingerprint & Face ID login
+- Access on one device
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/keeper/screenshot.png)
---
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"  
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$toolsDir"
+  fileType      = 'EXE'
+  file          = "$toolsDir\KeeperSetup32.exe"
+  file64        = "$toolsDir\KeeperSetup64.exe"
+
+  url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
+  checksum      = '993B8DC58C5C99293BE82E99A1B36C43E08F6E480F2E866330F91533F2A5BB84'
+  checksumType  = 'sha256'
+
+  url64bit      = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
+  checksum64    = '4C31F8D827EDFAECCA45D2B8D5DDD088641F3B7282724D093A187E225305604A'
+  checksumType64= 'sha256'
+
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs

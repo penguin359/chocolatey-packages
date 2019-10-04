95aca8a11ce8156212e7e5a8913da6094f1ac2dc
chtof <chtof62@gmail.com>
2019-10-04 03:07:37 +0200

    Update update.ps1

--- automatic/manuskript/update.ps1
+++ automatic/manuskript/update.ps1
@@ -4,7 +4,7 @@ $releases = "https://github.com/" + $github_repository + "/releases/latest"
 
 function global:au_GetLatest {	
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	 $regex   = $github_repository + '/releases/download/.*/manuskript-([\d\.]*)-win32.zip'
+	 $regex   = $github_repository + '/releases/download/.*/manuskript-([\d\.-]*)-win32.zip'
 	 $url = $download_page.links | ? href -match $regex
      $url.href[0] -match "manuskript-(?<Version>[\d\.-]*)-win32.zip"
 

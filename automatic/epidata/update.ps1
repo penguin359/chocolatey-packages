a9ddddf3a96d2d1d30970d3680fcd804dde8756a
chtof <chtof62@gmail.com>
2019-10-05 13:21:46 +0200

    Create update.ps1

---
+++ automatic/epidata/update.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+	$releases = 'http://www.epidata.dk/download.php'
+	$regex    = 'setup.all-in-one.(?<Version>[\d\.]+).exe'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$url = $download_page.links | ? href -match $regex
+    $version = $matches.Version
+
+    return @{ Version = $matchesversion ; URL32 = 'http://www.epidata.dk/downloads/setup.all-in-one.' + $version + '.exe' }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

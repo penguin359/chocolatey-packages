7b4e89640719171ae345c53d8271f5feb8c52d43
chtof <chtof62@gmail.com>
2019-10-07 20:49:09 +0200

    Create update.ps1

---
+++ automatic/ausweisapp2/update.ps1
@@ -0,0 +1,38 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://github.com/Governikus/AusweisApp2/releases'
+    $regex   = 'AusweisApp2-(?<Version>[\d\.]+).msi'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$url = $download_page.links | ? href -match $regex | Select -First 1
+    return @{
+        Version = $matches.Version
+        URL32 = 'https://www.ausweisapp.bund.de/uploads/tx_ausweisdownloads/AusweisApp2-' + $version + '.msi'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName32)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

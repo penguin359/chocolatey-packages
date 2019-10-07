c58eb6370844c5372c8f35366f4e40ef710ccc32
Chocolatey <chocolatey@realdimensions.net>
2019-10-07 16:25:16 +0000

    AU: 4 updated - dontsleep dontsleep.install station wireguard [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/a98f80e66c372255ac1987b55f2854a626899681

--- automatic/station/tools/chocolateyinstall.ps1
+++ automatic/station/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://github.com/getstation/desktop-app-releases/releases/download/1.51.1/browserX-Setup-1.51.1.exe'
-  checksum      = '08621db56509a89e9d3d43a6b043e6d8163054573c4a61c62195cead3b82fb9e'
+  url           = 'https://github.com/getstation/desktop-app-releases/releases/download/1.52.1/browserX-Setup-1.52.1.exe'
+  checksum      = 'eee8d296705cfa09636191f612db6ef58297d95a89ff4284f74e7b8ae51b340e'
   checksumType  = 'sha256'
 }
 

8be39a89001d440d304ea58b98cd97402b43de71
chtof <chtof62@gmail.com>
2019-10-10 04:29:47 +0200

    Update chocolateyinstall.ps1

--- automatic/torrent-file-editor/tools/chocolateyinstall.ps1
+++ automatic/torrent-file-editor/tools/chocolateyinstall.ps1
@@ -5,5 +5,5 @@ $OSArchitectureWidth = Get-OSArchitectureWidth
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Torrent File Editor.lnk"
-$targetPath = Join-Path $toolsDir "$10.3.16-x${OSArchitectureWidth}.exe"
+$targetPath = Join-Path $toolsDir "torrent-file-editor-0.3.16-x${OSArchitectureWidth}.exe"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

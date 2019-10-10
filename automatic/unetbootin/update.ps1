7da7fd83ab4443a8d13b3e6858cdd1bd6f308e98
chtof <chtof62@gmail.com>
2019-10-10 04:28:21 +0200

    Update update.ps1

--- automatic/unetbootin/update.ps1
+++ automatic/unetbootin/update.ps1
@@ -1,18 +1,20 @@
 ﻿import-module au
-$github_repository = "unetbootin/unetbootin"
-$releases = "https://github.com/" + $github_repository + "/releases/latest"
 
-function global:au_GetLatest {	
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	 $regex   = $github_repository + '/releases/download/.*/unetbootin-windows-(?<Version>.*).exe'
-	 $url = $download_page.links | ? href -match $regex
-	 $version = $matches.Version -replace '\w','$&.' -replace '\.$', ''
-     return @{ Version = $version ; URL32 = "https://github.com" + $url.href }
+function global:au_GetLatest {
+    $github_repository = "unetbootin/unetbootin"
+    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $regex   = $github_repository + '/releases/download/.*/unetbootin-windows-(?<Version>.*).exe'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$url = $download_page.links | ? href -match $regex
+	$version = $matches.Version -replace '\w','$&.' -replace '\.$', ''
+    
+    return @{ Version = $version ; URL32 = "https://github.com" + $url.href }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyInstall.ps1" = @{
+        "tools\chocolateyinstall.ps1" = @{
 			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }

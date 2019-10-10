a034aa816aa08c0292ee448a0191cc0f07d67117
chtof <chtof62@gmail.com>
2019-10-10 12:59:34 +0200

    Update chocolateyinstall.ps1

--- manual/djvu-converter/tools/chocolateyinstall.ps1
+++ manual/djvu-converter/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = Get-RedirectedUrl http://file2.softsea.com/get.php?id=836900551
+  url          = Get-RedirectedUrl 'http://file2.softsea.com/get.php?id=836900551' 'http://www.softsea.com/download/DjVu-Converter.html'
   checksum     = '600CDFB6A4093429EC792FAA46F5883EB662D9B6E674A24CD9D86408950A1122'
   checksumType = 'sha256'
 

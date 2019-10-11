e33b50992e3de71f372129173f37de4f721b07e2
Chocolatey <chocolatey@realdimensions.net>
2019-10-11 00:24:16 +0000

    AU: 3 updated - gravitdesigner gravitdesigner.install gravitdesigner.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/8f6c72349ae26fd6d8a4bfb89ad24229b35676c4

--- automatic/gravitdesigner.install/tools/chocolateyinstall.ps1
+++ automatic/gravitdesigner.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://designer.gravit.io/_downloads/windows/GravitDesignerInstaller.exe'
-  checksum      = 'ac32a56e0c552277415c80ce1d9e173558b62e21b27a2821f2fdc1a2cfcc9864'
+  checksum      = 'ce4ee48bc4697adaf84f489bcf9a5695ea1430bc8c8679fd31452222913c830e'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"

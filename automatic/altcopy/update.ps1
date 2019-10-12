74cdd3610779e4a163ac5db949cce013ec36f6bf
chtof <chtof62@gmail.com>
2019-10-12 10:55:09 +0200

    Update update.ps1

--- automatic/altcopy/update.ps1
+++ automatic/altcopy/update.ps1
@@ -1,22 +1,16 @@
 import-module au
 
 function global:au_GetLatest {
-    $releases = 'https://altcopy.net'
-    $regex    = '<p id="versionPc">Version: (?<Version>[\d\.]+)</p>'
+    $releases = 'https://altcopy.net/versionPC'
+    $version  = ((Invoke-WebRequest -uri $releases).RawContent.Split([Environment]::NewLine) | Select -Last 1) -Replace '\.000$', ''
 
-    $ie = New-Object -com internetexplorer.application
-    $ie.Visible = $false
-    $ie.Navigate($releases)
-    while ($ie.Busy -eq $true){Start-Sleep -seconds 4;}
-    $ie.Document.body.outerHTML -match '<p id="versionPc">Version: (?<Version>[\d\.]+)</p>' | Out-Null
-
-    return @{ Version = $matches.Version ; URL32 = 'https://altcopy.net/setup.exe' }
+    return @{ Version = $version ; URL32 = 'https://altcopy.net/setup.exe' }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyInstall.ps1" = @{
-            "(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+        "tools\chocolateyinstall.ps1" = @{
+            "(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }

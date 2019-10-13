bf6a6be4d35bdc3baf3aefb457cdd4a272c48c4a
Chocolatey <chocolatey@realdimensions.net>
2019-10-13 00:24:07 +0000

    AU: 2 updated - automouseclick dnscrypt-proxy [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/dc70be6b3e34f285f18d83420f44a03f594937fa

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'A175B0B9EFD2AC3FB2EBF2FA0D787C1B37273B1177042A43B260B82B784E970E'
+  checksum     = '7A00EC0569078729ACC52B303655E79CB9CDF36E1CB29BB7471BA1C97CBE93EA'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"

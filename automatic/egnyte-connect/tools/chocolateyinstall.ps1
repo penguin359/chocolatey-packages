d31b7b39f665c62a32c949bc129ad38db4526519
Chocolatey <chocolatey@realdimensions.net>
2019-10-17 00:24:19 +0000

    AU: 1 updated - egnyte-connect [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/5be187671d5657558dc3a6ab3691974764c22924

--- automatic/egnyte-connect/tools/chocolateyinstall.ps1
+++ automatic/egnyte-connect/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   filetype     = 'MSI'
 
-  url          = 'https://egnyte-cdn.egnyte.com/egnytedrive/win/en-us/3.5.1/EgnyteConnect_3.5.1_103.msi'
-  checksum     = '6d4fd5fdc6fd377a99b684aa2f3be68cd50826e98e2dd8de6b2ecb48131bd5d8'
+  url          = 'https://egnyte-cdn.egnyte.com/egnytedrive/win/en-us/3.5.2/EgnyteConnect_3.5.2_106.msi'
+  checksum     = '059a4efeb200db13b36c53e3cd3e2879be55acd5fa5672a0116711a2754a9740'
   checksumType = 'sha256'  
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

c2c3fbda0fcb908c8f2d0b269fcf4d25f47a664c
Chocolatey <chocolatey@realdimensions.net>
2019-10-17 08:29:09 +0000

    AU: 4 updated - dacia-toolbox voicemeeter voicemeeter.install voicemeeter.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/95cf8290a05a977c240b2b8f51d457ecfde77190

--- automatic/dacia-toolbox/tools/chocolateyinstall.ps1
+++ automatic/dacia-toolbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'http://cdns.distrib.naviextras.com/content/!application/TOOLBOX/Agents/ReDa_ULC_4/2019_09_03_16_20/Dacia/DaciaMediaNavEvolutionToolbox-inst.exe'
-  checksum     = 'b4bca81379d548fc40f1e9a7e34567e8bcdafb817df71e0eccb3abc5d96c5ca2'
+  url          = 'http://cdns.distrib.naviextras.com/content/!application/TOOLBOX/Agents/ReDa_ULC_4/2019_10_15_16_20/Dacia/DaciaMediaNavEvolutionToolbox-inst.exe'
+  checksum     = '9670c184b32940c280e4df30e55f4c62cf57a0ca969491e9f2751efc13bcd89b'
   checksumType = 'sha256'
 
   silentArgs   = "/S"

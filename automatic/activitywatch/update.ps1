18694d18f19c636e9230026fc7ecf9cc0051950e
chtof <chtof62@gmail.com>
2019-10-19 05:18:38 +0200

    Update update.ps1

--- automatic/activitywatch/update.ps1
+++ automatic/activitywatch/update.ps1
@@ -1,17 +1,22 @@
-﻿import-module au
-$github_repository = "ActivityWatch/activitywatch"
-$releases = "https://github.com/" + $github_repository + "/releases/latest"
+import-module au
 
 function global:au_GetLatest {	
+     $github_repository = "ActivityWatch/activitywatch"
+     $releases = "https://github.com/" + $github_repository + "/releases"
+
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	 $regex   = $github_repository + '/releases/download/.*/activitywatch[-_]v(?<Version>.*)-windows-x86_64.zip'
-	 $url = $download_page.links | ? href -match $regex
-     return @{ Version = $matches.Version ; URL32 = "https://github.com" + $url.href }
+	$regex   = $github_repository + '/releases/download/.*/activitywatch[-_]v(?<Version>.*)-windows-x86_64.zip'
+	$url = $download_page.links | ? href -match $regex
+	
+     return @{ 
+       Version = $matches.Version -Replace 'b','.b'
+       URL32 = "https://github.com" + $url.href
+     }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyInstall.ps1" = @{
+        "tools\chocolateyinstall.ps1" = @{
 			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }

5c0f589b5b6df542e36690a657a7d55b6f866f22
Chocolatey <chocolatey@realdimensions.net>
2019-10-20 16:23:54 +0000

    AU: 2 updated - dnscrypt-proxy libre-hardware-monitor [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/0d730966e7d7cb70913e4ec5e78c59ce6178e8ef

--- automatic/dnscrypt-proxy/tools/chocolateyinstall.ps1
+++ automatic/dnscrypt-proxy/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   destination    = "$toolsDir"
-  filefullpath   = "$toolsDir\dnscrypt-proxy-win32-2.0.29-beta.1.zip"
-  filefullpath64 = "$toolsDir\dnscrypt-proxy-win64-2.0.29-beta.1.zip"
+  filefullpath   = "$toolsDir\dnscrypt-proxy-win32-2.0.29-beta.2.zip"
+  filefullpath64 = "$toolsDir\dnscrypt-proxy-win64-2.0.29-beta.2.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

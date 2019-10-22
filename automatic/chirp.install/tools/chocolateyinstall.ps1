113a5e6d9950f6fd317f5659f446b18dd60f7e72
Chocolatey <chocolatey@realdimensions.net>
2019-10-22 16:25:02 +0000

    AU: 6 updated - chirp chirp.install chirp.portable openorienteering-mapper openorienteering-mapper.install realpopup [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/37abed51e0ceed4e305cef296f65069237adf9ca

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-daily-20191018-installer.exe"
+  file          = "$toolsDir\chirp-daily-20191022-installer.exe"
   silentArgs    = '/S'
 }
 
--- automatic/realpopup/tools/chocolateyinstall.ps1
+++ automatic/realpopup/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype     = 'MSI'
 
   url          = 'https://www.realpopup.it/files/RealPopupSetup.msi'
-  checksum     = 'f59e1b562966a8acd18321d05d2976927c981fec8f5f5d9b7ff3b17cc348d87a'
+  checksum     = '7894d114ae1d84355184237ea7a1e06767aa95d9fe2775d68d263e47db04f690'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

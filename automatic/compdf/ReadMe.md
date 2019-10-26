ba4b4c8a1af0f7380340580cc7ca5579e19e2634
chtof <chtof62@gmail.com>
2019-10-26 22:42:14 +0200

    compdf initial commit

---
+++ automatic/compdf/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/compdf/compdf.png" width="48" height="48"/> [ComPDF](https://chocolatey.org/packages/compdf)
+
+It allows to merge PDF files, extract pages from PDF file and give security to PDF files.
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/compdf/screenshot.png)
---
+++ automatic/compdf/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"  
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$toolsDir"
+
+  url           = 'https://www.ne.jp/asahi/foresth/home/cpdf1071.zip'
+  checksum      = '9C91A82106E4A1D7304E5F438C6120670C655FCB6D2907704D89128335CFF85E'
+  checksumType  = 'sha256'  
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "ComPDF.lnk"
+$targetPath = Join-Path $toolsDir "ComPDF.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

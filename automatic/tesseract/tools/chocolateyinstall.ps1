db5417bba50bba219978757c22b9085b971ce1f9
chtof <chtof62@gmail.com>
2019-10-26 20:22:39 +0200

    tesseract initial commit

---
+++ automatic/tesseract/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  file        = "$toolsDir\tesseract-ocr-w32-setup-v5.0.0-alpha.20190708.exe"
+  file64      = "$toolsDir\tesseract-ocr-w64-setup-v5.0.0-alpha.20190708.exe"
+  silentArgs  = '/S'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyPath $env:ProgramFiles\Tesseract-OCR

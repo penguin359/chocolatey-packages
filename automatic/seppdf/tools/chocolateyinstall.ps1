56200e87120f6f68c3dff4efcc76e361cdbb392b
chtof <chtof62@gmail.com>
2019-10-26 22:19:40 +0200

    seppdf initial commit

---
+++ automatic/seppdf/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"  
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$toolsDir" 
+
+  url           = 'http://www.ne.jp/asahi/foresth/home/spdf324.zip'
+  checksum      = '346848D59D6F016F7F06F688F434020064165CA048F787A567EA80E48BA4D340'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "SepPDF.lnk"
+$targetPath = Join-Path $toolsDir "SepPDF.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

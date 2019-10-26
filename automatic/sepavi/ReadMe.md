12068ee768674d08b8d90b6066b7174cdb1f2810
chtof <chtof62@gmail.com>
2019-10-26 23:37:53 +0200

    sepavi initial commit

---
+++ automatic/sepavi/ReadMe.md
@@ -0,0 +1,10 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/sepavi/sepavi.png" width="48" height="48"/> [SepAVI](https://chocolatey.org/packages/sepavi)
+
+By slightly short time, you can divide Avi file into two Avi files.
+
+**Restrictions before registration**
+- "Set File Size and Divide" is not possible.
+- "Merge" is not possible (You can judge whether they can be merged).
+(There is no limitation in the trial period.)
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/sepavi/screenshot.png)
---
+++ automatic/sepavi/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"  
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$toolsDir"
+
+  url           = 'https://www.ne.jp/asahi/foresth/home/savi082.zip'
+  checksum      = '507C140B0140DCDFE92FF7D7CF447D5C19A7743582428DBE273BDA6CB5BE4EFF'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "SepAVI.lnk"
+$targetPath = Join-Path $toolsDir "SepAVI.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

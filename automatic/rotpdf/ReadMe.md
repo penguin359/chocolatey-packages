3bb15faa2cd14fbdb1e517eef981162786ad9f85
chtof <chtof62@gmail.com>
2019-10-26 23:05:43 +0200

    rotpdf initial commit

---
+++ automatic/rotpdf/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/rotpdf/rotpdf.png" width="48" height="48"/> [RotPDF](https://chocolatey.org/packages/rotpdf)
+
+RotPDF allows to spin pages of a PDF file by 90 degrees unit.
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/rotpdf/screenshot.png)
---
+++ automatic/rotpdf/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"  
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$toolsDir"
+
+  url           = 'https://www.ne.jp/asahi/foresth/home/rpdf080.zip'
+  checksum      = 'DE62E51B1333C26459A6CEAF50515839EC0D2F5A61CB280CC92CFC25C0E0DFF7'
+  checksumType  = 'sha256'  
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "RotPDF.lnk"
+$targetPath = Join-Path $toolsDir "RotPDF.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

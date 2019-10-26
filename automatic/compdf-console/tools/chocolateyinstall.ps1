90b7001ddbe797a5e2c763f8f1a4c0890276e989
chtof <chtof62@gmail.com>
2019-10-26 22:56:16 +0200

    Update chocolateyinstall.ps1

--- automatic/compdf-console/tools/chocolateyinstall.ps1
+++ automatic/compdf-console/tools/chocolateyinstall.ps1
@@ -5,9 +5,15 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$toolsDir"
 
-  url           = 'http://www.ne.jp/asahi/foresth/home/cpdfc107.zip'
+  url           = 'https://www.ne.jp/asahi/foresth/home/cpdfc107.zip'
   checksum      = '728EFE0F52D7A724B2EB34E0364C151EE2E5314551CD82721E6CF172D6365C55'
   checksumType  = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
+
+# Don't create shims for other executables
+$files = Get-ChildItem "$toolsDir" -Recurse -Include *.exe -Exclude ComPDFC.exe
+foreach ($file in $files) {
+  New-Item "$file.ignore" -type file -force | Out-Null
+}

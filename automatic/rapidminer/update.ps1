ca92949ba28467f23d6301df36aa830c10bda041
chtof <chtof62@gmail.com>
2019-10-26 14:46:12 +0200

    Create update.ps1

---
+++ automatic/rapidminer/update.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://docs.rapidminer.com/latest/studio/releases'
+    $regex = '<title>(?<Version>[\d\.]+) - RapidMiner Documentation</title>'
+
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex
+
+    return @{
+        Version = $matches.Version
+        URL32   = 'https://releases.rapidminer.com/latest/rapidminer-studio/rapidminer-studio-win32-install.exe?utm_source=nexus&utm_medium=nexus'
+        URL64   = 'https://releases.rapidminer.com/latest/rapidminer-studio/rapidminer-studio-win64-install.exe?utm_source=nexus&utm_medium=nexus'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update

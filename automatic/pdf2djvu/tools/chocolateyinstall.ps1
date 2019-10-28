622c98020dae7ef854cf8960e44cca35afd0ae60
chtof <chtof62@gmail.com>
2019-10-28 21:17:08 +0100

    Update chocolateyinstall.ps1

--- automatic/pdf2djvu/tools/chocolateyinstall.ps1
+++ automatic/pdf2djvu/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
-  file          = "$toolsDir\pdf2djvu-win32-0.9.13.zip"
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\pdf2djvu-win32-0.9.13.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

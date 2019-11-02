443b902746479d7d8e51939bff14ee0b87b1b0e2
Chocolatey <chocolatey@realdimensions.net>
2019-11-02 16:23:50 +0000

    AU: 4 updated - automouseclick dontsleep dontsleep.portable myfamilytree [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/b0ba8ccbe31646ce470d284578333ecdec33b12a

--- automatic/myfamilytree/tools/chocolateyinstall.ps1
+++ automatic/myfamilytree/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   filetype      = 'msi'
 
-  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree9.0.1.0.msi'
-  checksum      = 'b29a94658f196140f6a976ffc6aa195e9aa484ba3e5557ef890c58a8181c2431'
+  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree9.0.2.0.msi'
+  checksum      = 'a2d7fec72ca13452b839360a82ab84529dd2e9a1cb06670d25a3dd45f8411eb9'
   checksumType  = 'sha256'
 
-  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree9.0.1.0x64.msi'
-  checksum64    = 'dbf5ef69687d14bb310979206168717b7bbbf19489367b34b3526c4a3b5563a7'
+  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree9.0.2.0x64.msi'
+  checksum64    = 'e8eeaf5b12c1eebc6d9c2fd97ddd7386566e7a842641e38dc5779e79f6fc18fd'
   checksumType64= 'sha256'
   
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

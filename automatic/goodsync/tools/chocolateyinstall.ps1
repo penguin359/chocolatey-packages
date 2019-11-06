6c33bd29f37920eab90e6e3a122f6ad13b844d7b
chtof <chtof62@gmail.com>
2019-11-06 23:23:49 +0100

    Update chocolateyinstall.ps1

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-v10-Setup.msi'
-  checksum       = '003B25CBF99850E31E481B73714AA4205909A57EBFB400B7052D33157F1004E8'
+  checksum       = 'bbccf909e711f667e98322b68b469eb1a54948f71ddcd329d494e7c26a804df8'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

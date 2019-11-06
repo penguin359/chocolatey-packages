1bf45e41ca6fe786267849c4771c857ba8d0105a
chtof <chtof62@gmail.com>
2019-11-06 23:23:55 +0100

    Update update.ps1

--- automatic/goodsync/update.ps1
+++ automatic/goodsync/update.ps1
@@ -1,14 +1,14 @@
-﻿import-module au
+﻿$ErrorActionPreference = 'Stop'
+import-module au
 
 function global:au_GetLatest {
     $releases     = 'https://www.goodsync.com/download?os=windows'
     $regexVersion = 'GoodSync for Windows v (?<Version>[\d\.]+)\<'
     $regexUrl     = 'GoodSync-v[\d\.]+-Setup.msi$'
 
-    (Invoke-WebRequest -Uri $releases).RawContent -match $regexVersion | Out-Null
+    (Invoke-WebRequest -Uri $releases).Content -match $regexVersion | Out-Null
 	$version = $matches.Version
-
-    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links  | ? href -match $regexUrl
+    $url     = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links  | ? href -match $regexUrl
 
     return @{
         Version = $version

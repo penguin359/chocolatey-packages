b71e891394b2015b927c8e29f5ba27a60710e7c4
Chocolatey <chocolatey@realdimensions.net>
2019-11-09 14:57:28 +0000

    AU: 4 updated - chirp chirp.install chirp.portable sd [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/32e58c5a15412bd45f65cffcf2c4b023490e0b10

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-daily-20191029-installer.exe"
+  file          = "$toolsDir\chirp-daily-20191109-installer.exe"
   silentArgs    = '/S'
 }
 

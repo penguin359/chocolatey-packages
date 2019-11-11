6e11b650ce9620e47df5e8d5b085c28d5d4c5613
chtof <chtof62@gmail.com>
2019-11-11 22:41:05 +0100

    Create update.ps1

---
+++ automatic/gopro-quik/update.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+    $url = Get-RedirectedUrl 'https://gopro.com/downloads/PC/Quik/latest'
+	$regex    = 'GoPro_Quik-WinInstaller-(?<Version>[\d\.]+).exe'
+
+    $url -match $regex | Out-Null
+
+    return @{
+        Version = $matches.Version
+        URL32   = $url
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+
+        "tools\chocolateyinstall.ahk" = @{
+            "GoPro_Quik-WinInstaller-([\d\.]+).exe" = "GoPro_Quik-WinInstaller-`${1}.exe"
+        }
+    }
+}
+
+update

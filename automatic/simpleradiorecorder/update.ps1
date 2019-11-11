b4dc8abcd0f1ca7d341b9ddca4884dc836fab1ab
chtof <chtof62@gmail.com>
2019-11-11 20:41:12 +0100

    Update update.ps1

--- automatic/simpleradiorecorder/update.ps1
+++ automatic/simpleradiorecorder/update.ps1
@@ -3,11 +3,12 @@ import-module au
 
 function global:au_GetLatest {
   $releases = "https://dennisbabkin.com/srr"
-  $regex   = 'ver=(?<Version>[\d\.]*)">Report Errors and Glitches'
+  $regex    = 'ver=(?<Version>[\d\.]*)">Report Errors and Glitches'
 
   (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
   # When the fourth segment is already used, it is recommended to add two zeroes (00) to the end of the version. Then when you need to fix, you just increment that number.
   $versionNbSegment = ($matches.Version.ToCharArray() | Where-Object {$_ -eq '.'} | Measure-Object).Count
+  $version = $matches.Version
   if ($versionNbSegment -eq 3) { $version += "00" }
 
   return @{ Version = $version }

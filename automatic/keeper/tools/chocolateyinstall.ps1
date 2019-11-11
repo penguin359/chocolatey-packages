7c951fe6e776bd6ee0ae34df1bc05c8aee562900
chtof <chtof62@gmail.com>
2019-11-11 20:39:32 +0100

    Update chocolateyinstall.ps1

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -12,7 +12,7 @@ $packageArgs = @{
   checksum      = '993B8DC58C5C99293BE82E99A1B36C43E08F6E480F2E866330F91533F2A5BB84'
   checksumType  = 'sha256'
 
-  url64bit      = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
+  url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
   checksum64    = '4C31F8D827EDFAECCA45D2B8D5DDD088641F3B7282724D093A187E225305604A'
   checksumType64= 'sha256'
 

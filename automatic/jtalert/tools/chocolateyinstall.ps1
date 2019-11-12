4821d16892794b42e0de22aa7e6be24c481d7e93
chtof <chtof62@gmail.com>
2019-11-13 00:36:34 +0100

    Update chocolateyinstall.ps1

--- automatic/jtalert/tools/chocolateyinstall.ps1
+++ automatic/jtalert/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://dnl.hamapps.com/JTAlert/JTAlert.2.15.0.Setup.exe'
-  checksum     = 'ddf1a3a8e37687213ae827a43a6505e53b8d5de243f9635b8b529e102fb8f513'
+  url          = 'https://files.hamapps.com/JTAlert/JTAlert.2.15.2.Setup.exe'
+  checksum     = 'dab26ef9b68cbf2300f726e67a3994ddef4c85ea20ad8139516c0f346d4a4638'
   checksumType = 'sha256'
 
   silentArgs 	= '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components=""'

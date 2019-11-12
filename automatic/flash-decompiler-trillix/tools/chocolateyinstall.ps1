569be7eebfcbed6de52e7a41ae90e4bacad538d3
chtof <chtof62@gmail.com>
2019-11-12 21:28:59 +0100

    Update chocolateyinstall.ps1

--- automatic/flash-decompiler-trillix/tools/chocolateyinstall.ps1
+++ automatic/flash-decompiler-trillix/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   file        = "$toolsDir\flash_decompiler.exe"
-  silentArgs  = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
 Install-ChocolateyInstallPackage @packageArgs

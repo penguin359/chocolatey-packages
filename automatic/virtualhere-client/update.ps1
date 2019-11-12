ad0810ef56bab42fc8cb7ab890f58cf43bc1aa45
chtof <chtof62@gmail.com>
2019-11-12 20:42:34 +0100

    Create update.ps1

---
+++ automatic/virtualhere-client/update.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+	$releases = 'https://virtualhere.com/usb_client_software'
+	$regex    = '\>Version (?<Version>[\d\.]+)\<'
+
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
+
+    return @{
+        Version = $matches.Version
+        URL32   = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui32.exe'
+        URL64   = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui64.exe'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update

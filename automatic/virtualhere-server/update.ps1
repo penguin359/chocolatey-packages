9992cb8856c640290d80d7cb1040fe087d450d54
chtof <chtof62@gmail.com>
2019-11-12 20:37:51 +0100

    Create update.ps1

---
+++ automatic/virtualhere-server/update.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+	$releases = 'https://virtualhere.com/windows_server_software'
+	$regex    = '\>version (?<Version>[\d\._]+) Changes\<'
+
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
+
+    return @{
+        Version = $matches.Version
+        URL32   = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin32.exe'
+        URL64   = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin64.exe'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update

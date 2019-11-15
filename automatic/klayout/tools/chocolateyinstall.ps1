f9c9f957e09d5e846589e46016f32ac4493bc570
Chocolatey <chocolatey@realdimensions.net>
2019-11-15 00:25:34 +0000

    AU: 3 updated - klayout virtualhere-client virtualhere-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/2395909ed93e5fc23633e4e530edce2538c6f469

--- automatic/klayout/tools/chocolateyinstall.ps1
+++ automatic/klayout/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.26-win32-install.exe'
-  checksum      = 'b6f50c97e5a80bb123269bbf594590e28cb85188d7ea32d9dee1523f5a1b7a31'
+  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.26.1-win32-install.exe'
+  checksum      = '1c5889b80cbfa3e4cb82dd5862961519ff39e0190080cf511ad82975e6fbb5c2'
   checksumType  = 'sha256'
 
-  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.26-win64-install.exe'
-  checksum64    = '0a729cd64c6b6a478eb8e725610efe0c664ca923a3dfd1ed35f7dc340bca3929'
+  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.26.1-win64-install.exe'
+  checksum64    = 'f2562ed038e0b0e0e0a92d3df3fc1308e62a2912266e1eb72ac8b7e8d389588f'
   checksumType64= 'sha256'
 
   silentArgs	= "/S"

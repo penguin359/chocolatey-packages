8b875de916da24b812f42ab8797f427c3921b648
chtof <chtof62@gmail.com>
2019-11-16 17:05:35 +0100

    audioflyout initial commit

---
+++ automatic/audioflyout/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/audioflyout/audioflyout.png" width="48" height="48"/> [AudioFlyout](https://chocolatey.org/packages/audioflyout)
+
+Replace the Volume/SMTC UI with a custom one. Only for Windows 10 17763+
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/audioflyout/screenshot.png)
---
+++ automatic/audioflyout/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\AudioFlyout.zip"
+}
+
+if ([System.Environment]::OSVersion.Version.Build -lt 17763) {
+  Write-Warning "Skipping installation because AudioFlyout supports only Windows 10 Build 17763 and later."
+  return
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -Path $packageArgs.file
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "AudioFlyout.lnk"
+$targetPath = Join-Path $toolsDir "ADeltaX\AudioFlyout\AudioFlyout.exe"
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

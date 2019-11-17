acacaefd13b681f7761a68d8c7345170f1e16aa0
Chocolatey <chocolatey@realdimensions.net>
2019-11-17 16:24:57 +0000

    AU: 1 updated - virtualdub2 [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/deae4ce83a9245ca796193d300bbc067005c2ad1

--- automatic/virtualdub2/tools/chocolateyinstall.ps1
+++ automatic/virtualdub2/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = "$env:ChocolateyPackageName"
   destination   = "$toolsDir"    
-  file          = "$toolsDir\VirtualDub2_43803.zip"  
+  file          = "$toolsDir\VirtualDub2_43907.zip"  
 }
 
 # Shims

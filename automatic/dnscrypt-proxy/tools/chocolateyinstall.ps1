608007c6bec61c7735995a8b130f41cea21616e3
Chocolatey <chocolatey@realdimensions.net>
2019-11-18 16:25:37 +0000

    AU: 2 updated - dnscrypt-proxy virtualdub2 [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/6130dbf79f2d4159f6b22f58d15b8ab6efaf6eee

--- automatic/dnscrypt-proxy/tools/chocolateyinstall.ps1
+++ automatic/dnscrypt-proxy/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   destination    = "$toolsDir"
-  filefullpath   = "$toolsDir\dnscrypt-proxy-win32-2.0.32.zip"
-  filefullpath64 = "$toolsDir\dnscrypt-proxy-win64-2.0.32.zip"
+  filefullpath   = "$toolsDir\dnscrypt-proxy-win32-2.0.33.zip"
+  filefullpath64 = "$toolsDir\dnscrypt-proxy-win64-2.0.33.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

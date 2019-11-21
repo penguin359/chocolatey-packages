8389b88365dd7da9e20bf97a677524fb56fd8ec4
Chocolatey <chocolatey@realdimensions.net>
2019-11-21 00:24:52 +0000

    AU: 2 updated - internet-download-manager plantronicshub [skip ci]

--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman635build11.exe?b=1&filename=idman635build11.exe'
-  checksum      = 'ebedca6e4ecaa0228939914e6714bbd7a7abd0b635b69c1f52f41c1f3c3e8c15'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman635build12.exe?b=1&filename=idman635build12.exe'
+  checksum      = '1251534a74070ec56fe4dc869e545e5d45ae10f3aec48238c3e63b3a8e3dee9e'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"

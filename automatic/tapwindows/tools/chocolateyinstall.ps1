e35947d3f6a99c98f77e235ea913cdaa66138874
chtof <chtof62@gmail.com>
2019-11-26 20:06:41 +0100

    Update chocolateyinstall.ps1

--- automatic/tapwindows/tools/chocolateyinstall.ps1
+++ automatic/tapwindows/tools/chocolateyinstall.ps1
@@ -1,14 +1,25 @@
-$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+if ( [environment]::OSVersion.Version.Major -ge 10 )  {
+    $url_win10      = 'https://build.openvpn.net/downloads/releases/tap-windows-9.24.2-I601-Win10.exe'
+    $checksum_win10 = '1782d56568092e8fba575fe7e11b2e86f04518f40a18a4ce594bd1209e0cb547'
+    $url            = $url_win10
+    $checksum       = $checksum_win10
+} else {
+  $url_others     = 'https://build.openvpn.net/downloads/releases/tap-windows-9.24.2-I601-Win7.exe'
+  $checksum_others= '35cfa71fe2952192c13cbbd8a2f3f62a6486af406008e654646ea1d823928d46'
+  $url            = $url_others
+  $checksum       = $checksum_others
+}
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  url           = 'https://build.openvpn.net/downloads/releases/tap-windows-9.21.2.exe'
-
-  checksum      = '645BEE92BA4E9F32DDFDD9F8519DC1B9F9FF0B0A8E87E342F08D39DA77E499A9'
+  url           = "$url"
+  checksum      = "$checksum"
   checksumType  = 'sha256'
-  
-  silentArgs	= "/S"
+
+  silentArgs	= '/S'
 }
 
 Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""

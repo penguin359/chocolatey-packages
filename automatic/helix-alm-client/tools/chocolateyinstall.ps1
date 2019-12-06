d01215b9250985c2abb0c2be7b224c2e5b692532
Chocolatey <chocolatey@realdimensions.net>
2019-12-06 16:26:27 +0000

    AU: 7 updated - chirp chirp.install chirp.portable helix-alm-client internet-download-manager openorienteering-mapper.install openorienteering-mapper.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/56c8f531903b154e2d881aeec268b13b3ac910a5

--- automatic/helix-alm-client/tools/chocolateyinstall.ps1
+++ automatic/helix-alm-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://cdist2.perforce.com/alm/helixalm/r2019.4.0/ttwinclientinstall.exe'
-  checksum     = 'be02a21065a497411d4ad0d2078db918f0da17472fedd5837f4c8cc3263f7f77'
+  url          = 'https://cdist2.perforce.com/alm/helixalm/r2019.5.0/ttwinclientinstall.exe'
+  checksum     = 'd33d35d20b0d61fd0a47f11514926132ee461fcfc09a571ad2cb04c399587f5e'
   checksumType = 'sha256'
 
   silentArgs   = "-i SILENT LAX_VM resource\jre\bin\java.exe"
--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman635build14.exe?b=1&filename=idman635build14.exe'
-  checksum      = '50f4f407727fc5f083ee68f6052bc4480f4db0ce823cb73a27ec41a355821362'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman635build15.exe?b=1&filename=idman635build15.exe'
+  checksum      = 'd7fa961d3a0d081b91c8ed88a5530243d984721ae01741e2dd1ce8c33ae18b92'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"

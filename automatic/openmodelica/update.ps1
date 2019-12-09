3ba7f2b0a718881b29a6d37e8ed0dc085116921f
chtof <chtof62@gmail.com>
2019-12-09 20:56:13 +0100

    Update update.ps1

--- automatic/openmodelica/update.ps1
+++ automatic/openmodelica/update.ps1
@@ -3,12 +3,18 @@ import-module au
 [Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
 function global:au_GetLatest {
-  $releases = 'https://openmodelica.org/download/download-windows'
-  $regex    = "/windows/releases/(?<Version>[\d\.]+).*/$"
+  $releases          = 'https://openmodelica.org/download/download-windows'
+  $regexVersionMajor = '/windows/releases/(?<VersionMajor>[\d\.]+).*/$'
+  $regexVersion      = '<a href="https://build.openmodelica.org/omc/builds/windows/.*>(?<Version>[\d\.]+)<br />'
+  # <a href="https://build.openmodelica.org/omc/builds/windows/releases/1.14/final/"><span style="font-size: 13.3333px;">1.14.0<br />
 
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $url           = $download_page.links | ? href -match $regex | Select -First 1
-  $version       = $matches.Version -replace "/", "."
+
+  $url           = $download_page.links | ? href -match $regexVersionMajor | Select -First 1
+  $versionMajor  = $matches.VersionMajor
+
+  $download_page -match $regexVersion | Select -First 1 | Out-Null
+  $version       = $matches.Version
 
 	return @{
     Version = $version

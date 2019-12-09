af221369be1bdfbd1d30bd2c5f60881d0f3ba1bd
chtof <chtof62@gmail.com>
2019-12-09 21:05:20 +0100

    Update chocolateyinstall.ps1

--- automatic/openmodelica/tools/chocolateyinstall.ps1
+++ automatic/openmodelica/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   checksum      = '0B882076FC8D84BB418EA69CB73BD7A4176C78D7903BEBFB6E61F4C230BB19F2'
   checksumType  = 'sha256'
   
-  url64bit      = 'https://build.openmodelica.org/omc/builds/windows/releases/1.13/2/64bit/OpenModelica-v1.13.2-64bit.exe'
+  url64         = 'https://build.openmodelica.org/omc/builds/windows/releases/1.13/2/64bit/OpenModelica-v1.13.2-64bit.exe'
   checksum64    = 'CF94107E61B5E340D861AD6F4F4951CCB4AD58CAEF4BE43EEFA44F8536E4F5BC'
   checksumType64= 'sha256'
   

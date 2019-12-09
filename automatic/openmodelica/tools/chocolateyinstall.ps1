81c146eab30653fa4351d8048ab937802b354167
Chocolatey <chocolatey@realdimensions.net>
2019-12-09 20:38:13 +0000

    AU: 1 updated - openmodelica [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/91871e6098dd76e9d80f3e14be7100b04fe8ab0e

--- automatic/openmodelica/tools/chocolateyinstall.ps1
+++ automatic/openmodelica/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://build.openmodelica.org/omc/builds/windows/releases/1.13/2/32bit/OpenModelica-v1.13.2-32bit.exe'
-  checksum      = '0B882076FC8D84BB418EA69CB73BD7A4176C78D7903BEBFB6E61F4C230BB19F2'
+  url           = 'https://build.openmodelica.org/omc/builds/windows/releases/1.14/final/32bit/OpenModelica-v1.14.0-32bit.exe'
+  checksum      = '7689bfcb07abd1dbd285ea55bc249b86accfddf9dd56d0b59dfee1c4b36b6cf5'
   checksumType  = 'sha256'
   
-  url64         = 'https://build.openmodelica.org/omc/builds/windows/releases/1.13/2/64bit/OpenModelica-v1.13.2-64bit.exe'
-  checksum64    = 'CF94107E61B5E340D861AD6F4F4951CCB4AD58CAEF4BE43EEFA44F8536E4F5BC'
+  url64         = 'https://build.openmodelica.org/omc/builds/windows/releases/1.14/final/64bit/OpenModelica-v1.14.0-64bit.exe'
+  checksum64    = '998afeab4b6530537c1f5e4821d46756f2c0ca1e806ef94b54bac8624ab216ea'
   checksumType64= 'sha256'
   
   silentArgs	= "/S"

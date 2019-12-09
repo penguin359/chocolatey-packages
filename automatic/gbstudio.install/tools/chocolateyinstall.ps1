ae2bdd522523216ec3112402dbd84ffc164bae00
chtof <chtof62@gmail.com>
2019-12-09 20:57:44 +0100

    Update chocolateyinstall.ps1

--- automatic/gbstudio.install/tools/chocolateyinstall.ps1
+++ automatic/gbstudio.install/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   checksum      = '8DF8AFC82AFF215BE8CE218CD7157950FBB0B80D5C7CA0C88532F765E0146AF5'
   checksumType  = 'sha256'
   
-  url64bit      = 'https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-win32-x64-squirrel-1.1.0.zip'
+  url64         = 'https://github.com/chrismaltby/gb-studio/releases/download/v1.1.0/GB.Studio-win32-x64-squirrel-1.1.0.zip'
   checksum64    = '5C7AD41817685D0AF2E47C6B98CFBE8571C73DC5242495E22F5A8A2DAFF57A06'
   checksumType64= 'sha256'
 }

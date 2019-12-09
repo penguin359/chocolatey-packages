3bb06e0ff9932b83e77fba9c300d6ffa855827ac
chtof <chtof62@gmail.com>
2019-12-09 20:31:48 +0100

    Update update.ps1

--- automatic/gbstudio/update.ps1
+++ automatic/gbstudio/update.ps1
@@ -3,7 +3,7 @@
 function global:au_GetLatest {
     $github_repository = "chrismaltby/gb-studio"
     $releases = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex64  = 'GB.Studio-win32-x64-squirrel-(?<Version>[\d\.]+).zip$'
+    $regex64  = 'GB.Studio-.*64.*-squirrel-(?<Version>[\d\.]+).zip$'
 
     $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links
     $download_page | ? href -match $regex64 | Out-Null
@@ -16,9 +16,9 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
    @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
         }
     }
 }
 
-update -ChecksumFor none -NoCheckUrl
+update

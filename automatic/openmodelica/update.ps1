ef73ccefd11bc6a05565556cad52928997f5cf89
chtof <chtof62@gmail.com>
2019-12-09 20:19:36 +0100

    Update update.ps1

--- automatic/openmodelica/update.ps1
+++ automatic/openmodelica/update.ps1
@@ -3,27 +3,27 @@ import-module au
 [Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
 function global:au_GetLatest {
-  $releases      = 'https://openmodelica.org/download/download-windows'
+  $releases = 'https://openmodelica.org/download/download-windows'
+  $regex    = "/windows/releases/(?<Version>[\d\.]+).*/$"
 
-  $regex = "/windows/releases/(?<Version>[\d\.]+.*)/$"
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $url = $download_page.links | ? href -match $regex | Select -First 1
-  $version = $matches.Version -replace "/", "."
+  $url           = $download_page.links | ? href -match $regex | Select -First 1
+  $version       = $matches.Version -replace "/", "."
 
 	return @{
-    Version = $matches.Version -Replace "/", "."
-    URL32 = $url.href + '32bit/OpenModelica-v' + $version + '-32bit.exe'
-    URL64 = $url.href + '64bit/OpenModelica-v' + $version + '-64bit.exe'
+    Version = $version
+    URL32   = $url.href + '32bit/OpenModelica-v' + $version + '-32bit.exe'
+    URL64   = $url.href + '64bit/OpenModelica-v' + $version + '-64bit.exe'
   }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyInstall.ps1" = @{
-          "(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-          "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-          "(^(\s)*url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
-          "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        "tools\chocolateyinstall.ps1" = @{
+          "(^(\s)*url\s*=\s*)('.*')"        = "`${1}'$($Latest.URL32)'"
+          "(^(\s)*checksum\s*=\s*)('.*')"   = "`${1}'$($Latest.Checksum32)'"
+          "(^(\s)*url64\s*=\s*)('.*')"      = "`${1}'$($Latest.URL64)'"
+          "(^(\s)*checksum64\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum64)'"
         }
     }
 }

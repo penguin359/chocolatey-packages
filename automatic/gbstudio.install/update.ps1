2494b07c940f25a78bb8df2e86a4ac85afc6667f
chtof <chtof62@gmail.com>
2019-12-13 22:16:35 +0100

    Update update.ps1

--- automatic/gbstudio.install/update.ps1
+++ automatic/gbstudio.install/update.ps1
@@ -22,10 +22,11 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-            "(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
             "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"            
+            "(GB Studio-)[\d\.]+( Setup.exe)"   = "`${1}$($Latest.Version)`${2}"
         }
     }
 }

3f06644e9a52b649f17eeb1049e0924ca42f40f2
chtof <chtof62@gmail.com>
2019-12-28 23:04:28 +0100

    Update chocolateyinstall.ps1

--- automatic/gridtracker/tools/chocolateyinstall.ps1
+++ automatic/gridtracker/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://uc396a2b688fc2ed1550ef224389.dl.dropboxusercontent.com/cd/0/get/AvH_fjjURIpEekxIqhHBofeSXGmCbZkxJBynYLbKEXoMjaE2voPMi_vQNTWbCMuOHPtodM67WT7c-Yrv9SrV5qjGHYqNRr_Q6Eyl_-dbFb98hezoazpPgy51eosEhpokJuE/file?dl=1#'
-  checksum     = ''
+  checksum     = '036eeb2c873f4ae621a3ce3270d3e7ec30ea282883219b4f3d155000536433e6'
   checksumType = 'sha256'
 
   silentArgs   = '/S'

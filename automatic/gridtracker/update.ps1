c30721ebe63838ad8e6bb25a16ebede6b132e13b
chtof <chtof62@gmail.com>
2019-12-28 23:03:49 +0100

    Create update.ps1

---
+++ automatic/gridtracker/update.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+	$releases = 'https://tagloomis.com/downloads'
+	$regex    = 'GridTracker-Installer.(?<Version>[\d\.]+).exe\?dl=1'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$url = $download_page.links | ? href -match $regex | select -Last 1
+
+    return @{
+        Version = $matches.Version
+        URL32   = Get-RedirectedUrl $url.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

66c969382555c91bf1850eac628c4d652853abce
chtof <chtof62@gmail.com>
2020-01-01 17:39:05 +0100

    Update chocolateyuninstall.ps1

--- automatic/openssl/tools/chocolateyuninstall.ps1
+++ automatic/openssl/tools/chocolateyuninstall.ps1
@@ -1,1 +1,12 @@
 ﻿Uninstall-ChocolateyEnvironmentVariable -VariableName OPENSSL_CONF
+
+$path = Get-AppInstallLocation OpenSSL-Win
+$pathToRemove = "$path\bin\"
+ 
+foreach ($registryPath in 'hklm:\system\currentcontrolset\control\session manager\environment', 'hkcu:\environment') {
+  $p = (Get-Itemproperty -path "$registryPath" -Name Path).Path
+  if ("$p" | Select-String -SimpleMatch "$pathToRemove"){
+    $newPath = ("$p" -split ';' | ForEach-Object { if (-not ($_ -eq "$pathToRemove")) { $_ } } ) -join ';'
+    Set-ItemProperty -path "$registryPath" -Name Path -Value $NewPath
+  }
+}

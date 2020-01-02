41e12197263119c8b7b145bf9e3fed439efabf03
chtof <chtof62@gmail.com>
2020-01-02 22:04:39 +0100

    Update chocolateyinstall.ps1

--- automatic/afedteated/tools/chocolateyinstall.ps1
+++ automatic/afedteated/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
+  packageName  = $env:ChocolateyPackageName
 
-  url           = 'http://update.iobit.com/dl/advanced-systemcare-setup.exe'
-  checksum      = '537fe6eca918cb4fc21b60f659428c728627c2da88541977b169ebda7a7ab9faeacee93f131610428bda13e023319f54fc26ddf4da9dc90362ade8f41af92594'
-  checksumType  = 'sha512'
+  url          = 'http://update.iobit.com/dl/advanced-systemcare-setup.exe'
+  checksum     = '537fe6eca918cb4fc21b60f659428c728627c2da88541977b169ebda7a7ab9faeacee93f131610428bda13e023319f54fc26ddf4da9dc90362ade8f41af92594'
+  checksumType = 'sha512'
 
-  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
 Install-ChocolateyPackage @packageArgs

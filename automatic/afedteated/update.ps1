819a481c6980b92bc7ff4a4660f3c43a00d95393
chtof <chtof62@gmail.com>
2020-01-02 21:23:59 +0100

    Update update.ps1

--- automatic/afedteated/update.ps1
+++ automatic/afedteated/update.ps1
@@ -1,16 +1,21 @@
-﻿import-module au
+﻿Import-Module au
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 
 function global:au_GetLatest {
-    $releases = 'https://www.iobit.com/en/advancedsystemcarefree.php'
-    $regex    = '<span>V (?<Version>[\d\.]+)</span>'
-    
-    $download_page = Invoke-WebRequest -Uri $releases
-	$url = $download_page.RawContent -match $regex
+    $downloadUrl = 'http://update.iobit.com/dl/advanced-systemcare-setup.exe'
+    $etag = GetETagIfChanged -uri $downloadUrl    
 
-    return @{
-        Version = $matches.Version
-        URL32   = 'http://update.iobit.com/dl/advanced-systemcare-setup.exe'
+    if ($etag) {        
+        $result = GetResultInformation $downloadUrl
+        $result["ETAG"] = $etag
     }
+    else {        
+        $result = @{
+            URL32   = $downloadUrl
+            Version = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' } | select -Last 1
+        }
+    }
+    return $result
 }
 
 function global:au_SearchReplace {
@@ -22,4 +27,38 @@ function global:au_SearchReplace {
     }
 }
 
-update
+function global:au_AfterUpdate {
+  "$($Latest.ETAG)|$($Latest.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
+}
+
+function GetResultInformation([string]$url32) {
+  $dest = "$env:TEMP\advanced-systemcare-setup.exe"
+  Get-WebFile $url32 $dest | Out-Null
+
+  $result = @{
+    URL32          = $url32
+    Version        = (Get-Item $dest).VersionInfo.FileVersion.Trim()
+    Checksum       = Get-FileHash $dest -Algorithm SHA512 | % Hash
+    ChecksumType32 = 'sha512'
+  }
+  Remove-Item -Force $dest
+  return $result
+}
+
+function GetETagIfChanged() {
+  param([string]$uri)
+  if (($global:au_Force -ne $true) -and (Test-Path $PSScriptRoot\info)) {
+    $existingETag = $etag = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' } | select -first 1
+  }
+  else { $existingETag = $null }
+
+  $etag = Invoke-WebRequest -Method Head -Uri $uri -UseBasicParsing
+  $etag = $etag | % { $_.Headers.ETag }
+  if ($etag -eq $existingETag) { return $null }
+
+  return $etag
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

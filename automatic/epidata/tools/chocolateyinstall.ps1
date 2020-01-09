b34f9e18b53fd81f130dc0cfd12065e5ff0fdc52
Chocolatey <chocolatey@realdimensions.net>
2020-01-09 00:24:19 +0000

    AU: 1 updated - epidata [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/4e5cf83600789dda31e1fc349f0c39b45ca3c58c

--- automatic/epidata/tools/chocolateyinstall.ps1
+++ automatic/epidata/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'http://www.epidata.dk/downloads/setup.all-in-one.4.6.0.0.exe'
-  checksum     = '781FD9A5474662B1D2CD013E8ACFB7836DD49CD7557A29711386B1F7FE6EE9FC'
+  url          = 'http://www.epidata.dk/downloads/setup.all-in-one.4.6.0.2.exe'
+  checksum     = 'f69bd8616eed4de89acb789b5bee6bcc2c5fc8f961ea02d0361926dba044afc5'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"

f8ad8e7070465ebfca1872e7e28d43dcc0699bdf
Chocolatey <chocolatey@realdimensions.net>
2020-01-09 16:26:05 +0000

    AU: 2 updated - sonarlint-vs2017 station [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/c8f9d1a3c80ffeccb15c9922bccbdd3f452ce78c

--- automatic/sonarlint-vs2017/tools/chocolateyinstall.ps1
+++ automatic/sonarlint-vs2017/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$file = "$toolsDir\SonarLint.VSIX-4.14.1.14005-2017.vsix"
+$file = "$toolsDir\SonarLint.VSIX-4.15.0.14069-2017.vsix"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
--- automatic/station/tools/chocolateyinstall.ps1
+++ automatic/station/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://github.com/getstation/desktop-app-releases/releases/download/1.63.1/browserX-Setup-1.63.1.exe'
-  checksum      = '82aec7f9e946198026e191098a8578dc35656b7222e467ce8022bb08eaa5a735'
+  url           = 'https://github.com/getstation/desktop-app-releases/releases/download/1.63.3/browserX-Setup-1.63.3.exe'
+  checksum      = 'e35694b42ff8a604e94d77b4fbd80aed0bd402591688f588c3825f4c14e860a3'
   checksumType  = 'sha256'
 }
 

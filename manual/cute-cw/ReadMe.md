2e274637d4354eea068f09703e166da8fcca171b
chtof <chtof62@gmail.com>
2020-01-10 20:20:42 +0100

    cute-cw initial commit

---
+++ manual/cute-cw/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/manual/cute-cw/cute-cw.png" width="48" height="48"/> [Cute CW](https://chocolatey.org/packages/cute-cw)
+
+Learning morse code is hard; this software breaks it into phases to make learning easier.
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/manual/cute-cw/screenshot.png)
---
+++ manual/cute-cw/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\cutecw-1.0.zip"
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -Path $packageArgs.file
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs "Cute CW.lnk"
+$targetPath = Join-Path $toolsDir "cutecw-1.0\cutecw.exe"
+Install-ChocolateyShortcut -iconLocation "$toolsDir\cute-cw.ico" -shortcutFilePath $shortcutFilePath -targetPath $targetPath

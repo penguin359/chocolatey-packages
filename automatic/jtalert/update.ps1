a62608343fbd4932313f9d6f4fd9cf26880e0c65
chtof <chtof62@gmail.com>
2020-01-13 21:05:57 +0100

    Update update.ps1

--- automatic/jtalert/update.ps1
+++ automatic/jtalert/update.ps1
@@ -13,11 +13,16 @@ function global:au_GetLatest {
     return @{ Version = $matches.Version ; URL32 = $url.href }
 }
 
-function global:au_SearchReplace {    
+function global:au_SearchReplace {
+    #$Checksum32_win10 = Get-RemoteChecksum "$($Latest.URL32_win10)"
     @{
-        "tools\chocolateyinstall.ps1" = @{
-            "(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        "tools\chocolateyInstall.ps1" = @{
+            #"(^(\s)*\`$url_others\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*\`$url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            #"(^(\s)*\`$checksum_others\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*\`$checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            #"(^(\s)*\`$url_win10\s*=\s*)('.*')" = "`$1'$($Latest.URL32_win10)'"
+            #"(^(\s)*\`$checksum_win10\s*=\s*)('.*')" = "`$1'$($Checksum32_win10)'"
         }
     }
 }

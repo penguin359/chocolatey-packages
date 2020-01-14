1211d829a56b95167093ab26871924fcbd757427
chtof <chtof62@gmail.com>
2020-01-14 12:57:42 +0100

    Create update.ps1

---
+++ automatic/pspad.portable/update.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+    $releases = 'http://www.pspad.com/en/download.php'
+    $regex_version = 'PSPad - current version (?<Version>[\d\.]+) \('
+
+    $download_page = Invoke-WebRequest -Uri $releases 
+	$download_page -match $regex_version | Out-Null
+    $version    = $matches.Version
+    $versionURL = $version -Replace '\.',''
+
+    return @{
+        Version = $version
+        URL32   = 'https://www.pspad.com/files/pspad/pspad' + $versionURL + 'en.zip'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

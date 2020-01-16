40a6af305f6a569bb7475010fd93c6f8412fbcdd
chtof <chtof62@gmail.com>
2020-01-16 19:49:52 +0100

    Update update.ps1

--- automatic/basilisk.portable/update.ps1
+++ automatic/basilisk.portable/update.ps1
@@ -1,16 +1,22 @@
 ﻿import-module au
 
 function global:au_GetLatest {
-    $releases = "https://github.com/MoonchildProductions/UXP/releases"
-    $regex    = '\>Basilisk (?<Version>[\d\.]+)'
+    $releases = 'http://archive.palemoon.org/basilisk/'
+    $regex32  = 'basilisk-(?<FullVersion32>[\d]+[\d]{6}).win32.zip'
+    $regex64  = 'basilisk-(?<FullVersion64>(?<Version>[\d]+)[\d]{6}).win64.zip'
+    
+    $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links
+    $url32 = $download_page |? href -match $regex32 | Select -Last 1
+    $fullVersion32 = $matches.FullVersion32
+    $url64 = $download_page |? href -match $regex64 | Select -Last 1
+    $fullVersion64 = $matches.FullVersion64
+    $version     = $matches.Version -replace '([\d]+)([\d]{2})([\d]{2})','$1.$2.$3'
 
-    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | out-null
-
-	return @{
-        Version = $matches.Version
-        URL32   = 'https://eu.basilisk-browser.org/release/basilisk-latest.win32.zip'
-        URL64   = 'https://eu.basilisk-browser.org/release/basilisk-latest.win64.zip'
-        }
+    return @{
+        Version = $version
+        URL32   = $releases + $url32.href
+        URL64   = $releases + $url64.href
+    }
 }
 
 function global:au_SearchReplace {

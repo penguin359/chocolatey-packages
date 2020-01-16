ab43a876edbeb2e497dde243baf93eb9c9add9db
chtof <chtof62@gmail.com>
2020-01-16 19:48:41 +0100

    Update update.ps1

--- automatic/basilisk/update.ps1
+++ automatic/basilisk/update.ps1
@@ -1,13 +1,14 @@
 ﻿import-module au
 
 function global:au_GetLatest {
-    $releases = 'https://github.com/MoonchildProductions/UXP/releases'
-    $regex    = '\>Basilisk (?<Version>[\d\.]+)'
-    
-    (Invoke-WebRequest -Uri $releases).RawContent -match $regex |`Out-Null
+    $releases = 'http://archive.palemoon.org/basilisk/'    
+    $regex64  = 'basilisk-(?<Version>[\d]+)[\d]{6}.win64.installer.exe'
+
+    $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links
+    $download_page |? href -match $regex64 | Select -Last 1 | Out-Null        
 
     return @{
-        Version = $matches.Version
+        Version = $matches.Version -replace '([\d]+)([\d]{2})([\d]{2})','$1.$2.$3'    
     }
 }
 

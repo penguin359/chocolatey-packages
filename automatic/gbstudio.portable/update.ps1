4cef7e5cdb0107b17b924f409220208129c9bf6e
chtof <chtof62@gmail.com>
2020-01-17 21:15:55 +0100

    Update update.ps1

--- automatic/gbstudio.portable/update.ps1
+++ automatic/gbstudio.portable/update.ps1
@@ -3,9 +3,9 @@
 function global:au_GetLatest {
     $github_repository = "chrismaltby/gb-studio"
     $releases          = "https://github.com/" + $github_repository + "/releases/latest"
-    
-    $regex32  = 'GB.Studio-.*32[^-]*-([\d\.]+).zip$'
-    $regex64  = 'GB.Studio-.*64[^-]*-(?<Version>[\d\.]+).zip$'
+
+    $regex32  = 'GB.Studio-.*32.*-standalone-([\d\.]+).zip$'
+    $regex64  = 'GB.Studio-.*64.*-standalone-(?<Version>[\d\.]+).zip$'
 
     $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links
     $url32         = $download_page | ? href -match $regex32
@@ -13,16 +13,16 @@ function global:au_GetLatest {
 
     return @{
         Version = $matches.Version
-        URL32   = 'https://github.com/' + $url32.href
-        URL64   = 'https://github.com/' + $url64.href
+        URL32   = 'https://github.com' + $url32.href
+        URL64   = 'https://github.com' + $url64.href
     }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
             "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }

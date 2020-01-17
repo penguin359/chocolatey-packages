0ae0b6159df27262b03ca3d670abfce80dcd3a36
chtof <chtof62@gmail.com>
2020-01-17 21:05:11 +0100

    Update update.ps1

--- automatic/log4om.portable/update.ps1
+++ automatic/log4om.portable/update.ps1
@@ -1,19 +1,21 @@
 import-module au
 
 function global:au_GetLatest {
-    $releases = 'http://www.log4om.com/dl/'
-    $regex    = 'Log4OM_(?<Version>[\d_]+)_Portable.zip'
+    $releases = 'https://www.log4om.com/download/'
+    $regex    = 'Log4OM\d+_(?<Version>[\d_]+)_Portable.zip'
 
-    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | select -First 1 | Out-Null
+    $url     = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links |? href -match $regex | select -First 1
     $version = $matches.Version -Replace "_", "."
 	
-	return @{ Version = $version ; URL32 = 'http://www.log4om.com/log4om/release/Log4OM_' + $matches.Version + '_Portable.zip' }
+	return @{
+        Version = $version
+        URL32   = $url.href
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-			"(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+			"(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }

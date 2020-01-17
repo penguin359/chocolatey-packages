e820fd9ad70c62961c1e9558c858294ad496269e
chtof <chtof62@gmail.com>
2020-01-17 20:49:45 +0100

    Update update.ps1

--- automatic/jtalert/update.ps1
+++ automatic/jtalert/update.ps1
@@ -6,8 +6,8 @@ function global:au_GetLatest {
     $regex       = '(HamApps_)?JTAlert[_\.](?<Version>.*?)[_\.]Setup.exe">Download'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	#$url_win10     = $download_page.links | ? outerHTML -match $regex_win10 | select -Last 1	
-	
+	#$url_win10     = $download_page.links | ? outerHTML -match $regex_win10 | select -Last 1
+
     $url = $download_page.links | ? outerHTML -match $regex | select -First 1
 
     return @{ Version = $matches.Version ; URL32 = $url.href }
@@ -16,11 +16,11 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     #$Checksum32_win10 = Get-RemoteChecksum "$($Latest.URL32_win10)"
     @{
-        "tools\chocolateyInstall.ps1" = @{
+        "tools\chocolateyinstall.ps1" = @{
             #"(^(\s)*\`$url_others\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(^(\s)*\`$url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             #"(^(\s)*\`$checksum_others\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^(\s)*\`$checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             #"(^(\s)*\`$url_win10\s*=\s*)('.*')" = "`$1'$($Latest.URL32_win10)'"
             #"(^(\s)*\`$checksum_win10\s*=\s*)('.*')" = "`$1'$($Checksum32_win10)'"
         }

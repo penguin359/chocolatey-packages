9cb6ddae065d6babede65f377ee0fda1d01614e1
chtof <chtof62@gmail.com>
2020-01-17 21:05:13 +0100

    Update update.ps1

--- automatic/log4om/update.ps1
+++ automatic/log4om/update.ps1
@@ -1,15 +1,4 @@
-import-module au
-
-$releases = 'http://www.log4om.com/dl/'
-
-function global:au_GetLatest {
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	 $regex   = '\.zip$'
-	 $regex_version = "Actual Version is (.*?)<"
-	 $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
-	 $download_page -match $regex_version
-	 return @{ Version = $matches[1]}
-}
+. $PSScriptRoot\..\cpu-z.install\update.ps1
 
 function global:au_SearchReplace {
    @{

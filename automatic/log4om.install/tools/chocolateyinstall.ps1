c185bbe04f58cfaadade9608fce102c80dbd8a83
chtof <chtof62@gmail.com>
2020-01-17 22:34:04 +0100

    Update chocolateyinstall.ps1

--- automatic/log4om.install/tools/chocolateyinstall.ps1
+++ automatic/log4om.install/tools/chocolateyinstall.ps1
@@ -10,7 +10,9 @@ Get-ChocolateyUnzip @packageArgs
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
-  file          = "$toolsDir\Log4OM_.exe"
+  file          = "$toolsDir\Log4OM2_2_0_0_0.exe"
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components=""'
 }
 Install-ChocolateyInstallPackage @packageArgs
+
+Write-Warning 'Users of Log4OM version 1 must export an ADIF file from version 1 and import it into version 2 once a database has been created in version 2 as described in Quick_Start_Guide.pdf.'

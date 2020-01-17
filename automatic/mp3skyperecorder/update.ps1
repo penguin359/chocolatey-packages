bfff59f6097455ecd95e1712ad950a8b5838f8dd
chtof <chtof62@gmail.com>
2020-01-17 20:54:06 +0100

    Update update.ps1

--- automatic/mp3skyperecorder/update.ps1
+++ automatic/mp3skyperecorder/update.ps1
@@ -7,12 +7,12 @@ function global:au_GetLatest {
     $regex    = 'version=(?<Version>[\d\.]+)$'    
     
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url           = $download_page.links | ? href -match $regex | select -First 1
-    $versionUrl    = $matches.Version -Replace '\.', ''
+    $url           = $download_page.links | ? href -match $regex | select -First 1    
+    $version       = $matches.Version
 
     return @{
-        Version = $matches.Version
-        URL32   = $releases + '/old/MP3SkypeRecorderSetup' + $versionUrl + '.msi'
+        Version = $version
+        URL32   = 'http://download.mp3skyperecorder.com/MP3SkypeRecorderSetup.exe?version=' + $version
     }
 }
 

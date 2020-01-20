644ae07f7475935dcaa8651e6facbcc01ad825d7
chtof <chtof62@gmail.com>
2020-01-20 21:48:46 +0100

    Update update.ps1

--- automatic/ospray.portable/update.ps1
+++ automatic/ospray.portable/update.ps1
@@ -5,7 +5,7 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {	
     $github_repository = 'ospray/ospray'
     $releases = 'https://github.com/' + $github_repository + '/releases/latest'
-    $regex   = 'ospray-(?<Version>[\d\.]+).windows.zip$'
+    $regex   = 'ospray-(?<Version>[\d\.]+)(x86_64)?.windows.zip$'
 
 	$url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
 

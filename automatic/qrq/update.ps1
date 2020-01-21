fad41c886a6edabee8d3fa41031ffd3d5b2b27a0
chtof <chtof62@gmail.com>
2020-01-21 20:58:01 +0100

    Create update.ps1

---
+++ automatic/qrq/update.ps1
@@ -0,0 +1,37 @@
+﻿import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://fkurz.net/ham/qrq/'
+    $regex   = 'qrq-(?<Version>[\d\._]+).exe'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
+	$url = $download_page.links | ? href -match $regex | Select -Last 1
+
+    return @{
+        Version = $matches.Version
+        URL32 = $releases + $url.href
+    }    
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName32)`""
+          "(`"[$]toolsDir\\dexed-)[\d\.]+(-win.exe)"   = "`$1$($Latest.Version)`$3`""
+        }
+    }
+}
+
+update -ChecksumFor none

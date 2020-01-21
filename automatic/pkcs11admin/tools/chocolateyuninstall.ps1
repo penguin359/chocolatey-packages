1bf6b756af7a096e0091ff664bde1aef08d93cd6
chtof <chtof62@gmail.com>
2020-01-21 20:50:31 +0100

    Update chocolateyuninstall.ps1

--- automatic/pkcs11admin/tools/chocolateyuninstall.ps1
+++ automatic/pkcs11admin/tools/chocolateyuninstall.ps1
@@ -1,12 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 Uninstall-ChocolateyZipPackage $env:ChocolateyPackageName 'Pkcs11Admin-0.5.0.zip'
+$link = "Pkcs11Admin.lnk"
 
 # Remove start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path $programs "Pkcs11Admin.lnk"
+$shortcutFilePath = Join-Path "$programs" "$link"
 if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
 
 # Remove desktop icon
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = "$Env:USERPROFILE\Desktop\Pkcs11Admin.lnk"
+$shortcutFilePath = Join-Path "$Env:USERPROFILE\Desktop" "$link"
 if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

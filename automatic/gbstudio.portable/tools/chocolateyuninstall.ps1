1f9c3a51f822dcde5234ff24725a0766a40d952a
chtof <chtof62@gmail.com>
2020-01-21 20:42:28 +0100

    Update chocolateyuninstall.ps1

--- automatic/gbstudio.portable/tools/chocolateyuninstall.ps1
+++ automatic/gbstudio.portable/tools/chocolateyuninstall.ps1
@@ -1,4 +1,5 @@
-﻿# Remove start menu shortcut
+﻿$ErrorActionPreference = 'Stop'
+# Remove start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "GB Studio.lnk"
 if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

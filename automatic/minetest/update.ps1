419ccf70480504aefc98123898af2ce574259deb
chtof <chtof62@gmail.com>
2020-01-21 20:54:53 +0100

    Create update.ps1

---
+++ automatic/minetest/update.ps1
@@ -0,0 +1,42 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    $releases = 'https://www.minetest.net/downloads/'
+    $regex32  = 'minetest-([\d\._]+)-win32.zip'
+    $regex64  = 'minetest-(?<Version>[\d\._]+)-win64.zip'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
+	$url32 = $download_page.links | ? href -match $regex32
+    $url64 = $download_page.links | ? href -match $regex64
+
+    return @{
+        Version = $matches.Version
+        URL32   = $url64.href
+        URL64   = $url32.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+          "(`"[$]toolsDir\\dexed-)[\d\.]+(-win.exe)"     = "`$1$($Latest.Version)`$3`""
+        }
+    }
+}
+
+update -ChecksumFor none

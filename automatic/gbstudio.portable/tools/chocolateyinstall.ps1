da99c6ea4f1cc452320b88fa54000e7b984c6553
chtof <chtof62@gmail.com>
2020-01-21 20:42:26 +0100

    Update chocolateyinstall.ps1

--- automatic/gbstudio.portable/tools/chocolateyinstall.ps1
+++ automatic/gbstudio.portable/tools/chocolateyinstall.ps1
@@ -5,12 +5,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$toolsDir"
 
-  url           = 'https://github.com//chrismaltby/gb-studio/releases/download/v1.2.0/GB-Studio-Windows-32bit-1.2.0.zip'
-  checksum      = 'cac187d0b0990bb7939c88c934fa0faaa45226e80d30287c3434ea679624f4fc'
+  url           = 'https://github.com/chrismaltby/gb-studio/releases/download/v1.2.1/GB-Studio-Windows-32bit-standalone-1.2.1.zip'
+  checksum      = 'A021996F200385171978A2447CFBB3C432904A660625B7B78B8D2CEFDD765C1F'
   checksumType  = 'sha256'
 
-  url64         = 'https://github.com//chrismaltby/gb-studio/releases/download/v1.2.0/GB-Studio-Windows-64bit-1.2.0.zip'
-  checksum64    = '3121dc299a4dea882e6b463e304c612ff1848106e3e7da37edee3ee8bba57de9'
+  url64         = 'https://github.com/chrismaltby/gb-studio/releases/download/v1.2.1/GB-Studio-Windows-64bit-standalone-1.2.1.zip'
+  checksum64    = 'BF3FA1F3001DF566BD2FC682A3C63B767D6DA15FA8DD8F0F59180DA7A1CAB3A4'
   checksumType64= 'sha256'
 }
 

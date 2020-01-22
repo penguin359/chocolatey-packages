4b36060bb7fb975be6d31d3932e38da012962965
chtof <chtof62@gmail.com>
2020-01-22 21:26:51 +0100

    Update update.ps1

--- automatic/ospray/update.ps1
+++ automatic/ospray/update.ps1
@@ -1,16 +1,4 @@
-﻿import-module au
-
-function global:au_GetLatest {	
-  $github_repository = 'ospray/ospray'
-  $releases = 'https://github.com/' + $github_repository + '/releases/latest'
-  $regex   = 'ospray-(?<Version>[\d\.]+).msi$'
-
-  (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Out-Null
-
-	return @{
-		Version = $matches.Version
-	}
-}
+﻿. $PSScriptRoot\..\ospray.install\update.ps1
 
 function global:au_SearchReplace {
     @{

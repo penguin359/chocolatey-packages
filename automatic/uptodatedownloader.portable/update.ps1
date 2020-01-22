d81d76cc079d46d27fff5aee768b72b7a1ee0396
chtof <chtof62@gmail.com>
2020-01-22 20:59:19 +0100

    Update update.ps1

--- automatic/uptodatedownloader.portable/update.ps1
+++ automatic/uptodatedownloader.portable/update.ps1
@@ -1,15 +1,20 @@
-﻿import-module au
-$releases = 'https://www.wintools.info/index.php/uptodate-downloader-changelog'
+﻿Import-Module au
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 function global:au_GetLatest {
-  $regex   = '>Version: (?<Version>[\d\.]*) \|'
-  (Invoke-WebRequest -Uri $releases) -match $regex
-  return @{ Version = $matches.Version }
+    $releases = 'https://www.wintools.info/index.php/uptodate-downloader-changelog'
+    $regex    = '>Version: (?<Version>[\d\.]*) \|'
+    (Invoke-WebRequest -Uri $releases) -match $regex
+
+    return @{ 
+        Version = $matches.Version
+        URL32   = 'https://www.wintools.info/Download/uptodatedl.exe'
+    }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyInstall.ps1" = @{			
+        "tools\chocolateyinstall.ps1" = @{			
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }

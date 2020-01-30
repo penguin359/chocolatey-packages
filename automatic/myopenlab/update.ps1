74b482bdd174e4dbc93fffa85b14d680c671cc92
chtof <chtof62@gmail.com>
2020-01-30 21:09:01 +0100

    Update update.ps1

--- automatic/myopenlab/update.ps1
+++ automatic/myopenlab/update.ps1
@@ -6,6 +6,20 @@ function global:au_GetLatest {
     $releases = 'https://myopenlab.org/inicio/descargar-myopenlab/'
     $regex = 'MyOpenLab v(?<Version>[\d\.]+)'
 
+    add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+    $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
     (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
 	
     return @{

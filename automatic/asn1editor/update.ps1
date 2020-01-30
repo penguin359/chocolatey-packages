5ec819e1d02defe0eaab073c9c135101195f0cb8
chtof <chtof62@gmail.com>
2020-01-30 20:56:03 +0100

    Update update.ps1

--- automatic/asn1editor/update.ps1
+++ automatic/asn1editor/update.ps1
@@ -1,26 +1,14 @@
-﻿import-module au
+﻿Import-Module au
 
-function global:au_BeforeUpdate {
-    Remove-Item "$PSScriptRoot\tools\*.zip"
-    Invoke-WebRequest -Uri $Latest.URL32 -OutFile "$PSScriptRoot\tools\$($Latest.FileName32)"
-
-    $Latest.ChecksumType32 = 'sha256'
-    $Latest.Checksum32     = Get-RemoteChecksum $Latest.URL32 -Algorithm $Latest.ChecksumType32
-}
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-    $releases  = 'https://www.pkisolutions.com/tools/asn1editor/'
-    $regex     = '(?<Filename>Asn1Editor-v(?<Version>[\d\.]+)\.zip)'    
-    $regex_url = 'href="(?<Url>.*/download/\d+/)"'
+  $download_url = 'https://www.pkisolutions.com/download/16449/'
+  $regex        = '(Asn1Editor-v(?<Version>[\d\.]+).zip)'
 
-    $download_page = Invoke-WebRequest -Uri $releases
-    $download_page -match $regex | Out-Null
-    $version  = $matches.Version
-    $filename = $matches.Filename
-
-    $download_page -match $regex_url | Out-Null
-
-    return @{ Version = $version ; FileName32 = $filename ; URL32 = $matches.Url }
+  $download.Headers.'Content-Disposition' -match 'Asn1Editor-v(?<Version>[\d\.]+).zip' | Out-Null
+ 
+  return @{ Version = $matches.Version ; FileName32 = $matches[0] ; URL32 = $download_url }
 }
 
 function global:au_SearchReplace {
@@ -35,11 +23,9 @@ function global:au_SearchReplace {
 
         "tools\chocolateyinstall.ps1" = @{        
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName32)`""
-           "(^.*`"Asn1Editor-v)[\d\.]+(.*)$" = "`${1}$($Latest.Version)`${2}"
+           "(^.*`"Asn1Editor-v)[\d\.]+(.*)$"           = "`${1}$($Latest.Version)`${2}"
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none -noCheckUrl
-}
+update -ChecksumFor none -noCheckUrl

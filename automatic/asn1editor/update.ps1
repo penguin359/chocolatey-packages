21604d186d8093e0b97c945f56d1148b62ffdd5f
chtof <chtof62@gmail.com>
2020-01-30 21:08:54 +0100

    Update update.ps1

--- automatic/asn1editor/update.ps1
+++ automatic/asn1editor/update.ps1
@@ -1,12 +1,18 @@
 ﻿Import-Module au
 
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+function global:au_BeforeUpdate {
+    Remove-Item "$PSScriptRoot\tools\*.zip"
+    Invoke-WebRequest -Uri $Latest.URL32 -OutFile "$PSScriptRoot\tools\$($Latest.FileName32)"
+
+    $Latest.ChecksumType32 = 'sha256'
+    $Latest.Checksum32     = Get-RemoteChecksum $Latest.URL32 -Algorithm $Latest.ChecksumType32
+}
 
 function global:au_GetLatest {
   $download_url = 'https://www.pkisolutions.com/download/16449/'
   $regex        = '(Asn1Editor-v(?<Version>[\d\.]+).zip)'
 
-  $download.Headers.'Content-Disposition' -match 'Asn1Editor-v(?<Version>[\d\.]+).zip' | Out-Null
+  $download.Headers.'Content-Disposition' -match $regex | Out-Null
  
   return @{ Version = $matches.Version ; FileName32 = $matches[0] ; URL32 = $download_url }
 }

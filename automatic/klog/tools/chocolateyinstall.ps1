698712a16d357a6ef321893b75d3f31869eac95a
Chocolatey <chocolatey@realdimensions.net>
2020-02-01 16:24:30 +0000

    AU: 2 updated - klog vuescan [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/25290ce03a9d44f90b44e246697d97a9bdf1762a

--- automatic/klog/tools/chocolateyinstall.ps1
+++ automatic/klog/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'KLog'
   fileType      = 'exe'
-  file          = "$ToolsDir\KLog-0.9.9-windows-installer.exe"
+  file          = "$ToolsDir\KLog-0.9.9.1-windows-installer.exe"
   silentArgs    = "--mode unattended"
 }
 

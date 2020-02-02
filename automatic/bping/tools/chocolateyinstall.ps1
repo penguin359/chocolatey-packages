265a6e14338cafb81f0f2423e97e59bd68fceb88
chtof <chtof62@gmail.com>
2020-02-02 12:47:48 +0100

    bping manual to automatic

---
+++ automatic/bping/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  file         =  'bping.exe'
+
+  url           = 'https://gizmoware.net/bping/bping.zip'
+  checksum      = '266B391E2166F3059F17F7ED0499E64377BBA1C8AFD0A7F9AB7A11A03781940C'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/bping/update.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+    $releases = 'http://gizmoware.net/bping'
+    $regex    = 'v(?<Version>[\d\.]+) \('    
+
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing).RawContent -match $regex | Out-Null
+	
+    return @{
+        Version = $matches.Version
+        URL32   = 'https://gizmoware.net/bping/bping.zip'        
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"            
+            "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"            
+        }
+    }
+}
+
+update

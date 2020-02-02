ca9e6f1d967d10138c8db00b7f6d29bc5a00db20
chtof <chtof62@gmail.com>
2020-02-02 14:21:02 +0100

    Update update.ps1

--- automatic/launchyqt/update.ps1
+++ automatic/launchyqt/update.ps1
@@ -7,7 +7,8 @@ function global:au_GetLatest {
     $regex    = '/samsonwang/LaunchyQt/tree/v(?<Version>[\d\.]+)'
     
     (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
-    $version = $matches.Version    
+    $version = $matches.Version
+
     return @{
         Version = $version
         URL32 = 'https://github.com/samsonwang/LaunchyQt/releases/download/v' + $version + '/Launchy-' + $version + '-win-x86.7z'
@@ -18,8 +19,8 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"            
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
@@ -27,12 +28,10 @@ function global:au_SearchReplace {
 
         "tools\chocolateyinstall.ps1" = @{        
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
-          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
           "(?i)Launchy-[\d\.]+-win"   = "Launchy-$($Latest.Version)-win"
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none -noCheckUrl

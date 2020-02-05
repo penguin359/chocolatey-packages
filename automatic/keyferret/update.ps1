544c66cef37352a7a7e9ac5ddcd5e19a2081a796
chtof <chtof62@gmail.com>
2020-02-05 20:02:25 +0100

    Update update.ps1

--- automatic/keyferret/update.ps1
+++ automatic/keyferret/update.ps1
@@ -1,24 +1,11 @@
-import-module au
-
-function global:au_GetLatest {
-    $releases     = 'https://www.keyferret.com/get'
-    $regexVersion = 'title="Version (?<Version>[\d\.]+)"'
-    
-    $download_page = Invoke-WebRequest -Uri $releases
-
-    $download_page.RawContent -match $regexVersion | Out-Null
-
-    return @{
-        Version = $matches.version
-    }
-}
+. $PSScriptRoot\..\keyferret.install\update.ps1
 
 function global:au_SearchReplace {
    @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
-update -ChecksumFor none -NoCheckUrl
+update -ChecksumFor none

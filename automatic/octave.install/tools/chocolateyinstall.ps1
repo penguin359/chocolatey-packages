fb8bdcd48abe6467723928fce89824268390f112
chtof <chtof62@gmail.com>
2020-02-05 19:31:36 +0100

    Update chocolateyinstall.ps1

--- automatic/octave.install/tools/chocolateyinstall.ps1
+++ automatic/octave.install/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
     
-  url           = "https://ftpmirror.gnu.org/octave/windows/octave-5.1.0-w32-installer.exe"
+  url           = 'https://ftpmirror.gnu.org/octave/windows/octave-5.1.0-w32-installer.exe'
   checksum      = 'D2E4D5821496A0C7533ABB660810BB31C73CF6DC1BC19430B6B35586BC2F5FF9'
   checksumType  = 'sha256'
   
-  url64         = "https://ftpmirror.gnu.org/octave/windows/octave-5.1.0-w64-installer.exe"
+  url64         = 'https://ftpmirror.gnu.org/octave/windows/octave-5.1.0-w64-installer.exe'
   checksum64    = '9F80C37BAF041BD741C2660E7E5E12AFBD19DAAB7E59AABA96288E630282122B'
   checksumType64= 'sha256'
   
@@ -19,7 +19,7 @@ $packageArgs = @{
 }
 
 # Don't create shims for any executables
-$files = Get-ChildItem "$toolsDir"  -Recurse -Include *.exe -Exclude "octave-cli.exe"
+$files = Get-ChildItem "$toolsDir"  -Recurse -Include *.exe -Exclude 'octave-cli.exe'
 foreach ($file in $files) {
     New-Item "$file.ignore" -type file -force | Out-Null
 }

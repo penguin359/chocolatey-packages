48eb91bb81dc354b4ef948a06a67be7d89bfc38e
chtof <chtof62@gmail.com>
2020-02-10 20:43:04 +0100

    Update update.ps1

--- automatic/pyzo/update.ps1
+++ automatic/pyzo/update.ps1
@@ -1,23 +1,11 @@
-﻿import-module au
-
-function global:au_GetLatest {
-    $github_repository = 'pyzo/pyzo'
-    $releases   = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex      = 'pyzo-(?<Version>[\d\.]+)-win64-windows10.exe$'
-
-    (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Select -First 1 | Out-Null
-
-    return @{
-        Version = $matches.Version
-    }
-}
+﻿. $PSScriptRoot\..\pyzo.install\update.ps1
 
 function global:au_SearchReplace {
    @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
-update -ChecksumFor none -NoCheckUrl
+update -ChecksumFor none

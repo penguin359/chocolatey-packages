39069b9bc06c1b33b36c9853e4c6cc90e62a3827
chtof <chtof62@gmail.com>
2020-02-10 20:24:54 +0100

    Update update.ps1

--- automatic/launchyqt/update.ps1
+++ automatic/launchyqt/update.ps1
@@ -12,7 +12,7 @@ function global:au_GetLatest {
     return @{
         Version = $version
         URL32 = 'https://github.com/samsonwang/LaunchyQt/releases/download/v' + $version + '/Launchy-' + $version + '-win-x86.7z'
-        URL64 = 'https://github.com/samsonwang/LaunchyQt/releases/download/v' + $version + '/Launchy-' + $version + '-win-amd64.7z'
+        #URL64 = 'https://github.com/samsonwang/LaunchyQt/releases/download/v' + $version + '/Launchy-' + $version + '-win-amd64.7z'
     }
 }
 
@@ -20,15 +20,17 @@ function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{            
             "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"            
+            #"(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+            #"(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
         }
 
         "tools\chocolateyinstall.ps1" = @{        
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
-          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+          #"(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
           "(?i)Launchy-[\d\.]+-win"   = "Launchy-$($Latest.Version)-win"
         }
     }

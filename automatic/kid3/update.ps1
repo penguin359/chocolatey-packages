90558ca46a9e7997dc2bb1cda13fb30a93d28775
chtof <chtof62@gmail.com>
2020-02-10 20:20:34 +0100

    Update update.ps1

--- automatic/kid3/update.ps1
+++ automatic/kid3/update.ps1
@@ -5,16 +5,20 @@ import-module au
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-	$releases = 'https://kid3.sourceforge.io/'
-	$regex    = 'kid3-(?<Version>[\d\.]+)-win32.zip'
+	$releases = 'https://kid3.kde.org'
+  $regex64  = 'kid3-(?<Version>[\d\.]+)-win32-x64.zip'
+	$regex32  = 'kid3-([\d\.]+)-win32.zip'
 
 	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	$url     = $download_page.links | ? href -match $regex
-  $version = $matches.Version
-
+  $url32  = $download_page.links | ? href -match $regex32
+  $file32 = $matches.0
+	$url64  = $download_page.links | ? href -match $regex64
+  $file64 = $matches.0
+  
 	return @{
-    Version = $version
-    URL32   = 'https://netcologne.dl.sourceforge.net/project/kid3/kid3/' + $version + '/kid3-' + $version + '-win32.zip'
+    Version = $matches.Version
+    URL32   = 'https://netcologne.dl.sourceforge.net/project/kid3/files/' + $version + '/' + $file32
+    URL64   = 'https://netcologne.dl.sourceforge.net/project/kid3/files/' + $version + '/' + $file64    
   }
 }
 
@@ -22,19 +26,18 @@ function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{
             "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
-          "([$]toolsDir `"kid3-)[\d\.]+(-win32\\kid3.exe)`""   = "`${1}$($Latest.Version)`${2}`""          
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+          "([$]toolsDir `"kid3-)[\d\.]+(-win32[$]{architectureWidth}\\kid3.exe)`"" = "`${1}$($Latest.Version)`${2}`""          
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none

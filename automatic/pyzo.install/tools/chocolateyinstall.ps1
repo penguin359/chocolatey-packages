79ef1ddcc7c658318448a7123071de2c8c619f0e
chtof <chtof62@gmail.com>
2020-02-14 12:37:51 +0100

    Update chocolateyinstall.ps1

--- automatic/pyzo.install/tools/chocolateyinstall.ps1
+++ automatic/pyzo.install/tools/chocolateyinstall.ps1
@@ -1,17 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-if ( [environment]::OSVersion.Version.Major -ge 10 )  {
+<#if ( [environment]::OSVersion.Version.Major -ge 10 )  {
     $file64_win10 = "$toolsDir\pyzo-4.9.0-win64-windows10.exe"    
     $file64       = $file64_win10
 } else {
   $file64_win7 = "$toolsDir\pyzo-4.9.0-win64-windows7.exe"
   $file64      = $file64_win7
-}
+}#>
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName  
-  file64      = "$file64"
+  file64      = "$toolsDir\pyzo-4.9.0-win64-windows7.exe"
   silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 

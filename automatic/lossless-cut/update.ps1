dc5d1e3d66f2c4ce00ea7cf7f81e2bc4d8643668
chtof <chtof62@gmail.com>
2020-02-15 13:07:47 +0100

    Update update.ps1

--- automatic/lossless-cut/update.ps1
+++ automatic/lossless-cut/update.ps1
@@ -11,8 +11,7 @@ function global:au_GetLatest {
 
     return @{
         Version = $matches.Version
-        URL32   = 'https://github.com/mifi/lossless-cut/releases/download/v' + $matches.Version +'/LosslessCut-win32-ia32.zip'
-        URL64   = 'https://github.com/mifi/lossless-cut/releases/download/v' + $matches.Version +'/LosslessCut-win32-x64.zip'
+        URL32   = 'https://github.com/mifi/lossless-cut/releases/download/v' + $matches.Version +'/LosslessCut-win.zip'        
     }
 }
 
@@ -20,9 +19,9 @@ function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
             "(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }        
     }
 }

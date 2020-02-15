93d4959a84103181fe17ea47346c07b5debdf4fa
chtof <chtof62@gmail.com>
2020-02-15 12:48:32 +0100

    Update chocolateyuninstall.ps1

--- automatic/lossless-cut/tools/chocolateyuninstall.ps1
+++ automatic/lossless-cut/tools/chocolateyuninstall.ps1
@@ -1,17 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$link       = 'LosslessCut.lnk'
-$installDir = Get-Content -Path "$toolsDir\installDir"
-
 # Remove start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path "$programs" "$link"
+$shortcutFilePath = Join-Path $programs "LosslessCut.lnk"
 if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
-
-# Remove desktop icon
-$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path "$Env:USERPROFILE\Desktop" "$link"
-if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
-
-# Remove parent folder if empty
-if( (Get-ChildItem "$installDir" | Measure-Object).Count -eq 0) { Remove-item -Path "$installDir" }

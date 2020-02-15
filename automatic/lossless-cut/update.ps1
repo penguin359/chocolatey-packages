91566a3f4a6a557b3d47ee17365f82cf9c0791c5
chtof <chtof62@gmail.com>
2020-02-15 12:48:35 +0100

    Update update.ps1

--- automatic/lossless-cut/update.ps1
+++ automatic/lossless-cut/update.ps1
@@ -1,25 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
-function global:au_BeforeUpdate() {
-  $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32  
-}
-
 function global:au_GetLatest {
-    $github_repository = 'mifi/lossless-cut'
-    $releases          = "https://github.com/" + $github_repository + "/releases/latest"
-    $regexVersion      = $github_repository + '/tree/v(?<Version>[\d\.]+)$'
-    $regexUrl          = 'LosslessCut-(.*).exe$'
-    #$regex    = 'LosslessCut-(?<Version>[\d\.]+).exe$'
+    $github_repository = "mifi/lossless-cut"
+    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $regex    = $github_repository + '/tree/v(?<Version>[\d\.]+)$'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $download_page.links | ? href -match $regexVersion | Out-Null
-    $version = $matches.Version
-    $url = $download_page.links | ? href -match $regexUrl
+    $download_page.links | ? href -match $regex | Out-Null
 
     return @{
-        Version = $version
-        URL32   = 'https://github.com' + $url.href
+        Version = $matches.Version
+        URL32   = 'https://github.com/mifi/lossless-cut/releases/download/v' + $matches.Version +'/LosslessCut-win32-ia32.zip'
+        URL64   = 'https://github.com/mifi/lossless-cut/releases/download/v' + $matches.Version +'/LosslessCut-win32-x64.zip'
     }
 }
 
@@ -28,10 +21,10 @@ function global:au_SearchReplace {
         "tools\chocolateyinstall.ps1" = @{
             "(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-        }
+            "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }        
     }
 }
 
-update -ChecksumFor none
+update

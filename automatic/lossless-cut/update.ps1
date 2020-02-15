a64b0618bac74935c992c1594d7773601479bf65
chtof <chtof62@gmail.com>
2020-02-15 12:36:43 +0100

    Update update.ps1

--- automatic/lossless-cut/update.ps1
+++ automatic/lossless-cut/update.ps1
@@ -19,7 +19,7 @@ function global:au_GetLatest {
 
     return @{
         Version = $version
-        URL32   = 'https://github.com/' + $url.href
+        URL32   = 'https://github.com' + $url.href
     }
 }
 

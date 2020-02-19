665a0500c43ac12d1bce303f3c5bd42c44a25e85
Chocolatey <chocolatey@realdimensions.net>
2020-02-19 16:24:25 +0000

    AU: 1 updated - automouseclick [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/e5abbc08006c13ad08d92696fe96682c427a4815

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'B80F43E0B273D336CDCBB0E86011B84F41368AEF26192B67A017A3919CBE6A78'
+  checksum     = '1F427B0A6FDEA237ACA881E9088F940D69B0894AD4BC8DC6170C7AE48E8E2959'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"

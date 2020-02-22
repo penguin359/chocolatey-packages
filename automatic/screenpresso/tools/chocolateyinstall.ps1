3411f68ef7e2c5caf6e41ac77f115f675c711c61
chtof <chtof62@gmail.com>
2020-02-22 13:23:08 +0100

    screenpresso initial commit

---
+++ automatic/screenpresso/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+    
+  url          = 'https://cdn.screenpresso.com/binaries/v1/Screenpresso.exe'
+  checksum     = '5FBAEED37029D04DF451338CC013D57ADD5C5BEDFCF4C941FFA86BDD680F37EB'
+  checksumType = 'sha256'
+  
+  silentArgs   = '-install pf -silent'
+}
+
+Install-ChocolateyPackage @packageArgs

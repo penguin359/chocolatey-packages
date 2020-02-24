7e96e2d9290759d4ed1e2c25bd9b4615be0cd144
Chocolatey <chocolatey@realdimensions.net>
2020-02-24 19:02:55 +0000

    AU: 2 updated - botpress myfamilytree [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/7a2bca7f27164aed7263e4bff849b65219ba70f0

--- automatic/myfamilytree/tools/chocolateyinstall.ps1
+++ automatic/myfamilytree/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   filetype      = 'msi'
 
-  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree9.3.0.0.msi'
-  checksum      = 'f0925097ed5aa8e2f6703c6d4bf79e8bc1301c98326674469a6bd615ec47871d'
+  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree9.3.1.0.msi'
+  checksum      = 'e13ab32f6fd66f0e0ae5bc3906e6c24f3a0e2e094d6e39aa0e0cf9f9b6d9e291'
   checksumType  = 'sha256'
 
-  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree9.3.0.0x64.msi'
-  checksum64    = '50e3e61dec684d461ce3481d5e5104948c0f00f8c106f561217c3160c7f97484'
+  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree9.3.1.0x64.msi'
+  checksum64    = 'e56fc087538f4a4bc36b5bee6bdfa66f23aefa70858c0bfc1f72e7d26fd1c95d'
   checksumType64= 'sha256'
   
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

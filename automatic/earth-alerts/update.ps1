b70a7bffba31642327818a560402dfc23be7e8b0
chtof <chtof62@gmail.com>
2020-02-25 21:23:51 +0100

    Create update.ps1

---
+++ automatic/earth-alerts/update.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+     $releases = 'https://earthalerts.manyjourneys.com'
+     $regexVersion = 'Latest build:&nbsp;\<span style="color: #B22222; font-weight:bold"\>(?<Version>[\d+.]+)\<'	
+
+	 (Invoke-WebRequest -Uri $releases -UseBasicParsing).Content -match $regexVersion | Out-Null
+
+     return @{
+        Version = $matches.Version
+        URL32 = 'https://earthalerts.manyjourneys.com/files/Setup_EarthAlerts.zip'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`${1}'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

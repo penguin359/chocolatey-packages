55746070f13a0f2317778d19ee905845211675c2
Chocolatey <chocolatey@realdimensions.net>
2020-02-25 20:28:00 +0000

    AU: 2 updated - crypto-notepad earth-alerts [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/a402de76ba70591ec66d017732073df527b303f0

--- automatic/earth-alerts/tools/chocolateyinstall.ps1
+++ automatic/earth-alerts/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   unzipLocation = "$toolsDir"
 
   url           = 'https://earthalerts.manyjourneys.com/files/Setup_EarthAlerts.zip'
-  checksum      = 'fe92e52593128b2ec48d750aa1a03b0a08d669920a57b8c303a624442b0af01b'
+  checksum      = '0e7eb135519c5df23327f29ea335db3c76d0421d464ef619e9d94e8c58a5af53'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs

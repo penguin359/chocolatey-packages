c453963dc2bbba705e935aa385f288728d18a482
Chocolatey <chocolatey@realdimensions.net>
2020-02-27 16:25:31 +0000

    AU: 4 updated - chirp chirp.portable ocenaudio.install ocenaudio.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/e851e1aa6b38c243dce50a68bee8680fe4c181f5

--- automatic/ocenaudio.install/tools/chocolateyinstall.ps1
+++ automatic/ocenaudio.install/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
     
   url            = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio.exe'
-  checksum       = 'fd5a01ba0d1fc86d3f1f2471799ef8e8825a33c0006e913911c661ac5acf969b'
+  checksum       = 'd83a9ba9cee0a9cbe8b20115de9698fec93d45465119b96997ecd26a895c0f11'
   checksumType   = 'sha256'
 
   url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio64.exe'
-  checksum64     = '10445ff3a04eeffdf1570964f6abfa57ce38ea6b75de649b477e661f755fe09c'
+  checksum64     = '84ee217ef3796f7ddca41cdd23947a1db005c5a0ece8f2563e4c75b15844aff4'
   checksumType64 = 'sha256'
 
   silentArgs     = "/S"

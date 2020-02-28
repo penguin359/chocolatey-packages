0fa5af5175de660f2e58bd00dbed27c9798d5f41
chtof <chtof62@gmail.com>
2020-02-28 19:51:22 +0100

    Update update.ps1

--- automatic/lossless-cut/update.ps1
+++ automatic/lossless-cut/update.ps1
@@ -1,9 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
+function global:au_BeforeUpdate() {
+  $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+  $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+}
+
 function global:au_GetLatest {
-    $github_repository = "mifi/lossless-cut"
-    $releases = "https://github.com/" + $github_repository + "/releases/latest"
+    $github_repository = 'mifi/lossless-cut'
+    $releases = 'https://github.com/' + $github_repository + '/releases/latest'
     $regex    = $github_repository + '/tree/v(?<Version>[\d\.]+)$'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
@@ -11,17 +16,17 @@ function global:au_GetLatest {
 
     return @{
         Version = $matches.Version
-        URL32   = 'https://github.com/mifi/lossless-cut/releases/download/v' + $matches.Version +'/LosslessCut-win.zip'        
+        URL32   = 'https://github.com/mifi/lossless-cut/releases/download/v' + $matches.Version +'/LosslessCut-win.zip'
     }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-            "(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-        }        
+            "(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
     }
 }
 
-update
+update -ChecksumFor none

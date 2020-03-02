78f26fed3a2910a9ec259ba8549791ddce20c98b
chtof <chtof62@gmail.com>
2020-03-02 21:08:27 +0100

    Update update.ps1

--- automatic/dust3d/update.ps1
+++ automatic/dust3d/update.ps1
@@ -7,7 +7,7 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {
     $github_repository = "huxingyi/dust3d"
     $releases = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex = 'dust3d-(?<Version>[\d\.]+)(-beta)?(?<VersionBeta>\.[\d\.]*)-x86.zip'
+    $regex = 'dust3d-(?<Version>[\d\.]+)(-beta)?(?<VersionBeta>\.[\d\.]*)(-x86)?.zip'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
 	$url = $download_page.links | ? href -match $regex

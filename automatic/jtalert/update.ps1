6a41a3a63ce2161f082470feaff6e9f66e52d722
chtof <chtof62@gmail.com>
2020-03-15 20:16:50 +0100

    Update update.ps1

--- automatic/jtalert/update.ps1
+++ automatic/jtalert/update.ps1
@@ -1,14 +1,16 @@
 import-module au
 
 function global:au_GetLatest {
-    $releases    = 'https://hamapps.com/JTAlert/'
-    #$regex_win10 = '(HamApps_)?JTAlert[_\.]AL[_\.](.*?)[_\.]Setup.exe">Download'
-    $regex       = '(HamApps_)?JTAlert[_\.](?<Version>.*?)[_\.]Setup.exe">Download'
+    $releases     = 'https://hamapps.com/JTAlert/'    
+    #$regex       = '(HamApps_)?JTAlert[_\.](?<Version>.*?)[_\.]Setup.exe">Download'
+    $regex        = 'https://dnl.hamapps.com/JTAlert/'
+    $regexVersion = 'JTAlert\ +(?<Version>[\d\.]+)\ +Download'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	#$url_win10     = $download_page.links | ? outerHTML -match $regex_win10 | select -Last 1
 
     $url = $download_page.links | ? outerHTML -match $regex | select -First 1
+    $download_page.Content -match $regexVersion | Out-Null
 
     return @{ Version = $matches.Version ; URL32 = $url.href }
 }

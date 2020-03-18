65bb0cc58209746f982dbf27d83e4e7e65dc5f88
chtof <chtof62@gmail.com>
2020-03-18 19:10:39 +0100

    Update update.ps1

--- automatic/eclipse-cpp-oxygen/update.ps1
+++ automatic/eclipse-cpp-oxygen/update.ps1
@@ -4,12 +4,15 @@
 
 function global:au_GetLatest {
     $releases = 'https://www.eclipse.org/downloads/packages'
-    $regex    = 'eclipse-cpp-(?<VersionUrl>(?<VersionYear>\d+)-(?<VersionMonth>\d+))-R-win32-x86_64.zip$'
+    $regex    = 'eclipse-cpp-(?<VersionUrl>(?<VersionYear>\d+)-(?<VersionMonth>\d+))-R(-incubation)?-win32-x86_64.zip$'
 
     (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Out-Null
 	$version = $matches.VersionYear + '.' + $matches.VersionMonth
 
-    return @{ Version = $version ; URL32 = 'https://ftp.halifax.rwth-aachen.de/eclipse//technology/epp/downloads/release/' + $matches.VersionUrl + '/R/eclipse-cpp-' + $matches.VersionUrl + '-R-win32-x86_64.zip' }
+    return @{
+        Version = $version
+        URL32   = 'https://ftp.halifax.rwth-aachen.de/eclipse//technology/epp/downloads/release/' + $matches.VersionUrl + '/R/eclipse-cpp-' + $matches.VersionUrl + '-R-win32-x86_64.zip'
+    }
 }
 
 function global:au_SearchReplace {

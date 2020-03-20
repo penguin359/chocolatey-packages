6299919016c0bed1b00991335cce98c15031a721
Chocolatey <chocolatey@realdimensions.net>
2020-03-20 00:26:14 +0000

    AU: 2 updated - keeper unified-hosts-autoupdate [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/8328febe78b3b7feacb11aded2809eadd4e624eb

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ $packageArgs = @{
   file64        = "$toolsDir\KeeperSetup64.exe"
 
   url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = '22a4c64467520a84b3a2d3f200ed1299b8e899c4512c036d79eb55bf7a6ac7f0'
+  checksum      = '786431b7ca24825b8e52ec4fec8d43cd01313adf96a7f02017681c0c0dc5d24c'
   checksumType  = 'sha256'
 
   url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = 'fdbf63f06f22bc01820d5be5241f4fe89dee9e10da52c5d80820878730122d36'
+  checksum64    = '75dd590c3dfb17ed11633cf40d35b9a62d9b6ffc1762d5ec4d8ff2fb17101ff8'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'

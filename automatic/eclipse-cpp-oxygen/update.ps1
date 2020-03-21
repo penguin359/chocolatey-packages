f29bfd96b7b722a4230bf9c6851b19864ff3ccd1
chtof <chtof62@gmail.com>
2020-03-21 16:26:34 +0100

    Update update.ps1

--- automatic/eclipse-cpp-oxygen/update.ps1
+++ automatic/eclipse-cpp-oxygen/update.ps1
@@ -10,9 +10,8 @@ function global:au_GetLatest {
 	$version = $matches.VersionYear + '.' + $matches.VersionMonth
 
     return @{
-        Version = $version
-        #URL32   = 'https://ftp.halifax.rwth-aachen.de/eclipse//technology/epp/downloads/release/' + $matches.VersionUrl + '/R/eclipse-cpp-' + $matches.VersionUrl + '-R-win32-x86_64.zip'
-        URL32   = 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/' + $matches.VersionUrl + '/R/' + $matches.0
+        Version = $version        
+        URL32   = 'http://ftp.fau.de/eclipse/technology/epp/downloads/release/' + $matches.VersionUrl + '/R/' + $matches.0        
     }
 }
 

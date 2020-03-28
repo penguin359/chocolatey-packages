96592f072044949c3a8246eeac82935b8477b115
chtof <chtof62@gmail.com>
2020-03-28 21:49:08 +0100

    Update update.ps1

--- automatic/marktext/update.ps1
+++ automatic/marktext/update.ps1
@@ -1,22 +1,11 @@
-﻿import-module au
-
-function global:au_GetLatest {
-    $github_repository = "marktext/marktext"
-    $releases = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex   = 'marktext-setup-(?<Version>[\d\.]*)[\w-]*.exe$'
-
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
-	$url = $download_page.links | ? href -match $regex
-
-    return @{ Version = $matches.Version ; URL32 = "https://github.com" + $url.href }
-}
+﻿. $PSScriptRoot\..\marktext.install\update.ps1
 
 function global:au_SearchReplace {
-    @{        
+   @{
         "$($Latest.PackageName).nuspec" = @{
             "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
         }
     }
 }
 
-update
+update -ChecksumFor none -NoCheckUrl

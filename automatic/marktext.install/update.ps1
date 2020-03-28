cb860340ef7515ab8c3f3c2298e511aff7aabbe5
chtof <chtof62@gmail.com>
2020-03-28 21:49:03 +0100

    Update update.ps1

--- automatic/marktext.install/update.ps1
+++ automatic/marktext.install/update.ps1
@@ -3,18 +3,21 @@
 function global:au_GetLatest {
     $github_repository = "marktext/marktext"
     $releases = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex   = 'marktext-setup-(?<Version>[\d\.]*)[\w-]*.exe$'
+    $regex   = '/v(?<Version>[\d\.]*)/.*.exe'
 
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	$url = $download_page.links | ? href -match $regex
 
-    return @{ Version = $matches.Version ; URL32 = "https://github.com" + $url.href }
+    return @{
+        Version = $matches.Version        
+        URL32   = 'https://github.com' + $url.href
+    }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }        
     }

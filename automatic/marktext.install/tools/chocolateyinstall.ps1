b457fb7d8f4288ca4090e748dc9af7029f2127c4
Chocolatey <chocolatey@realdimensions.net>
2020-03-29 10:56:52 +0000

    AU: 1 updated - marktext.install [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/44d67e6804e756ded43558b4d9829f68b1b9bd9d

--- automatic/marktext.install/tools/chocolateyinstall.ps1
+++ automatic/marktext.install/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://github.com/marktext/marktext/releases/download/v0.15.1/marktext-setup-0.15.1.exe'
-  checksum     = 'd92d685b22b594720040034bf26cb1935f8a88dff1bda122e08bc4070d4978cc'
+  url          = 'https://github.com/marktext/marktext/releases/download/v0.16.1/marktext-setup.exe'
+  checksum     = '2b389c0bf3e7d9348d1e69ee5ffc9485b4c18da017c955c019e92029c824b037'
   checksumType = 'sha256'
   
   silentArgs   = "/S"

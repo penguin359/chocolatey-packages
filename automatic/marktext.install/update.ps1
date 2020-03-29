5a9139d755d8ead96620a96b90bb24542553d639
chtof <chtof62@gmail.com>
2020-03-29 13:50:40 +0200

    Update update.ps1

--- automatic/marktext.install/update.ps1
+++ automatic/marktext.install/update.ps1
@@ -17,8 +17,8 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^(\s)*url\s*=\s*)('.*')"      = "`${1}'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum32)'"
         }        
     }
 }

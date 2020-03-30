a256338799e97fc7fc8a277f27e2c8232409970f
chtof <chtof62@gmail.com>
2020-03-30 23:22:00 +0200

    Create update.ps1

---
+++ automatic/beets/update.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+	$github_repository = 'beetbox/beets'
+	$releases = 'https://github.com/' + $github_repository + '/releases/latest'
+	$regex    = '/beetbox/beets/tree/v(?<Version>[\d\.]+)'
+
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing).RawContent -match $regex | Out-Null
+
+	return @{
+		Version = $matches.Version
+	}
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+			"(^(\s)*[$]version\s*=\s*)('.*')" = "`$1'$($Latest.Version)'"
+        }        
+    }
+}
+
+update -ChecksumFor none -NoCheckUrl

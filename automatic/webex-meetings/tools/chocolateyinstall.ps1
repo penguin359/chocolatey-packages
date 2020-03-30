416bc9c52fcbc4b89abc1768cbb50a28cdec7dc4
chtof <chtof62@gmail.com>
2020-03-30 13:55:11 +0200

    webex-meetings initial commit

---
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  fileType     = 'MSI'
+
+  url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
+  checksum     = '9C2D9C58CE7FA49E1FA5ACA50661B192DCC9595EABD15A92FC6E3BE8E9124783'
+  checksumType = 'sha256'
+
+  silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+}
+
+Install-ChocolateyPackage @packageArgs

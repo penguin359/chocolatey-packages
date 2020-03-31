a509b6bb8ca10316e313619991e4cdd7a215f759
chtof <chtof62@gmail.com>
2020-03-31 15:16:53 +0200

    Update update.ps1

--- automatic/vuescan/update.ps1
+++ automatic/vuescan/update.ps1
@@ -1,9 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
-import-module au
+Import-Module au
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 
-function global:au_BeforeUpdate() {
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
-    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+function global:au_BeforeUpdate() {  
+    $Latest.Checksum32 = Get-RemoteChecksum $($Latest.Url32)    
+    $Latest.Checksum64 = Get-RemoteChecksum $($Latest.Url64)
 }
 
 function global:au_GetLatest {    
@@ -17,11 +18,20 @@ function global:au_GetLatest {
     $download_page -match $regex64 | Out-Null
     $file64  = $matches.File64
 
-    return @{
-        Version = $matches.Version
-        URL32   = 'https://www.hamrick.com/' +$file32
-        URL64   = 'https://www.hamrick.com/' +$file64
+    $etag = GetETagIfChanged -uri "https://www.hamrick.com/$file32"
+
+    if ($etag) {        
+        $result = GetResultInformation "https://www.hamrick.com/$file32"
+        $result["ETAG"] = $etag
     }
+    else {        
+        $result = @{
+            URL32   = 'https://www.hamrick.com/' + $file32
+            URL64   = 'https://www.hamrick.com/' + $file64
+            Version = $matches.Version
+        }
+    }
+    return $result
 }
 
 function global:au_SearchReplace {
@@ -33,6 +43,38 @@ function global:au_SearchReplace {
             "(^(\s)*checksum64\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum64)'"
         }
     }
+}
+
+function global:au_AfterUpdate {
+  "$($Latest.ETAG)|$($Latest.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
+}
+
+function GetResultInformation([string]$url32) {
+  $dest = "$env:TEMP\vuex32.exe"
+  Get-WebFile $url32 $dest | Out-Null
+
+  $result = @{
+    URL32          = $url32    
+    Version        = (Get-Item $dest).VersionInfo.FileVersion.Trim()
+    Checksum       = Get-FileHash $dest -Algorithm SHA512 | % Hash
+    ChecksumType32 = 'sha512'
+  }
+  Remove-Item -Force $dest
+  return $result
+}
+
+function GetETagIfChanged() {
+  param([string]$uri)
+  if (($global:au_Force -ne $true) -and (Test-Path $PSScriptRoot\info)) {
+    $existingETag = $etag = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' } | select -first 1
+  }
+  else { $existingETag = $null }
+
+  $etag = Invoke-WebRequest -Method Head -Uri $uri -UseBasicParsing
+  $etag = $etag | % { $_.Headers.ETag }
+  if ($etag -eq $existingETag) { return $null }
+
+  return $etag
 }
 
 update -ChecksumFor none -NoCheckUrl

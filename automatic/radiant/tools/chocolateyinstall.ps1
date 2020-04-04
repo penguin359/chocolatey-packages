12413d8caeb271e87ff163c7a86548f2bddc56c4
Chocolatey <chocolatey@realdimensions.net>
2020-04-04 00:24:21 +0000

    AU: 2 updated - cudatext radiant [skip ci]

--- automatic/radiant/tools/chocolateyinstall.ps1
+++ automatic/radiant/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://www.radiantviewer.com/files/RadiAnt-5.5.1-Setup.exe'
-  checksum      = '82d0835c6d7cf7b76635918236f0044619f37d3f4d9271928c4f84008a629857'
+  url           = 'https://www.radiantviewer.com/files/RadiAnt-2020.1-Setup.exe'
+  checksum      = 'c9090a8531dcb4e5c7bd7e22e29348e07a9ae0041852de71952e0d0c696e6db9'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"

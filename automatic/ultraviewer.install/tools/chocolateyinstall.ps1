c4b8988bad7cf79a4d3964f04a336af9e5c47f83
chtof <chtof62@gmail.com>
2020-04-04 23:25:08 +0200

    ultraviewer/ultraviewer.install initial commit

---
+++ automatic/ultraviewer.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+
+  url          = 'https://ultraviewer.net/en/UltraViewer_setup_6.2_en.exe'
+  checksum     = 'E324CEAAE878B973FDA7A0F8A33AB988785F5D671EA1EAAD21AFEB68D76E5257'
+  checksumType = 'sha256'  
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/ultraviewer/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/ultraviewer.install/ultraviewer.install.png" width="48" height="48"/> [UltraViewer](https://chocolatey.org/packages/ultraviewer.install)
+
+[[Description]]
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/ultraviewer.install/screenshot.png)

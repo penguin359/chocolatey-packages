81f68f9848b52c8d7e003b4803f3c58ad9cdcdca
chtof <chtof62@gmail.com>
2020-04-09 21:08:45 +0200

    Update update.ps1

--- automatic/dust3d/update.ps1
+++ automatic/dust3d/update.ps1
@@ -5,15 +5,15 @@
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-    $github_repository = "huxingyi/dust3d"
-    $releases = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex = 'dust3d-(?<Version>[\d\.]+)(-beta)?(?<VersionBeta>\.[\d\.]*)(-x86)?.zip'
+    $github_repository = 'huxingyi/dust3d'
+    $releases = 'https://github.com/' + $github_repository + '/releases/latest'
+    $regex    = 'dust3d-(?<Version>[\d\.]+)(-beta|-rc)?(?<VersionBeta>\.[\d\.]*)(-x86)?.zip'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
 	$url = $download_page.links | ? href -match $regex
 
     if ($matches.VersionBeta) {
-        $version = $matches.Version + $matches.VersionBeta + '-beta'
+        $version = $matches.Version + $matches.VersionBeta + $matches.1
     } else {
         $version = $matches.Version
     }
@@ -37,6 +37,4 @@ function global:au_SearchReplace {
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none

50a3f973ed3a58fdd9206785849546aa688e45bc
chtof <chtof62@gmail.com>
2020-04-09 20:55:53 +0200

    Update update.ps1

--- automatic/eddie.portable/update.ps1
+++ automatic/eddie.portable/update.ps1
@@ -5,7 +5,7 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {
     $github_repository = 'AirVPN/Eddie'
     $releases = 'https://github.com/' + $github_repository + '/releases/latest'    
-    $regex    = 'v(?<Version>[\d\.]+).zip$'
+    $regex    = 'tree/(?<Version>[\d\.]+)'
 
     $url     = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
     $version = $matches.Version

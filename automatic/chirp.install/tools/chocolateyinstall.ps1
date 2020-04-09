aa547c4634aa4868f2ae92353f45dd09a937108b
Chocolatey <chocolatey@realdimensions.net>
2020-04-09 16:26:43 +0000

    AU: 2 updated - chirp.install photoflare.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/250216a1cb52e62597f81d855b319e0f408262c7

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-daily-20200212-installer.exe"
+  file          = "$toolsDir\chirp-daily-20200409-installer.exe"
   silentArgs    = '/S'
 }
 

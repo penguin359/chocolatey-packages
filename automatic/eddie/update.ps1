c437d3a1f4a16ef40c24759ad539085973de7739
chtof <chtof62@gmail.com>
2020-04-09 20:56:02 +0200

    Update update.ps1

--- automatic/eddie/update.ps1
+++ automatic/eddie/update.ps1
@@ -1,23 +1,11 @@
-﻿import-module au
-
-[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-
-function global:au_GetLatest {
-    $github_repository = 'AirVPN/Eddie'
-    $releases = 'https://github.com/' + $github_repository + '/releases/latest'    
-    $regex    = 'v(?<Version>[\d\.]+).zip$'
-
-    $url     = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex  
-
-    return @{ Version = $matches.Version }
-}
+﻿. $PSScriptRoot\..\eddie.install\update.ps1
 
 function global:au_SearchReplace {
    @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
-update -ChecksumFor none -NoCheckUrl
+update -ChecksumFor none

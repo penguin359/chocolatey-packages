54322e0ee78642c5cfe7fb924be2461df203e0e9
Chocolatey <chocolatey@realdimensions.net>
2020-04-10 00:24:55 +0000

    AU: 3 updated - ospray ospray.install ospray.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/6cb407d6c6c7027e46f7661930a1b53cd21c4b4c

--- automatic/ospray.portable/tools/chocolateyinstall.ps1
+++ automatic/ospray.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\ospray-2.0.1.x86_64.windows.zip"
+  file        = "$toolsDir\ospray-2.1.0.x86_64.windows.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

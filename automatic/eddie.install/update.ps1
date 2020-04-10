d197f4a521e0a80e433e9edaa5a76b60fc2c5e87
chtof <chtof62@gmail.com>
2020-04-10 19:00:46 +0200

    Update update.ps1

--- automatic/eddie.install/update.ps1
+++ automatic/eddie.install/update.ps1
@@ -35,5 +35,5 @@ function global:au_SearchReplace {
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
+    update -ChecksumFor none -noCheckUrl
 }

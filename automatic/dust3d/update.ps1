394b8fe27f1d4a6b4b9a3d7da3752ca233dad5bf
chtof <chtof62@gmail.com>
2020-04-10 19:06:03 +0200

    Update update.ps1

--- automatic/dust3d/update.ps1
+++ automatic/dust3d/update.ps1
@@ -7,10 +7,12 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {
     $github_repository = 'huxingyi/dust3d'
     $releases = 'https://github.com/' + $github_repository + '/releases/latest'
-    $regex    = 'dust3d-(?<Version>[\d\.]+)(-beta|-rc)?(?<VersionBeta>\.[\d\.]*)(-x86)?.zip'
+    $regex32  = 'dust3d-[\d\.]+(-beta|-rc)?\.[\d\.]*-x86.zip'
+    $regex64  = 'dust3d-(?<Version>[\d\.]+)(-beta|-rc)?(?<VersionBeta>\.[\d\.]*).zip'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
-	$url = $download_page.links | ? href -match $regex
+	$url32 = $download_page.links | ? href -match $regex32
+    $url64 = $download_page.links | ? href -match $regex64
 
     if ($matches.VersionBeta) {
         $version = $matches.Version + $matches.VersionBeta + $matches.1
@@ -18,21 +20,26 @@ function global:au_GetLatest {
         $version = $matches.Version
     }
 
-    return @{ Version = $version ; URL32 = "https://github.com" + $url.href }
+    return @{
+        Version = $version
+        URL32   = 'https://github.com' + $url32.href
+        URL64   = 'https://github.com' + $url64.href
+    }
 }
 
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{
             "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }

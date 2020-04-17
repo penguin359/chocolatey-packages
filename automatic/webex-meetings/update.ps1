52c90a9dfc945f09624f025ad67fb6196f90bee0
chtof <chtof62@gmail.com>
2020-04-17 14:05:19 +0200

    Update update.ps1

--- automatic/webex-meetings/update.ps1
+++ automatic/webex-meetings/update.ps1
@@ -4,7 +4,7 @@ import-module au
 function global:au_GetLatest {    
 	$releases          = 'https://cisco.webex.com/webappng/sites/cisco/dashboard/download'
     $downloadUrl_part1 = 'https://akamaicdn.webex.com/client/'
-    $regex             = 'https://cisco.webex.com/client/(?<downloadUrl_part2>WBXclient-(?<version>[\d\.-]+)/webexapp.msi)'
+    $regex             = 'https://cisco.webex.com/client/WBXclient-(?<version>[\d\.-]+)/webexapp.msi'
 
     $ie = New-Object -com internetexplorer.application
     $ie.Visible = $false
@@ -12,12 +12,12 @@ function global:au_GetLatest {
     while ($ie.Busy -eq $true){Start-Sleep -seconds 4;}
     $ie.Document.body.outerHTML -match $regex | Out-Null
 
-	$version           = $matches.Version -replace '-', '.'
-    $downloadUrl_part2 = $matches.downloadUrl_part2
+	$UrlVersion        = $matches.Version
+    $NugetVersion      = $UrlVersion -replace '-', '.'    
 
     return @{
         Version = $version
-        URL32   = $downloadUrl_part1 + $downloadUrl_part2
+        URL32   = $downloadUrl_part1 + $UrlVersion + '/webexapp.msi'
     }
 }
 

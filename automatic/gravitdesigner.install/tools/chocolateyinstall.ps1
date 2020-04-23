7747c0920b106fb0c96f102c320385932860390a
Chocolatey <chocolatey@realdimensions.net>
2020-04-23 00:25:25 +0000

    AU: 5 updated - botpress gravitdesigner gravitdesigner.install gravitdesigner.portable jami [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/8404f3cc305f4b659ec41dde8060c6ba9d6786c5

--- automatic/gravitdesigner.install/tools/chocolateyinstall.ps1
+++ automatic/gravitdesigner.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://designer.gravit.io/_downloads/windows/GravitDesignerInstaller.exe'
-  checksum      = 'b186a08a3789445cf382a4d4f4ea86a25da52d45b2d51e403e0994445fcbd59f'
+  checksum      = '7302a4db1919ea54b39e40ca54dcebba671e9a6a4c710558440625cb2450c188'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"

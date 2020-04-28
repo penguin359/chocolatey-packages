16629df2d7b85775042479a31982e9c67fb9072c
chtof <chtof62@gmail.com>
2020-04-28 19:06:41 +0200

    Update update.ps1

--- automatic/jekyll/update.ps1
+++ automatic/jekyll/update.ps1
@@ -2,17 +2,17 @@
 import-module au
 
 function global:au_GetLatest {
-    $releases = "https://jekyllrb.com"
-    $regex    = "Jekyll v(?<Version>[\d\.]+)"
+    $releases = 'https://jekyllrb.com'
+    $regex    = '<a href="https://github.com/jekyll/jekyll/releases/tag/v(?<Version>[\d\.]+)"'
 
-    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null    
+    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
     return @{ Version = $matches.Version }
 }
 
 function global:au_SearchReplace {    
     @{
 	   "tools\chocolateyinstall.ps1" = @{            
-        "(gem install jekyll -v)\s*[\d\.]+.*" = "`$1 $($Latest.Version)"
+        "(gem install jekyll -v)\s*[\d\.]+.*" = "`${1} $($Latest.Version)"
         }
     }
 }

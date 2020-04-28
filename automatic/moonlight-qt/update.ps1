13602561e5a6cd652613dd3a51f92c0e1ceef7b6
chtof <chtof62@gmail.com>
2020-04-28 19:08:32 +0200

    Update update.ps1

--- automatic/moonlight-qt/update.ps1
+++ automatic/moonlight-qt/update.ps1
@@ -1,15 +1,4 @@
-import-module au
-
-function global:au_GetLatest {
-    $github_repository = "moonlight-stream/moonlight-qt"
-    $releases          = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex32           = 'MoonlightSetup-x86-(?<Version>[\d\.]+).exe'
-    
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url32         = $download_page.links | ? href -match $regex32 | Select -First 1
-
-    return @{ Version = $matches.Version }
-}
+. $PSScriptRoot\..\moonlight-qt.install\update.ps1
 
 function global:au_SearchReplace {
    @{

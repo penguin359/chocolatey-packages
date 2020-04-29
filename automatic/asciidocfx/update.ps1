ce12a352131660f57c6ba83b536441ae88e5cfc5
chtof <chtof62@gmail.com>
2020-04-29 20:59:43 +0200

    Update update.ps1

--- automatic/asciidocfx/update.ps1
+++ automatic/asciidocfx/update.ps1
@@ -1,23 +1,11 @@
-﻿import-module au
-
-function global:au_GetLatest {
-    $github_repository = "asciidocfx/AsciidocFX"
-    $releases = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex    = '/asciidocfx/AsciidocFX/tree/v(?<Version>[\d\.]*)\"'
-
-    (Invoke-WebRequest -Uri $releases).Content -match $regex | Out-Null    
-
-    return @{
-        Version = $matches.Version
-    }
-}
+﻿. $PSScriptRoot\..\asciidocfx.install\update.ps1
 
 function global:au_SearchReplace {
    @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
-update -ChecksumFor none -NoCheckUrl
+update -ChecksumFor none

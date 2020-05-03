2c7f3e92f82f4ff277517fad669598140cbc4d94
chtof <chtof62@gmail.com>
2020-05-03 12:55:54 +0200

    Create update.ps1

---
+++ automatic/adobe-connect/update.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+	$release = 'http://www.adobe.com/go/Connectmsi'
+	$regex   = 'Connect(?<Version>[\d\._]+).msi'
+
+	$url = Get-RedirectedUrl "$release"
+    $url -match $regex | Out-Null
+    $version = $matches.Version -Replace '_','.'
+
+    return @{
+        Version = $version
+        URL32   = $url
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

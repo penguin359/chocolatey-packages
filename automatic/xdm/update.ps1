f43afdb010ec650f826c7b5e5eb0917cd6318cf7
chtof <chtof62@gmail.com>
2020-05-03 15:35:01 +0200

    Create update.ps1

---
+++ automatic/xdm/update.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+. $PSScriptRoot\..\..\helpers\au_Helper_GitHub.ps1
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+    return au_Helper_GitHub 'subhra74/xdm' -RegexFile32 'xdmsetup.msi'
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"            
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{        
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName32)`""          
+        }
+    }
+}
+
+update -ChecksumFor none

493d06925d003231413d2c6cc4bf0ad464e67055
chtof <chtof62@gmail.com>
2020-05-03 11:55:05 +0200

    Update update.ps1

--- automatic/ispc/update.ps1
+++ automatic/ispc/update.ps1
@@ -1,9 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
-[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge -FileNameSkip 1 -FileNameBase "ispc-v$($Latest.Version)-windows" }
 
 function global:au_GetLatest {
     $releases = 'https://ispc.github.io/downloads.html'
@@ -11,9 +9,10 @@ function global:au_GetLatest {
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	$url = $download_page.links | ? href -match $regex | Select -First 1
-    return @{
+
+    return @{        
         Version = $matches.Version
-        URL32   = Get-RedirectedUrl $url.href
+        URL32   = (Get-RedirectedUrl $url.href) -replace '(.*)(&response-content-disposition=attachment%3B%20filename%3D.*-windows.zip)(.*)','$1$3$2'
     }
 }
 
@@ -33,6 +32,4 @@ function global:au_SearchReplace {
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none

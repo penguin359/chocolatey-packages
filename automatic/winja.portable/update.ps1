974d523418e3c71a33678fcea1ebbadb64d08aaf
chtof <chtof62@gmail.com>
2020-05-03 16:28:42 +0200

    Create update.ps1

---
+++ automatic/winja.portable/update.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://www.phrozen.io/docs/windows/projects/freeware/winja'
+	$regex    = 'winja(?<Version>[\d\-]+).zip'
+ 
+	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$url = $download_page.links | ? href -match $regex
+
+    return @{
+        Version = $version
+        URL32   = 'https://www.phrozen.io' + $url.href
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update
+}

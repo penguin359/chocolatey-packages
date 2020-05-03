d1310202593208e682b6c6266e3b13fc32a77a0b
chtof <chtof62@gmail.com>
2020-05-03 11:55:02 +0200

    Update chocolateyinstall.ps1

--- automatic/ispc/tools/chocolateyinstall.ps1
+++ automatic/ispc/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
-  file          = "$toolsDir\ispc-v1.12.0-windows.zip"
+  packageName = $env:ChocolateyPackageName
+  destination = "$toolsDir"
+  file        = "$toolsDir\ispc-v1.13.0-windows.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

160c7a968724d9a9dc22739c262d76857b9a30f2
Chocolatey <chocolatey@realdimensions.net>
2020-05-05 00:26:28 +0000

    AU: 2 updated - gridtracker klayout [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/d694f500007a46d48aa8d67a816bdf53ddff0308

--- automatic/gridtracker/tools/chocolateyinstall.ps1
+++ automatic/gridtracker/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://uc0e6466898ca26db13ce1916d5b.dl.dropboxusercontent.com/cd/0/get/A3CRdE6KIzN0go-zeKy0FiMPWL4E66p2Zk7265P2d2Pmu95joab0LWK6IyD51oKQhDO2yUcKS2sBmQ9u8CdTqcmiuZ-Fe0U22JOCicLfsviboRKWOk3c138addhmolij3rQ/file?dl=1#'
-  checksum     = '8981fe2c533e0cb13e090da4e48bae97b515c6bb7758e93a103c04fb6bcc8dd8'
+  url          = 'https://uc5d43b1e75df1e4ca0be856771e.dl.dropboxusercontent.com/cd/0/get/A3GDMa5OxUR8yI7GE8qM7-3yAwQxM5FjdcMWIhvPSy24fObx1Zwgu_TL9auQBBLzDYs-LXQTY6wadAzb-DJLytn9jnq06_DDw9JJR5igqG_5ToN0u39YfrV0zZ8ulKqU2ko/file?dl=1#'
+  checksum     = '0a0cbd42c4ad36854dff1401f76228d29c8ed47d7df38dff128fb094c8c30dad'
   checksumType = 'sha256'
 
   silentArgs   = '/S'

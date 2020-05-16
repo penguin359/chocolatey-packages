dfb031153adc53d440f3fc008461e2ee217665dd
Chocolatey <chocolatey@realdimensions.net>
2020-05-16 00:29:20 +0000

    AU: 3 updated - egnyte-connect umbrello umbrello.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/f72c54387e3b6524a8228f93ce6988f4fadc6ca1

--- automatic/egnyte-connect/tools/chocolateyinstall.ps1
+++ automatic/egnyte-connect/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   filetype     = 'MSI'
 
-  url          = 'https://egnyte-cdn.egnyte.com/egnytedrive/win/en-us/3.7.2/EgnyteConnect_3.7.2_12.msi'
-  checksum     = '3059d28865ea7cf295281e2311dc2cb1f4e8afc48cb7c5384c9eefd82d34e45b'
+  url          = 'https://egnyte-cdn.egnyte.com/egnytedrive/win/en-us/3.7.3/EgnyteConnect_3.7.3_18.msi'
+  checksum     = '86ede9feb22d7c2b8bf9d3e73b8f8ade0448b6c099ff08e4ce9356ad2f3fb0c9'
   checksumType = 'sha256'  
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

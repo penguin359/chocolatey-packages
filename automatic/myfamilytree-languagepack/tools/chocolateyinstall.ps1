20ed56e5c671dc2c27b8a06fbd114228916a4058
Chocolatey <chocolatey@realdimensions.net>
2020-05-27 16:24:53 +0000

    AU: 3 updated - libre-hardware-monitor myfamilytree-languagepack ospray.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/b80022155b68233a39876a869addcee2136dea82

--- automatic/myfamilytree-languagepack/tools/chocolateyinstall.ps1
+++ automatic/myfamilytree-languagepack/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType		= "msi"
   
-  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack10.1.1000.0.msi'
-  checksum      = '77ff3d5a3d9012b16263fff2f7dd223ae1a9528b93f0369e75231f874570723c'
+  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack10.1.2000.0.msi'
+  checksum      = '62bcaefbbc6bec9cad0fb59f2ba2470dbf85c980f906360f8b011f8ef8757b3a'
   checksumType  = 'sha256'
   
-  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack10.1.1000.0x64.msi'
-  checksum64    = '0647d4b696fc9f459c4baf1bdb4321d101712c7579b6b5ebbd7d09bf8b74848f'
+  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack10.1.2000.0x64.msi'
+  checksum64    = '5cb53f3943b83df1491199a6444a0afb45fea6786e77e42f663f308178730509'
   checksumType64= 'sha256'
   
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

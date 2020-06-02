84ab9c55833302eb4ea9b4e7fe78ddd6c0704a25
chtof <chtof62@gmail.com>
2020-06-02 19:01:43 +0200

    Update update.ps1

--- automatic/firestorm-secondlife/update.ps1
+++ automatic/firestorm-secondlife/update.ps1
@@ -19,12 +19,12 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^(\s)*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType)'"
-            "(^(\s)*url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
-            "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-            "(^(\s)*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+			"(^(\s)*url\s*=\s*)('.*')"            = "`${1}'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')"       = "`${1}'$($Latest.Checksum32)'"
+            "(^(\s)*checksumType\s*=\s*)('.*')"   = "`${1}'$($Latest.ChecksumType32)'"
+            "(^(\s)*url64\s*=\s*)('.*')"          = "`${1}'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')"     = "`${1}'$($Latest.Checksum64)'"
+            "(^(\s)*checksumType64\s*=\s*)('.*')" = "`${1}'$($Latest.ChecksumType64)'"
         }
     }
 }

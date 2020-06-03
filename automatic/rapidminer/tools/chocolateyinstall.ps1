f8b25021ae2a6f2f90afcd5dd70bad19eb1d8435
Chocolatey <chocolatey@realdimensions.net>
2020-06-03 16:26:04 +0000

    AU: 4 updated - chirp chirp.install chirp.portable rapidminer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/6db555b5c0261463b82f3edc246861a5ef677d95

--- automatic/rapidminer/tools/chocolateyinstall.ps1
+++ automatic/rapidminer/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
    
   url           = 'https://releases.rapidminer.com/latest/rapidminer-studio/rapidminer-studio-win32-install.exe?utm_source=nexus&utm_medium=nexus'
-  checksum      = '9f64d20f84e125084237ddf6db6eade87ec917f234614512d66b0876bf3a7a7c'
+  checksum      = 'f3d7e132b6af8a30710e5733d6bd65662d419f5a7c94dec83d85e2b3b00160cb'
   checksumType  = 'sha256'
 
   url64bit      = 'https://releases.rapidminer.com/latest/rapidminer-studio/rapidminer-studio-win64-install.exe?utm_source=nexus&utm_medium=nexus'
-  checksum64    = 'd50e4077ab98f821709640008559c658cd430c413554750c3c5a68a0387494b2'
+  checksum64    = '83755fba7237ceb977ac15b7216e8377e9256c75ceb2822e9c3c8e47ea4ed418'
   checksumType64= 'sha256'
 
   silentArgs    = '/S'

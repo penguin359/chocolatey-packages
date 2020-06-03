35171c73504d485f30188969a406f3ee38f8bbba
chtof <chtof62@gmail.com>
2020-06-03 23:08:44 +0200

    Update chocolateyuninstall.ps1

--- automatic/miktex.install/tools/chocolateyuninstall.ps1
+++ automatic/miktex.install/tools/chocolateyuninstall.ps1
@@ -5,5 +5,5 @@ write-warning $miktex_dir
 $arch       = Get-OSArchitectureWidth
 write-warning $arch
 
-Start-Process "${miktex_dir}\miktex\bin\x${arch}\miktexsetup.exe" -Verb RunAs -ArgumentList "uninstall"
+Start-Process -Wait "${miktex_dir}\miktex\bin\x${arch}\miktexsetup.exe" -Verb RunAs -ArgumentList "uninstall"
 Remove-Item -Recurse "${miktex_dir}"

07c876c169ce8ff899b997cd2689d86d7d935671
chtof <chtof62@gmail.com>
2020-06-03 21:56:21 +0200

    Update update.ps1

--- automatic/seppdf/update.ps1
+++ automatic/seppdf/update.ps1
@@ -1,5 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
-import-module au
+Import-Module au
+
+function global:au_BeforeUpdate {
+    $releases     = 'https://www.ne.jp/asahi/foresth/home/'    
+    $regexVersion = 'SepPDF Ver(?<Version>[\d\.]+)'
+    $regex        = 'spdf([\d\._]+).zip'
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url = $download_page.links | ? href -match $regex | Select -First 1
+
+    $Latest.URL32 = $releases + $url.href
+    $Latest.ChecksumType32 = 'sha256'
+    $Latest.Checksum32     = Get-RemoteChecksum $Latest.URL32 -Algorithm $Latest.ChecksumType32
+  }
 
 function global:au_GetLatest {
     $releases     = 'https://www.ne.jp/asahi/foresth/home/'    
@@ -7,22 +20,21 @@ function global:au_GetLatest {
 	$regex        = 'spdf([\d\._]+).zip'
 
 	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	$url = $download_page.links | ? href -match $regex | Select -First 1
-
+	$download_page.links | ? href -match $regex | Select -First 1 | Out-Null   
     $HTML = New-Object -Com "HTMLFile"
     $HTML.IHTMLDocument2_write($download_page.Content)
     ($HTML.all.tags("strong") |% InnerText | Out-String) -match $regexVersion | Out-Null
 
-    return @{ Version = $matches.Version ; URL32 = $releases + $url.href }
+    return @{ Version = $matches.Version }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^(\s)*url\s*=\s*)('.*')"      = "`${1}'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum32)'"
         }
     }
 }
 
-update
+update -ChecksumFor none

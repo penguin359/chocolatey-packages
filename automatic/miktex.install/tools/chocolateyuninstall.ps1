aa124b1eb468693c464cc10073793c47f307bb31
chtof <chtof62@gmail.com>
2020-06-03 23:14:01 +0200

    Update chocolateyuninstall.ps1

--- automatic/miktex.install/tools/chocolateyuninstall.ps1
+++ automatic/miktex.install/tools/chocolateyuninstall.ps1
@@ -1,9 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $miktex_dir = Get-AppInstallLocation MiKTeX
-write-warning $miktex_dir
 $arch       = Get-OSArchitectureWidth
-write-warning $arch
 
 Start-Process -Wait "${miktex_dir}\miktex\bin\x${arch}\miktexsetup.exe" -Verb RunAs -ArgumentList "uninstall"
-Remove-Item -Recurse "${miktex_dir}"

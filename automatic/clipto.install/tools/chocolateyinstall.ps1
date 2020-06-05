b1ef85c9125bc4b69aa3f32b05846f4e732ac4c6
Chocolatey <chocolatey@realdimensions.net>
2020-06-05 00:24:26 +0000

    AU: 3 updated - clipto clipto.install sd [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/6d6e4dbcac61900c45f8b2dcbec0ca75488c84af

--- automatic/clipto.install/tools/chocolateyinstall.ps1
+++ automatic/clipto.install/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://github.com/clipto-pro/Desktop/releases/download/v2.7.9/clipto-2.7.9.exe'
-  checksum     = 'c5519682e9edbd14889c2aba291d67dd79289f438460bdaa083ab556b0a586be'
+  url          = 'https://github.com/clipto-pro/Desktop/releases/download/v2.8.1/clipto-2.8.1.exe'
+  checksum     = '9fc9e50b09aa7b69186eb3c01c2934e42b04c00d261eeb1dddaddaa287cc84c8'
   checksumType = 'sha256'
 
   silentArgs   = '/S'

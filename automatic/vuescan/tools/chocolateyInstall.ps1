d2e009fcdba781cac3e4e10f9eff20e2b9563b63
chtof <chtof62@gmail.com>
2020-06-05 22:00:29 +0200

    Update chocolateyInstall.ps1

--- automatic/vuescan/tools/chocolateyInstall.ps1
+++ automatic/vuescan/tools/chocolateyInstall.ps1
@@ -3,25 +3,25 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
- 
+
   url            = 'https://www.hamrick.com/files/vuex3297.exe'
-  checksum       = '237b8fc69abe4243a046d330c032b3bc79c9159f2d13206e28b90e0827c4d5d2'
+  checksum       = '4863e51737902c63f6c66b1c2b5c3417d78a52742e6d4bf128dbda2e2bc87f46'
   checksumType   = 'sha256'
- 
+
   url64          = 'https://www.hamrick.com/files/vuex6497.exe'
-  checksum64     = 'b7d52f1ad8350f4256d32c7409e05b99e66eefeabf362bdcd01539e4ddf04d76'
+  checksum64     = 'bac4d674cb2997545b54ce010a4653e0bb1ddf72ecc53de75aa94c6e37d37952'
   checksumType64 = 'sha256'
 }
 
 if (Get-ChildItem Cert:\CurrentUser\TrustedPublisher\|Where subject -eq "CN=Hamrick Software, O=Hamrick Software, L=Sunny Isles Beach, S=Florida, C=US") {
-  Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
+  Start-Process 'AutoHotKey' -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
 } else {
-  Start-Process "AutoHotKey" -Verb runas -ArgumentList "`"$toolsDir\chocolateyInstallCertificate.ahk`""
+  Start-Process 'AutoHotKey' -Verb runas -ArgumentList "`"$toolsDir\chocolateyInstallCertificate.ahk`""
 }
 Install-ChocolateyPackage @packageArgs
 
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path $programs "VueScan.lnk"
-$targetPath = Join-Path $env:ProgramFiles "VueScan\vuescan.exe"
-Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
+$shortcutFilePath = Join-Path $programs 'VueScan.lnk'
+$targetPath = Join-Path $env:ProgramFiles 'VueScan\vuescan.exe'
+Install-ChocolateyShortcut -ShortcutFilePath "$shortcutFilePath" -TargetPath "$targetPath"

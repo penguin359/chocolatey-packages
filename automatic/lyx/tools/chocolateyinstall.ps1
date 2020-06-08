2067bef654c2d1f9d28b354af3627fc300f39f22
Chocolatey <chocolatey@realdimensions.net>
2020-06-08 00:24:29 +0000

    AU: 8 updated - clavier-plus clavier-plus.install clavier-plus.portable cudatext ffmpeg-batch ffmpeg-batch.install ffmpeg-batch.portable lyx [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/7d6ed83dc443ab1ce9c9413ee2a35db4fc17012f

--- automatic/lyx/tools/chocolateyinstall.ps1
+++ automatic/lyx/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\LyX-2344-Installer-4.exe"
+  file        = "$toolsDir\LyX-2351-Installer-3.exe"
   silentArgs  = "/S"
 }
 

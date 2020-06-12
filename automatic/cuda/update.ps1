21e8419e68d95315ad956944d269140714d28b95
chtof <chtof62@gmail.com>
2020-06-12 20:45:59 +0200

    Update update.ps1

--- automatic/cuda/update.ps1
+++ automatic/cuda/update.ps1
@@ -2,21 +2,21 @@
 
 function global:au_GetLatest {
     $releases = 'https://developer.nvidia.com/cuda-downloads'
-    $regex    = '/(?<MajorVersion>[\d\.]+)/Prod/local_installers/(?<File>cuda_(?<Version>[\d\.]+)_[\d\.]+_win10.exe)'
+    $regex    = '(/(?<MajorVersion>[\d\.]+)/Prod/local_installers/(?<File>cuda_(?<Version>[\d\.]+)_[\d\.]+_win10.exe))'
 
-    (Invoke-WebRequest -Uri $releases -UseBasicParsing).RawContent -match $regex | Out-Null
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing).RawContent -match '/(?<MajorVersion>[\d\.]+)(/Prod)?/local_installers/(?<File>cuda_(?<Version>[\d\.]+)_[\d\.]+_win10.exe)' | Out-Null
 
     return @{
         Version = $matches.Version
-        URL32   = 'https://developer.download.nvidia.com/compute/cuda/' + $matches.MajorVersion + '/Prod/local_installers/' + $matches.File
+        URL32   = 'https://developer.download.nvidia.com/compute/cuda' + $matches.0
     }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^(\s)*url\s*=\s*)('.*')"      = "`${1}'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum32)'"
         }
     }
 }

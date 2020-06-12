6471d6377c9bf1a1583ffd04a53c92ba0539a09e
chtof <chtof62@gmail.com>
2020-06-12 21:05:04 +0200

    Update update.ps1

--- automatic/natron/update.ps1
+++ automatic/natron/update.ps1
@@ -1,23 +1,11 @@
-﻿import-module au
-
-function global:au_GetLatest {
-    $github_repository = 'NatronGitHub/Natron'
-    $releases = 'https://github.com/' + $github_repository + '/releases/latest'
-    $regex    = 'Natron-(?<Version>[\d\.]+)-Windows-x86_32bit-setup.exe$'
-
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url      = $download_page.links | ? href -match $regex | select -First 1
-    $version  = $matches.Version
-
-    return @{ Version = $version }
-}
+﻿. $PSScriptRoot\..\natron.install\update.ps1
 
 function global:au_SearchReplace {
-    @{
+   @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
-update -ChecksumFor none -NoCheckUrl
+update -ChecksumFor none

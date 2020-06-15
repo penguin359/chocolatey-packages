077d12acd0145a18b879b72cf17c9de519bc39ef
Chocolatey <chocolatey@realdimensions.net>
2020-06-15 08:24:33 +0000

    AU: 3 updated - automouseclick gitextensions.portable spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/cce3aa28ab892060167c4fda8662d6a24be75628

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'C854C4ECFB4CD0110C3EC33BBB41EC6D8AB37032E8E0035ED844B180EC040405'
+  checksum     = '0A6D94DA659D99651F8285AC08CE55EF3E24C4032E52CBEE15DD545F0B301FE9'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"

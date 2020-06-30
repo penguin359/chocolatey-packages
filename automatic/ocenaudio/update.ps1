7e0756cbffd19cb9ee3a333ee8a683c74eb25a1d
chtof <chtof62@gmail.com>
2020-06-30 19:50:50 +0200

    Update update.ps1

--- automatic/ocenaudio/update.ps1
+++ automatic/ocenaudio/update.ps1
@@ -1,20 +1,12 @@
-﻿import-module au
-
-function global:au_GetLatest {  
-    $releases = 'https://www.ocenaudio.com'
-    $regex    = '\<h1\>ocenaudio (?<Version>[\d\.]+)\</h1\>'
-
-    (Invoke-WebRequest -Uri $releases).RawContent  -match $regex | Out-Null
-
-    return @{ Version = $matches.Version }
-}
+﻿$ErrorActionPreference = 'Stop'
+. $PSScriptRoot\..\ocenaudio.install\update.ps1
 
 function global:au_SearchReplace {
-    @{
+   @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
-update
+update -ChecksumFor none -noCheckUrl

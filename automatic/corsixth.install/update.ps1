27ee8379f0f22183773c5aef23a72896392ba2d8
chtof <chtof62@gmail.com>
2020-07-01 20:48:21 +0200

    Update update.ps1

--- automatic/corsixth.install/update.ps1
+++ automatic/corsixth.install/update.ps1
@@ -3,31 +3,28 @@
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-    $github_repository = "CorsixTH/CorsixTH"
-    $releases = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex = '<title>Release CorsixTH (?<Version>[\d\.]+) · '
-
-    $download_page = (Invoke-WebRequest -Uri $releases).RawContent -match $regex
-    $version = $matches.Version
+    $github_repository = 'CorsixTH/CorsixTH'
+    $releases = Get-RedirectedUrl ('https://github.com/' + $github_repository + '/releases/latest')
+    $version  = $releases -replace '.*?([\d\.]+)$', '$1'
 
     return @{
         Version = $version
-        URL32 = 'https://github.com/' + $github_repository + '/releases/download/v' + $version + '/CorsixTHInstaller.exe'
+        URL32   = 'https://github.com/' + $github_repository + '/releases/download/v' + $version + '/CorsixTHInstaller.exe'
     }
 }
 
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"            
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
         }
 
         "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`${1}$($Latest.FileName32)`""
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`${1}$($Latest.FileName32)`""
         }
     }
 }

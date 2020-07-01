5bbc6036c82de529104e27cd583a8b6e8b4da9e6
chtof <chtof62@gmail.com>
2020-07-01 20:48:32 +0200

    Update update.ps1

--- automatic/corsixth/update.ps1
+++ automatic/corsixth/update.ps1
@@ -1,24 +1,11 @@
-﻿import-module au
-
-function global:au_GetLatest {
-    $github_repository = "CorsixTH/CorsixTH"
-    $releases = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex = '<title>Release CorsixTH (?<Version>[\d\.]+) · '
-
-    $download_page = (Invoke-WebRequest -Uri $releases).RawContent -match $regex
-    $version = $matches.Version
-
-    return @{
-        Version = $version
-    }
-}
+﻿. $PSScriptRoot\..\corsixth.install\update.ps1
 
 function global:au_SearchReplace {
    @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
-update -ChecksumFor none -NoCheckUrl
+update -ChecksumFor none

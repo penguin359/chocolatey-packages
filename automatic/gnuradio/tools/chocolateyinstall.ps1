8031f3919834a093a428ac1669d2393a853ec21d
Chocolatey <chocolatey@realdimensions.net>
2020-07-02 17:58:06 +0000

    AU: 1 updated - gnuradio [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/7e3b97f0a28a80a7d150d0ada6d9bf7dcd865eb5

--- automatic/gnuradio/tools/chocolateyinstall.ps1
+++ automatic/gnuradio/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   filetype       = 'MSI'
 
-  url64          = 'http://www.gcndevelopment.com/gnuradio/downloads/installers/v1.7.0Beta/gnuradio_3.8.0.0_win64.msi'
-  checksum64     = '6251F99B166BE2DA5C82ACCB3CC1E1D3CAA7A8A4E573C54DD79F4F51D47C8E77'
+  url64          = 'http://www.gcndevelopment.com/gnuradio/downloads/installers/v1.7.0/gnuradio_3.8.1.0_win64.msi'
+  checksum64     = '32223e46e3e8136689a6b6f64238607abb66fe7e272c38a5acc9043bbf6da241'
   checksumType64 = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

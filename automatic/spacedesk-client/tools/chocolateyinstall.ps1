fd069cc9f5914033c19ea2ab3aba61a33013b1c7
Chocolatey <chocolatey@realdimensions.net>
2020-07-02 18:16:33 +0000

    AU: 1 updated - spacedesk-client [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/fa758ff0610b3b7749eb840c08685b687e493252

--- automatic/spacedesk-client/tools/chocolateyinstall.ps1
+++ automatic/spacedesk-client/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   filetype      = 'MSI'
 
-  url           = 'https://spacedesk.net/downloads/spacedeskWindowsVIEWER_v0921_BETA.msi'
-  checksum      = '29367073721b37adcf8f70e95e1656b72dd39509ccf2dd83bb323d7f0eeac134'
+  url           = 'https://spacedesk.net/downloads/spacedeskWindowsVIEWER_v0922_BETA.msi'
+  checksum      = '1d2d5eee24fe5e84a3cb47b3ebc74578adc5c6c967a2d7734609709036125c08'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

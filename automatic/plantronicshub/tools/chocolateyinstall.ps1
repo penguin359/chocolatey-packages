1c4b5cab3c05b3c9b7a1e7e690eb6012d36bfda3
Chocolatey <chocolatey@realdimensions.net>
2020-07-02 16:26:35 +0000

    AU: 3 updated - plantronicshub umbrello umbrello.install [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/2e6449389419b9c35c5cd447699220d0aa570e16

--- automatic/plantronicshub/tools/chocolateyinstall.ps1
+++ automatic/plantronicshub/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.poly.com/content/dam/www/software/PlantronicsHubInstaller.exe'
-  checksum     = '1d7eb32d0dc0abf859740f01415f6511600ce029876cc7eb951e3dd53ec1fbf1'
+  checksum     = 'd45d46c1f336caf9f2b5a96fffffc1b574ddb383d90c8a3fa3ac6cc3a4a97c43'
   checksumType = 'sha256'
 
   silentArgs   = '/install /quiet /norestart'
--- automatic/umbrello.install/tools/chocolateyinstall.ps1
+++ automatic/umbrello.install/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://download.kde.org/stable/umbrello/latest/win32/umbrello-mingw32-2.31.1-lp151.46.1-setup.exe'
-  checksum     = '19AA1D7D1B24EB324DFADE1F61D5B8A96007C607A4F919AA01AFFE28A3874490'
+  url          = 'https://download.kde.org/stable/umbrello/latest/win32/umbrello-mingw32-2.31.2-lp151.49.1-setup.exe'
+  checksum     = ''
   checksumType = 'sha256'
 
-  file64       = "$toolsDir\umbrello-mingw64-2.31.1-lp151.118.1-setup.exe"
+  file64       = "$toolsDir\umbrello-mingw64-2.31.2-lp151.121.1-setup.exe"
 
   silentArgs   = '/S'
 }

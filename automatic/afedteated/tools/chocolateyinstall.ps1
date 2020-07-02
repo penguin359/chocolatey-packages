3907c978ae5752311fda882a9ed5b4587f25d7f3
Chocolatey <chocolatey@realdimensions.net>
2020-07-02 08:24:32 +0000

    AU: 5 updated - afedteated log4om log4om.install log4om.portable spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/64877f93eb41b12e33ef29268c8aecf9816f759d

--- automatic/afedteated/tools/chocolateyinstall.ps1
+++ automatic/afedteated/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'http://update.iobit.com/dl/advanced-systemcare-setup.exe'
-  checksum     = 'a25502f6ef3fa776c56f3716190fba9286c805cad1817065e6b653016930ecc58df80545e52406f920d3f8f812eb836e6dac6477aa38e523788cdd608041445b'
+  checksum     = '7c9cb2ec898c6239ec49e028e5584414fd6d81b3c867a99014e29dc32868eb3c93a77314975eb91e49e8b7be2326c8f53b60c058dec955cee5b8d51d0a86f9c2'
   checksumType = 'sha512'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'

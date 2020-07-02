547a07cc02310d7bedd3a526c701c40280d26c12
chtof <chtof62@gmail.com>
2020-07-02 20:01:55 +0200

    Update chocolateyinstall.ps1

--- automatic/stremio/tools/chocolateyinstall.ps1
+++ automatic/stremio/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   checksum     = 'a1d8ef3cdab9ddbff6eda702677288e5df106f0feb369661395ebc5c2c691aa9'
   checksumType = 'sha256'
 
-  silentArgs   = "/S"
+  silentArgs   = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs

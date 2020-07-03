4c1bd41711b9086141829c6f4a7f0f931a932ead
Chocolatey <chocolatey@realdimensions.net>
2020-07-03 16:25:44 +0000

    AU: 6 updated - apo-ok apo-ok.install apo-ok.portable automouseclick cudatext umbrello.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/ff03bb27b7f242aa7e06d01dae29cf15df243b37

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'B430D665E02F4A3DAFDFBC16C40B9D9F55FCB38920748F5AD48A6E36329CD927'
+  checksum     = 'A76DB65F9E99A6D70C6350EEF13DCC586DCBEF5B6804D7B323763AE089D5CF13'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"

302688cbe92a3ae7f7e126708fa7328bb52e4d9d
Chocolatey <chocolatey@realdimensions.net>
2020-07-07 16:25:06 +0000

    AU: 4 updated - automouseclick slm spacedesk-client spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/6b0e8896366d6bbdb868154e9d3162a4899bb806

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '6A163AB11F6E19057AB89109D85CC9897A7655CE392BF6D45E68661C998411D0'
+  checksum     = '447EAA4E38DF2146144540231B6FE53E35EFFC4C1F872A39655B33A7B17AB041'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"

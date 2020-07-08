2666bd3d99909e8da5dee1877be673d1dc800488
Chocolatey <chocolatey@realdimensions.net>
2020-07-08 08:25:02 +0000

    AU: 5 updated - gravitdesigner gravitdesigner.install gravitdesigner.portable spacedesk-client spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/907786ac693c4497c15287757ef2776b0ac0847c

--- automatic/gravitdesigner.install/tools/chocolateyinstall.ps1
+++ automatic/gravitdesigner.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'https://designer.gravit.io/_downloads/windows/GravitDesignerInstaller.exe'
-  checksum      = '7302a4db1919ea54b39e40ca54dcebba671e9a6a4c710558440625cb2450c188'
+  checksum      = '8ddf92b87b761f68a91299252b438ad30548fa7d495f3ce1f56a6764ab8863f7'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"

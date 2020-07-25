f947bad8ebdcd30639c3a93f3a861eac2871da60
chtof <chtof62@gmail.com>
2020-07-25 12:42:23 +0200

    Update update.ps1

--- automatic/stremio/update.ps1
+++ automatic/stremio/update.ps1
@@ -3,7 +3,7 @@
 function global:au_GetLatest {
     $releases      = 'https://www.stremio.com'
     $regex         = '(https://www.strem.io/download.*)'
-    $regex_version = 'https://dl.strem.io/win/v[\d\.]+/Stremio\+(?<Version>[\d\.]+).exe'
+    $regex_version = 'https://dl.strem.io/(shell-)?win/v[\d\.]+/Stremio\+(?<Version>[\d\.]+).exe'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	$url = $download_page.links.href -match $regex | select -First 1

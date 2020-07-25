d4be2ddeb5eb17762d21e1e1fe0fd67f0ba1d7d0
chtof <chtof62@gmail.com>
2020-07-25 13:03:43 +0200

    Update update.ps1

--- automatic/jtdx/update.ps1
+++ automatic/jtdx/update.ps1
@@ -11,7 +11,7 @@ function global:au_GetLatest {
 
     return @{
         Version = $matches.Version
-        URL32   = 'https://www.jtdx.tech/downloads/Windows/' +$matches.0
+        URL32   = 'https://www.jtdx.tech' + $url
     }
 }
 
@@ -31,4 +31,4 @@ function global:au_SearchReplace {
     }
 }
 
-update -ChecksumFor none
+update -ChecksumFor none -noCheckUrl

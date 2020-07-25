a7a26d4f11a97f3b29d8c3ca83138391965bb5c7
chtof <chtof62@gmail.com>
2020-07-25 11:54:14 +0200

    Update chocolateyinstall.ps1

--- automatic/screenpresso/tools/chocolateyinstall.ps1
+++ automatic/screenpresso/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
     
   url          = 'https://www.screenpresso.com/binaries/releases/stable/dotnet47/Screenpresso.exe'
-  checksum     = '4403adef0fee961ed47c915896706de6c200560fc3feedf4415207e8bc28c79c'
+  checksum     = '43e3325d417add29a33c1e3a3a701fa2aa24e660694ceeead7a460841cd58886'
   checksumType = 'sha256'
   
   silentArgs   = '-install pf -silent'

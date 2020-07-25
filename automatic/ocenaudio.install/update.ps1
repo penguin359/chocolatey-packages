1db37883b0d5fbab2ac8e155a01951afe446188b
chtof <chtof62@gmail.com>
2020-07-25 12:34:21 +0200

    Update update.ps1

--- automatic/ocenaudio.install/update.ps1
+++ automatic/ocenaudio.install/update.ps1
@@ -29,4 +29,6 @@ function global:au_SearchReplace {
     }
 }
 
-update -ChecksumFor none -noCheckUrl
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+  update -ChecksumFor none -noCheckUrl
+}

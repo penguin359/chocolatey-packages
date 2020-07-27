badcf22e5c444833a6e91b28838b033962a0d0fc
Chocolatey <chocolatey@realdimensions.net>
2020-07-27 08:24:43 +0000

    AU: 2 updated - virtualhere-client virtualhere-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/e18697088529b9a9cb5457f893150d36044f9a65

--- automatic/virtualhere-client/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-client/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   fileFullPath   = "$toolsDir\vhui$(Get-OSArchitectureWidth).exe"
 
   url            = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui32.exe'
-  checksum       = 'ec11ddae91c5feddd4678de2e0a570e12c98c14d188844f844c9dfc069209cd9'
+  checksum       = 'a7ea9a7a52d7250511493113ff78ebdd6320a856dcf111db613808d15c8c2308'
   checksumType   = 'sha256'
 
   url64          = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui64.exe'
-  checksum64     = '19a4184cd7eaf4954c50c0a5bba3b224b1c46920d6856e27830a19cd5ea8316a'
+  checksum64     = 'fc2ec83929cf077b0d02ee6f15a0d0e2159c21665b281a220badbd4b342089f1'
   checksumType64 = 'sha256'
 }
 
--- automatic/virtualhere-server/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-server/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   fileFullPath   = "$toolsDir\vhusbdwin$(Get-OSArchitectureWidth).exe"
 
   url            = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin32.exe'
-  checksum       = 'bcf5fec311794def6ae0bbe30828b667b43a92765b1aad41751037d59f3f84dd'
+  checksum       = 'a1f7851ff867a74c9ddfe3cee79c117eafad87188cc16b7e0dce9382319e6373'
   checksumType   = 'sha256'
 
   url64          = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin64.exe'
-  checksum64     = 'e902731b5326f2573647b156059df330ece4715a06c4e6c3f2e5023d36fbd956'
+  checksum64     = '58b14a18d047d6d13c743c7cc95fc6526eac8783b76f7ad699a01157abf9da05'
   checksumType64 = 'sha256'
 }
 

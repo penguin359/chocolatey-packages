bf656645d0cba324f3831b7aef1da6f3626dd6a8
Chocolatey <chocolatey@realdimensions.net>
2020-07-27 16:27:27 +0000

    AU: 3 updated - buffalo-nas-navigator spacedesk-client spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/7a4b934968b2bdfc39ae84d5a22a748b7e66dd1a

--- automatic/buffalo-nas-navigator/tools/chocolateyinstall.ps1
+++ automatic/buffalo-nas-navigator/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   unzipLocation = "$toolsDir"
   fileType      = 'exe'  
 
-  url           = 'https://www.buffalotech.com/support/download/nasnavi-29b.zip'
-  checksum      = 'a5d3c73aa23dd709aa4161deaeff815a0ece17eb51ed4bcadc6836ac28cf3fed'
+  url           = 'https://www.buffalotech.com/support/download/nasnavi-302.zip'
+  checksum      = '3c8bc161a0c47039058f96d225a9d15012022c54cfb20cd4473652e4fd71bdf3'
   checksumType  = 'sha256'  
 }
 Install-ChocolateyZipPackage @packageArgs
@@ -15,7 +15,7 @@ Install-ChocolateyZipPackage @packageArgs
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName 
   fileType    = 'exe'
-  file        = "$toolsDir\nasnavi-29b\NASNaviInst.exe"
+  file        = "$toolsDir\nasnavi-302\NASNaviInst.exe"
 }
 Start-Process 'AutoHotKey' -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
 Install-ChocolateyInstallPackage @packageArgs

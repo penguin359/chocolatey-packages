d53f556ae6a08a3e82cfbbd39bbc3fc130926658
Chocolatey <chocolatey@realdimensions.net>
2020-07-31 00:25:08 +0000

    AU: 7 updated - apo-ok.portable egnyte-connect geogebra6 geogebra-geometry luxcorerender luxcorerender-sdk webex-teams [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/c608c9dfd0da69ac4fb294522259c87a8ada42c6

--- automatic/egnyte-connect/tools/chocolateyinstall.ps1
+++ automatic/egnyte-connect/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   filetype     = 'MSI'
 
-  url          = 'https://egnyte-cdn.egnyte.com/egnytedrive/win/en-us/3.8.2/EgnyteConnect_3.8.2_25.msi'
-  checksum     = 'e230fce6344dea255431919aabc735147abe70f8678764271feb7ddc9ae84778'
+  url          = 'https://egnyte-cdn.egnyte.com/egnytedrive/win/en-us/3.8.3/EgnyteConnect_3.8.3_29.msi'
+  checksum     = 'a4a84c1eebf216c5da2c2a76a6c910542452f355293e619a549e311912ff6c7f'
   checksumType = 'sha256'  
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/webex-teams/tools/chocolateyinstall.ps1
+++ automatic/webex-teams/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/WebexTeams.msi'
-  checksum     = 'c73a02a8b444b26b047f616213b9ce08da27f9973cfdbc287dfb1e5237caae98'
+  checksum     = 'd7c80af85305facf7f0cd587f5c273e91bc69e60856394419850cdbadfb0ec3f'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

86ae25a327b7856fcfeafeac9753b676decc73ab
chtof <chtof62@gmail.com>
2020-08-06 21:56:44 +0200

    Update update.ps1

--- automatic/spacedesk-server/update.ps1
+++ automatic/spacedesk-server/update.ps1
@@ -2,27 +2,43 @@
 
 function global:au_GetLatest {
     $releases = 'https://spacedesk.net'
-    $regex = 'spacedesk DRIVER Beta III v(?<Version>[\d\.]+)\</td\>'
-    
-    $url32 = Get-RedirectedUrl https://spacedesk.net/downloadidd32
-    $url64 = Get-RedirectedUrl https://spacedesk.net/downloadidd64
-    $url32_win7_81 = Get-RedirectedUrl https://spacedesk.net/downloadlegacy
-    $checksum32_win7_81 = Get-RemoteChecksum $url32_win7_81
-    $url64_win7_81 = Get-RedirectedUrl https://spacedesk.net/downloadlegacy64
-    $checksum64_win7_81 = Get-RemoteChecksum $url64_win7_81
-    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
+    $regexVersion = 'spacedesk DRIVER (Beta )?(III )?v(?<Version>[\d\.]+)\</td\>'
+    $regex_Win_10_64  = 'spacedesk_driver_Win_10_64_v\d+_BETA.msi'
+    $regex_Win_10_32  = 'spacedesk_driver_Win_10_32_v\d+_BETA.msi'
+    $regex_Win_8_1_64 = 'spacedesk_driver_Win_8.1_64_v\d+_BETA.msi'
+    $regex_Win_8_1_32 = 'spacedesk_driver_Win_8.1_32_v\d+_BETA.msi'
+    #https://spacedesk.net/downloads/spacedesk_driver_Win_10_64_v0954_BETA.msi
+    #https://spacedesk.net/downloads/spacedesk_driver_Win_10_32_v0954_BETA.msi
+    #https://spacedesk.net/downloads/spacedesk_driver_Win_8.1_64_v0943_BETA.msi
+    #https://spacedesk.net/downloads/spacedesk_driver_Win_8.1_32_v0943_BETA.msi
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $download_page.Content -match $regexVersion | Out-Null
+    $version = $matches.Version
+    $url32 = $download_page.links | ? href -match $regex_Win_10_32
+    $url64 = $download_page.links | ? href -match $regex_Win_10_64
+    $url32_win81 = $download_page.links | ? href -match $regex_Win_8_1_32
+    $url64_win81 = $download_page.links | ? href -match $regex_Win_8_1_64
+
+    #$url32 = Get-RedirectedUrl https://spacedesk.net/downloadidd32
+    #$url64 = Get-RedirectedUrl https://spacedesk.net/downloadidd64
+    #$url32_win81 = Get-RedirectedUrl https://spacedesk.net/downloadlegacy
+    $checksum32_win81 = Get-RemoteChecksum $url32_win81.href
+    #$url64_win81 = Get-RedirectedUrl https://spacedesk.net/downloadlegacy64
+    $checksum64_win81 = Get-RemoteChecksum $url64_win81.href
+    #(Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
 
     return @{
-        Version = $matches.Version
+        Version = $version
 
-        URL32 = $url32
-        URL64 = $url64        
+        URL32 = $url32.href
+        URL64 = $url64.href
 
-        URL32_win7_81 = $url32_win7_81
-        Checksum32_win7_81 = $checksum32_win7_81
+        URL32_win81 = $url32_win81.href
+        Checksum32_win81 = $checksum32_win81
 
-        URL64_win7_81 = $url64_win7_81
-        Checksum64_win7_81 = $checksum64_win7_81
+        URL64_win81 = $url64_win81.href
+        Checksum64_win81 = $checksum64_win81
     }
 }
 
@@ -33,10 +49,10 @@ function global:au_SearchReplace {
             "(^(\s)*[$]checksum32_win10\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
             "(^(\s)*[$]url64_win10\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
             "(^(\s)*[$]checksum64_win10\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
-            "(^(\s)*[$]url32_win7_81\s*=\s*)('.*')"      = "`$1'$($Latest.URL32_win7_81)'"
-            "(^(\s)*[$]checksum32_win7_81\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32_win7_81)'"
-            "(^(\s)*[$]url64_win7_81\s*=\s*)('.*')"      = "`$1'$($Latest.URL64_win7_81)'"
-            "(^(\s)*[$]checksum64_win7_81\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64_win7_81)'"
+            "(^(\s)*[$]url32_win7_81\s*=\s*)('.*')"      = "`$1'$($Latest.URL32_win81)'"
+            "(^(\s)*[$]checksum32_win7_81\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32_win81)'"
+            "(^(\s)*[$]url64_win7_81\s*=\s*)('.*')"      = "`$1'$($Latest.URL64_win81)'"
+            "(^(\s)*[$]checksum64_win7_81\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64_win81)'"
         }
     }
 }

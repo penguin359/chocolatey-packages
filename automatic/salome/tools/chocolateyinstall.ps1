d68c20ff02886258a4b91273e02a084d58e970ff
chtof <chtof62@gmail.com>
2020-08-10 23:00:17 +0200

    Update chocolateyinstall.ps1

--- automatic/salome/tools/chocolateyinstall.ps1
+++ automatic/salome/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$toolsDir"
 
-  url64bit      = 'ftp://ftp.cea.fr/pub/salome/targz/SALOME-9.4.0/SALOME-9.4.0-W10.tar.gz'
+  url64         = 'ftp://ftp.cea.fr/pub/salome/targz/SALOME-9.4.0/SALOME-9.4.0-W10.tar.gz'
   checksum64    = '1CD0EA104CDC006B8B4E17DAAC2271C89B2C0A49A8DD4B48F7FE1D5CBA36D8DA'
   checksumType64= 'sha256'
 }

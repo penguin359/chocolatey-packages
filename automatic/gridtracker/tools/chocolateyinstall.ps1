95960d282dbba19ef40709e552167ac335cf0f3e
Chocolatey <chocolatey@realdimensions.net>
2020-08-19 16:40:57 +0000

    AU: 4 updated - gridtracker tjs tjs.install tjs.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/baf75cb94e93bf3f4f85d759d194d7a65af56d81

--- automatic/gridtracker/tools/chocolateyinstall.ps1
+++ automatic/gridtracker/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://uc6238df92ea9996adf461a5bd29.dl.dropboxusercontent.com/cd/0/get/A89uQtB29iBh-_6zzLgvlGwKR1nMAiduX46vcEzyJmdEO9_nflYr6DBTT-nOErMrrPDWSNGNOcYt39KZd-W4nbOaBfZ5YRUw7r5KVhGlLo7Uhux7huRytIn_V5kIAXPgnAQ/file?dl=1#'
-  checksum     = '3a2ff71d760675ad4c51006a193aa924a80529146efcf8365d7aaf7e56b22c00'
+  url          = 'https://ucf437382485f56470b815913b75.dl.dropboxusercontent.com/cd/0/get/A9y0TGaiex4KHrcLQJmJ32ou_gqsJTCzg5LyKA4X4-zQrWbr8SIUsozXbm854zpCSyKGTXjIwXHT6xHlpE8MNdTe8W8rUBJSnyFME3PWjkHxXciu3yyMEL8szjRiuySjK2E/file?dl=1#'
+  checksum     = '84658e597839cbd7dec09a1c1d91cee3cb01a5074de6fba51e43f6675124b161'
   checksumType = 'sha256'
 
   silentArgs   = '/S'
--- automatic/tjs.install/tools/chocolateyinstall.ps1
+++ automatic/tjs.install/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
-  url64          = 'https://netcologne.dl.sourceforge.net/project/jasperstudio/JaspersoftStudio-6.13.0/TIB_js-studiocomm_6.13.0_windows_x86_64.exe'
-  checksum64     = '0642f39d1161fdec2347b6eb6ca2474e259c6866a9260d508ec2071479972caf'
+  url64          = 'https://netcologne.dl.sourceforge.net/project/jasperstudio/JaspersoftStudio-6.14.0/TIB_js-studiocomm_6.14.0_windows_x86_64.exe'
+  checksum64     = 'ab37d2e03e14958a414c6cbbf33cd675040286723a99ef4ce330bfb0ee7e01dd'
   checksumType64 = 'sha256'
 
   silentArgs     = "/S"

d799b068069cd52c3e6a946b7535d4e8864177a4
Chocolatey <chocolatey@realdimensions.net>
2020-08-25 00:24:30 +0000

    AU: 1 updated - kafka-tool [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/bb26549ed98d1c2740660a73e24f3a46a3e99ac5

--- automatic/kafka-tool/tools/chocolateyinstall.ps1
+++ automatic/kafka-tool/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
   url           = 'http://www.kafkatool.com/download2/kafkatool.exe'
-  checksum      = 'f5dc6b9fe17c6ab6fe3e3d39109689824edd276401e45b19ba5d1ac52597e4ab'
+  checksum      = '150bb85a78023a6fb5d9cab233ea551657a38089fc28ce390761e1b69823145e'
   checksumType  = 'sha256'
 
   url64bit      = 'http://www.kafkatool.com/download2/kafkatool_64bit.exe'
-  checksum64    = '3ba44d11b15172701c3c9e398dbe78b2427813ea647d97300ec80c884dc5a97e'
+  checksum64    = '02190c8db5d87e49dc39bf3ebff6d9e907107db782bb9a2caae86e7163ccbb27'
   checksumType64= 'sha256'
 
   silentArgs    = "-q"

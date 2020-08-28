6c72d637a6f12d0436971f068f7f9a80b903373f
Chocolatey <chocolatey@realdimensions.net>
2020-08-28 16:24:53 +0000

    AU: 1 updated - mp3skyperecorder [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/7f112430eebe52d25741be30a23a70812caf85da

--- automatic/mp3skyperecorder/tools/chocolateyinstall.ps1
+++ automatic/mp3skyperecorder/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   filetype       = 'msi'
 
-  url            = 'http://download.mp3skyperecorder.com/MP3SkypeRecorderSetup.exe?version=6.0.12'
-  checksum       = '2500f3e59eda7ad4d70dc3119c2895d5a1037b54a77907b76b8394e8108a658c'
+  url            = 'http://download.mp3skyperecorder.com/MP3SkypeRecorderSetup.exe?version=6.1.3'
+  checksum       = '4156ad44f854451dd0c127d389427e7d4932608fd8fbc86de9c3f6c7b3014fcd'
   checksumType   = 'sha256'
 
   silentArgs	 = "/qn /norestart /l*v `"$env:Temp\$env:ChocolateyPackageName.MsiInstall.log`""

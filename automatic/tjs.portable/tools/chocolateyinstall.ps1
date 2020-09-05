656fdd6e21a04a083831740ddf00875e1af36754
chtof <chtof62@gmail.com>
2020-09-06 01:07:23 +0200

    Update chocolateyinstall.ps1

--- automatic/tjs.portable/tools/chocolateyinstall.ps1
+++ automatic/tjs.portable/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$toolsDir"  
 
-  url64          = 'https://netcologne.dl.sourceforge.net/project/jasperstudio/JaspersoftStudio-6.13.0/TIB_js-studiocomm_6.13.0_windows_x86_64.zip'
+  url64          = 'https://downloads.sourceforge.net/project/jasperstudio/JaspersoftStudio-6.13.0/TIB_js-studiocomm_6.13.0_windows_x86_64.zip'
   checksum64     = 'b2a3b099c1ef5ff2c7fcc92aed1fbe8b52883a81528e34a16502c4b0b1a583af'
   checksumType64 = 'sha256'  
 }
@@ -13,13 +13,13 @@ $packageArgs = @{
 Install-ChocolateyZipPackage @packageArgs
 
 # Don't create shims for other executables
-$files = Get-ChildItem "$toolsDir" -Recurse -Include *.exe -Exclude "JasperSoft Studio.exe"
+$files = Get-ChildItem "$toolsDir" -Recurse -Include *.exe -Exclude 'JasperSoft Studio.exe'
 foreach ($file in $files) {
   New-Item "$file.ignore" -type file -force | Out-Null
 }
 
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path $programs "JasperSoft Studio.lnk"
-$targetPath = Join-Path $toolsDir "jaspersoftstudio\JasperSoft Studio.exe"
+$shortcutFilePath = Join-Path $programs 'JasperSoft Studio.lnk'
+$targetPath = Join-Path $toolsDir 'jaspersoftstudio\JasperSoft Studio.exe'
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

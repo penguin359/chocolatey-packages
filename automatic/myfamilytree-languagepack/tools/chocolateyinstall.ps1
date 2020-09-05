28751e6ad68c05082efbdd55a61585983099d6ad
Chocolatey <chocolatey@realdimensions.net>
2020-09-05 23:15:57 +0000

    AU: 3 updated - jami myfamilytree myfamilytree-languagepack [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/941d20d81ace4c312d7d319db46cd332903fd1b2

--- automatic/myfamilytree-languagepack/tools/chocolateyinstall.ps1
+++ automatic/myfamilytree-languagepack/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType		= "msi"
   
-  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack10.2.5000.0.msi'
-  checksum      = '1daf8f2d6eaf0844d5eab999fbfc50eb57c1420f5effcfae0123845604d221e5'
+  url           = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack10.4.1000.0.msi'
+  checksum      = '9cb1d068804b80fe22e29dc16a9aff8036283b00a140023bbc64957c2b4cc8b2'
   checksumType  = 'sha256'
   
-  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack10.2.5000.0x64.msi'
-  checksum64    = '526a836fd3a7c9ec0c175ed1e45b8ec0359b25013dee0b0e0f2822724407a4c3'
+  url64bit      = 'https://chronoplexsoftware.com/myfamilytree/downloads/myfamilytree_languagepack10.4.1000.0x64.msi'
+  checksum64    = '88045762b9ea5df632751d748435fb37274c1ec5eaa60ba0c3a6c50811b4b345'
   checksumType64= 'sha256'
   
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

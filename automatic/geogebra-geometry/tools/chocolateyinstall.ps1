95e3a259b698a8e7526f63c1b2176860ba2d0efc
Chocolatey <chocolatey@realdimensions.net>
2020-09-07 08:44:10 +0000

    AU: 4 updated - dontsleep dontsleep.install dontsleep.portable geogebra-geometry [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/82f66f344ade65041e4bb4d7ab49e58c3ce473cc

--- automatic/geogebra-geometry/tools/chocolateyinstall.ps1
+++ automatic/geogebra-geometry/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file     = 'GeoGebraGeometry-Windows-Installer-6-0-598-0.exe'
+$file     = 'GeoGebraGeometry-Windows-Installer-6-0-603-0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName

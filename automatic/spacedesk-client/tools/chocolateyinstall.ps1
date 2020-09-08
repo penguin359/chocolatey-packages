17185dd21e47fc26cde9194db7cca0351c70bb0f
Chocolatey <chocolatey@realdimensions.net>
2020-09-08 16:24:39 +0000

    AU: 2 updated - bbc-iplayer spacedesk-client [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/2bf1cd171cbe66a8c5135a717c23122f80f9e05b

--- automatic/spacedesk-client/tools/chocolateyinstall.ps1
+++ automatic/spacedesk-client/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   filetype      = 'MSI'
 
-  url           = 'https://spacedesk.net/downloads/spacedeskWindowsVIEWER_v0922_BETA.msi'
-  checksum      = '1d2d5eee24fe5e84a3cb47b3ebc74578adc5c6c967a2d7734609709036125c08'
+  url           = 'https://spacedesk.net/downloads/spacedeskWindowsVIEWER_v0925_BETA.msi'
+  checksum      = 'f272be1e4433dfc6871f06fd0139398aea771d7a05ba51599dccff08e8a7b121'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

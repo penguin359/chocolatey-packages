877e8be7364506bd06efd8560c9d9593a3c2590a
Chocolatey <chocolatey@realdimensions.net>
2020-09-09 16:25:12 +0000

    AU: 4 updated - chirp chirp.install chirp.portable libre-hardware-monitor [skip ci]

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-daily-20200807-installer.exe"
+  file          = "$toolsDir\chirp-daily-20200909-installer.exe"
   silentArgs    = '/S'
 }
 

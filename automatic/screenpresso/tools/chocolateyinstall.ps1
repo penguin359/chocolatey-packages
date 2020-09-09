c2b5f0d8c35ab5cc6997a2d7b43aad875292d045
Chocolatey <chocolatey@realdimensions.net>
2020-09-09 21:04:32 +0000

    AU: 1 updated - screenpresso [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/6aca26e82ce429663a4472218f0abf4b4f3d849c

--- automatic/screenpresso/tools/chocolateyinstall.ps1
+++ automatic/screenpresso/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.screenpresso.com/binaries/releases/stable/dotnet47/Screenpresso.exe'
-  checksum     = '43e3325d417add29a33c1e3a3a701fa2aa24e660694ceeead7a460841cd58886'  
+  checksum     = 'c7767deb817984cb8d15a2e1a70b23d67dadf422e6df4cf66f42886184a8a0ab'  
   checksumType = 'sha256'
 
   silentArgs   = '-install pf -silent'

02455b0870cc403f98b1e91019e611d480ba1dcc
Chocolatey <chocolatey@realdimensions.net>
2020-09-18 00:24:31 +0000

    AU: 1 updated - seppdf [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/7ddc937b1fb814d122f2f1cff48d5488a95cdf32

--- automatic/seppdf/tools/chocolateyinstall.ps1
+++ automatic/seppdf/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$toolsDir" 
 
-  url           = 'https://www.ne.jp/asahi/foresth/home/spdf333.zip'
-  checksum      = 'edb0381e4d1d3d351ba2e5207172ce266086badfdf853a83006bddde3f7205f5'
+  url           = 'https://www.ne.jp/asahi/foresth/home/spdf335.zip'
+  checksum      = 'c5b10bea43ef64717d5bca519de4f5c0c8b5959ab11f055a125dbeba3cbc7f65'
   checksumType  = 'sha256'
 }
 

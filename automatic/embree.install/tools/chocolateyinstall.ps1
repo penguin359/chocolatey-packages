063e2c9f0a484e87fb7e94942ec76e0987f94d80
Chocolatey <chocolatey@realdimensions.net>
2020-09-23 08:24:21 +0000

    AU: 2 updated - embree embree.install [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/2b2268851ff3d386b42cc52014452137242d2470

--- automatic/embree.install/tools/chocolateyinstall.ps1
+++ automatic/embree.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   filetype    = "MSI"
-  file64      = "$toolsDir\embree-3.11.0.x64.vc14.msi"
+  file64      = "$toolsDir\embree-3.12.0.x64.vc14.msi"
   silentArgs  = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 }
 

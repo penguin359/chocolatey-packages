0917dd691eac0c2a27e3e7e337858c5e69527388
Chocolatey <chocolatey@realdimensions.net>
2020-09-23 00:26:17 +0000

    AU: 4 updated - gridtracker ocenaudio ocenaudio.install ocenaudio.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/fbea0d92b0d7b149dfedb8cc17460595d5de89dd

--- automatic/gridtracker/tools/chocolateyinstall.ps1
+++ automatic/gridtracker/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://uc2108038200c69e00c7e5e77285.dl.dropboxusercontent.com/cd/0/get/A-5nXFb41730k3u09zKb0tTmiRjG4QgGA_b4WaOFd-olOyBoS4QHvkB6mVSfoBBt9zYx3VDUWrWbD5Jlx0G7VI3bU_pJKcwSvANt5UTJQGxkQiw-cqXbNCq9gSyz151-RUQ/file?dl=1#'
-  checksum     = '0df860f3c8d1fe9913da48e4d5260ef011d27a3c11de4886167f56ea0e0d6c08'
+  url          = 'https://ucfdf9565ab9d7472e8565ff9367.dl.dropboxusercontent.com/cd/0/get/A_6MRHZFP9OI4nEj9csk-Ap2i3OJ-VdtoP0hsPMMQQktlrki2kPx7uOHb1Dta0blKT-_H5myp99mc2KaJw0NQFNx8_fuvJbPPRAOlMVag6LuE6fQfMmS0EqzW5mDwZeLIXk/file?dl=1#'
+  checksum     = '67cbee2bc9c1736f4b9305382edba2e3f5290274cad2a844bdeed92a866ae8a7'
   checksumType = 'sha256'
 
   silentArgs   = '/S'

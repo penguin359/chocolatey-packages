880fb1ac83a48182958cc8bf4239ab14b4248003
chtof <chtof62@gmail.com>
2020-09-25 20:07:29 +0200

    Update chocolateyinstall.ps1

--- automatic/auto-dark-mode/tools/chocolateyinstall.ps1
+++ automatic/auto-dark-mode/tools/chocolateyinstall.ps1
@@ -1,28 +1,23 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$packageArgs = @{
+<#$packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   destination  = "$toolsDir"
   fileFullPath = "$toolsDir\AutoDarkMode_v3_Setup.zip"  
-}
+}#>
 
 if ( [environment]::OSVersion.Version.Major -ne 10 )  {
-  Write-Warning "Skipping installation because Auto Dark Mode supports only Windows 10."  
+  Write-Warning 'Skipping installation because Auto Dark Mode supports only Windows 10.'
   return
 }
 
-Get-ChocolateyUnzip @packageArgs
+#Get-ChocolateyUnzip @packageArgs
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\AutoDarkMode_v3_Setup.exe"
+  file        = "$toolsDir\AutoDarkMode_v3_0_1_Setup.exe"
   silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-}
-
-if ( [environment]::OSVersion.Version.Major -ne 10 )  {
-  Write-Warning "Skipping installation because Auto Dark Mode supports only Windows 10."  
-  return
 }
 
 Install-ChocolateyInstallPackage @packageArgs

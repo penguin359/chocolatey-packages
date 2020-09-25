f107496a9462ea2ca0ecc62fba9feaf30d20e1c2
Chocolatey <chocolatey@realdimensions.net>
2020-09-25 00:24:36 +0000

    AU: 3 updated - clipto clipto.install moneydance [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/ad196edb0eed9fb62a3bcc1f8c829fbb4717ff5c

--- automatic/clipto.install/tools/chocolateyinstall.ps1
+++ automatic/clipto.install/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://github.com/clipto-pro/Desktop/releases/download/v4.1.1/clipto-4.1.1.exe'
-  checksum     = '638152f5a409e8d7071f607d99abab8ecd9979680c718362fc4898065d2bef63'
+  url          = 'https://github.com/clipto-pro/Desktop/releases/download/v4.3.0/clipto-4.3.0.exe'
+  checksum     = 'b33cf88eed203dfa486fbe9909652f951849d84c0c877e8e725225686e9e1e49'
   checksumType = 'sha256'
 
   silentArgs   = '/S'

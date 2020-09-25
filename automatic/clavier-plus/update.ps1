d8a5139aee229a998d4ab296b50e9f92951b24c7
chtof <chtof62@gmail.com>
2020-09-25 21:04:40 +0200

    Update update.ps1

--- automatic/clavier-plus/update.ps1
+++ automatic/clavier-plus/update.ps1
@@ -1,22 +1,11 @@
-﻿import-module au
-
-function global:au_GetLatest {
-    $releases = 'http://utilfr42.free.fr/util/Clavier.php'
-    $regex    = 'content="Clavier\+ (?<Version>[\d\.]+)"'
-
-    (Invoke-WebRequest -Uri $releases).Content -match $regex | Out-Null
-
-     return @{
-        Version = $matches.Version
-    }
-}
+﻿. $PSScriptRoot\..\clavier-plus.install\update.ps1
 
 function global:au_SearchReplace {
-    @{
+   @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
-update
+update -ChecksumFor none

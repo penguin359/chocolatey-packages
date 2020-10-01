ed8487e71d036c1e1d1c4d7ec719cebd38621394
chtof <chtof62@gmail.com>
2020-10-01 19:29:43 +0200

    Update update.ps1

--- automatic/phonerlite/update.ps1
+++ automatic/phonerlite/update.ps1
@@ -1,12 +1,4 @@
-import-module au
-
-function global:au_GetLatest {
-    $releases = 'http://phonerlite.de/download_en.htm'
-    $regex    = '<td>(?<Version>[\d\.]+)</td>'
-
-    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
-    return @{ Version = $matches.Version }
-}
+. $PSScriptRoot\..\phonerlite.install\update.ps1
 
 function global:au_SearchReplace {
    @{

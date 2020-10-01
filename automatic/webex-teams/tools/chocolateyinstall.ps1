71712624810e451dcbeb9e587821cf44b51ca8b3
Chocolatey <chocolatey@realdimensions.net>
2020-10-01 00:24:29 +0000

    AU: 1 updated - webex-teams [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/08b37f790d6eda4ee513a0de95c349597a46b32c

--- automatic/webex-teams/tools/chocolateyinstall.ps1
+++ automatic/webex-teams/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/WebexTeams.msi'
-  checksum     = 'd7c80af85305facf7f0cd587f5c273e91bc69e60856394419850cdbadfb0ec3f'
+  checksum     = '7b5e7723495094959af4f90aa818d0122ef0e127a2bc1bb7a193e9dd7677f6b9'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

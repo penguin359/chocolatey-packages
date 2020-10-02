a9919d20f01855640a5a7ed761556ad2203c24db
chtof <chtof62@gmail.com>
2020-10-02 19:29:00 +0200

    Update update.ps1

--- automatic/dexed/update.ps1
+++ automatic/dexed/update.ps1
@@ -29,6 +29,4 @@ function global:au_SearchReplace {
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none

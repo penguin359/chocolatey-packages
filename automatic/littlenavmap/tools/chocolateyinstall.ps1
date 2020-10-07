d805a5148fde27d89e88fab1ee3abd9215dc064a
chtof <chtof62@gmail.com>
2020-10-07 20:22:38 +0200

    littlenavmap initial commit

---
+++ automatic/littlenavmap/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$toolsDir"
+  file          = "$toolsDir\LittleNavmap-win-2.4.5.zip"
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -Path $packageArgs.file
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs 'Little Navmap.lnk'
+$targetPath = Join-Path $toolsDir 'Little Navmap\littlenavmap.exe'
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
---
+++ automatic/littlenavmap/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop'
+# Remove start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs 'Little Navmap.lnk'
+if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

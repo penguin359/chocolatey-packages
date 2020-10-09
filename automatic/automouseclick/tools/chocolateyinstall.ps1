19d637f289b6f9d2af86daa1f143385f9f2fc5e0
Chocolatey <chocolatey@realdimensions.net>
2020-10-09 16:24:39 +0000

    AU: 7 updated - automouseclick chirp chirp.install chirp.portable libre-hardware-monitor spacedesk-client spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/f45be255e6b3c57792f857669a2f4310527958b7

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '9F661FE8910FAF0AD246389C2B1C9C52AF8A326D7CC92F74752E236E16D73DEE'
+  checksum     = '6EF0E06E52A1FAB3F6B28461C5BA7DB804FC399B0354203AEF3BD4C57D2A85E1'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"

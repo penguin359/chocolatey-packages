809c17308bc701005a1f282274b13d28e56881dd
chtof <chtof62@gmail.com>
2020-10-09 23:10:00 +0200

    Fix to take into account windows user names containing space character

--- automatic/magicsplat-tcl-tk/tools/chocolateyinstall.ps1
+++ automatic/magicsplat-tcl-tk/tools/chocolateyinstall.ps1
@@ -5,12 +5,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
 
-  file          = "$toolsDir\tcl-8.6.10-installer-1.10.0-x86.msi"
-  filetype      = "MSI"
-  file64        = "$toolsDir\tcl-8.6.10-installer-1.10.0-x64.msi"
-  filetype64    = "MSI"
+  file          = "$toolsDir\tcl-8.6.10-installer-1.10.1-x86.msi"
+  filetype      = 'MSI'
+  file64        = "$toolsDir\tcl-8.6.10-installer-1.10.1-x64.msi"
+  filetype64    = 'MSI'
 
-  silentArgs	= "/qn /norestart /l*v $env:Temp\$env:ChocolateyPackageName.MsiInstall.log"
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 }
 
 Install-ChocolateyInstallPackage @packageArgs

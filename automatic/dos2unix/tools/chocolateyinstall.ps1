69cd1c07191aa9f05c738296859230fed529930a
Chocolatey <chocolatey@realdimensions.net>
2020-10-13 00:24:26 +0000

    AU: 1 updated - dos2unix [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/493fad5cec115fed3f8bfbf6ae37b1fda09f47ab

--- automatic/dos2unix/tools/chocolateyinstall.ps1
+++ automatic/dos2unix/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\dos2unix-7.4.1-win32-nls.zip"
-  file64      = "$toolsDir\dos2unix-7.4.1-win64-nls.zip"
+  file        = "$toolsDir\dos2unix-7.4.2-win32-nls.zip"
+  file64      = "$toolsDir\dos2unix-7.4.2-win64-nls.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

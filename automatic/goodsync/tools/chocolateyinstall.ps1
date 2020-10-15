338aec761c6f3026693036eb2594c09b68085152
Chocolatey <chocolatey@realdimensions.net>
2020-10-15 00:24:24 +0000

    AU: 4 updated - dontsleep dontsleep.install dontsleep.portable goodsync [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/d8e691a6d6ef13b56d15170fa6cb63ddaa4e41a9

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-v11-Setup.msi'
-  checksum       = 'ae22e4a2b2b130471c8192317b0ee40e00585cef15be111253c0d3947bc66bca'
+  checksum       = 'd51a7f9ea33e27e74bdfc2f7f3877c84ddce0bd2080fb2d8be86f06b2fcb5f0e'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

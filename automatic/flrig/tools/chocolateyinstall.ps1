2a3424b9a475d75373a2137ac80d51e2884189be
Chocolatey <chocolatey@realdimensions.net>
2020-10-30 16:24:39 +0000

    AU: 3 updated - clockify flrig goodsync [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/f4ce557ace96280209d09bf7dc3dc54bf98e628e

--- automatic/flrig/tools/chocolateyinstall.ps1
+++ automatic/flrig/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ if ($pp['DIR']){
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName  
-  file        = "$toolsDir\flrig-1.3.51_setup.exe"
+  file        = "$toolsDir\flrig-1.3.52_setup.exe"
   silentArgs  = $silentArgs
 }
 
--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-v11-Setup.msi'
-  checksum       = '23bcda7775bca6f54592daefee28cce3a7d12b06a9911f59d278ca2e2a929ca6'
+  checksum       = 'be56e67cc0ede1047ba4bc342c6506c32c33bad8620b7ddafc1ed553948046bc'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

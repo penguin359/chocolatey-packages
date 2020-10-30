2a9418adfc312394996b1b4bf3e1b812d4bfa500
Chocolatey <chocolatey@realdimensions.net>
2020-10-30 08:24:34 +0000

    AU: 2 updated - sublimemerge virtualhere-client [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/b59083106223c9377f0f13d078ab000c1c33cc36

--- automatic/sublimemerge/tools/chocolateyinstall.ps1
+++ automatic/sublimemerge/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://download.sublimetext.com/sublime_merge_build_2027_x64_setup.exe'
-  checksum      = '2f320198cc5d7c137fa0a53b87479c2134473833ee9bfdd5189779a679bea3f1'
+  url           = 'https://download.sublimetext.com/sublime_merge_build_2038_x64_setup.exe'
+  checksum      = 'c25ad40f6829fc9f19865130d6ffaeb3f81a10f621ee5297911bba286b882475'
   checksumType  = 'sha256'
   
   
--- automatic/virtualhere-client/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-client/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   fileFullPath   = "$toolsDir\vhui$(Get-OSArchitectureWidth).exe"
 
   url            = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui32.exe'
-  checksum       = '731e7d41e4870d45518a01de61e9db00a1b8e5ca6424a73416bf6e413f4fa6f2'
+  checksum       = 'ea81a192cc65a9d38306608090563cd37323f5d1a2b437ac5ada06dd36fa2153'
   checksumType   = 'sha256'
 
   url64          = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui64.exe'
-  checksum64     = '6d96385eab93fefa18e70437b5d99de4427f3e86604af9c94d436749efc42869'
+  checksum64     = '5dc579ca88a809781480c20ed6f6f3774be43e96949db9c5ec77cd43f2b06cc3'
   checksumType64 = 'sha256'
 }
 

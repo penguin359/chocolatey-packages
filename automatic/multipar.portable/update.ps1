25093e4fe645457320dc022971831a1b22eb6c00
chtof <chtof62@gmail.com>
2020-11-06 22:51:53 +0100

    Update update.ps1

--- automatic/multipar.portable/update.ps1
+++ automatic/multipar.portable/update.ps1
@@ -5,16 +5,17 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {
     $github_repository = 'Yutaka-Sawada/MultiPar'
     $releases     = 'https://github.com/' + $github_repository    
-    $regex        = "(.*/MultiPar[\d]+.zip)"
+    $regexURL     = "/(?<File>MultiPar[\d]+.zip)"
     $regexVersion = 'Installer version (?<Version>[\d\.]+)'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url_path = ($download_page.links | ? href -match $regex | Select -Last 1).href
+    ($download_page.links | ? href -match $regexURL | Select -Last 1).href | Out-Null
+    $file = $matches.File
     $version = (($download_page.RawContent | Select-String -Pattern $regexVersion -allmatches).matches | Select -last 1).Groups[1].Value
 
     return @{
         Version = $version
-        URL32   = 'https://github.com' + $url_path
+        URL32   = 'https://raw.githubusercontent.com/Yutaka-Sawada/MultiPar/master/' + $file        
     }
 }
 

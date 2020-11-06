07b19503ee6e76a928e829ca0f39ffff4e2d8e82
chtof <chtof62@gmail.com>
2020-11-06 22:31:20 +0100

    Update _update.ps1

--- automatic/multipar/_update.ps1
+++ automatic/multipar/_update.ps1
@@ -1,22 +1,11 @@
-﻿$ErrorActionPreference = 'Stop'
-import-module au
-
-function global:au_GetLatest {
-    $releases = 'https://multipar.eu'
-    $regex    = 'MultiPar(?<Version>[\d]+)_setup.exe$'
-
-    $url     = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
-    $version = $matches.Version -replace "(\d+)(\d)(\d)(\d)", '$1.$2.$3.$4'
-
-    return @{ Version = $version + "00" }
-}
+﻿. $PSScriptRoot\..\multipar.install\update.ps1
 
 function global:au_SearchReplace {
-    @{
+   @{
         "$($Latest.PackageName).nuspec" = @{
             "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
         }
     }
 }
 
-update
+update -ChecksumFor none -NoCheckUrl

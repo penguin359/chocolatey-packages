3d1fda35a2656c7e7c870ff8b4c1573ba55b2ce2
chtof <chtof62@gmail.com>
2020-11-07 20:56:53 +0100

    Update update.ps1

--- automatic/multipar.install/update.ps1
+++ automatic/multipar.install/update.ps1
@@ -35,4 +35,6 @@ function global:au_SearchReplace {
     }
 }
 
-update -ChecksumFor none
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}

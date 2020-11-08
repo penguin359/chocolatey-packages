52e670c277c3d95e50b44f512b2eeb45cc5ec423
Chocolatey <chocolatey@realdimensions.net>
2020-11-08 00:24:17 +0000

    AU: 2 updated - fontforge multipar [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/da2ec2ea86b679f7b40a5ebdec076ff49308bd6a

--- automatic/fontforge/tools/chocolateyinstall.ps1
+++ automatic/fontforge/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\FontForge-2019-08-01-Windows.exe"
+  file        = "$toolsDir\FontForge-2020-11-07-Windows.exe"
   silentArgs  = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 

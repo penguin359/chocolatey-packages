2bad2f1b82d8fffbd198c447a2f8d026f135042f
chtof <chtof62@gmail.com>
2020-11-15 10:42:51 +0100

    Update update.ps1

--- automatic/labplot/update.ps1
+++ automatic/labplot/update.ps1
@@ -1,8 +1,9 @@
 ﻿import-module au
 
-[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-
-function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -NoSuffix -Purge
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+}
 
 function global:au_GetLatest {
     $releases = 'https://download.kde.org/stable/labplot/'
@@ -13,12 +14,13 @@ function global:au_GetLatest {
     $releases = 'https://download.kde.org/stable/labplot/' + $majorVersion + '/labplot-' + $majorVersion + '-64bit-setup.exe.mirrorlist'
     $regex    = 'labplot-(?<Version>[\d\.]+)-64bit-setup.exe'
 
-    (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Select -First 1
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Select -First 1 | Out-Null
     $version = $matches.Version
+    $url = Get-RedirectedUrl ('https://download.kde.org/stable/labplot/' + $majorVersion + '/labplot-' + $majorVersion + '-64bit-setup.exe')
 
     return @{
         Version = $majorVersion
-        URL32   = Get-RedirectedUrl ('https://download.kde.org/stable/labplot/' + $majorVersion + '/labplot-' + $majorVersion + '-64bit-setup.exe')
+        URL64   = $url
     }
 }
 
@@ -26,14 +28,14 @@ function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{
             "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"            
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{
-          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+            "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`${1}$($Latest.FileName64)`""
         }
     }
 }
 
-update -ChecksumFor 64
+update -ChecksumFor none -NocheckUrl

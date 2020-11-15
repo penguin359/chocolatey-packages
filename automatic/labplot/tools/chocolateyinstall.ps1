c4e9ca6f50890be7d28fca10c7b143bf19f52bd2
Chocolatey <chocolatey@realdimensions.net>
2020-11-15 09:47:56 +0000

    AU: 1 updated - labplot [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/5777ea21fb06f6dbf1124d9a50b387b0b8b6093d

--- automatic/labplot/tools/chocolateyinstall.ps1
+++ automatic/labplot/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file64      = "$toolsDir\labplot-2.6.0-64bit-setup.exe"
+  file64      = "$toolsDir\labplot-2.8.1-64bit-setup.exe"
   silentArgs  = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 

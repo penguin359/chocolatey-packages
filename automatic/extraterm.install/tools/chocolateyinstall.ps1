35b6996a058117beb1f5ecf4c6666f80c9391ac2
Chocolatey <chocolatey@realdimensions.net>
2020-11-17 00:25:15 +0000

    AU: 8 updated - apo-ok apo-ok.install apo-ok.portable extraterm extraterm.install extraterm.portable tjs tjs.install [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/6aae69e2a90405ab090255717afd61d93df0c4ed

--- automatic/extraterm.install/tools/chocolateyinstall.ps1
+++ automatic/extraterm.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName  
-  file        = "$toolsDir\extraterm-setup-0.55.0.exe"
+  file        = "$toolsDir\extraterm-setup-0.55.1.exe"
   silentArgs  = "/S"
 }
 

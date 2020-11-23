fa03a95d5f6b8b9e75475f6395963fce715cd17d
Chocolatey <chocolatey@realdimensions.net>
2020-11-23 16:25:07 +0000

    AU: 2 updated - automouseclick wireguard [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'F47A223E3A49376B40DF63BFD12B4D9793DAE907D67F5DDE654464D5676246B7'
+  checksum     = 'DCB2EFEC5C39A7CF222BE431FED15808A898CDF97286DFFD103B0E716A0D5818'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"

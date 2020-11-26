193ccfa51d11c41b6f0a1322a5cdd307c8a57b17
chtof <chtof62@gmail.com>
2020-11-26 18:36:16 +0100

    Update chocolateyinstall.ps1

--- automatic/steelseries-engine/tools/chocolateyinstall.ps1
+++ automatic/steelseries-engine/tools/chocolateyinstall.ps1
@@ -2,13 +2,12 @@ $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-    
+
   url           = 'https://engine.steelseriescdn.com/SteelSeriesEngine3.17.6Setup.exe'
   checksum      = 'D1E763808387EF3E9C5DDE3E72D6608E03181694E8D7455C3D642FB8E684DFA3'
   checksumType  = 'sha256'
-  
-  
-  silentArgs    = "/S"
+
+  silentArgs    = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs

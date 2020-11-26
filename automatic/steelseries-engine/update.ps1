72e509d11fc0131fc18efbe21546de3f8193cf90
chtof <chtof62@gmail.com>
2020-11-26 18:37:47 +0100

    Create update.ps1

---
+++ automatic/steelseries-engine/update.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+    $releases = 'https://steelseries.com/engine'
+    $regex   = '>Download Engine (?<Version>[\d\.]+)<'
+
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing) -match $regex
+
+    return @{
+        Version = $matches.version
+        URL32 = 'https://engine.steelseriescdn.com/SteelSeriesEngine' + $matches.Version + 'Setup.exe'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

b32ce86ac39ae73d22132c81b5e25021c183a25f
chtof <chtof62@gmail.com>
2020-12-07 21:51:08 +0100

    Update chocolateyinstall.ps1

--- automatic/ultraviewer.install/tools/chocolateyinstall.ps1
+++ automatic/ultraviewer.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://ultraviewer.net/en/UltraViewer_setup_6.2_en.exe'
-  checksum     = 'E324CEAAE878B973FDA7A0F8A33AB988785F5D671EA1EAAD21AFEB68D76E5257'
+  checksum     = 'b07baee9b2d75e3f53366fd716c127f32a302dedc7b8a9fe3e01704688ace5a1'
   checksumType = 'sha256'  
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'

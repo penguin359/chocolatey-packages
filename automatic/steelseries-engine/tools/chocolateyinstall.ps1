11a18256fd6c823929dc80ae29bbef956db5c290
Chocolatey <chocolatey@realdimensions.net>
2020-12-09 00:29:20 +0000

    AU: 4 updated - keeper openssl steelseries-engine webex-teams [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/steelseries-engine/tools/chocolateyinstall.ps1
+++ automatic/steelseries-engine/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://engine.steelseriescdn.com/SteelSeriesEngine3.18.12Setup.exe'
-  checksum      = '09b909c5e3cc1799c35efd9d2b42d4dd60f47225df9c7b450d3afcbafad1dff8'
+  url           = 'https://engine.steelseriescdn.com/SteelSeriesEngine3.18.13Setup.exe'
+  checksum      = '47a87f4d11ce3eb778f8b0e3d8caedb0aec466cd5b684125da7dd428762250d4'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'

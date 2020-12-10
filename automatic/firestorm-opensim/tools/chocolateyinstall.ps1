788a81516ecaba538483f1aa853ccaddf6d22e15
Chocolatey <chocolatey@realdimensions.net>
2020-12-10 08:27:36 +0000

    AU: 2 updated - firestorm-opensim firestorm-secondlife [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/firestorm-opensim/tools/chocolateyinstall.ps1
+++ automatic/firestorm-opensim/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
-  url            = 'http://downloads.firestormviewer.org/windows/Phoenix-FirestormOS-Release-6-0-2-56680_Setup.exe'
-  checksum       = 'AA80ECAFD22AB262BBD6A02318B4603FF3D5E27FEA9A61A5B6DDF38DDBE9DDD9'
+  url            = 'https://downloads.firestormviewer.org/windows/Phoenix-FirestormOS-Release-6-4-12-62831_Setup.exe'
+  checksum       = '71ae44511b45c46c7f544695b7e07534cd52cac028dd2be62885777d62a39b6b'
   checksumType   = 'sha256'
 
-  url64          = 'http://downloads.firestormviewer.org/windows/Phoenix-FirestormOS-Releasex64-6-0-2-56680_Setup.exe'
-  checksum64     = 'F41B827191AC2C3DFB131F5397F5E741A37282289A61A37AC8ADC5E47AE07FDD'
+  url64          = 'https://downloads.firestormviewer.org/windows/Phoenix-FirestormOS-Releasex64-6-4-12-62831_Setup.exe'
+  checksum64     = 'd3500bba256bd51a5c89abc9ca57578596cd7d51846a90071dee4d3b84fde40b'
   checksumType64 = 'sha256'
 
   silentArgs     = '/S'

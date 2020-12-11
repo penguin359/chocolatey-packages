eafac857b4dce695d009cac835a23b895b99839b
Chocolatey <chocolatey@realdimensions.net>
2020-12-11 00:27:51 +0000

    AU: 3 updated - jami lossless-cut texmacs [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/lossless-cut/tools/chocolateyinstall.ps1
+++ automatic/lossless-cut/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = "$toolsDir"
 
-  url            = 'https://github.com/mifi/lossless-cut/releases/download/v3.27.0/LosslessCut-win.zip'
-  checksum       = '193684287f5b71b621d5734c7d6d7135130f55b426274fe16fabfc1caa2894ae'
+  url            = 'https://github.com/mifi/lossless-cut/releases/download/v3.29.1/LosslessCut-win.zip'
+  checksum       = '86311b64a7bb6d021092d417beaf422b3306da98651f939ce0eaf0b84a8287f8'
   checksumType   = 'sha256'
 }
 

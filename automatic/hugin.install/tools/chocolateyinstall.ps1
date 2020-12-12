8f81c2a049c54814f8edf4d340a09ee02ac26bbb
Chocolatey <chocolatey@realdimensions.net>
2020-12-12 16:25:43 +0000

    AU: 2 updated - hugin hugin.install [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/hugin.install/tools/chocolateyinstall.ps1
+++ automatic/hugin.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   filetype      = 'MSI'    
-  file          = "$toolsDir\Hugin-2019.0.0-win64.msi"
+  file          = "$toolsDir\Hugin-2020.0.0-win64.msi"
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }

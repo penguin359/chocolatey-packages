a5487bdb3327db4feec2404e673b71919c001073
chtof <chtof62@gmail.com>
2020-12-17 21:29:40 +0100

    Update update.ps1

--- automatic/cuda/update.ps1
+++ automatic/cuda/update.ps1
@@ -2,12 +2,12 @@
 
 function global:au_GetLatest {
     $releases = 'https://developer.nvidia.com/cuda-downloads'
-    $regex    = '(/(?<MajorVersion>[\d\.]+)/Prod/local_installers/(?<File>cuda_(?<Version>[\d\.]+)_[\d\.]+_win10.exe))'
+    $regex    = '(/(?<MajorVersion>[\d\.]+)/Prod/local_installers/(?<File>cuda_(?<VersionMajor>[\d\.]+)_(?<VersionMinor>[\d\.]+)_win10.exe))'
 
-    (Invoke-WebRequest -Uri $releases -UseBasicParsing).RawContent -match '/(?<MajorVersion>[\d\.]+)(/Prod)?/local_installers/(?<File>cuda_(?<Version>[\d\.]+)_[\d\.]+_win10.exe)' | Out-Null
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing).RawContent -match '/(?<MajorVersion>[\d\.]+)(/Prod)?/local_installers/(?<File>cuda_(?<VersionMajor>[\d\.]+)_(?<VersionMinor>[\d\.]+)_win10.exe)' | Out-Null
 
     return @{
-        Version = $matches.Version
+        Version = $matches.VersionMajor + '.' + ($matches.VersionMinor -replace '\.','')
         URL32   = 'https://developer.download.nvidia.com/compute/cuda' + $matches.0
     }
 }

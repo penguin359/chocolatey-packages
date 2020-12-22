cbd81260fbfe09c3fcdc7d061080c100ce16d510
Muhammad Hussein Ammari <xmha97@gmail.com>
2020-12-22 06:00:26 +0330

    File Association for .MP4

--- automatic/lossless-cut/tools/chocolateyuninstall.ps1
+++ automatic/lossless-cut/tools/chocolateyuninstall.ps1
@@ -3,3 +3,7 @@
 $programs = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\"
 $shortcutFilePath = Join-Path $programs "LosslessCut.lnk"
 if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
+
+# Remove File Association for .MP4
+Remove-Item -LiteralPath "HKLM:\SOFTWARE\Classes\SystemFileAssociations\.mp4\Shell\cut" -force;
+Remove-Item -LiteralPath "HKLM:\SOFTWARE\Classes\Applications\LosslessCut.exe" -force;

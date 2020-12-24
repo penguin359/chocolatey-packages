aad3b2e583351509e5902e35e865020737e99e00
Chocolatey <chocolatey@realdimensions.net>
2020-12-24 08:25:36 +0000

    AU: 1 updated - cudatext [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/cudatext/tools/chocolateyinstall.ps1
+++ automatic/cudatext/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ if (!$pp['InstallationPath']) { $pp['InstallationPath'] = 'C:\CudaText' }
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = $pp['InstallationPath']  
-  file64      = "$toolsDir\cudatext-win-x64-1.118.0.0.zip"
+  file64      = "$toolsDir\cudatext-win-x64-1.118.2.0.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

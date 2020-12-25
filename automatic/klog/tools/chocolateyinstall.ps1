303547b13ef55dc5a8803fbb54ebef9c2626f486
Chocolatey <chocolatey@realdimensions.net>
2020-12-25 00:25:42 +0000

    AU: 1 updated - klog [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/klog/tools/chocolateyinstall.ps1
+++ automatic/klog/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'KLog'
   fileType      = 'exe'
-  file          = "$ToolsDir\KLog-1.4.3-windows-installer.exe"
+  file          = "$ToolsDir\KLog-1.4.4-windows-installer.exe"
   silentArgs    = "--mode unattended"
 }
 

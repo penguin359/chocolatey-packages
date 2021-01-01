8cefb4d7d5483286a0d46ad6cbec1125db89ac7c
Chocolatey <chocolatey@realdimensions.net>
2021-01-01 00:25:54 +0000

    AU: 2 updated - goodsync keeper [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-v11-Setup.msi'
-  checksum       = '893d248c6ad4945acab69cb906d9ba14a7fad760e302fa2bceb29a746ef0649e'
+  checksum       = '4d3bc98c0214bc799875933c53fd48c4f3c4229e8de36ad7139782b898129585'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ $packageArgs = @{
   file64        = "$toolsDir\KeeperSetup64.exe"
 
   url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = '96fddba31e11abfae8ce3c9beb06bd2eab3082b36482eddb1862783c5a08527b'
+  checksum      = '680d2aa25b4abeba73a9345a8117cf0d01fda806873cd0e23627b1b9441f9022'
   checksumType  = 'sha256'
 
   url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = '1014b859fa34776342d6a0ce9a881165d04e3e6216f2a3e0e322d6d43b89cebf'
+  checksum64    = '418edaee636fe420d1d18222f247153d67af2b0bf34b464095ddb5bdca4fdc36'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'

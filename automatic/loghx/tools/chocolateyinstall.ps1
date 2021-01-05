e23df37370281f26847c2b3a26e0b3c2ef72bb19
Chocolatey <chocolatey@realdimensions.net>
2021-01-05 16:27:12 +0000

    AU: 3 updated - clockify cudatext loghx [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/loghx/tools/chocolateyinstall.ps1
+++ automatic/loghx/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_580.exe'
-  checksum      = '56cb55bd9c8b85a9a02ac56bd6c57a976532dbcb315ef70896dda007edacfb4f'
+  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_607.exe'
+  checksum      = 'e4da2f6f0c60a5ed7946d73c9140e0a55fd8be13706f73e81371f8fbc81bd8c6'
   checksumType  = 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'

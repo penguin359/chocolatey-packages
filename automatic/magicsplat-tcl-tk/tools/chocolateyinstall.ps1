e01704179fc9bb67a20aa7cdb2e3c23440238de4
Chocolatey <chocolatey@realdimensions.net>
2021-01-05 08:25:57 +0000

    AU: 1 updated - magicsplat-tcl-tk [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/magicsplat-tcl-tk/tools/chocolateyinstall.ps1
+++ automatic/magicsplat-tcl-tk/tools/chocolateyinstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
 
-  file          = "$toolsDir\tcl-8.6.10-installer-1.10.1-x86.msi"
+  file          = "$toolsDir\tcl-8.6.11-installer-1.11.0-x86.msi"
   filetype      = 'MSI'
-  file64        = "$toolsDir\tcl-8.6.10-installer-1.10.1-x64.msi"
+  file64        = "$toolsDir\tcl-8.6.11-installer-1.11.0-x64.msi"
   filetype64    = 'MSI'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

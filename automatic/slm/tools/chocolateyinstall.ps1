d189741e7af87bab1ab9c2fbaf025a2964163962
Chocolatey <chocolatey@realdimensions.net>
2021-01-07 16:43:17 +0000

    AU: 4 updated - cudatext slm spacedesk-client spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/slm/tools/chocolateyinstall.ps1
+++ automatic/slm/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype     = 'MSI'
 
   url          = 'https://download.softros.com/SoftrosLANMessengerSetup.msi'
-  checksum     = 'f83bb75ee40676a731a317baa32189e3631aa4760549b681ccb05b621cde0090'
+  checksum     = '8768e00f17fc811c16e0802b8a402a11f6595220928f3e67250a4ebde62319bf'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

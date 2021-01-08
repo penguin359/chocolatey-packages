38a50f56728845d92bdf05b3508f6f05ae53a44f
Chocolatey <chocolatey@realdimensions.net>
2021-01-08 00:25:47 +0000

    AU: 4 updated - cudatext extraterm extraterm.install extraterm.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/extraterm.install/tools/chocolateyinstall.ps1
+++ automatic/extraterm.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName  
-  file        = "$toolsDir\extraterm-setup-0.56.0.exe"
+  file        = "$toolsDir\extraterm-setup-0.57.0.exe"
   silentArgs  = "/S"
 }
 

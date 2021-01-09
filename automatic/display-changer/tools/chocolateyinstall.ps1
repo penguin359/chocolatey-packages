ef015e91c372fe7b5fd0cc71d828094d45786b86
chtof <chtof62@gmail.com>
2021-01-09 23:55:19 +0100

    Update chocolateyinstall.ps1

--- automatic/display-changer/tools/chocolateyinstall.ps1
+++ automatic/display-changer/tools/chocolateyinstall.ps1
@@ -1,7 +1,9 @@
 $ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
+  destination  = "$toolsDir"
 
   url          = 'https://12noon.com/files/dc.zip'
   checksum     = '01FAF70A4640E3AD89AE1DB78AA6B95F4FDC0CF162FCC92CF279442FBFFF20CB'

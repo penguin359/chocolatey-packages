89955ddf4130ee5bc4714b66e58ba35d963e44c9
chtof <chtof62@gmail.com>
2021-01-09 23:56:38 +0100

    Update chocolateyinstall.ps1

--- automatic/ispc/tools/chocolateyinstall.ps1
+++ automatic/ispc/tools/chocolateyinstall.ps1
@@ -3,9 +3,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  destination = "$toolsDir"
+  fileType    = 'MSI'
+
   file        = "$toolsDir\ispc-v1.14.0-windows.zip"
+  silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
 }
 
-Get-ChocolateyUnzip @packageArgs
-Remove-Item -Path $packageArgs.file
+Install-ChocolateyPackage @packageArgs

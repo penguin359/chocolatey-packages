31a01d7d74f777789458c8fd5de2fefb36cb6d6b
chtof <chtof62@gmail.com>
2021-01-09 23:33:13 +0100

    Update chocolateyinstall.ps1

--- automatic/display-changer/tools/chocolateyinstall.ps1
+++ automatic/display-changer/tools/chocolateyinstall.ps1
@@ -3,11 +3,12 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://12noon.com/files/dc-setup.exe'
+  url          = 'https://12noon.com/files/dc.zip'
   checksum     = '01FAF70A4640E3AD89AE1DB78AA6B95F4FDC0CF162FCC92CF279442FBFFF20CB'
   checksumType = 'sha256'
 
-  silentArgs   = '/S'
+  # silentArgs   = '/S'
 }
 
-Install-ChocolateyPackage @packageArgs
+# Install-ChocolateyPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs

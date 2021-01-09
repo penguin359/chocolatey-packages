285f28bad43c3670c3a47bf6f0d704ef42a11c08
chtof <chtof62@gmail.com>
2021-01-09 23:49:17 +0100

    Update update.ps1

--- automatic/lyx/update.ps1
+++ automatic/lyx/update.ps1
@@ -4,14 +4,17 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
     $releases = 'https://www.lyx.org/Download'
-    $regex    = 'https://ftp.lip6.fr/pub/lyx/bin/(?<Version>[\d\.]+)/LyX-[\d]+-Installer-(?<VersionMinor>[\d]+).exe$'
+    $regex32    = 'https://ftp.lip6.fr/pub/lyx/bin/(?<Version>[\d\.]+)/LyX-[\d]+-Installer-[\d]+-x64.exe$'
+    $regex64    = 'https://ftp.lip6.fr/pub/lyx/bin/(?<Version>[\d\.]+)/LyX-[\d]+-Installer-(?<VersionMinor>[\d]+)-x64.exe$'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	 
-    $url = $download_page.links | ? href -match $regex
+    $url32 = $download_page.links | ? href -match $regex32
+    $url64 = $download_page.links | ? href -match $regex64
 
     return @{
         Version = $matches.Version + '.' + $matches.VersionMinor
-        URL32   = $url.href
+        URL32   = $url32.href
+        URL64   = $url64.href
     }
 }
 
@@ -19,18 +22,17 @@ function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{
             "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{        
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none

9f79439f7583ce9f92cc49e46959a7de311790b9
Chocolatey <chocolatey@realdimensions.net>
2021-01-09 22:21:47 +0000

    AU: 1 updated - webex-meetings [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   fileType     = 'MSI'
 
-  url          = 'https://akamaicdn.webex.com/client/WBXclient-40.12.5-14/webexapp.msi'
-  checksum     = '81ad583b78ffff5323800dcdd4918d75308ad98a40f0a92e414268d752287a55'
-  url64        = 'https://akamaicdn.webex.com/client/WBXclient-40.12.5-14/webexapp.msi'
-  checksum64   = '81ad583b78ffff5323800dcdd4918d75308ad98a40f0a92e414268d752287a55'
+  url          = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex_x86.msi'
+  checksum     = '79f788db708b77175d0fd1f59ecdc117f8ea582a9f6c4326adc7a51effa56813'
+  url64        = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex.msi'
+  checksum64   = 'ce66951a89e2b88ccd4a81a38c8d4ee541c2882cdc785f6ed6a92efce4196a00'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"

0326c2b40b99791dd533fd39f961c13cc7d552a1
chtof <chtof62@gmail.com>
2021-01-09 23:49:13 +0100

    Update chocolateyinstall.ps1

--- automatic/lyx/tools/chocolateyinstall.ps1
+++ automatic/lyx/tools/chocolateyinstall.ps1
@@ -4,7 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   file        = "$toolsDir\LyX-2352-Installer-4.exe"
-  silentArgs  = "/S"
+  file64      = "$toolsDir\LyX-2352-Installer-4.exe"
+  silentArgs  = '/S'
 }
 
 Install-ChocolateyInstallPackage @packageArgs

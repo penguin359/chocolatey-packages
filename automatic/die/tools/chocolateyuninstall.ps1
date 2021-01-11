85077e1185f26e8c8757510e9e3d7c4c9b9f9479
chtof <chtof62@gmail.com>
2021-01-12 00:29:15 +0100

    Update chocolateyuninstall.ps1

--- automatic/die/tools/chocolateyuninstall.ps1
+++ automatic/die/tools/chocolateyuninstall.ps1
@@ -2,7 +2,4 @@
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 
 $shortcutFilePath = Join-Path $programs "Detect It Easy.lnk"
-if(Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
-
-$shortcutFilePath = Join-Path $programs "Detect It Easy Lite.lnk"
-if(Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
+If (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

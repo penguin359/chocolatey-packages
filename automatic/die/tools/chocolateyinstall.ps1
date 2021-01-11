7c20bc27a50aefdaf8341332fe21e244e8ab438f
Chocolatey <chocolatey@realdimensions.net>
2021-01-11 22:01:14 +0000

    AU: 1 updated - die [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/die/tools/chocolateyinstall.ps1
+++ automatic/die/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\die_win32_portable_2.05.zip"
-  file64        = "$toolsDir\die_win64_portable_2.05.zip"
+  file          = "$toolsDir\die_win32_portable_3.01.zip"
+  file64        = "$toolsDir\die_win64_portable_3.01.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

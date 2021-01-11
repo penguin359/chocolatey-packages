53b03f1301012c8e6ec831186fce46ab5790f28c
chtof <chtof62@gmail.com>
2021-01-11 20:30:22 +0100

    Update update.ps1

--- automatic/die/update.ps1
+++ automatic/die/update.ps1
@@ -3,26 +3,34 @@
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-    $releases = 'http://ntinfo.biz/index.html'
-    $regex   = 'die_win32_portable_(?<Version>[\d\.]+).zip$'
+    # $releases = 'http://ntinfo.biz/index.html'
+    $releases = 'https://github.com/horsicq/DIE-engine/releases'
+    $regex32   = 'die_win32_portable_(?<Version>[\d\.]+).zip$'
+    $regex64   = 'die_win64_portable_(?<Version>[\d\.]+).zip$'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	$url = $download_page.links | ? href -match $regex
-    return @{ Version = $matches.Version ; URL32 = $url.href }
+	$url32 = $download_page.links | ? href -match $regex32
+    $url64 = $download_page.links | ? href -match $regex64
+    return @{
+        Version = $matches.Version
+        URL32 = $url32.href
+        URL64 = $url64.href
+    }
 }
 
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{            
             "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"            
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{        
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }

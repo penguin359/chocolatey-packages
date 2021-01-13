b23253d0dec5b49f709cf5929b98a837a87511ed
chtof <chtof62@gmail.com>
2021-01-13 21:51:09 +0100

    evga-flow-control: initial version

---
+++ automatic/evga-flow-control/ReadMe.md
@@ -0,0 +1,7 @@
+﻿# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/evga-flow-control/evga-flow-control.png" width="48" height="48"/> [EVGA Flow Control](https://chocolatey.org/packages/evga-flow-control)
+
+## Features
+- Full control of your EVGA CLC Liquid Cooler
+- Settings save to firmware
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/evga-flow-control/screenshot.png)
---
+++ automatic/evga-flow-control/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"  
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$toolsDir"
+  fileType      = 'exe'
+  file          = "$toolsDir\EVGA_Flow_Control_Setup.exe"
+
+  url           = 'https://cdn.evga.com/utilities/EVGA_Flow_Control_Setup_v2.0.9.zip'
+  checksum      = '67D2FECD03820DD1F5DF51A83536B43108E3A433F3F5190F1558FAFE0735C048'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/S /v/qn'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs

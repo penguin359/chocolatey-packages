7a938685802f79725cfc5abb04b151a1ad705242
chtof <chtof62@gmail.com>
2021-01-13 23:18:49 +0100

    Update chocolateyinstall.ps1

--- automatic/die/tools/chocolateyinstall.ps1
+++ automatic/die/tools/chocolateyinstall.ps1
@@ -1,12 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$win32Dir   = Join-Path $toolsDir 'die_win32_portable'
 $programs   = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 
-# Remove the folder from previous versions that were win32 only
-if (Test-Path $win32Dir) { Remove-Item -Recurse -Path $win32Dir -ea 0 }
+# Remove folder(s) from previous versions
+ForEach ($arch in "32 64") {
+  $dir = Join-Path $toolsDir "die_win${arch}_portable"
+  If (Test-Path $dir) { Remove-Item -Recurse -Path $dir -ea 0 }
+}
 
-# Remove old shortcut for diel.exe
+# Remove old shortcut for diel.exe (to keep for users who upgrade from die 2.x versions)
 $shortcutFilePath = Join-Path $programs 'Detect It Easy Lite.lnk'
 if(Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath -ea 0 }
 
@@ -22,9 +24,9 @@ Remove-Item -Path "$toolsDir\*.zip"
 
 # Install start menu shortcuts
 if ((Get-OSArchitectureWidth -compare 32) -or ($env:chocolateyForceX86 -eq $true)) {
-    $targetPath = [System.IO.Path]::Combine("$toolsDir", "die_win32_portable", "die.exe")
+    $targetPath = [System.IO.Path]::Combine("$toolsDir", 'die_win32_portable', 'die.exe')
 } else {
-    $targetPath = [System.IO.Path]::Combine("$toolsDir", "die_win64_portable", "die.exe")
+    $targetPath = [System.IO.Path]::Combine("$toolsDir", 'die_win64_portable', 'die.exe')
 }
 
 $shortcutFilePath = Join-Path $programs 'Detect It Easy.lnk'

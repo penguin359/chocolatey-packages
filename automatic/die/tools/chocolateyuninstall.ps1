c91cd25998b4977d897c0c0cfb2a2b9b23cd6da0
chtof <chtof62@gmail.com>
2021-01-13 23:04:08 +0100

    Update chocolateyuninstall.ps1

--- automatic/die/tools/chocolateyuninstall.ps1
+++ automatic/die/tools/chocolateyuninstall.ps1
@@ -1,5 +1,5 @@
 ﻿# Remove start menu shortcuts
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 
-$shortcutFilePath = Join-Path $programs "Detect It Easy.lnk"
+$shortcutFilePath = Join-Path $programs 'Detect It Easy.lnk'
 If (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }

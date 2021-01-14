1d54a09a16d08b1f9676687f884cf0c078074d99
chtof <chtof62@gmail.com>
2021-01-14 23:52:14 +0100

    Create update.ps1

---
+++ automatic/evga-flow-control/update.ps1
@@ -0,0 +1,22 @@
+﻿import-module au
+
+function global:au_GetLatest {
+     $releases = 'https://www.evga.com/software/'
+	 $regex   = 'EVGA_Flow_Control_Setup_v(?<Version>[\d\.]+).zip'
+
+	 $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing  -Headers @{ "Accept-Encoding"="gzip, deflate, br" }
+	 $url = $download_page.links | ? href -match $regex | select -Last 1
+
+     return @{ Version = $matches.Version ; URL32 = $url.href }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+update

1f57dae09c397dcecc7b09dc0b88f032c0cc24f4
chtof <chtof62@gmail.com>
2021-01-14 21:57:23 +0100

    Switches added to disable options in the installer

--- automatic/laragon.install/tools/chocolateyinstall.ps1
+++ automatic/laragon.install/tools/chocolateyinstall.ps1
@@ -1,10 +1,38 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$PackageParameters = Get-PackageParameters
+[array]$arr_tasks = @()
 
+### PARAMETERS
+If ($PackageParameters.DisableRunAtStartup) {
+  $arr_tasks += '!runatstartup'
+  Write-Host 'You requested to do not run Laragon when Windows start.' -ForegroundColor Cyan
+} Else {
+  $arr_tasks += 'runatstartup'
+}
+
+If ($PackageParameters.DisableAutoVirtualHosts) {
+  $arr_tasks += '!autovirtualhosts'
+  Write-Host 'You requested to disable auto virtual hosts (Your app will not get a pretty url --> http://app.test).' -ForegroundColor Cyan
+} Else {
+  $arr_tasks += 'autovirtualhosts'
+}
+
+If ($PackageParameters.DisableRightClickMenu) {
+  $arr_tasks += '!rightclickmenu'
+  Write-Host 'You requested to do not add Notepad++ & Terminal to the Right-Click Menu.' -ForegroundColor Cyan
+} Else {
+  $arr_tasks += 'rightclickmenu'
+}
+
+$tasks = $arr_tasks -Join ","
+
+
+### INSTALLATION
 $packageArgs = @{
   packageName = "$env:ChocolateyPackageName"
   file        = "$toolsDir\laragon-full.exe"
-  silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components="'
+  silentArgs  = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components= /tasks=""$tasks"""
 }
 
 Install-ChocolateyInstallPackage @packageArgs

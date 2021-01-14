841c62fd66456e43e56b5a324a486f0a20afce48
chtof <chtof62@gmail.com>
2021-01-14 23:41:17 +0100

    Update chocolateyinstall.ps1

--- automatic/evga-flow-control/tools/chocolateyinstall.ps1
+++ automatic/evga-flow-control/tools/chocolateyinstall.ps1
@@ -8,6 +8,7 @@ $packageArgs = @{
   file          = "$toolsDir\EVGA_Flow_Control_Setup.exe"
 
   url           = 'https://cdn.evga.com/utilities/EVGA_Flow_Control_Setup_v2.0.9.zip'
+  options       = @{ Headers = @{ "Accept-Encoding"="gzip, deflate, br" } }
   checksum      = '67D2FECD03820DD1F5DF51A83536B43108E3A433F3F5190F1558FAFE0735C048'
   checksumType  = 'sha256'
 
@@ -16,3 +17,16 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 Install-ChocolateyInstallPackage @packageArgs
+
+
+
+$options =
+@{
+  Headers = @{
+    Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
+    'Accept-Charset' = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3';
+    'Accept-Language' = 'en-GB,en-US;q=0.8,en;q=0.6';
+    Cookie = 'requiredinfo=info';
+    Referer = 'https://somelocation.com/';
+  }
+}

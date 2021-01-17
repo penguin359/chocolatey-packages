68e62912a8d38a6b858eb22d0fdccbd563748bc3
chtof <chtof62@gmail.com>
2021-01-17 22:18:36 +0100

    Update chocolateyinstall.ps1

--- automatic/dexed/tools/chocolateyinstall.ps1
+++ automatic/dexed/tools/chocolateyinstall.ps1
@@ -4,16 +4,16 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\dexed-0.9.4-win.zip"
+  file        = "$toolsDir\dexed-0.9.5-win.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
 
-$exe_file = (gci $toolsDir\dexed-*-win.exe).name
+$exe_file = (Get-ChildItem -Recurse $toolsDir\dexed-*-win.exe).fullname | Select -First 1
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName  
-  fileFullPath = "$toolsDir\$exe_file"
+  fileFullPath = "$exe_file"
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 

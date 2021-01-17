dc969206e0036c37642b5cfcb4839f2ee9113a18
Chocolatey <chocolatey@realdimensions.net>
2021-01-17 21:34:15 +0000

    AU: 4 updated - goodsync moonlight-qt.portable virtualhere-server virtualmidisynth [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/virtualmidisynth/tools/chocolateyinstall.ps1
+++ automatic/virtualmidisynth/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\CoolSoft_VirtualMIDISynth_2.10.2.exe"
+  file        = "$toolsDir\CoolSoft_VirtualMIDISynth_2.11.0.exe"
   silentArgs  = '/S'
 }
 

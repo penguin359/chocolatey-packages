3931517c2e6a1406c813f0cde68dcb9ead2f56cd
chtof <chtof62@gmail.com>
2021-01-18 21:02:19 +0100

    Update chocolateyinstall.ps1

--- automatic/evga-flow-control/tools/chocolateyinstall.ps1
+++ automatic/evga-flow-control/tools/chocolateyinstall.ps1
@@ -1,6 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"  
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls11;
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$toolsDir"
@@ -8,7 +10,7 @@ $packageArgs = @{
   file          = "$toolsDir\EVGA_Flow_Control_Setup.exe"
 
   url           = 'https://cdn.evga.com/utilities/EVGA_Flow_Control_Setup_v2.0.9.zip'
-  options       = @{ Headers = @{ "Accept-Encoding"="gzip, deflate, br" } }
+  options       = @{ Headers = @{ "Accept-Encoding"="gzip, deflate, br"; } }
   checksum      = '67D2FECD03820DD1F5DF51A83536B43108E3A433F3F5190F1558FAFE0735C048'
   checksumType  = 'sha256'
 

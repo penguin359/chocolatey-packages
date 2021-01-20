81a456928de6810840ae48f1362e9a1bcd7a8851
Chocolatey <chocolatey@realdimensions.net>
2021-01-20 00:35:53 +0000

    AU: 2 updated - eddie webex-meetings [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum     = '706ee82c263b185c28b523900674b38a622a3e76c3678a043c00b39a0cc8e965'
+  checksum     = '845067ed80a8f5b87d6a43fc3f6c6e067b8691d78885116f82cef2ac551d3bf1'
   url64        = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum64   = '706ee82c263b185c28b523900674b38a622a3e76c3678a043c00b39a0cc8e965'
+  checksum64   = '845067ed80a8f5b87d6a43fc3f6c6e067b8691d78885116f82cef2ac551d3bf1'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"

17b4d2a507274ed89497d7027fb40325708f9847
Chocolatey <chocolatey@realdimensions.net>
2021-01-25 16:28:15 +0000

    AU: 8 updated - automouseclick dontsleep dontsleep.install dontsleep.portable embree embree.install lossless-cut seppdf [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '420E5C58B24E9F6D4BFCA5FB92AC26D4B102279F1D4EB36BCA95B19F92F0E932'
+  checksum     = '2D3F26875CE4980B277563C82F48C7EBF4AA6C95A2C9734DB9C7C14CFE4394F5'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"

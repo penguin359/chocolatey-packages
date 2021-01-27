d0f12a27fc117c57410bb6b6d527bc27bf4f4f80
Chocolatey <chocolatey@realdimensions.net>
2021-01-27 16:26:05 +0000

    AU: 2 updated - automouseclick yara [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '0352FE10BE38FDBF520A437EE6A952EFFE90A8414283D49E4A51EA52E03BD9D4'
+  checksum     = '77BDA9BAD4757B12506975D83496105A55E2AAFF45F5542F0178B0895C11AC73'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/yara/tools/chocolateyinstall.ps1
+++ automatic/yara/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\yara-v4.0.3-1538-win32.zip"
-  file64      = "$toolsDir\yara-v4.0.3-1538-win64.zip"
+  file        = "$toolsDir\yara-v4.0.4-1544-win32.zip"
+  file64      = "$toolsDir\yara-v4.0.4-1544-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

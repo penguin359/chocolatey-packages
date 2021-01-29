4f35177f6eed97c73a75f0323c4f06e9c1da77ba
Chocolatey <chocolatey@realdimensions.net>
2021-01-29 16:25:34 +0000

    AU: 4 updated - automouseclick clockify fldigi genymotion [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '77BDA9BAD4757B12506975D83496105A55E2AAFF45F5542F0178B0895C11AC73'
+  checksum     = '0025976B16C51A91F49F7696E35856E261227FC7E9D88C66FACA8C24C49C8747'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"

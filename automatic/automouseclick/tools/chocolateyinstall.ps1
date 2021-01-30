4a8338ad8ed6a6e2242da78c685c80522484c580
Chocolatey <chocolatey@realdimensions.net>
2021-01-30 12:25:36 +0000

    AU: 2 updated - automouseclick cudatext [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '0025976B16C51A91F49F7696E35856E261227FC7E9D88C66FACA8C24C49C8747'
+  checksum     = '1EB371E5518B35E62BD2B4B1F80E07B22EAF88130FDB44924D5A1B2A09B92D30'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/cudatext/tools/chocolateyinstall.ps1
+++ automatic/cudatext/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ if (!$pp['InstallationPath']) { $pp['InstallationPath'] = 'C:\CudaText' }
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = $pp['InstallationPath']  
-  file64      = "$toolsDir\cudatext-win-x64-1.123.0.5.zip"
+  file64      = "$toolsDir\cudatext-win-x64-1.123.0.6.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

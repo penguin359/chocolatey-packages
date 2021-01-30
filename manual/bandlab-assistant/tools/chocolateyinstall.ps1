8a52bf22d61fabcdd2b6d61a0d41135daa47af8e
chtof <chtof62@gmail.com>
2021-01-30 15:00:27 +0100

    Update chocolateyinstall.ps1

--- manual/bandlab-assistant/tools/chocolateyinstall.ps1
+++ manual/bandlab-assistant/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   checksum      = 'B9D75C6F31A1225A38555EC560650AE6592DE5E156557617E42D9A5F51451526'
   checksumType  = 'sha256'
 
-  silentArgs    = "/S"
+  silentArgs    = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs

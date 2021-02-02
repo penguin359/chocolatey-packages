66a266ebd8220ff356b1e7a8f6eb690a453c8121
Chocolatey <chocolatey@realdimensions.net>
2021-02-02 19:38:45 +0000

    AU: 1 updated - kmymoney [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -2,12 +2,12 @@
 
 $packageArgs = @{
   packageName    = $packageName  
-  url            = 'https://download.kde.org/stable/kmymoney/latest/win32/kmymoney-mingw32-4.8.4-3.2-setup.exe'
-  url64          = 'https://download.kde.org/stable/kmymoney/latest/win64/kmymoney-mingw64-4.8.4-2.1-setup.exe'  
+  url            = 'https://download.kde.org/stable/kmymoney/5.0.6//win32/kmymoney5-mingw32-5.0.6-7.1-setup.exe'
+  url64          = 'https://download.kde.org/stable/kmymoney/5.0.6//win64/kmymoney5-mingw64-5.0.6-8.1-setup.exe'  
 
-  checksum       = '24c3e4291feedf3e4dfae493b72d1563c7136263d0e449ff2f6ed4fdaa854864'
+  checksum       = 'bf0d25562e1f9f32a8ddf98028f7ebe2b4af47405c9463945cba731afdfeaa90'
   checksumType   = 'sha256'
-  checksum64     = 'caf78eceb7df1d0021698cb39c1302164e144d2afa9adc14fe619746f8db2027'
+  checksum64     = 'fb5a34343c82427b939c6d2fa17e13d719ce3c4f55017e651f26baf74acd8c54'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'

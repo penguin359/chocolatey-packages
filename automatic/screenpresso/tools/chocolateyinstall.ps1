e49857f0f156ac29fc4914d2f218a8853de5ca5c
chtof <chtof62@gmail.com>
2021-02-02 20:03:05 +0100

    Update chocolateyinstall.ps1

--- automatic/screenpresso/tools/chocolateyinstall.ps1
+++ automatic/screenpresso/tools/chocolateyinstall.ps1
@@ -3,11 +3,12 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://www.screenpresso.com/binaries/releases/stable/dotnet47/Screenpresso.exe'
-  checksum     = '16e01dcf6845d8f59a4069410c3881fb3a168bd3177b89390a9708d198597ee8'  
-  checksumType = 'sha256'
+  url            = 'https://www.screenpresso.com/binaries/releases/stable/dotnet47/Screenpresso.exe'
+  checksum       = '16e01dcf6845d8f59a4069410c3881fb3a168bd3177b89390a9708d198597ee8'  
+  checksumType   = 'sha256'
 
-  silentArgs   = '-install pf -silent'
+  silentArgs     = '-install pf -silent'
+  validExitCodes = @(0, 4)
 }
 
 Install-ChocolateyPackage @packageArgs

5350ff7727cfb5a6ff00ad87252088dac95faace
chtof <chtof62@gmail.com>
2021-02-04 22:23:36 +0100

    Update chocolateyinstall.ps1

--- automatic/moonlight-qt.install/tools/chocolateyinstall.ps1
+++ automatic/moonlight-qt.install/tools/chocolateyinstall.ps1
@@ -3,9 +3,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\MoonlightSetup-x86-2.2.0.exe"
-  file64        = "$toolsDir\MoonlightSetup-x64-2.2.0.exe"
-  silentArgs	= "/S"
+  file          = "$toolsDir\MoonlightSetup-x86-2.2.0.exe"  
+  silentArgs	= '/S'
 }
 
 Install-ChocolateyInstallPackage @packageArgs

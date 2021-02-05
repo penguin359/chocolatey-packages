e04760bf9dd7c47f2bfaff587bfbf43d535414fe
chtof <chtof62@gmail.com>
2021-02-05 22:03:43 +0100

    Update update.ps1

--- automatic/roomarranger/update.ps1
+++ automatic/roomarranger/update.ps1
@@ -1,25 +1,26 @@
 ﻿import-module au
-$releases = 'http://www.roomarranger.com/whatsnew.txt'
-$regex = 'version (?<Version>[\d\.]+) \('
 
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
+  $releases = 'http://www.roomarranger.com/whatsnew.txt'
+  $regex = 'version (?<Version>[\d\.]+) \('
+
   (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null
   $version_url = $matches.Version -Replace '\.', ''
 
   return @{
       Version = $matches.Version
-      URL32 = 'https://s3.eu-central-1.amazonaws.com/rooarr/rooarr' + $version_url + '.exe'
-      URL64 = 'https://s3.eu-central-1.amazonaws.com/rooarr/rooarr' + $version_url + '_64bit.exe'
+      URL32   = 'https://www.roomarranger.com/prg/rooarr' + $version_url + '.exe'
+      URL64   = 'https://www.roomarranger.com/prg/rooarr' + $version_url + '_64bit.exe'      
     }
 }
 
 function global:au_SearchReplace {
     @{
         "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"            
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
             "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
@@ -27,11 +28,9 @@ function global:au_SearchReplace {
 
         "tools\chocolateyinstall.ps1" = @{        
           "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
-          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none

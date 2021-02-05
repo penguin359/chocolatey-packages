efe763fac8ba8bffb9fa12d853b22718238eed38
Chocolatey <chocolatey@realdimensions.net>
2021-02-05 16:25:24 +0000

    AU: 3 updated - spacedesk-client spacedesk-server yara [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/yara/tools/chocolateyinstall.ps1
+++ automatic/yara/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\yara-v4.0.4-1544-win32.zip"
-  file64      = "$toolsDir\yara-v4.0.4-1544-win64.zip"
+  file        = "$toolsDir\yara-v4.0.5-1554-win32.zip"
+  file64      = "$toolsDir\yara-v4.0.5-1554-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

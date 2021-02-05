34a687e5943ac4cd52d82145419eaf210c4eae92
chtof <chtof62@gmail.com>
2021-02-05 22:35:35 +0100

    Update update.ps1

--- automatic/gravitdesigner/update.ps1
+++ automatic/gravitdesigner/update.ps1
@@ -1,24 +1,11 @@
-﻿import-module au
-
-function global:au_GetLatest {
-    $releases = 'https://www.designer.io/changelog'
-    $regex   = '<h3>Version (?<Version>[\d\.\-]+)</h3>'
-    (Invoke-WebRequest -Uri $releases) -match $regex | Out-Null    
-    	 
-	return @{ Version = $matches.Version -replace '-', '.' }
-}
+﻿. $PSScriptRoot\..\gravitdesigner.install\update.ps1
 
 function global:au_SearchReplace {
-    @{
-         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
-    
-try {
-    update -ChecksumFor none -NoCheckUrl
-} catch {
-    $ignore = 'The request was aborted: Could not create SSL/TLS secure channel.'
-    if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
-}
+
+update -ChecksumFor none

800acac6fdf41089d2cdf11fc6420579b0421c0f
chtof <chtof62@gmail.com>
2021-02-05 22:17:53 +0100

    Update update.ps1

--- automatic/myfamilytree-languagepack/update.ps1
+++ automatic/myfamilytree-languagepack/update.ps1
@@ -1,16 +1,16 @@
 ﻿import-module au
-$releases       = 'https://chronoplexsoftware.com/myfamilytree/configuration.js'
-$download_url   = 'https://chronoplexsoftware.com/myfamilytree/downloads'
 
 function global:au_GetLatest {
-     $download_page = Invoke-WebRequest -Uri $releases     
-     $regex   = 'var mftVersionNumber = "(?<Version>.*)";'
-     $regex_lp   = 'var mftlpVersionNumber = "(?<Version_lp>.*)";'
-     $download_page -match $regex
-     $version = $matches.Version
-     $download_page -match $regex_lp
-     $version_lp = $matches.Version_lp
-     return @{ Version = $version ; URL32 = $download_url + "/myfamilytree_languagepack" + $version_lp + ".msi" ; URL64 = $download_url + "/myfamilytree_languagepack" + $version_lp + "x64.msi"}
+    $releases       = 'https://chronoplexsoftware.com/myfamilytree/config.js'
+    $download_url   = 'https://chronoplexsoftware.com/myfamilytree/downloads'
+    $download_page = Invoke-WebRequest -Uri $releases     
+    $regex   = 'var appVersionNumber = "(?<Version>.*)";'
+    $regex_lp   = 'var applpVersionNumber = "(?<Version_lp>.*)";'
+    $download_page -match $regex
+    $version = $matches.Version
+    $download_page -match $regex_lp
+    $version_lp = $matches.Version_lp
+    return @{ Version = $version ; URL32 = $download_url + "/myfamilytree_languagepack" + $version_lp + ".msi" ; URL64 = $download_url + "/myfamilytree_languagepack" + $version_lp + "x64.msi"}
 }
 
 function global:au_SearchReplace {

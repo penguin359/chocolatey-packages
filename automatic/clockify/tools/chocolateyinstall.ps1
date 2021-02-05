3f57b1416ca3ef52352ba901a56a7e0a3c555dd3
chtof <chtof62@gmail.com>
2021-02-05 21:45:58 +0100

    Update chocolateyinstall.ps1

--- automatic/clockify/tools/chocolateyinstall.ps1
+++ automatic/clockify/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   checksum      = '6D725C0543F34F038230443AB8765B0E9598AE8C58437F67FA1094B50E6E0DA2B1F023338953B0E76EDED45C16852DAB19CC57594E1947EDABBC8A3DC579CE4C'
   checksumType  = 'sha512'
 
-  silentArgs    = "/S"
+  silentArgs    = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs

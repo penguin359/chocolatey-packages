513efbbae0ead7f3b97d51a2a377b7727cbe8f20
chtof <chtof62@gmail.com>
2021-02-06 12:19:33 +0100

    Update chocolateyinstall.ps1

--- automatic/ffmpeg-batch.portable/tools/chocolateyinstall.ps1
+++ automatic/ffmpeg-batch.portable/tools/chocolateyinstall.ps1
@@ -3,15 +3,17 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\FFmpeg_Batch_2.2.4_Portable_x86.exe"
-  file64      = "$toolsDir\FFmpeg_Batch_2.2.4_Portable_x64.exe"
+  destination = "$toolsDir"
+  file        = "$toolsDir\FFmpeg_Batch_2.3.2_Portable_x86.zip"
+  file64      = "$toolsDir\FFmpeg_Batch_2.3.2_Portable_x64.zip"
 }
 
+Get-ChocolateyUnzip @packageArgs
 
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-if (Get-OSArchitectureWidth -Compare "64") { $arch = "_x64" } else { $arch = "" }
+if (Get-OSArchitectureWidth -Compare '64') { $arch = '_x64' } else { $arch = '' }
 
-$shortcutFilePath = Join-Path $programs "FFmpeg Batch Portable.lnk"
-$targetPath = Join-Path $toolsDir "FFmpeg_Batch_2.2.4_Portable${arch}.exe"
+$shortcutFilePath = Join-Path $programs 'FFmpeg Batch Portable.lnk'
+$targetPath = Join-Path $toolsDir "FFmpeg_Batch_2.3.2_Portable${arch}.exe"
 Install-ChocolateyShortcut -RunAsAdmin -shortcutFilePath "$shortcutFilePath" -targetPath "$targetPath"

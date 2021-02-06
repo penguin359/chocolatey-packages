52c55b66f511140b8acc6446852b166ae689846f
chtof <chtof62@gmail.com>
2021-02-06 12:19:44 +0100

    Update update.ps1

--- automatic/ffmpeg-batch.portable/update.ps1
+++ automatic/ffmpeg-batch.portable/update.ps1
@@ -7,15 +7,15 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
     $releases = 'https://sourceforge.net/projects/ffmpeg-batch/files/'
-    $regex   = 'FFmpeg_Batch_(?<Version>[\d\.]+)_Portable_x64.exe'
+    $regex   = 'FFmpeg_Batch_(?<Version>[\d\.]+)_Portable_x64.zip'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $url = $download_page.links | ? href -match $regex | Select -First 1
 
     return @{
         Version = $matches.Version
-        URL32 = Get-RedirectedUrl ('https://downloads.sourceforge.net/project/ffmpeg-batch/FFmpeg_Batch_' + $matches.Version + '_Portable_x86.exe')
-        URL64 = Get-RedirectedUrl ('https://downloads.sourceforge.net/project/ffmpeg-batch/FFmpeg_Batch_' + $matches.Version + '_Portable_x64.exe')
+        URL32 = Get-RedirectedUrl ('https://downloads.sourceforge.net/project/ffmpeg-batch/FFmpeg_Batch_' + $matches.Version + '_Portable_x86.zip')        
+        URL64 = Get-RedirectedUrl ('https://downloads.sourceforge.net/project/ffmpeg-batch/FFmpeg_Batch_' + $matches.Version + '_Portable_x64.zip')
     }
 }
 

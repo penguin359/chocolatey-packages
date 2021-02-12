be4028bc32f8701facd306d1f7b4ad7f6d7ff66a
Chocolatey <chocolatey@realdimensions.net>
2021-02-12 00:25:03 +0000

    AU: 5 updated - carotdav carotdav.install carotdav.portable screenpresso webex-teams [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/carotdav.install/tools/chocolateyinstall.ps1
+++ automatic/carotdav.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\CarotDAV1.15.9.zip"
+  file        = "$toolsDir\CarotDAV1.15.10.zip"
 }
 Get-ChocolateyUnzip @packageArgs
 Remove-Item -Path $packageArgs.file
@@ -12,7 +12,7 @@ Remove-Item -Path $packageArgs.file
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   fileType     = "MSI"
-  fileFullPath = "$toolsDir\CarotDAV1.15.9\CarotDAV1.15.9.msi"
+  fileFullPath = "$toolsDir\CarotDAV1.15.10\CarotDAV1.15.10.msi"
   silentArgs   = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 }
 Install-ChocolateyInstallPackage @packageArgs
--- automatic/webex-teams/tools/chocolateyinstall.ps1
+++ automatic/webex-teams/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/WebexTeams.msi'
-  checksum     = 'd65dc6492ac605f0433039a736ecd1955511fa145cb5256ffca1b603822d2599'
+  checksum     = 'bf465b9ec8872be6d71c764042c663262bdd31a4cd5b98986d5936d0f8eb1d6c'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

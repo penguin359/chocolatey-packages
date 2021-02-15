c82bc93323993dcb8c2a8007cc705fc60bb10531
Chocolatey <chocolatey@realdimensions.net>
2021-02-15 16:24:24 +0000

    AU: 2 updated - slm virtualhere-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/slm/tools/chocolateyinstall.ps1
+++ automatic/slm/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype     = 'MSI'
 
   url          = 'https://download.softros.com/SoftrosLANMessengerSetup.msi'
-  checksum     = 'e639ac0b8b84702a55032d46b8ff945dc93a1aa1bbbd3e5d5499fc25e005fb32'
+  checksum     = '7026479fa2e47a1cbf694026186c09589b37816a1b9c1f12401b9486cea4055c'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

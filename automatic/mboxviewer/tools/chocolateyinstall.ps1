f73f5ebccadc26e8fbc89ddedd9dde813ac2a7c1
Chocolatey <chocolatey@realdimensions.net>
2021-02-18 00:24:53 +0000

    AU: 3 updated - mboxviewer openssl sd [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/mboxviewer/tools/chocolateyinstall.ps1
+++ automatic/mboxviewer/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\mbox-viewer.exe-v1.0.3.18.zip"
+  file        = "$toolsDir\mbox-viewer.exe-v1.0.3.19.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

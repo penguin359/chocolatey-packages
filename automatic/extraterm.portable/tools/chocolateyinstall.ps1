d6f482d5a74decf3f93e097eccaa95d1e369013e
Chocolatey <chocolatey@realdimensions.net>
2021-02-20 00:24:26 +0000

    AU: 3 updated - extraterm extraterm.install extraterm.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/extraterm.portable/tools/chocolateyinstall.ps1
+++ automatic/extraterm.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\extraterm-0.57.1-win32-x64.zip"
+  file        = "$toolsDir\extraterm-0.58.0-win32-x64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
@@ -12,5 +12,5 @@ Get-ChocolateyUnzip @packageArgs
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Extraterm.lnk"
-$targetPath = Join-Path "$toolsDir" "extraterm-0.57.1-win32-x64\extraterm.exe"
+$targetPath = Join-Path "$toolsDir" "extraterm-0.58.0-win32-x64\extraterm.exe"
 Install-ChocolateyShortcut -RunAsAdmin -shortcutFilePath "$shortcutFilePath" -targetPath "$targetPath"

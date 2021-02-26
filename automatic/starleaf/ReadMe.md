c48a86b6171a303089c7a0932591e6cdb9a4815c
chtof <chtof62@gmail.com>
2021-02-26 21:12:45 +0100

    starleaf: initial commit

---
+++ automatic/starleaf/ReadMe.md
@@ -0,0 +1,5 @@
+# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/starleaf/starleaf.png" width="48" height="48"/> [StarLeaf](https://chocolatey.org/packages/starleaf)
+
+StarLeaf is a videoconfering tool.
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/starleaf/screenshot.png)
---
+++ automatic/starleaf/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+    
+  url           = 'https://dl.starleaf.com/app/StarLeaf-Installer-Windows.exe'
+  checksum      = '1531950A21A85F33C67700901AC6C1AE68B2661E9C8EB6CE8BA5D5A3C3777124'
+  checksumType  = 'sha256'
+  
+  
+  silentArgs    = "/S _?=$Env:PROGRAMFILES\$packageName"
+}
+
+Install-ChocolateyPackage @packageArgs

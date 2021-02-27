9b28126c7c65649fab55da9a28e753b81af170b6
chtof <chtof62@gmail.com>
2021-02-27 09:59:06 +0100

    Update chocolateyinstall.ps1

--- automatic/tartube/tools/chocolateyinstall.ps1
+++ automatic/tartube/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   file        = "$toolsDir\install-tartube-2.3.085-32bit.exe"
   file64      = "$toolsDir\install-tartube-2.3.085-64bit.exe"
-  silentArgs  = "/S _?=$Env:PROGRAMFILES\$packageName"
+  silentArgs  = '/S'
 }
 
 Install-ChocolateyInstallPackage @packageArgs

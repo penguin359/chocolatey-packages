f9947377a04ee0fbdf7926ca4c132a6b7bb8ef87
chtof <chtof62@gmail.com>
2021-02-27 10:20:09 +0100

    pluto-tv: initial commit

---
+++ automatic/pluto-tv/ReadMe.md
@@ -0,0 +1,5 @@
+# <img src="https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/pluto-tv/pluto-tv.png" width="48" height="48"/> [Pluto TV](https://chocolatey.org/packages/pluto-tv)
+
+Pluto TV is free TV over the Internet.
+
+![screenshot](https://cdn.jsdelivr.net/gh/chtof/chocolatey-packages/automatic/pluto-tv/screenshot.png)
---
+++ automatic/pluto-tv/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+    
+  url           = 'https://downloads.pluto.tv/desktop/electron/0.4.2/win/PlutoTVSetup.exe'
+  checksum      = '514CC6B5847E27B9AC7605117A4BF1508022450E0358F8B4FA1F5F395139FDBF'
+  checksumType  = 'sha256'
+  
+  
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+}
+
+Install-ChocolateyPackage @packageArgs
+
+Stop-Process -Name 'PlutoTV'

39c4feda5090f88d759fd542134e07fa08197df0
chtof <chtof62@gmail.com>
2021-03-01 23:07:46 +0100

    Update chocolateyinstall.ps1

--- automatic/minetest/tools/chocolateyinstall.ps1
+++ automatic/minetest/tools/chocolateyinstall.ps1
@@ -2,11 +2,13 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $toolsLocation = Get-ToolsLocation
 
+# Keep previous versions as games and downloaded content are downloaded in folder of previous versions.
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsLocation"
-  file          = "$toolsDir\minetest-5.4.0-win64.zip"
-  file64        = "$toolsDir\minetest-5.4.0-win32.zip"
+  file          = "$toolsDir\minetest-5.4.0-win32.zip"
+  file64        = "$toolsDir\minetest-5.4.0-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
@@ -14,6 +16,6 @@ Remove-Item -Path $packageArgs.file,$packageArgs.file64
 
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path $programs "Minetest.lnk"
-$targetPath = Join-Path $toolsDir "minetest-5.4.0-win$(Get-OSArchitectureWidth)\bin\minetest.exe"
+$shortcutFilePath = Join-Path $programs 'Minetest.lnk'
+$targetPath = Join-Path "$toolsLocation" "minetest-5.4.0-win$(Get-OSArchitectureWidth)\bin\minetest.exe"
 Install-ChocolateyShortcut -ShortcutFilePath $shortcutFilePath -TargetPath $targetPath

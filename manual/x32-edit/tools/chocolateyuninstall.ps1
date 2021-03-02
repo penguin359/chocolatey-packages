37094a7b6e09235a6e207e33d7e504af8b09618b
chtof <chtof62@gmail.com>
2021-03-02 21:02:56 +0100

    Update chocolateyuninstall.ps1

--- manual/x32-edit/tools/chocolateyuninstall.ps1
+++ manual/x32-edit/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
 ﻿# Remove start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path $programs "X32 Edit.lnk"
-if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
+$shortcutFilePath = Join-Path $programs 'X32 Edit.lnk'
+If ( Test-Path "$shortcutFilePath" ) { Remove-Item "$shortcutFilePath" }

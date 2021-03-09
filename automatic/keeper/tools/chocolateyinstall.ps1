d173e6677ce462fc30a7bcca9f4dc356a4f9ba5f
Chocolatey <chocolatey@realdimensions.net>
2021-03-09 08:28:38 +0000

    AU: 2 updated - bandlab-assistant keeper [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ $packageArgs = @{
   file64        = "$toolsDir\KeeperSetup64.exe"
 
   url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = '268ad17a44a5e7e759ef7f28940815a6baa8a9eae8c78438b324a97372c55c72'
+  checksum      = '22a70867551df5df523527c70a552b73ecb91e67ac975e8a544fe571ed9daf6b'
   checksumType  = 'sha256'
 
   url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = 'd8d312160516aa5d3184b270f057c3275184a51e62588c9d53ae265745aae0ad'
+  checksum64    = '256c5467483cc92ce01d91994b26291e10015ed32c1a11ce2ed81ff0a6ace16b'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'

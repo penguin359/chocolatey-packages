599b0eb4c535be53389a204dff4d447947fac2e7
Chocolatey <chocolatey@realdimensions.net>
2021-03-11 00:24:23 +0000

    AU: 1 updated - cuda [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/cuda/tools/chocolateyinstall.ps1
+++ automatic/cuda/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://developer.download.nvidia.com/compute/cuda/11.2.1/local_installers/cuda_11.2.1_461.09_win10.exe'
-  checksum     = '63ecc5389af870f338f9cc11de4b0df250deaa947d3ec8165214467a1c7c40b0'
+  url          = 'https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_461.33_win10.exe'
+  checksum     = ''
   checksumType = 'sha256'
 
   silentArgs   = '/s'

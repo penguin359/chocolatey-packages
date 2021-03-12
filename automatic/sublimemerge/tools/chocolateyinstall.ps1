4704b53b1c8671203d8c45d69b48bb633a81daa2
Chocolatey <chocolatey@realdimensions.net>
2021-03-12 08:24:24 +0000

    AU: 7 updated - dontsleep dontsleep.install dontsleep.portable openorienteering-mapper openorienteering-mapper.install openorienteering-mapper.portable sublimemerge [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/sublimemerge/tools/chocolateyinstall.ps1
+++ automatic/sublimemerge/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://download.sublimetext.com/sublime_merge_build_2047_x64_setup.exe'
-  checksum      = '123ac66d098e9ee1708d7869db978eb53a82ce8b453896bbd0119bc7c621b91b'
+  url           = 'https://download.sublimetext.com/sublime_merge_build_2049_x64_setup.exe'
+  checksum      = 'd15c3bf66e0c26b75f1c35aa39d514256d6e3453c2f64f047580a9497f1805eb'
   checksumType  = 'sha256'
   
   

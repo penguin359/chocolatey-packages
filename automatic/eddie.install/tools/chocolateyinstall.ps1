1ce6da6476c252fc46c9cd15ecd081d86b587424
Chocolatey <chocolatey@realdimensions.net>
2021-03-12 00:24:46 +0000

    AU: 2 updated - eddie.install webex-teams [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/eddie.install/tools/chocolateyinstall.ps1
+++ automatic/eddie.install/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\eddie-ui_2.19.7_windows-10_x86_installer.exe"
-  file64      = "$toolsDir\eddie-ui_2.19.7_windows-10_x64_installer.exe"
+  file        = "$toolsDir\eddie-ui_2.20.0_windows-10_x86_installer.exe"
+  file64      = "$toolsDir\eddie-ui_2.20.0_windows-10_x64_installer.exe"
   silentArgs  = '/S'
 }
 

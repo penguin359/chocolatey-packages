1b254c8a766a9e60523889a13c0470212aa76a87
chtof <chtof62@gmail.com>
2021-03-20 12:18:51 +0100

    Update update.ps1

--- automatic/dmde/update.ps1
+++ automatic/dmde/update.ps1
@@ -4,31 +4,35 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
     $releases = 'https://dmde.com/download.html'
-    $regex    = 'dmde-(?<Version>[\d\.]+)-win32-gui.zip'
+    $regex32    = 'dmde-[\d\.-]+-win32-gui.zip'
+    $regex64    = 'dmde-(?<Version>[\d\.-]+)-win64-gui.zip'
     
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url = $download_page.links | ? href -match $regex | Select -Last 1
+    $url32 = $download_page.links | ? href -match $regex32 | Select -Last 1
+    $url64 = $download_page.links | ? href -match $regex64 | Select -Last 1
     
      return @{
-        Version = $matches.Version
-        URL64   = $url.href
+        Version = $matches.Version -Replace '-','.'
+        URL32   = $url32.href
+        URL64   = $url64.href
     }
 }
 
 function global:au_SearchReplace {
     @{
         "legal\VERIFICATION.txt"  = @{            
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
             "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
         "tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*file32\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
             "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none

ee0f318a3033ef00217e87eb86b2b5bad79b04a4
chtof <chtof62@gmail.com>
2021-03-20 11:39:33 +0100

    Update chocolateyinstall.ps1

--- automatic/meshroom/tools/chocolateyinstall.ps1
+++ automatic/meshroom/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   unzipLocation  = "$toolsDir"
 
   url64          = 'https://github.com/alicevision/meshroom/releases/download/v2021.1.0/Meshroom-2021.1.0-win64.zip'
-  checksum64     = ''
+  checksum64     = '25b12e074e1d1fa6c0fbbf3b11317c50b368d28f740f2a70cca0a739138da7bc'
   checksumType64 = 'sha256'
 }
 

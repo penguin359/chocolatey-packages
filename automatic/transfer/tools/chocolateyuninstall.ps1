b8962e84aee8ddba7a90ea3d5799a6f4fb2d7a67
chtof <chtof62@gmail.com>
2021-03-21 11:13:25 +0100

    Update chocolateyuninstall.ps1

--- automatic/transfer/tools/chocolateyuninstall.ps1
+++ automatic/transfer/tools/chocolateyuninstall.ps1
@@ -13,16 +13,13 @@ $uninstalled = $false
 if ($key.Count -eq 1) {
   $key | % {    
     $packageArgs['file'] = "$($_.QuietUninstallString)" -Replace '(".*").*', '$1'
-    Uninstall-ChocolateyPackage @packageArgs
-    write-host "1111"
+    Uninstall-ChocolateyPackage @packageArgs    
   }
 } elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
-  write-host "2222"
+  Write-Warning "$packageName has already been uninstalled by other means."  
 } elseif ($key.Count -gt 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
-  write-host "3333"
+  $key | % {Write-Warning "- $($_.DisplayName)"}  
 }

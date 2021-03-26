b1abdcf22f77124210b8d68d1e8aa440572a00ee
Chocolatey <chocolatey@realdimensions.net>
2021-03-26 08:24:55 +0000

    AU: 3 updated - afedteated magicsplat-tcl-tk openssl [skip ci]

--- automatic/afedteated/tools/chocolateyinstall.ps1
+++ automatic/afedteated/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://cdn.iobit.com/dl/advanced-systemcare-setup.exe'
-  checksum     = '731b1a65197efd7a53e5f3395975d78ce19b7a79481661bcc6fb4bb058a6ec4f79df3289c1e871fd9554a89ccab67d66205bb7cc8a24358cf29b57822f2d6fc6'
+  checksum     = '3498665de315b4650dd8e45f522e58a4e713f742e3ddb9037482679f0269e296680db5b6acd43c2b55dc73bbb0a254001cb1e3d8c2e62af38948c2e88b8c2cca'
   checksumType = 'sha512'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'

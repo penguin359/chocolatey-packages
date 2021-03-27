9506c52a8e6b331f6bb91628954a6f9f829179e6
chtof <chtof62@gmail.com>
2021-03-27 22:40:24 +0100

    Update chocolateyuninstall.ps1

--- automatic/win10-brightnessslider/tools/chocolateyuninstall.ps1
+++ automatic/win10-brightnessslider/tools/chocolateyuninstall.ps1
@@ -1,6 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+# Stop win10_brightnessslider.exe if needed
+If ( Get-Process "Win10_BrightnessSlider" | Out-Null ) {
+  Stop-Process -Name "Win10_BrightnessSlider" | Out-Null
+}
+
 If ( Get-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run | Select-Object -ExpandProperty Win10_BrightnessSlider -ErrorAction Stop | Out-Null ){
   Remove-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -Name Win10_BrightnessSlider
 }

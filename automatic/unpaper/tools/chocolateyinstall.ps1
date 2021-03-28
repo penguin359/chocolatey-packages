a25228a558aabd3c2969e54d1682fcf65ad496e0
chtof <chtof62@gmail.com>
2021-03-28 11:35:41 +0200

    unpaper: initial commit

---
+++ automatic/unpaper/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$PackageParameters     = Get-PackageParameters
+
+# PARAMETERS
+If ($PackageParameters.InstallDir) { $installDir = $PackageParameters.InstallDir }
+  Else { $installDir = "$toolsDir" }
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = "$installDir"
+  file          = "$toolsDir\unpaper-6.1-windows-i686.zip"
+  file64        = "$toolsDir\unpaper-6.1-windows-x86_64.zip"
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -Path $packageArgs.file

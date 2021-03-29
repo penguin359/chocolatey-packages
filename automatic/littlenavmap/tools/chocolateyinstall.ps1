ebdfaf41279004e25088f049c28414ec5a749f0c
Chocolatey <chocolatey@realdimensions.net>
2021-03-29 16:24:49 +0000

    AU: 1 updated - littlenavmap [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/littlenavmap/tools/chocolateyinstall.ps1
+++ automatic/littlenavmap/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\LittleNavmap-win-2.6.10.zip"
+  file        = "$toolsDir\LittleNavmap-win-2.6.11.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

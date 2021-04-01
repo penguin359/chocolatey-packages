068b2752dfcbe3ed5ffee5bb998f32ef1e0cc2dd
Chocolatey <chocolatey@realdimensions.net>
2021-04-01 00:24:30 +0000

    AU: 1 updated - labplot [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/labplot/tools/chocolateyinstall.ps1
+++ automatic/labplot/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file64      = "$toolsDir\labplot-2.8.1-64bit-setup.exe"
+  file64      = "$toolsDir\labplot-2.8.2-64bit-setup.exe"
   #silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' No longer used since version 2.8.1
   silentArgs  = '/S'
 }

280d703a16065935d8ed38e4529cdb0af29e8ca1
chtof <chtof62@gmail.com>
2021-04-02 12:29:25 +0200

    overwolf: initial commit

---
+++ automatic/overwolf/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+
+  url            = 'https://download.overwolf.com/installer/web_dl_btn2/OverwolfInstaller.exe' 
+  file           = "$env:temp\OverwolfInstaller.exe"
+  checksum32     = 'D1EEAD86272D3BE627CA223D5D74578A33756FE3F3369BADF16BF7DFCACE1AEE'
+  checksumType   = 'sha256'
+
+  silentArgs     = '-silent'
+  validExitCodes = @(1223)
+}
+
+$result = Invoke-WebRequest -method GET -Uri $packageArgs.url
+[System.IO.File]::WriteAllBytes("$env:temp\OverwolfInstaller.exe", $result.content)
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Stop-Process -Name overwolf

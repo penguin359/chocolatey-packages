aa3005520902c99602685a8e72243d85c5084b83
Chocolatey <chocolatey@realdimensions.net>
2021-04-04 00:24:07 +0000

    AU: 1 updated - keeper [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ $packageArgs = @{
   file64        = "$toolsDir\KeeperSetup64.exe"
 
   url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = '3b26f3002737660e661b67e805cad2301bc6fd18bb960c530dae228bc22d33e8'
+  checksum      = 'ada353bb87f6cc2aa136a4eadc9afdb638793b27563454b218165d260cc8655e'
   checksumType  = 'sha256'
 
   url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = '38258b4055cf2eb571b33c2a239d79d7acd70d6cdf76d1356dc56be981c957ca'
+  checksum64    = '832315c4ae8ba9d09ab866b2bbbbceb4aabdddbea6cfd5a327290e1127739ca0'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'

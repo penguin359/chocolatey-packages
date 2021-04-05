903ca2810c3ba5097f4f6f7077058416f5185747
chtof <chtof62@gmail.com>
2021-04-05 10:37:45 +0200

    evoluentmousemanager: initial commit

---
+++ automatic/evoluentmousemanager/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  filetype      = 'MSI'
+
+  url           = 'https://evoluent.com/download/EvoSetup-6.0.9.2.msi'
+  checksum      = 'D5850394230E8B60A9A3E4EE75B71B99F78316EFF2586626D6F1CCBB47D43F78'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+}
+
+Install-ChocolateyPackage @packageArgs

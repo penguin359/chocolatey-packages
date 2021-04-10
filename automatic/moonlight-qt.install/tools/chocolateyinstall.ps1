f9723d9764bfb135c4e9c830ff4da30711d2b16d
Chocolatey <chocolatey@realdimensions.net>
2021-04-10 08:24:20 +0000

    AU: 3 updated - moonlight-qt moonlight-qt.install moonlight-qt.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/moonlight-qt.install/tools/chocolateyinstall.ps1
+++ automatic/moonlight-qt.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  file          = "$toolsDir\MoonlightSetup-3.1.0.exe"  
+  file          = "$toolsDir\MoonlightSetup-3.1.1.exe"  
   silentArgs	= '/S'
 }
 
--- automatic/moonlight-qt.portable/tools/chocolateyinstall.ps1
+++ automatic/moonlight-qt.portable/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\MoonlightPortable-x86-3.1.0.zip"
-  file64        = "$toolsDir\MoonlightPortable-x64-3.1.0.zip"
+  file          = "$toolsDir\MoonlightPortable-x86-3.1.1.zip"
+  file64        = "$toolsDir\MoonlightPortable-x64-3.1.1.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

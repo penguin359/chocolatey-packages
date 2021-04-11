753331e165f6c3f76dfe501d715272bab0e3a29b
Chocolatey <chocolatey@realdimensions.net>
2021-04-11 08:24:44 +0000

    AU: 2 updated - ffmpeg-batch webex-meetings [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum     = 'cf3cd1047ce8df855493467a512dfecfed4e967f08875edb42f36b16b9d1a52eab0f53c293a1a7dc19af09c29938ceb6a9f3d74028410b5e8d87243e9e4632d7'  
+  checksum     = 'aa3d212cda870c49225fe24c93883deb168d57107ecfdf08275bb14d1bc50c5717139fcbb656e588a3a68a13e270890bbd83917d913e61fbee6c3fb7719f8e45'  
   checksumType = 'sha512'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"

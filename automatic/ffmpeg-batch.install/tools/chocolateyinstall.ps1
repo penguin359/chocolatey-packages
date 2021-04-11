df511f0857de9a46bb2fb38a9c39dc2dbc5c7e20
Chocolatey <chocolatey@realdimensions.net>
2021-04-11 00:24:51 +0000

    AU: 2 updated - ffmpeg-batch.install ffmpeg-batch.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/ffmpeg-batch.install/tools/chocolateyinstall.ps1
+++ automatic/ffmpeg-batch.install/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\FFbatch_setup_2.3.6_x86.exe"
-  file64      = "$toolsDir\FFbatch_setup_2.3.6_x64.exe"
+  file        = "$toolsDir\FFbatch_setup_2.3.7_x86.exe"
+  file64      = "$toolsDir\FFbatch_setup_2.3.7_x64.exe"
   silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
--- automatic/ffmpeg-batch.portable/tools/chocolateyinstall.ps1
+++ automatic/ffmpeg-batch.portable/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\FFmpeg_Batch_2.3.6_Portable_x86.zip"
-  file64      = "$toolsDir\FFmpeg_Batch_2.3.6_Portable_x64.zip"
+  file        = "$toolsDir\FFmpeg_Batch_2.3.7_Portable_x86.zip"
+  file64      = "$toolsDir\FFmpeg_Batch_2.3.7_Portable_x64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
@@ -15,5 +15,5 @@ $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 if (Get-OSArchitectureWidth -Compare '64') { $arch = '_x64' } else { $arch = '' }
 
 $shortcutFilePath = Join-Path $programs 'FFmpeg Batch Portable.lnk'
-$targetPath = Join-Path $toolsDir "FFmpeg_Batch_2.3.6_Portable${arch}.exe"
+$targetPath = Join-Path $toolsDir "FFmpeg_Batch_2.3.7_Portable${arch}.exe"
 Install-ChocolateyShortcut -RunAsAdmin -shortcutFilePath "$shortcutFilePath" -targetPath "$targetPath"

70f5aaf4a22119dfe72649767167616ea7090695
chtof <chtof62@gmail.com>
2021-04-18 01:04:42 +0200

    Update chocolateyinstall.ps1

--- automatic/jack/tools/chocolateyinstall.ps1
+++ automatic/jack/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\jack2-win32-v1.9.17.exe"
-  file64        = "$toolsDir\jack2-win64-v1.9.17.exe"  
+  file          = "$toolsDir\jack2-win32-v1.9.18.exe"
+  file64        = "$toolsDir\jack2-win64-v1.9.18.exe"  
 }
 
 Start-Process 'AutoHotKey' -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""

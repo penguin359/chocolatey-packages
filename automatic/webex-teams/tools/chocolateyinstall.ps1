23f555542eab9a745c4befed10ca75d32f69532a
chtof <chtof62@gmail.com>
2021-04-18 00:55:22 +0200

    webex-teams renamed to webex

--- automatic/webex-teams/tools/chocolateyinstall.ps1
+++ automatic/webex-teams/tools/chocolateyinstall.ps1
@@ -1,14 +1,4 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageArgs = @{
-  packageName  = $env:ChocolateyPackageName
-  fileType     = 'MSI'
-
-  url          = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/WebexTeams.msi'
-  checksum     = '5e805267e2b721343e8a4aaa8c7eef314d0d149c57ae0ade230c2421a00a701b'
-  checksumType = 'sha256'
-
-  silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-}
-
-Install-ChocolateyPackage @packageArgs
+Write-Warning "The webex-teams package is deprecated due to a naming change!"
+Write-Warning "The new package webex have superceded this package!"

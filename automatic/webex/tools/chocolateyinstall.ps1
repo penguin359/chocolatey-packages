b3cd1df19d99ab9a1cca38202dc891fa17b952b1
chtof <chtof62@gmail.com>
2021-04-27 23:39:12 +0200

    Update chocolateyinstall.ps1

--- automatic/webex/tools/chocolateyinstall.ps1
+++ automatic/webex/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   checksum      = '64DE58DF8F7C34D0FC6559DA5324C5530A54895038559ABB7D49EAB1509873A2'
   checksumType  = 'sha256'
 
-  url64bit      = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex.msi'
+  url64         = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex.msi'
   checksum64    = 'A61837879A0D7E9A838D8CD3B16A312CF86FA261ADCEBAD11E0D0358ABAC0CA1'
   checksumType64= 'sha256'
 

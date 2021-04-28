c30e08c2ac3b63cde60649c48e8cca4216e3b683
chtof <chtof62@gmail.com>
2021-04-28 20:21:31 +0200

    Delete chocolateyinstall.ps1

--- automatic/webex-teams/tools/chocolateyinstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-Write-Warning "The webex-teams package is deprecated due to a naming change!"
-Write-Warning "The new package webex have superceded this package!"

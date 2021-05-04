11af0f92921a58879270ba8d302be300d38e7480
Chocolatey <chocolatey@realdimensions.net>
2021-05-04 00:24:50 +0000

    AU: 2 updated - geogebra-geometry realpopup [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/realpopup/tools/chocolateyinstall.ps1
+++ automatic/realpopup/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype     = 'MSI'
 
   url          = 'https://www.realpopup.it/files/RealPopupSetup.msi'
-  checksum     = 'dedc1e05895dfdfa1bafc23992257bcbca150f4021c4cbf3aff063eadeb6c9d6'
+  checksum     = 'dbb33f743a7bbe39d3a3f0b0d3f72ba6bd8f07a90cbb9928925d252aa20b51b5'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

7857c899e9242495862cbc6007acfebe37c6d46e
Chocolatey <chocolatey@realdimensions.net>
2021-05-07 00:25:19 +0000

    AU: 4 updated - goodsync roomarranger sonarlint-vs2017 texmacs [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-v11-Setup.msi'
-  checksum       = '9332f0dbb8ffbe92360cdf396557de9f28a7ee3ac8560aaddc118d7d6fca07de'
+  checksum       = 'ac5d7a844f0b9ef04da1234991b64dd4ea28afa280b430fa84ffaf72e97dca19'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/sonarlint-vs2017/tools/chocolateyinstall.ps1
+++ automatic/sonarlint-vs2017/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$file = "$toolsDir\SonarLint.VSIX-4.33.1.28913-2017.vsix"
+$file = "$toolsDir\SonarLint.VSIX-4.34.0.31639-2017.vsix"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName

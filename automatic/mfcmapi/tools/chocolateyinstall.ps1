8d88a84493c7b5df295864b7ff25cab09652d11e
Chocolatey <chocolatey@realdimensions.net>
2021-05-08 00:24:40 +0000

    AU: 1 updated - mfcmapi [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/mfcmapi/tools/chocolateyinstall.ps1
+++ automatic/mfcmapi/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageName = $env:ChocolateyPackageName
 $binRoot     =  Get-ToolsLocation
 $installDir  = Join-Path "$binRoot" "$packageName"
 
-$file        = "$toolsDir\MFCMAPI.exe.20.2.20104.1136.zip"
-$file64      = "$toolsDir\MFCMAPI.exe.x64.20.2.20104.1136.zip"
+$file        = "$toolsDir\MFCMAPI.exe.21.0.21127.03.zip"
+$file64      = "$toolsDir\MFCMAPI.exe.x64.21.0.21127.03.zip"
 
 # We're going to copy both the 32-bit and 64-bit executables, because
 # the bitness of Outlook won't necessarily match the bitness of the
 # OS.
-Get-ChocolateyUnzip -PackageName $packageName -Destination "$InstallDir\x86" -FileFullPath "$toolsDir\MFCMAPI.exe.20.2.20104.1136.zip" 
-Get-ChocolateyUnzip -PackageName $packageName -Destination "$InstallDir\x64" -FileFullPath "$toolsDir\MFCMAPI.exe.x64.20.2.20104.1136.zip"
+Get-ChocolateyUnzip -PackageName $packageName -Destination "$InstallDir\x86" -FileFullPath "$toolsDir\MFCMAPI.exe.21.0.21127.03.zip" 
+Get-ChocolateyUnzip -PackageName $packageName -Destination "$InstallDir\x64" -FileFullPath "$toolsDir\MFCMAPI.exe.x64.21.0.21127.03.zip"

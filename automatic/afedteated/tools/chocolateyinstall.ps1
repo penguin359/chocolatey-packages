e2400b26090c37ed9820401576a3ade3c7c43a20
Chocolatey <chocolatey@realdimensions.net>
2021-05-11 08:24:44 +0000

    AU: 2 updated - afedteated webex-meetings [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/afedteated/tools/chocolateyinstall.ps1
+++ automatic/afedteated/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://cdn.iobit.com/dl/advanced-systemcare-setup.exe'
-  checksum     = '3498665de315b4650dd8e45f522e58a4e713f742e3ddb9037482679f0269e296680db5b6acd43c2b55dc73bbb0a254001cb1e3d8c2e62af38948c2e88b8c2cca'
+  checksum     = '282dbdde8664caf51d018abbb3b673dff05803c241b5e051b7812533e91e99510a5a8add3163a93f7cca2ff6cb354d83f3a57ea97b6b8ab9546b1639f2a4e9b7'
   checksumType = 'sha512'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum     = '8969a6103d5e422e12895e9fc55cb31f129daaaa9ad4e809f35307965c15a9c015ad261f88783f15e0e6eaf54f4166b02cc8e30b896067436cb897dd25e055e7'  
+  checksum     = '0d4b7aecbe0af2693b5ae79dfc905bab3826098f4f00091d25581e8c32d9f663ec20bd6713a797ccc108c9c907363db1209bd754199dd0b8a62db4759e984893'  
   checksumType = 'sha512'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"

546c9e5432894ef8aafcce5aacee1ef37e67a511
Chocolatey <chocolatey@realdimensions.net>
2021-05-11 00:24:45 +0000

    AU: 3 updated - cudatext gns3 internet-download-manager [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman638build23.exe'
-  checksum      = '80bcaa3d882b1ca1db100aa3a43f568217b816e5066aba70c3a07569cc659b10'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman638build25.exe'
+  checksum      = 'ed40ae750b67675aac45de7b91ae541a1ac057e056d5f1c63226fad678b5f5ef'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"

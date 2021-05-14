048fa88bfe23d4b9aab74fa262e93780e0116912
Chocolatey <chocolatey@realdimensions.net>
2021-05-14 16:24:52 +0000

    AU: 2 updated - clipto clipto.install [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/clipto.install/tools/chocolateyinstall.ps1
+++ automatic/clipto.install/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://github.com/clipto-pro/Desktop/releases/download/v5.0.3/clipto-5.0.3.exe'
-  checksum     = 'b6a0cc159d21b85e06c15b9789714aa3a17022ce4e419e997189164ea7f78fba'
+  url          = 'https://github.com/clipto-pro/Desktop/releases/download/v5.1.1/clipto-5.1.1.exe'
+  checksum     = 'ff4d24c2c812cfeb58b727be0f81dbb53e551cf9eb95a033444e923232219e5d'
   checksumType = 'sha256'
 
   silentArgs   = '/S'

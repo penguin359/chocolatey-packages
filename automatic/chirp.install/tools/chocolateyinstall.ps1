183e42acf161fb67731803c7bc3d20042b0263e8
Chocolatey <chocolatey@realdimensions.net>
2021-05-20 16:25:01 +0000

    AU: 6 updated - chirp chirp.install chirp.portable ospray ospray.install ospray.portable [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-daily-20210429-installer.exe"
+  file          = "$toolsDir\chirp-daily-20210520-installer.exe"
   silentArgs    = '/S'
 }
 
--- automatic/chirp.portable/tools/chocolateyinstall.ps1
+++ automatic/chirp.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = "$env:ChocolateyPackageName"
   destination   = "$toolsDir"
-  file          = "$toolsDir\chirp-daily-20210429-win32.zip"
+  file          = "$toolsDir\chirp-daily-20210520-win32.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

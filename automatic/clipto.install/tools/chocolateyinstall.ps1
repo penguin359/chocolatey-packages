c39401388efd9b910d8a1d3aa8caff39d881227e
Chocolatey <chocolatey@realdimensions.net>
2021-05-22 00:25:00 +0000

    AU: 4 updated - clipto clipto.install jami sd [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/clipto.install/tools/chocolateyinstall.ps1
+++ automatic/clipto.install/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://github.com/clipto-pro/Desktop/releases/download/v5.1.1/clipto-5.1.1.exe'
-  checksum     = 'ff4d24c2c812cfeb58b727be0f81dbb53e551cf9eb95a033444e923232219e5d'
+  url          = 'https://github.com/clipto-pro/Desktop/releases/download/v5.1.2/clipto-5.1.2.exe'
+  checksum     = '2fa856801fe0e36ed895a9de190d3aff15c9fa18e53701507fb8d79ed8df34a2'
   checksumType = 'sha256'
 
   silentArgs   = '/S'

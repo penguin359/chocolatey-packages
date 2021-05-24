d1da4f3cffc102f4a0e992b83131336860236398
Chocolatey <chocolatey@realdimensions.net>
2021-05-24 16:24:56 +0000

    AU: 3 updated - webex wsjtx yara [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/webex/tools/chocolateyinstall.ps1
+++ automatic/webex/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   filetype      = 'MSI'
 
   url           = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex_x86.msi'
-  checksum      = '0C4249AF946E5347DC5106108B6AF4D074D7FC8AD951EE38F5AF283627C3FF74'
+  checksum      = '36E29909E9F36223103B65086ED125769727DD7AAAEB9D9A86A7836C1F90465E'
   checksumType  = 'sha256'
 
   url64         = 'https://binaries.webex.com/WebexTeamsDesktop-Windows-Gold/Webex.msi'
-  checksum64    = 'CB5C556C5FE88B987B8B643AB216D9905AE10867F32D421F298F102C8DE8D626'
+  checksum64    = '6BE89351F25C1CE2DE2503CB5F2CBF6E13DFC45C9A7AC65ACD0A9BD19D3828EF'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"

9ffa96b0fb2066d0d510e0af57e5d869e87414d6
Chocolatey <chocolatey@realdimensions.net>
2021-06-01 16:25:08 +0000

    AU: 2 updated - clockify webex-meetings [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/webex-meetings/tools/chocolateyinstall.ps1
+++ automatic/webex-meetings/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType     = 'MSI'
 
   url          = 'https://akamaicdn.webex.com/client/webexapp.msi'
-  checksum     = '82cee56c2233a5c871c50de63f64268569a890ae0ba13a367d2984d6a45a8a03800e8a095f188c932bf6cef02e301c07eba5989b8a90f62abbb8a968aebac870'  
+  checksum     = '3fc0d9b439b2873e5c870b955c137a9ae94c36b6b4ffd3c34a7331af6c8f0a355b624eb98680af428f8d23beef896cf2d3694ecbdce9e5f8279ac86637398e4c'  
   checksumType = 'sha512'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"

14b1947ea5c631961dcba6a1923480c6545b23d4
Chocolatey <chocolatey@realdimensions.net>
2021-06-06 16:24:29 +0000

    AU: 1 updated - nsudo [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/nsudo/tools/chocolateyinstall.ps1
+++ automatic/nsudo/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\NSudo_8.0.1_All_Components.zip"
+  file          = "$toolsDir\NSudo_8.2_All_Components.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
@@ -13,5 +13,5 @@ Remove-Item -Path $packageArgs.file
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "NSudo.lnk"
-$targetPath = Join-Path $toolsDir "NSudo_8.0.1_All_Components\NSudo Launcher\NSudo.bat"
+$targetPath = Join-Path $toolsDir "NSudo_8.2_All_Components\NSudo Launcher\NSudo.bat"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

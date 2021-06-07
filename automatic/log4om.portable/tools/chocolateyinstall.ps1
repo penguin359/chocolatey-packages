5a5cabd376b9fefcbd757c95047bd51885c3dc85
Chocolatey <chocolatey@realdimensions.net>
2021-06-07 16:25:18 +0000

    AU: 5 updated - log4om log4om.install log4om.portable spacedesk-client spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/log4om.portable/tools/chocolateyinstall.ps1
+++ automatic/log4om.portable/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ Remove-Item -Path "$toolsDir\*.exe" -ErrorAction SilentlyContinue
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\Log4OM2_2_13_0_0_portable.zip"              
+  file          = "$toolsDir\Log4OM2_2_14_0_0_portable.zip"               
 }
 
 Get-ChocolateyUnzip @packageArgs

d3460e82f984d8ad9b3da700504d643eb3c41e34
Chocolatey <chocolatey@realdimensions.net>
2021-06-18 16:24:43 +0000

    AU: 5 updated - chirp chirp.install chirp.portable wireguard z-cron [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-daily-20210612-installer.exe"
+  file          = "$toolsDir\chirp-daily-20210618-installer.exe"
   silentArgs    = '/S'
 }
 

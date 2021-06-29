8d7c722a4dcad28599ddfe110f599328a0f2cdc9
Chocolatey <chocolatey@realdimensions.net>
2021-06-29 16:24:48 +0000

    AU: 1 updated - vuescan [skip ci]

--- automatic/vuescan/tools/chocolateyInstall.ps1
+++ automatic/vuescan/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
   url            = 'https://www.hamrick.com/files/vuex3297.exe'
-  checksum       = 'fd1d10b0a417dcd44b85d444b24365c258df11926730ddc03f174bd68d00d5f8'
+  checksum       = 'ec37385b1503223019bd16f178ab3fa92698a83d177630ab2dbf0d3594717651'
   checksumType   = 'sha256'
 
   url64          = 'https://www.hamrick.com/files/vuex6497.exe'
-  checksum64     = '9419516b7b35b8b86ad3c8ad864ecca553a63a66fbeb86f7ebed29fdec074732'
+  checksum64     = '9f3af44cdfb6c253c5652cec147de3eb18d4bddcc7218bde0caf7a3180b0b7b1'
   checksumType64 = 'sha256'
 }
 

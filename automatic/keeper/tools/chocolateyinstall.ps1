2c739a9284193428a05f3167bdcea13ae3149282
Chocolatey <chocolatey@realdimensions.net>
2021-07-01 08:24:53 +0000

    AU: 3 updated - afedteated keeper ultraviewer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/8ab2e7b4b4de78ec17039760cde18542a96c42e6

--- automatic/keeper/tools/chocolateyinstall.ps1
+++ automatic/keeper/tools/chocolateyinstall.ps1
@@ -9,11 +9,11 @@ $packageArgs = @{
   file64        = "$toolsDir\KeeperSetup64.exe"
 
   url           = 'https://keepersecurity.com/desktop_electron/Win32/KeeperSetup32.zip'
-  checksum      = '24016b260f9540d5cea0ae6f62bb7d736550b6b4deba3d0025ee76fa5f2d000e'
+  checksum      = '78b0415916dba442c40a8da96ea58c92690d0ed172592a4545b3df9c8ba58bdc'
   checksumType  = 'sha256'
 
   url64         = 'https://keepersecurity.com/desktop_electron/Win64/KeeperSetup64.zip'
-  checksum64    = 'fef5db64634ef5d9f9787f0228bb3557c02a0b0f89a92b46c146a394f898fa8e'
+  checksum64    = '71b7e4dba01d5760675685090c5dafe9bba101490d373fa89a0125599b000033'
   checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'

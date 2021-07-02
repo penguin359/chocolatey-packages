017227fb7f65993160f46a440351ccd375dbcf98
chtof <chtof62@gmail.com>
2021-07-02 20:19:25 +0200

    Update update.ps1

--- automatic/klog/update.ps1
+++ automatic/klog/update.ps1
@@ -1,18 +1,18 @@
 import-module au
-$releases = 'https://mirrors.up.pt/pub/nongnu/klog/win/'
-$regex    = 'KLog-(.*)-windows-installer.exe$'
 
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-     $url     = $download_page.links | ? href -match $regex | select -Last 1
-     $version = $url -split '-|.exe' | select -Last 1 -Skip 3
+    $releases = 'https://github.com/ea4k/klog/releases/latest'
+    $regex    = 'KLog-(.*)-windows-installer.exe$'
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url     = $download_page.links | ? href -match $regex | select -Last 1
+    $version = $url.href -split '-|.exe' | select -Last 1 -Skip 3
 
      # When the fourth segment is already used, it is recommended to add two zeroes (00) to the end of the version. Then when you need to fix, you just increment that number.
      $versionNbSegment = ($version.ToCharArray() | Where-Object {$_ -eq '.'} | Measure-Object).Count
-     if ($versionNbSegment -eq 3) { $version += "00" }
-     return @{ Version = $version ; URL32 = $releases + $url.href }
+     # if ($versionNbSegment -eq 3) { $version += "00" }
+     return @{ Version = $version ; URL32 = 'https://github.com' + $url.href }
 }
 
 function global:au_SearchReplace {

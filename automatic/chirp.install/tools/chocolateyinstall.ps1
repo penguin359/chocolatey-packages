8fbc57456a43a2fd1c1a9bfdf4c8dd785fc67e66
Chocolatey <chocolatey@realdimensions.net>
2021-07-06 16:25:04 +0000

    AU: 4 updated - chirp chirp.install chirp.portable stremio [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/b5bea22b24b6ff91d136ee5a292261a03f6a631a

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-daily-20210705-installer.exe"
+  file          = "$toolsDir\chirp-daily-20210706-installer.exe"
   silentArgs    = '/S'
 }
 
--- automatic/stremio/tools/chocolateyinstall.ps1
+++ automatic/stremio/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://dl.strem.io/shell-win/v4.4.137/Stremio+4.4.137.exe'
-  checksum     = 'c719c426be76657f5a11c050728a79e0ca3f5e4e6deb9d836faeb2368fac0ed3'
+  url          = 'https://dl.strem.io/shell-win/v4.4.141/Stremio+4.4.141.exe'
+  checksum     = '282a4ee6851dc86802b670aab6f092bf55c43c02567c08c238c3628c4091f320'
   checksumType = 'sha256'
 
   silentArgs   = '/S'

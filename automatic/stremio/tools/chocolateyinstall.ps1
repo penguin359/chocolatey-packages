deb4f38b06f227618c9a7333aa200205c645e414
Chocolatey <chocolatey@realdimensions.net>
2021-07-07 16:24:37 +0000

    AU: 1 updated - stremio [skip ci]

--- automatic/stremio/tools/chocolateyinstall.ps1
+++ automatic/stremio/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://dl.strem.io/shell-win/v4.4.141/Stremio+4.4.141.exe'
-  checksum     = '282a4ee6851dc86802b670aab6f092bf55c43c02567c08c238c3628c4091f320'
+  url          = 'https://dl.strem.io/shell-win/v4.4.142/Stremio+4.4.142.exe'
+  checksum     = 'ff44d700f809071353ab3f6751bd257b5e6918a76288af1e5d086316a6e91b78'
   checksumType = 'sha256'
 
   silentArgs   = '/S'

e5b3b16becf1245355809359d8ad48ba94432c27
Chocolatey <chocolatey@realdimensions.net>
2021-07-08 16:24:38 +0000

    AU: 2 updated - automouseclick slm [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/9dfa6ac2999714481eb18426fba6de2836d8f156

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '5B28780191C09168F1758F56407E070561207128C5761BFB369CFC66145680BE'
+  checksum     = '55B31772AFD579F1500D573CBCA9D064E1FFA07BDAC7C1DC124B0638434E2BD9'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/slm/tools/chocolateyinstall.ps1
+++ automatic/slm/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype     = 'MSI'
 
   url          = 'https://download.softros.com/SoftrosLANMessengerSetup.msi'
-  checksum     = '202deb5ef3806877e54c201d68331f36b1e044e3445a34149b50dbce85e81e06'
+  checksum     = '91a1a4bd19fc04cb2e741b653315fd9171ebacb44acce4fcd68604d40965ca12'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

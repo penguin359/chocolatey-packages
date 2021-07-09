8a1279ba918ce05a44a2d65ebf1f0aa31521bef2
Chocolatey <chocolatey@realdimensions.net>
2021-07-09 00:24:49 +0000

    AU: 1 updated - egnyte-desktop-app [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/ee79952c57a622413f2fea6ba13d344ebbfebc96

--- automatic/egnyte-desktop-app/tools/chocolateyinstall.ps1
+++ automatic/egnyte-desktop-app/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   filetype     = 'MSI'
 
-  url          = 'https://egnyte-cdn.egnyte.com/egnytedrive/win/en-us/3.11.2/EgnyteDesktopApp_3.11.2_104.msi'
-  checksum     = '1ce01edcf8dbe66b5545357df2bf524537a7a7f1c21a5f445cd196424d82e5fb'
+  url          = 'https://egnyte-cdn.egnyte.com/egnytedrive/win/en-us/3.12.0/EgnyteDesktopApp_3.12.0_107.msi'
+  checksum     = '841d38c2c600bc1328576ac7a3c4f5e28387e73dfbd796344d33535dec9dc2dc'
   checksumType = 'sha256'  
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

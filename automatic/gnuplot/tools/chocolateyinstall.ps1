6957c4ecab52de3d28cb8be273d12fb8218750da
Chocolatey <chocolatey@realdimensions.net>
2021-07-11 00:24:42 +0000

    AU: 1 updated - gnuplot [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/ca3485997eb5c27a12a7b0be077172b71c27a3ef

--- automatic/gnuplot/tools/chocolateyinstall.ps1
+++ automatic/gnuplot/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName  
-  file64      = "$toolsDir\gp541-win64-mingw.exe"
+  file64      = "$toolsDir\gp542-win64-mingw.exe"
   silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /MERGETASKS="modifypath"'
 }
 

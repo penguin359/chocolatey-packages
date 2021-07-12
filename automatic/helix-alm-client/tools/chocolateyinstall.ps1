12763e3df76a8690b5b10cbac7aac86276bac4fe
Chocolatey <chocolatey@realdimensions.net>
2021-07-12 16:25:28 +0000

    AU: 3 updated - geogebra6 geogebra-geometry helix-alm-client [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/617e441c6bd3ea348e18a6bf6edfc7c8160ce9e9

--- automatic/helix-alm-client/tools/chocolateyinstall.ps1
+++ automatic/helix-alm-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://cdist2.perforce.com/alm/helixalm/r2021.1.0/ttwinclientinstall.exe'
-  checksum     = 'fe4c727c8890bae3120b65aa43bf75a146915f73262456c319b2d1648c0fbdbb'
+  url          = 'https://cdist2.perforce.com/alm/helixalm/r2021.2.0/ttwinclientinstall.exe'
+  checksum     = '43b689192d41b1aa4b96b60f523ae78b5813ec03cd11712f3bfc66635e2340fb'
   checksumType = 'sha256'
 
   silentArgs   = "-i SILENT LAX_VM resource\jre\bin\java.exe"

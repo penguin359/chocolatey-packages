4d05725da202e0d5c5c533f125b4840b2f5206b6
Chocolatey <chocolatey@realdimensions.net>
2021-07-17 08:24:40 +0000

    AU: 2 updated - internet-download-manager mboxviewer [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/257613fe5bff7dbf35d52f702dbfc2de7ce55ce8

--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman639build1.exe'
-  checksum      = '19a2d658a2fa7286c039fc84bcdd68dec0b00fa5eea4203cad9901b83604edf8'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman639build2.exe'
+  checksum      = '1d364c185082bf798f4ff21f33b63c84cc1407ca33be17793990190b59d2042c'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"
--- automatic/mboxviewer/tools/chocolateyinstall.ps1
+++ automatic/mboxviewer/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\mbox-viewer.exe-v1.0.3.21.zip"
+  file        = "$toolsDir\mbox-viewer.exe-v1.0.3.23.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs

ec670ff9f78f5a2a63ce406e898f5793df1279ec
Chocolatey <chocolatey@realdimensions.net>
2021-07-19 16:24:48 +0000

    AU: 5 updated - geogebra6 geogebra-geometry sonarlint-vs2017 spacedesk-client spacedesk-server [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/7a10ea44fbc8f6b3107d42dbfa91ef7ad55ad717

--- automatic/geogebra6/tools/chocolateyinstall.ps1
+++ automatic/geogebra6/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file     = 'GeoGebraGraphing-Windows-Installer-6-0-652-0.exe'
+$file     = 'GeoGebraGraphing-Windows-Installer-6-0-654-0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- automatic/sonarlint-vs2017/tools/chocolateyinstall.ps1
+++ automatic/sonarlint-vs2017/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$file = "$toolsDir\SonarLint.VSIX-4.36.0.34265-2017.vsix"
+$file = "$toolsDir\SonarLint.VSIX-4.37.0.34839-2017.vsix"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName

c37b9a25999219b08b5adc6f1e5f1fa33e114126
Chocolatey <chocolatey@realdimensions.net>
2021-07-20 16:24:52 +0000

    AU: 1 updated - launchyqt [skip ci] https://gist.github.com/393704c64c2344f0f8e029cd57bebd2b/362db9978720ff0a844a7349e3f8e03a58a31bc4

--- automatic/launchyqt/tools/chocolateyinstall.ps1
+++ automatic/launchyqt/tools/chocolateyinstall.ps1
@@ -4,14 +4,14 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\Launchy-3.1.1-win-x86.7z"
-  #file64        = "$toolsDir\Launchy-3.1.1-win-amd64.7z"
+  file          = "$toolsDir\Launchy-3.1.2-win-x86.7z"
+  #file64        = "$toolsDir\Launchy-3.1.2-win-amd64.7z"
 }
 
 #if (Get-OSArchitectureWidth -Compare "32") { $arch = 'x86' } else { $arch = 'amd64'}
 
 foreach ($file in 'python.exe', 'pythonw.exe') {
-  New-Item -path $toolsDir\Launchy-3.1.1-win-${arch} -name "$file.ignore" -type File -force | Out-Null
+  New-Item -path $toolsDir\Launchy-3.1.2-win-${arch} -name "$file.ignore" -type File -force | Out-Null
 }
 
 Get-ChocolateyUnzip @packageArgs
@@ -19,6 +19,6 @@ Get-ChocolateyUnzip @packageArgs
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs "Launchy.lnk"
-#$targetPath = Join-Path $toolsDir "Launchy-3.1.1-win-${arch}\Launchy.exe"
-$targetPath = Join-Path $toolsDir "Launchy-3.1.1-win-x86\Launchy.exe"
+#$targetPath = Join-Path $toolsDir "Launchy-3.1.2-win-${arch}\Launchy.exe"
+$targetPath = Join-Path $toolsDir "Launchy-3.1.2-win-x86\Launchy.exe"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath

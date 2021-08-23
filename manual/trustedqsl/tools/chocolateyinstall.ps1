9bec61f172b31a4a59d67d35a6528bb26988e5b5
Loren M. Lang <lorenl@north-winds.org>
2021-08-22 20:46:40 -0700

    Added package install for Trusted QSL

---
+++ manual/trustedqsl/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = "https://www.arrl.org/tqsl/tqsl-$($env:chocolateyPackageVersion).msi"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+
+  softwareName  = 'Trusted QSL*'
+
+  checksum      = 'B83FED8C2EB1A97B46F1BABCBC6CF14AC3B25E70182741B814FFEE6C604DDC8E'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs

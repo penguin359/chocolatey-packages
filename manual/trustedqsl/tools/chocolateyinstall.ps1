63e5de9a2133552beb2f43a680402c3d0b97f820
Loren M. Lang <lorenl@north-winds.org>
2022-11-09 02:17:13 -0800

    Bumped trustedqsl to version 2.6

--- manual/trustedqsl/tools/chocolateyinstall.ps1
+++ manual/trustedqsl/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'Trusted QSL*'
 
-  checksum      = '1ABF0CA9906D666FB756BDC26FE6069D716FA0E1EEB4C0904A93A0C26ABE741F'
+  checksum      = '26A3E70FED230DAE2A809DBA00F158FFE68533ED26EF6687D18DC8746B61E89F'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""

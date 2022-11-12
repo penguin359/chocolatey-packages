2da4b31a5265793fcc40dad6d57efab653248a47
Loren M. Lang <lorenl@north-winds.org>
2022-11-12 00:37:22 -0800

    Initial release of RufzXP package for version 1.1.2

---
+++ manual/rufzxp/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.rufzxp.net/software/rufzxp_fw2_0_setup_1_1_2.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'RufzXP*'
+
+  checksum      = 'FF13746162E44125F80C90E8ED269B4716CC7337152D0BB4FFBDB850ABB1B1F3'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs

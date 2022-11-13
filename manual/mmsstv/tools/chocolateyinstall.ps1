ed54668e5425293d79304a3ed5aff3af9f6d6d89
Loren M. Lang <lorenl@north-winds.org>
2022-11-12 23:48:57 -0800

    Added package for MMSSTV (broken)
    Currently, it is unable to download the package correctly because the site requires a cookie from an initial visit to the site and generates a random URL for the actual download. I am hoping I can resolve this with the author.

---
+++ manual/mmsstv/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://hamsoft.ca/pages/mmsstv.php?dwn=d17d711bbfe4cc8c'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'MMSSTV*'
+
+  checksum      = 'C03B3E863A3F1580672DDC9BC2B0740C911C508CB20D00B254EEB725C2084F4A'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs

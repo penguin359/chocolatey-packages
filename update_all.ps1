464189891880b3db2edd6ffd64aa407b1a29c445
Loren M. Lang <lorenl@north-winds.org>
2022-11-12 23:30:58 -0800

    Added release notes for Morse Runner

--- update_all.ps1
+++ update_all.ps1
@@ -39,14 +39,14 @@ $Options = [ordered]@{
     Git = @{
         User     = ''                                       #Git username, leave empty if github api key is used
         Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
-	Branch   = 'main'
+        Branch   = 'main'
     }
 
     GitReleases = @{
         ApiToken   = $Env:github_releases_api_key
         ReleaseType= 'package'
         Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
-	Branch   = 'main'
+        Branch   = 'main'
     }
 
     Gitter = @{

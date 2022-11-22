ceec44cf08f39c4755089a235bd808653ee4bda0
Loren M. Lang <lorenl@north-winds.org>
2022-11-22 01:05:10 -0800

    Added RCForb Server package

---
+++ automatic/rcforb/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://download.remotehams.com/download.php?dir=orb&file=RCForb_0.7.8014.exe'
+$checksum   = 'AF325EE785B567BE61D715F07DC547DFFC89B1367BA5FFB33A10EAAA5A5CA90D'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'RCForb (Server)*'
+
+  checksum      = $checksum
+  checksumType  = 'sha256'
+
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs

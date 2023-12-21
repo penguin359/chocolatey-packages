ecc8c7a450dd0ac281c42d6e71e4221f429b6b59
Chocolatey <chocolatey@realdimensions.net>
2023-12-21 03:59:00 +0000

    AU: 1 updated - winlink-express [skip ci] https://gist.github.com/penguin359/82fb09d745a7785fa8580f1a4075d96b/b7a5347c3ba03df833b0e213a90b994db458c92b

--- winlink-express/tools/chocolateyinstall.ps1
+++ winlink-express/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://downloads.winlink.org/User%20Programs/Winlink_Express_install_1-7-10-0.zip'
-$checksum   = '7ee1b6e01fb09c98a36fabe72d940ba920929c27382fd6596cd28da065f3b247'
+$url        = 'https://downloads.winlink.org/User%20Programs/Winlink_Express_install_1-7-12-0.zip'
+$checksum   = 'd80850e71407629819f8a3d9ebaec7af951a0baf2a05aa9f3f390796dd578036'
 $setupName  = 'Winlink_Express_install.exe'
 
 $packageArgs = @{

36cbf485f68a750de8b2c05d80ca2bce7545a55c
Loren M. Lang <lorenl@north-winds.org>
2023-12-23 19:44:07 -0800

    Added WSJT-X

---
+++ wsjtx/_update.ps1
@@ -0,0 +1,51 @@
+import-module au
+
+$releases = 'http://www.arrl.org/tqsl-download'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+        'trustedqsl.nuspec' = @{
+            "(<releaseNotes>).*(</releaseNotes>)" = "`$1$($Latest.ReleaseNotes)`$2"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+
+    #tqsl-2.5.1.msi
+    $re    = 'tqsl.*\.msi$'
+    $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version  = ($url -split '-' | select -Last 1) -split '\.msi' | select -First 1
+
+    $releaseNotesUrl = 'https://physics.princeton.edu/pulsar/K1JT/Release_Notes.txt'
+    $page = Invoke-WebRequest -Uri $releaseNotesUrl -UseBasicParsing
+    $start = $false
+    $found = $false
+    $lines = $page -split '\n' | %{
+	    if ($_ -match ('Release:.* ' + $version)) {
+		    $found = $true
+	    } elseif ($found -and ($_ -match '----------')) {
+		    $start = $true
+	    } elseif($_ -match 'Release:') {
+		    $start = $false
+		    $found = $false
+	    } elseif($start) {
+		    echo $_.Trim("`r")
+	    }
+    }
+    $releaseNotes = ($lines -join "`n").Trim()
+
+    @{
+        URL32 = $url
+        Version = $version
+        ReleaseNotes = $releaseNotes
+    }
+}
+
+update
---
+++ wsjtx/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://physics.princeton.edu/pulsar/k1jt/wsjtx-2.5.3-win32.exe'
+$url64      = 'https://physics.princeton.edu/pulsar/k1jt/wsjtx-2.5.3-win64.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'WSJT-X:*'
+
+  checksum      = '7E461B40DBA106A4D4FAF53A5BEB862D2C1D80663E4F08748F8B498BCD7F0C6B'
+  checksumType  = 'sha256'
+  checksum64    = 'F0C89987DAB4F0666DA0E79F66D45295710B7926259FC9B259950C792321E860'
+  checksumType64= 'sha256'
+
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs

39f07d5f0bd2f95c06ce113d363545477ce82af9
Loren M. Lang <lorenl@north-winds.org>
2023-12-23 17:33:18 -0800

    Added auto-update script for RCForb Client

---
+++ rcforb-client/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$releases = 'http://download.remotehams.com/'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+
+    #RCForb_0.7.8014.exe
+    $re    = 'RCForbClientSetup_v[0-9].*\.exe$'
+    $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version  = ($url -split '_v' | select -Last 1) -split '\.exe' | select -First 1
+
+    @{
+        URL32 = $url
+        Version = $version
+    }
+}
+
+update

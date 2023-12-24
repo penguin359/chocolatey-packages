b6a4f76e0e69615db5bdb4381e763fe7def28860
Loren M. Lang <lorenl@north-winds.org>
2023-12-23 19:38:03 -0800

    Bumped RCForb Client to v0.9.319 and updated dependencies

--- rcforb-client/tools/chocolateyinstall.ps1
+++ rcforb-client/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://download.remotehams.com/download.php?dir=orb&file=RCForbClientSetup_v0.9.297.exe'
-$checksum   = 'f95b9a67ab6384e4c4eba9af07cd26bea1af65b0aa07eeb746fd772623f43568'
+$url        = 'http://download.remotehams.com/download.php?dir=orb&file=RCForbClientSetup_v0.9.319.exe'
+$checksum   = '2718e7b1b7e658cb43a00c0a5fba430501ce6b27ba8c4d752382cb5619e9f035'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName

32792465a854a1beedaa1e64e766c425878cdef4
Loren M. Lang <lorenl@north-winds.org>
2024-02-13 23:52:01 -0800

    Made MMSSTV an embedded installer as it's OSS, tiny, and avoids cookies

--- mmsstv/tools/chocolateyinstall.ps1
+++ mmsstv/tools/chocolateyinstall.ps1
@@ -1,34 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$baseUrl    = 'https://hamsoft.ca/pages/mmsstv.php'
-$checksum   = 'C03B3E863A3F1580672DDC9BC2B0740C911C508CB20D00B254EEB725C2084F4A'
+$fileLocation = Join-Path $toolsDir 'MMSSTV113A.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'exe'
+  file          = $fileLocation
 
   softwareName  = 'MMSSTV*'
-
-  checksum      = $checksum
-  checksumType  = 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 }
 
-$download = Invoke-WebRequest -Uri $baseUrl -SessionVariable session -UseBasicParsing
-$re = "mmsstv.*\.exe"
-$downloadUrl = $download.links | ? {$_.outerHTML -match $re} | Select-Object -First 1 -ExpandProperty href
-
-#$packageArgs['url'] = $baseUrl + $downloadUrl
-#[Hashtable]$packageArgs['options'] = @{
-#  WebSession = $session
-#}
-#Install-ChocolateyPackage @packageArgs
-
-$url = $baseUrl + $downloadUrl
-$installerFile = "$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).exe"
-Invoke-WebRequest -Uri $url -WebSession $session -OutFile $installerFile
-$packageArgs['file'] = $installerFile
 Install-ChocolateyInstallPackage @packageArgs

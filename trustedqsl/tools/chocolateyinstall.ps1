dfffd0e5c56a06f02cfdfef2cd5850df7c27883f
Chocolatey <chocolatey@realdimensions.net>
2025-09-21 00:04:05 +0000

    AU: trustedqsl upgraded from 2.8.1 to 2.8.2 [skip ci] https://gist.github.com/penguin359/82fb09d745a7785fa8580f1a4075d96b/f0af3ab69f3dccb1acc7b23cd6b5b2e372ed1e9a

--- trustedqsl/tools/chocolateyinstall.ps1
+++ trustedqsl/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.arrl.org/files/file/LoTW%20Instructions/tqsl-2.8.1.msi'
-$checksum   = 'c9a37d0a8c088368929cb6179eddc77d638cb367eb0cb46185c24a0b78e2be33'
+$url        = 'https://www.arrl.org/files/file/LoTW%20Instructions/tqsl-2.8.2.msi'
+$checksum   = '294c12fd5ab937d5bc79f62ae5f36dcc2e5547a21d2a8facd32b385d3361c3db'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
